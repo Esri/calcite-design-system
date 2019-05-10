c8d0be5a2c677e1a4658b4ff8a5f844cbd3cee06
Adam Tirella <adam@tirel.la>
2019-05-10 08:57:22 -0700

    Add global sass and base CW utils
    Adds sass and config for using global includes.
    Adds Calcite Web, colors, fonts as examples

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,13 +1,22 @@
 import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
 
 export const config: Config = {
   namespace: "calcite",
-  outputTargets: [
+  outputTargets:[
     { type: "dist" },
     { type: "docs" },
     {
       type: "www",
       serviceWorker: null // disable service workers
     }
+  ],
+  globalStyle: "src/assets/styles/includes.scss",
+  plugins: [
+    sass({
+      injectGlobalPaths: [
+       "src/assets/styles/includes.scss"
+      ]
+    })
   ]
 };
