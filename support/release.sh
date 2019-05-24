651b19069580828bdd38e8ff3e1d125cdf8bd656
Patrick Arlt <patrick.arlt@gmail.com>
2019-05-24 11:09:33 -0700

    more release tweaks

--- support/release.sh
+++ support/release.sh
@@ -1,9 +1,14 @@
 # get the version from package.json
 VERSION=$(node --eval "console.log(require('./package.json').version);")
 
+npm run build
+
 # commit the changes from `npm run release:prepare`
 git add --all
 git commit -am "v$VERSION" --no-verify
+
+# tag a new version
+git tag v$VERSION
 
 # push everything up to this point to master
 git push https://github.com/ArcGIS/calcite-components.git master
@@ -18,7 +23,7 @@ zip -r $TEMP_FOLDER.zip $TEMP_FOLDER
 rm -rf $TEMP_FOLDER
 
 # Run gh-release to create a new release with our changelog changes and ZIP archive
-gh-release --t v$VERSION --repo calcite-components --owner ArcGIS -a $TEMP_FOLDER.zip
+gh-release --t v$VERSION -a $TEMP_FOLDER.zip
 
 # Delete the ZIP archive
 rm $TEMP_FOLDER.zip
