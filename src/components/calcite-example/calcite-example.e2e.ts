22ea88ac25dd269123a77ed3909cf0354315f97b
Patrick Arlt <patrick.arlt@gmail.com>
2019-06-04 20:26:09 -0700

    fix types for Jest, add boilerplate component

---
+++ src/components/calcite-example/calcite-example.e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from "@stencil/core/testing";
+
+describe("calcite-example", () => {
+  it("renders", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-example></calcite-example>");
+    const element = await page.find("calcite-example");
+    expect(element).toHaveClass("hydrated");
+  });
+});
