0bbd2b1f75c94e99baf30c63d1a4ca9f767bcee5
Adam Tirella <adam@tirel.la>
2019-07-22 13:48:53 -0700

    Cleans up tests

--- src/components/calcite-alert/calcite-alert.e2e.ts
+++ src/components/calcite-alert/calcite-alert.e2e.ts
@@ -5,56 +5,106 @@ describe("calcite-alert", () => {
     const page = await newE2EPage();
 
     await page.setContent(`
-    <calcite-alert id="alert">
+    <calcite-alert>
     <div slot="alert-title">Title Text</div>
-    <div slot="alert-message"Message Text</div>
+    <div slot="alert-message">Message Text</div>
     <a slot="alert-link" href="">Action</a>
     </calcite-alert>`);
 
-    const element = await page.find("#alert");
-    const closeButton = await page.find("#alert .alert-close");
+    const element = await page.find("calcite-alert");
+    const close = await page.find("calcite-alert >>> .alert-close");
+    const icon = await page.find("calcite-alert >>> .alert-icon");
+
     expect(element).toHaveClass("hydrated");
     expect(element).toEqualAttribute("color", "blue");
     expect(element).toEqualAttribute("theme", "light");
-    expect(closeButton);
+    expect(close).not.toBeNull();
+    expect(icon).toBeNull();
   });
 });
 
 describe("calcite-alert", () => {
   it("renders requested props", async () => {
     const page = await newE2EPage();
-
     await page.setContent(`
-    <calcite-alert theme="dark" color="yellow" duration="fast" dismiss id="alert" >
+    <calcite-alert theme="dark" color="yellow" duration="fast" dismiss>
     <div slot="alert-title">Title Text</div>
-    <div slot="alert-message"Message Text</div>
+    <div slot="alert-message">Message Text</div>
     <a slot="alert-link" href="">Action</a>
     </calcite-alert>`);
 
-    const element = await page.find("#alert");
-    const closeButton = await page.find("#alert .alert-close");
+    const element = await page.find("calcite-alert");
+    const close = await page.find("calcite-alert >>> .alert-close");
+    const icon = await page.find("calcite-alert >>> .alert-icon");
+
     expect(element).toHaveClass("hydrated");
     expect(element).toEqualAttribute("color", "yellow");
     expect(element).toEqualAttribute("duration", "fast");
     expect(element).toEqualAttribute("theme", "dark");
-    expect(closeButton).toBeNull();
+    expect(close).toBeNull();
+    expect(icon).toBeNull();
   });
 });
 
 describe("calcite-alert", () => {
-  it("validates incorrect props", async () => {
+  it("renders with an icon", async () => {
     const page = await newE2EPage();
-
     await page.setContent(`
-    <calcite-alert color="zip" duration="zot" theme="zat" dismiss id="alert">
+    <calcite-alert icon>
     <div slot="alert-title">Title Text</div>
-    <div slot="alert-message"Message Text</div>
+    <div slot="alert-message">Message Text</div>
     <a slot="alert-link" href="">Action</a>
     </calcite-alert>`);
 
-    const element = await page.find("#alert");
+    const element = await page.find("calcite-alert");
+    const close = await page.find("calcite-alert >>> .alert-close");
+    const icon = await page.find("calcite-alert >>> .alert-icon");
+    expect(element).toHaveClass("hydrated");
+    expect(close).not.toBeNull();
+    expect(icon).not.toBeNull();
+  });
+});
+
+describe("calcite-alert", () => {
+  it("validates incorrect props with dismiss", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+    <calcite-alert color="zip" duration="zot" theme="zat" dismiss>
+    <div slot="alert-title">Title Text</div>
+    <div slot="alert-message">Message Text</div>
+    <a slot="alert-link" href="">Action</a>
+    </calcite-alert>`);
+
+    const element = await page.find("calcite-alert");
+    const close = await page.find("calcite-alert >>> .alert-close");
+    const icon = await page.find("calcite-alert >>> .alert-icon");
+    expect(element).toHaveClass("hydrated");
     expect(element).toEqualAttribute("color", "blue");
     expect(element).toEqualAttribute("duration", "medium");
     expect(element).toEqualAttribute("theme", "light");
+    expect(close).toBeNull();
+    expect(icon).toBeNull();
+  });
+});
+
+describe("calcite-alert", () => {
+  it("validates incorrect props with close and icon", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+    <calcite-alert color="zip" duration="zot" theme="zat" icon>
+    <div slot="alert-title">Title Text</div>
+    <div slot="alert-message">Message Text</div>
+    <a slot="alert-link" href="">Action</a>
+    </calcite-alert>`);
+
+    const element = await page.find("calcite-alert");
+    const close = await page.find("calcite-alert >>> .alert-close");
+    const icon = await page.find("calcite-alert >>> .alert-icon");
+    expect(element).toHaveClass("hydrated");
+    expect(element).toEqualAttribute("color", "blue");
+    expect(element).toEqualAttribute("theme", "light");
+    expect(element).not.toHaveAttribute("dismiss");
+    expect(close).not.toBeNull();
+    expect(icon).not.toBeNull();
   });
 });
