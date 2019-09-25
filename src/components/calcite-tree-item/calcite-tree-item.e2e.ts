b647ad59f5aa6dde60023d4acf1f7b741dd665ed
Harry Robbins <hrobbins@esri.com>
2019-09-25 15:43:24 -0700

    Add tests for common click behaviors

--- src/components/calcite-tree-item/calcite-tree-item.e2e.ts
+++ src/components/calcite-tree-item/calcite-tree-item.e2e.ts
@@ -8,4 +8,87 @@ describe("calcite-tree", () => {
     const element = await page.find("calcite-tree");
     expect(element).toHaveClass("hydrated");
   });
+
+  // click on icon
+  it("should expand/collapse children when the icon is clicked", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<calcite-tree lines id="parentTree">
+      <calcite-tree-item id="firstItem">
+        <a href="#">Child 2</a>
+
+        <calcite-tree slot="children">
+          <calcite-tree-item>
+            <a href="http://www.google.com">Grandchild 1</a>
+          </calcite-tree-item>
+        </calcite-tree>
+      </calcite-tree-item>
+    </calcite-tree>`);
+
+    const icon = await page.find('#firstItem >>> [data-test-id="icon"]');
+    await icon.click();
+
+    const childContainer = await page.find('#firstItem >>> [data-test-id="calcite-tree-children"]');
+    const isVisible = await childContainer.isVisible();
+    expect(isVisible).toBe(true);
+  });
+
+  // click on link
+  it("should navigate when the link inside the tree item is clicked", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<calcite-tree lines id="parentTree">
+      <calcite-tree-item id="firstItem">
+        <a href="#">Child 1</a>
+      </calcite-tree-item>
+    </calcite-tree>`);
+
+    const anchor = await page.find('#firstItem a');
+    await anchor.click();
+
+    await page.waitForChanges();
+    expect(page.url()).toContain("#");
+  });
+
+  // click on item
+  it("should navigate to the link url when the item but not the link is clicked", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<calcite-tree lines id="parentTree">
+      <calcite-tree-item id="firstItem">
+        <a href="#">Child 1</a>
+      </calcite-tree-item>
+    </calcite-tree>`);
+
+    const item = await page.find('#firstItem');
+    await item.click();
+
+    await page.waitForChanges();
+    expect(page.url()).toContain("#");
+  });
+
+  // click on childItem
+  it.only("should navigate to the inner link when a child item is clicked and not the outer link", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<calcite-tree lines id="parentTree">
+      <calcite-tree-item expanded>
+        <a href="#outer">Child 2</a>
+
+        <calcite-tree slot="children">
+          <calcite-tree-item id="secondItem">
+            <a href="#inner">Grandchild 1</a>
+          </calcite-tree-item>
+        </calcite-tree>
+      </calcite-tree-item>
+    </calcite-tree>`);
+
+    await page.waitForChanges();
+    const item = await page.find('#secondItem');
+    await item.click();
+
+    await page.waitForChanges();
+    expect(page.url()).toContain("#inner");
+    expect(page.url()).not.toContain("#outer");
+  });
 });
