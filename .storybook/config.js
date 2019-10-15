81e52b820fe8bf21831a556b54c3defefbd3a397
Paul C Pederson <paul.c.pederson@gmail.com>
2019-10-15 10:39:51 -0700

    add minimal theme to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,14 @@
 import { configure, addParameters, addDecorator } from '@storybook/html';
 import centered from '@storybook/addon-centered/html';
+import theme from './theme';
 
 addDecorator(centered);
 addParameters({
   backgrounds: [
     { name: 'Light', value: '#f8f8f8', default: true },
     { name: 'Dark', value: '#202020' }
-  ]
+  ],
+  options: {theme}
 });
+
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
