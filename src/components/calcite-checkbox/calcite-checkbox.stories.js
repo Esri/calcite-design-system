7b625ef5a285e5e331bb119331f4218ac727ef75
Paul C Pederson <paul.c.pederson@gmail.com>
2019-10-15 17:01:33 -0700

    stories for calcite-checkbox

---
+++ src/components/calcite-checkbox/calcite-checkbox.stories.js
@@ -0,0 +1,29 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs'
+import { darkBackground } from "../../../.storybook/helpers";
+import notes from './readme.md';
+
+storiesOf('Checkbox', module)
+  .addDecorator(withKnobs)
+  .add('Simple', () => `
+    <label>
+      <calcite-checkbox
+        checked=${boolean('checked', true)}
+        disabled=${boolean('disabled', false)}
+        indeterminate=${boolean('indeterminate', false)}
+        size=${select('size', {large: 'large', small: 'small'}, 'small')}
+      ></calcite-checkbox>
+      Text for the checkbox
+    </label>
+  `, { notes })
+  .add('Dark mode', () => `
+    <label>
+      <calcite-checkbox
+        theme="dark"
+        checked=${boolean('checked', true)}
+        disabled=${boolean('disabled', false)}
+        indeterminate=${boolean('indeterminate', false)}
+        size=${select('size', {large: 'large', small: 'small'}, 'small')}
+      ></calcite-checkbox>
+    </label>
+`, { notes, backgrounds: darkBackground });
