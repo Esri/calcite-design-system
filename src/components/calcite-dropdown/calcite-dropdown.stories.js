27e60913e7f95a75bf2a00f131a6bf2861c75a0a
Paul C Pederson <paul.c.pederson@gmail.com>
2019-10-16 09:48:14 -0700

    add stories for dropdown

---
+++ src/components/calcite-dropdown/calcite-dropdown.stories.js
@@ -0,0 +1,34 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs, select } from '@storybook/addon-knobs'
+import { darkBackground } from "../../../.storybook/helpers";
+import notes from './readme.md';
+
+storiesOf('Dropdown', module)
+  .addDecorator(withKnobs)
+  .add('Simple', () => `
+    <calcite-dropdown
+      alignment="${select("alignment", {left: "left", right: "right", center: "center"}, "left")}"
+      scale="${select("scale", {s: "s", m: "m", l: "l"}, "m")}"
+    >
+      <calcite-button slot="dropdown-trigger">Open Dropdown</calcite-button>
+      <calcite-dropdown-group grouptitle="Sort by">
+        <calcite-dropdown-item>Relevance</calcite-dropdown-item>
+        <calcite-dropdown-item active>Date modified</calcite-dropdown-item>
+        <calcite-dropdown-item>Title</calcite-dropdown-item>
+      </calcite-dropdown-group>
+    </calcite-dropdown>
+  `, { notes })
+  .add('Dark mode', () => `
+    <calcite-dropdown
+      theme="dark"
+      alignment="${select("alignment", {left: "left", right: "right", center: "center"}, "left")}"
+      scale="${select("scale", {s: "s", m: "m", l: "l"}, "m")}"
+    >
+      <calcite-button slot="dropdown-trigger" theme="dark">Open Dropdown</calcite-button>
+      <calcite-dropdown-group grouptitle="Sort by">
+        <calcite-dropdown-item>Relevance</calcite-dropdown-item>
+        <calcite-dropdown-item active>Date modified</calcite-dropdown-item>
+        <calcite-dropdown-item>Title</calcite-dropdown-item>
+      </calcite-dropdown-group>
+    </calcite-dropdown>
+  `, { notes, backgrounds: darkBackground });
