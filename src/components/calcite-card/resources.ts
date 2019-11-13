b16415d6ea878fef78a7f10e18bef4a5005be7cd
Katy Robinson <k.robinson@esri.com>
2019-11-12 16:17:19 -0800

    card resources slots

---
+++ src/components/calcite-card/resources.ts
@@ -0,0 +1,7 @@
+export const enum SLOTS {
+  thumbnail = "thumbnail",
+  heading = "heading",
+  buttonAction = "button-action",
+  footerLeading = "footer-leading",
+  footerTrailing = "footer-trailing",
+}
