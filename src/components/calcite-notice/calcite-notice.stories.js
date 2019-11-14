4525b27f54a75e4e9f1b5f3629553e2481bfab66
Adam Tirella <adam@tirel.la>
2019-11-13 16:32:09 -0800

    Update storybook

--- src/components/calcite-notice/calcite-notice.stories.js
+++ src/components/calcite-notice/calcite-notice.stories.js
@@ -9,48 +9,54 @@ storiesOf("Notice", module)
   .add(
     "Simple",
     () => `
+    <div style="width:600px;max-width:100%">
     <calcite-notice
     theme="light"
     icon="${boolean("icon", true)}"
     active="${boolean("active", true)}"
     scale="${select("scale", ["s", "m", "l"], "m")}"
+    width="${select("width", ["auto", "half", "full"], "auto")}"
     color="${select("color", ["green", "red", "yellow", "blue"], "blue")}">
     <div slot="notice-title">Something failed</div>
     <div slot="notice-message">
       That thing you wanted to do didn't work as expected
     </div>
-    <calcite-button slot="notice-link" title="my action" appearance="inline">Retry</calcite-button>
-  </calcite-notice>
+      <calcite-button slot="notice-link" title="my action" appearance="inline">Retry</calcite-button>
+    </calcite-notice>
+    </div>
   `,
     { notes }
   )
   .add(
     "Dark Mode",
     () => `
+    <div style="width:600px;max-width:100%">
     <calcite-notice
     theme="dark"
     icon="${boolean("icon", true)}"
     active="${boolean("active", true)}"
     scale="${select("scale", ["s", "m", "l"], "m")}"
+    width="${select("width", ["auto", "half", "full"], "auto")}"
     color="${select("color", ["green", "red", "yellow", "blue"], "blue")}">
     <div slot="notice-title">Something failed</div>
     <div slot="notice-message">
       That thing you wanted to do didn't work as expected
     </div>
     <calcite-button slot="notice-link" title="my action" appearance="inline">Retry</calcite-button>
-  </calcite-notice>
+    </calcite-notice>
+    </div>
     `,
     { notes, backgrounds: darkBackground }
   )
   .add(
     "RTL",
     () => `
-      <div dir="rtl">
+      <div dir="rtl" style="width:600px;max-width:100%">
       <calcite-notice
       theme="light"
       icon="${boolean("icon", true)}"
       active="${boolean("active", true)}"
-
+      width="${select("width", ["auto", "half", "full"], "auto")}"
       scale="${select("scale", ["s", "m", "l"], "m")}"
       color="${select(
         "color",
