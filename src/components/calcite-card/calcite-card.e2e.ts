b16e3444912ebcc9ed676c6f99ba82fc7b2e8cb4
Katy Robinson <k.robinson@esri.com>
2019-11-19 14:49:41 -0800

    render test

---
+++ src/components/calcite-card/calcite-card.e2e.ts
@@ -0,0 +1,8 @@
+import { newE2EPage } from "@stencil/core/testing";
+import { defaults, renders } from "../../tests/commonTests";
+
+describe("calcite-card", () => {
+  it("renders", async () => renders("calcite-card"));
+
+
+})
