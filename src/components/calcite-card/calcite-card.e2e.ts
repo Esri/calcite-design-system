10ebc8e77ccb62f7209de6120d29d87586bb37a7
Katy Robinson <k.robinson@esri.com>
2019-11-19 15:23:45 -0800

    defaults test

--- src/components/calcite-card/calcite-card.e2e.ts
+++ src/components/calcite-card/calcite-card.e2e.ts
@@ -1,8 +1,33 @@
 import { newE2EPage } from "@stencil/core/testing";
 import { defaults, renders } from "../../tests/commonTests";
+import { createFalse } from "typescript";
 
 describe("calcite-card", () => {
   it("renders", async () => renders("calcite-card"));
 
+  it("has property defaults", async () => {
+    defaults("calcite-card", [
+      {
+        propertyName: "disabled",
+        defaultValue: false
+      },
+      {
+        propertyName: "loading",
+        defaultValue: false,
 
+      },
+      {
+        propertyName: "respectImageHeight",
+        defaultValue: false
+      },
+      {
+        propertyName: "selected",
+        defaultValue:false
+      },
+      {
+        propertyName: "theme",
+        defaultValue: "light"
+      }
+    ])
+  })
 })
