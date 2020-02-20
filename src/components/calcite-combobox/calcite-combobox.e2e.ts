eae7d6dc49aeea69d7e0261d0a5bf870d7fcc96c
Harry Robbins <arrogantgopher@gmail.com>
2020-02-20 14:38:13 -0800

    add filter test

--- src/components/calcite-combobox/calcite-combobox.e2e.ts
+++ src/components/calcite-combobox/calcite-combobox.e2e.ts
@@ -21,8 +21,30 @@ describe("calcite-pagination", () => {
     expect(listBoxVisible).toBe(true);
   });
 
-  it("should filter the items in listbox when typing into the input", async () => {
+  it.only("should filter the items in listbox when typing into the input", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`<calcite-combobox>
+      <calcite-combobox-item value="one" text-label="one"></calcite-combobox-item>
+      <calcite-combobox-item value="two" text-label="two"></calcite-combobox-item>
+    </calcite-combobox>`);
 
+    await page.keyboard.press("Tab");
+    await page.keyboard.type("one");
+
+    await page.evaluate(() => {
+      const combobox = document.querySelector("calcite-combobox");
+      const input = combobox.shadowRoot.querySelector("input");
+      input.value = "one";
+      input.dispatchEvent(new Event("input"));
+    });
+
+    const items = await page.findAll("calcite-combobox-item");
+    await items[1].waitForNotVisible();
+    const item1Visible = await items[0].isVisible();
+    const item2Visible = await items[1].isVisible();
+
+    expect(item1Visible).toBe(true);
+    expect(item2Visible).toBe(false);
   });
 
   describe("item selection", () => {
