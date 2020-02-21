1150a6046ad511fa00c0706309f8d0721db38321
Adam Tirella <adam@tirel.la>
2020-02-21 13:33:09 -0800

    Clean up dropdown tsx
    Removes unneeded theme from child items

--- src/components/calcite-dropdown-group/calcite-dropdown-group.tsx
+++ src/components/calcite-dropdown-group/calcite-dropdown-group.tsx
@@ -8,7 +8,6 @@ import {
   Listen,
   Prop
 } from "@stencil/core";
-import { getElementTheme} from "../../utils/dom";
 import { guid } from "../../utils/guid";
 
 @Component({
@@ -73,13 +72,12 @@ export class CalciteDropdownGroup {
   }
 
   render() {
-    const theme = getElementTheme(this.el);
     const groupTitle = this.groupTitle ? (
       <span class="dropdown-title">{this.groupTitle}</span>
     ) : null;
 
     return (
-      <Host theme={theme}>
+      <Host>
         {groupTitle}
         <slot />
       </Host>
