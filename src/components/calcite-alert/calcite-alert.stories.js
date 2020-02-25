9af19b878a7fe12055e6a9e24554e15a8fc8c17f
Adam Tirella <adam@tirel.la>
2020-02-25 11:57:23 -0800

    Add example of queued alerts to Storybook

--- src/components/calcite-alert/calcite-alert.stories.js
+++ src/components/calcite-alert/calcite-alert.stories.js
@@ -6,6 +6,7 @@ const notes = parseReadme(readme);
 
 storiesOf("Alert", module)
   .addDecorator(withKnobs)
+
   .add(
     "Alert - title, message, link",
     () => `
@@ -13,11 +14,6 @@ storiesOf("Alert", module)
     theme="light"
     icon="${boolean("icon", true)}"
     auto-dismiss="${boolean("auto-dismiss", false)}"
-    auto-dismiss-duration="${select("auto-dismiss-duration", [
-      "slow",
-      "medium",
-      "fast"
-    ])};
     auto-dismiss-duration="${select(
       "auto-dismiss-duration",
       ["fast", "medium", "slow"],
@@ -42,11 +38,6 @@ storiesOf("Alert", module)
     theme="light"
     icon="${boolean("icon", true)}"
     auto-dismiss="${boolean("auto-dismiss", false)}"
-    auto-dismiss-duration="${select("auto-dismiss-duration", [
-      "slow",
-      "medium",
-      "fast"
-    ])};
     auto-dismiss-duration="${select(
       "auto-dismiss-duration",
       ["fast", "medium", "slow"],
@@ -70,11 +61,6 @@ storiesOf("Alert", module)
     theme="light"
     icon="${boolean("icon", true)}"
     auto-dismiss="${boolean("auto-dismiss", false)}"
-    auto-dismiss-duration="${select("auto-dismiss-duration", [
-      "slow",
-      "medium",
-      "fast"
-    ])};
     auto-dismiss-duration="${select(
       "auto-dismiss-duration",
       ["fast", "medium", "slow"],
@@ -98,11 +84,6 @@ storiesOf("Alert", module)
     theme="light"
     icon="${boolean("icon", true)}"
     auto-dismiss="${boolean("auto-dismiss", false)}"
-    auto-dismiss-duration="${select("auto-dismiss-duration", [
-      "slow",
-      "medium",
-      "fast"
-    ])};
     auto-dismiss-duration="${select(
       "auto-dismiss-duration",
       ["fast", "medium", "slow"],
@@ -119,22 +100,86 @@ storiesOf("Alert", module)
     { notes }
   )
   .add(
-    "Alert - dark mode",
+    "Alert - Queue",
+    () => `
+   <div>
+    <h5>Open or add to queue</h5>
+    <calcite-button onclick=document.querySelector("#one").open()>Open Alert 1</calcite-button>
+    <calcite-button onclick=document.querySelector("#two").open()>Open Alert 2</calcite-button>
+    <calcite-button onclick=document.querySelector("#three").open()>Open Alert 3</calcite-button>
+    <br/>
+    <br/>
+    <h5>Close or remove from queue</h5>
+    <calcite-button color="red" onclick=document.querySelector("#one").close()>Close Alert 1</calcite-button>
+    <calcite-button color="red" onclick=document.querySelector("#two").close()>Close Alert 2</calcite-button>
+    <calcite-button color="red" onclick=document.querySelector("#three").close()>Close Alert 3</calcite-button>
+      <calcite-alert
+      id="one"
+      theme="light"
+      icon="${boolean("icon", true)}"
+      auto-dismiss="${boolean("auto-dismiss", false)}"
+      auto-dismiss-duration="${select(
+        "auto-dismiss-duration",
+        ["fast", "medium", "slow"],
+        "medium"
+      )}"
+      color="${select("color", ["green", "red", "yellow", "blue"], "green")}">
+      <div slot="alert-title">Your great thing happened</div>
+      <div slot="alert-message">
+        Successfully duplicated <strong>2019 Sales Demographics by County</strong> layer
+      </div>
+      <calcite-button slot="alert-link" title="my action" appearance="inline">View layer</calcite-button>
+    </calcite-alert>
+    <calcite-alert
+    id="two"
+    theme="light"
+    icon="${boolean("icon-2", true)}"
+    auto-dismiss="${boolean("auto-dismiss-2", false)}"
+    auto-dismiss-duration="${select(
+      "auto-dismiss-duration-2",
+      ["fast", "medium", "slow"],
+      "medium"
+    )}"
+    color="${select("color-2", ["green", "red", "yellow", "blue"], "blue")}">
+    <div slot="alert-title">Your great thing happened</div>
+    <div slot="alert-message">
+    Successfully duplicated <strong>2019 Sales Demographics by County</strong> layer
+    </div>
+    <calcite-button slot="alert-link" title="my action" appearance="inline">View layer</calcite-button>
+    </calcite-alert>
+    <calcite-alert
+      id="three"
+      theme="light"
+      icon="${boolean("icon-3", true)}"
+      auto-dismiss="${boolean("auto-dismiss-3", true)}"
+      auto-dismiss-duration="${select(
+        "auto-dismiss-duration-3",
+        ["fast", "medium", "slow"],
+        "medium"
+      )}"
+      color="${select("color-3", ["green", "red", "yellow", "blue"], "red")}">
+      <div slot="alert-title">That didn't work out</div>
+      <div slot="alert-message">
+        That thing you wanted to do didn't work
+      </div>
+      <calcite-button slot="alert-link" title="my action" appearance="inline">View layer</calcite-button>
+    </calcite-alert>
+   </div>
+  `,
+    { notes }
+  )
+  .add(
+    "Alert - Dark Theme",
     () => `
     <calcite-alert
     theme="dark"
     icon="${boolean("icon", true)}"
     auto-dismiss="${boolean("auto-dismiss", false)}"
-    auto-dismiss-duration="${select("auto-dismiss-duration", [
-      "slow",
-      "medium",
-      "fast"
-    ])};
-    auto-dismiss-duration="${select(
-      "auto-dismiss-duration",
-      ["fast", "medium", "slow"],
-      "medium"
-    )}"
+  auto-dismiss-duration="${select(
+    "auto-dismiss-duration",
+    ["fast", "medium", "slow"],
+    "medium"
+  )}"
     active="${boolean("active", true)}"
     scale="${select("scale", ["s", "m", "l"], "m")}"
     color="${select("color", ["green", "red", "yellow", "blue"], "red")}">
@@ -142,10 +187,77 @@ storiesOf("Alert", module)
     <div slot="alert-message">
       That thing you wanted to do didn't work as expected
     </div>
-    <calcite-button slot="alert-link" title="my action" appearance="inline">Retry</calcite-button>
+    <calcite-button theme="dark" slot="alert-link" title="my action" appearance="inline">Retry</calcite-button>
   </calcite-alert>
   `,
-    { notes }
+    { notes, backgrounds: darkBackground }
+  )
+  .add(
+    "Alert - Dark Theme Queue",
+    () => `
+   <div>
+    <h5 style="color:white">Open or add to queue</h5>
+    <calcite-button theme="dark" onclick=document.querySelector("#one").open()>Open Alert 1</calcite-button>
+    <calcite-button theme="dark" onclick=document.querySelector("#two").open()>Open Alert 2</calcite-button>
+    <calcite-button theme="dark" onclick=document.querySelector("#three").open()>Open Alert 3</calcite-button>
+    <br/>
+    <br/>
+    <h5 style="color:white">Close or remove from queue</h5>
+    <calcite-button theme="dark" color="red" onclick=document.querySelector("#one").close()>Close Alert 1</calcite-button>
+    <calcite-button theme="dark" color="red" onclick=document.querySelector("#two").close()>Close Alert 2</calcite-button>
+    <calcite-button theme="dark" color="red" onclick=document.querySelector("#three").close()>Close Alert 3</calcite-button>
+      <calcite-alert
+      id="one"
+      theme="dark"
+      icon="${boolean("icon", true)}"
+      auto-dismiss="${boolean("auto-dismiss", false)}"
+      auto-dismiss-duration="${select(
+        "auto-dismiss-duration",
+        ["fast", "medium", "slow"],
+        "medium"
+      )}"
+      color="${select("color", ["green", "red", "yellow", "blue"], "green")}">
+      <div slot="alert-title">Your great thing happened</div>
+      <div slot="alert-message">
+        Successfully duplicated <strong>2019 Sales Demographics by County</strong> layer
+      </div>
+      <calcite-button theme="dark" slot="alert-link" title="my action" appearance="inline">View layer</calcite-button>
+    </calcite-alert>
+    <calcite-alert
+    id="two"
+    theme="dark"
+    icon="${boolean("icon-2", true)}"
+    auto-dismiss="${boolean("auto-dismiss-2", false)}"
+    auto-dismiss-duration="${select(
+      "auto-dismiss-duration-2",
+      ["fast", "medium", "slow"],
+      "medium"
+    )}"
+    color="${select("color-2", ["green", "red", "yellow", "blue"], "blue")}">
+    <div slot="alert-title">Your great thing happened</div>
+    <div slot="alert-message">
+    Successfully duplicated <strong>2019 Sales Demographics by County</strong> layer
+    </div>
+    <calcite-button theme="dark" slot="alert-link" title="my action" appearance="inline">View layer</calcite-button>
+    </calcite-alert>
+    <calcite-alert
+      id="three"
+      theme="dark"
+      icon="${boolean("icon-3", true)}"
+      auto-dismiss="${boolean("auto-dismiss-3", true)}"
+      auto-dismiss-duration="${select(
+        "auto-dismiss-duration-3",
+        ["fast", "medium", "slow"],
+        "medium"
+      )}"
+      color="${select("color-3", ["green", "red", "yellow", "blue"], "red")}">
+      <div slot="alert-message">
+        That thing you wanted to do didn't work out so well.
+      </div>
+    </calcite-alert>
+   </div>
+  `,
+    { notes, backgrounds: darkBackground }
   )
   .add(
     "Alert - RTL",
@@ -155,11 +267,6 @@ storiesOf("Alert", module)
     theme="light"
     icon="${boolean("icon", true)}"
     auto-dismiss="${boolean("auto-dismiss", false)}"
-    auto-dismiss-duration="${select("auto-dismiss-duration", [
-      "slow",
-      "medium",
-      "fast"
-    ])};
     auto-dismiss-duration="${select(
       "auto-dismiss-duration",
       ["fast", "medium", "slow"],
