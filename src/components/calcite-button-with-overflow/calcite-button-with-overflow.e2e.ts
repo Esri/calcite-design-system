e476f592dd43f1518a5b4bcbef72f2d3249bee07
Katrina Stinson <katrina.m.stinson@gmail.com>
2020-02-26 11:34:15 -0500

    Rename description in test file

--- src/components/calcite-button-with-overflow/calcite-button-with-overflow.e2e.ts
+++ src/components/calcite-button-with-overflow/calcite-button-with-overflow.e2e.ts
@@ -1,6 +1,6 @@
 import { newE2EPage } from "@stencil/core/testing";
 
-describe("calcite-dropdown", () => {
+describe("calcite-button-with-overflow", () => {
   it("renders", async () => {
     const page = await newE2EPage();
     await page.setContent(`
