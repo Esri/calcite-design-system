4ee45abba419aba70e60297c6d51671d7818b70d
JC Franco <jfranco@esri.com>
2020-04-02 09:46:41 -0700

    fix(icon): always render in the target scale to avoid shifts in layout (#433)
    Reverts c601720bb8756f848de03fc02d8274621bc2a94f

--- src/components/calcite-icon/calcite-icon.e2e.ts
+++ src/components/calcite-icon/calcite-icon.e2e.ts
@@ -69,18 +69,6 @@ describe("calcite-icon", () => {
       expect(await path.getAttribute("d")).toBe(iconPathData);
     });
 
-    it("it does not render SVG for invalid icons", async () => {
-      const page = await newE2EPage();
-      await page.setContent(
-        `<calcite-icon icon="this-does-not-exist"></calcite-icon>`
-      );
-      await page.waitForChanges();
-
-      let svg = await page.find(`calcite-icon >>> svg`);
-
-      expect(svg).toBeNull();
-    });
-
     it("loads icon when it's close to viewport", async () => {
       const page = await newE2EPage();
       await page.setContent(
@@ -88,17 +76,13 @@ describe("calcite-icon", () => {
       );
       await page.waitForChanges();
 
-      const iconPathSelector = `calcite-icon >>> path`;
-
       const icon = await page.find(`calcite-icon`);
-      let path = await page.find(iconPathSelector);
+      let path = await page.find(`calcite-icon >>> path`);
 
-      expect(path).toBeNull();
+      expect(await path.getAttribute("d")).toBeNull();
 
       icon.setProperty("style", null);
       await page.waitForChanges();
-
-      path = await page.find(iconPathSelector);
 
       expect(await path.getAttribute("d")).toBeTruthy();
     });
