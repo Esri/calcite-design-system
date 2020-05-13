6b8fdbdc3e80be83722f254ff66ce2ce8c0ee90e
Erik Harper <eharperdicianno@esri.com>
2020-05-13 14:31:26 -0700

    removing calcite-label wrap story until we can address this in a follow-up ticket

--- src/components/calcite-radio-button-group/calcite-radio-button-group.stories.js
+++ src/components/calcite-radio-button-group/calcite-radio-button-group.stories.js
@@ -18,7 +18,7 @@ storiesOf("Radio Button Group", module)
       layout="${select("layout", ["horizontal", "vertical"], "horizontal")}"
       scale="${select("scale", ["s", "m", "l"], "m")}"
     >
-      <calcite-radio-button value="react" checked>React</calcite-radio-button>
+      <calcite-radio-button value="react">React</calcite-radio-button>
       <calcite-radio-button value="ember">Ember</calcite-radio-button>
       <calcite-radio-button value="angular">Angular</calcite-radio-button>
       <calcite-radio-button value="vue">Vue</calcite-radio-button>
@@ -37,32 +37,11 @@ storiesOf("Radio Button Group", module)
       layout="${select("layout", ["horizontal", "vertical"], "horizontal")}"
       scale="${select("scale", ["s", "m", "l"], "m")}"
     >
-      <calcite-radio-button value="react" checked>React</calcite-radio-button>
+      <calcite-radio-button value="react">React</calcite-radio-button>
       <calcite-radio-button value="ember">Ember</calcite-radio-button>
       <calcite-radio-button value="angular">Angular</calcite-radio-button>
       <calcite-radio-button value="vue">Vue</calcite-radio-button>
     </calcite-radio-button-group>
   `,
     { notes, backgrounds: darkBackground }
-  )
-  .add(
-    "Wrapping Calcite Label",
-    () => `
-    <calcite-label>
-      <span style="margin-bottom: 15px;">My great radio button group</span>
-      <calcite-radio-button-group
-        name="vertical"
-        disabled="${boolean("disabled", false)}"
-        hidden="${boolean("hidden", false)}"
-        layout="${select("layout", ["horizontal", "vertical"], "horizontal")}"
-        scale="${select("scale", ["s", "m", "l"], "m")}"
-      >
-        <calcite-radio-button value="react" checked>React</calcite-radio-button>
-        <calcite-radio-button value="ember">Ember</calcite-radio-button>
-        <calcite-radio-button value="angular">Angular</calcite-radio-button>
-        <calcite-radio-button value="vue">Vue</calcite-radio-button>
-      </calcite-radio-button-group>
-    </calcite-label>
-  `,
-    { notes }
   );
