0f238afd13f19babd96b55a95faba38e1e71ea08
JC Franco <jfranco@esri.com>
2020-05-22 15:28:39 -0700

    add focusable test to hex input

--- src/components/calcite-hex-input/calcite-hex-input.e2e.ts
+++ src/components/calcite-hex-input/calcite-hex-input.e2e.ts
@@ -1,22 +1,31 @@
 import { newE2EPage } from "@stencil/core/testing";
-import { defaults, reflects, renders } from "../../tests/commonTests";
+import {
+  defaults,
+  focusable,
+  reflects,
+  renders,
+} from "../../tests/commonTests";
 
 describe("calcite-hex-input", () => {
   it("renders", () => renders("calcite-hex-input"));
 
-  it("has defaults", () => defaults("calcite-hex-input", [
-    {
-      propertyName: "value",
-      defaultValue: "#000000"
-    }
-  ]));
+  it("has defaults", () =>
+    defaults("calcite-hex-input", [
+      {
+        propertyName: "value",
+        defaultValue: "#000000",
+      },
+    ]));
 
-  it("reflects", () => reflects("calcite-hex-input", [
-    {
-      propertyName: "value",
-      value: "#ffffff"
-    }
-  ]));
+  it("reflects", () =>
+    reflects("calcite-hex-input", [
+      {
+        propertyName: "value",
+        value: "#ffffff",
+      },
+    ]));
+
+  it("can be focused", async () => focusable("calcite-hex-input"));
 
   it("accepts shorthand hex", async () => {
     const page = await newE2EPage();
@@ -26,7 +35,7 @@ describe("calcite-hex-input", () => {
     await input.setProperty("value", "#fff");
     await page.waitForChanges();
 
-    expect(await input.getProperty("value")).toBe("#ffffff")
+    expect(await input.getProperty("value")).toBe("#ffffff");
   });
 
   it("accepts longhand hex", async () => {
@@ -37,21 +46,25 @@ describe("calcite-hex-input", () => {
     await input.setProperty("value", "#fafafa");
     await page.waitForChanges();
 
-    expect(await input.getProperty("value")).toBe("#fafafa")
+    expect(await input.getProperty("value")).toBe("#fafafa");
   });
 
   it("normalizes value when initialized", async () => {
     const page = await newE2EPage();
-    await page.setContent("<calcite-hex-input value='#f0f'></calcite-hex-input>");
+    await page.setContent(
+      "<calcite-hex-input value='#f0f'></calcite-hex-input>"
+    );
     await page.waitForChanges();
     const input = await page.find(`calcite-hex-input`);
 
-    expect(await input.getProperty("value")).toBe("#ff00ff")
+    expect(await input.getProperty("value")).toBe("#ff00ff");
   });
 
   it("ignores invalid hex", async () => {
     const page = await newE2EPage();
-    await page.setContent("<calcite-hex-input value='#b33f33'></calcite-hex-input>");
+    await page.setContent(
+      "<calcite-hex-input value='#b33f33'></calcite-hex-input>"
+    );
 
     const input = await page.find(`calcite-hex-input`);
     await input.setProperty("value", "wrong");
@@ -72,12 +85,14 @@ describe("calcite-hex-input", () => {
     await input.setProperty("value", "#aaaaa");
     await page.waitForChanges();
 
-    expect(await input.getProperty("value")).toBe("#b33f33")
+    expect(await input.getProperty("value")).toBe("#b33f33");
   });
 
   it("emits event when color changes", async () => {
     const page = await newE2EPage();
-    await page.setContent("<calcite-hex-input value='#b33f33'></calcite-hex-input>");
+    await page.setContent(
+      "<calcite-hex-input value='#b33f33'></calcite-hex-input>"
+    );
 
     const input = await page.find("calcite-hex-input");
     const spy = await input.spyOnEvent("calciteHexInputChange");
