b61e2dcacb3c22978f02fde64dfe4705ddf1fda5
Adam Tirella <adam@tirel.la>
2020-07-08 11:09:48 -0700

    Update storybook

--- src/components/calcite-card/calcite-card.stories.js
+++ src/components/calcite-card/calcite-card.stories.js
@@ -88,9 +88,9 @@ storiesOf("Card", module)
       <span slot="subtitle">Johnathan Smith</span>
       <span slot="footer-leading">Nov 25, 2018</span>
       <div slot="footer-trailing">
-        <calcite-button id="card-icon-test-6" scale="s" appearance="transparent" color="dark" icon='circle'>
+        <calcite-button id="card-icon-test-6" scale="s" appearance="transparent" color="dark" icon-start='circle'>
         </calcite-button>
-        <calcite-button id="card-icon-test-7" scale="s" appearance="transparent" color="dark" icon='circle'>
+        <calcite-button id="card-icon-test-7" scale="s" appearance="transparent" color="dark" icon-start='circle'>
         </calcite-button>
       </div>
     </calcite-card>
@@ -122,12 +122,12 @@ storiesOf("Card", module)
           <br />
           View Count: 0
         </div>
-        <calcite-button slot="footer-leading" color="light" scale="s" icon='circle'></calcite-button>
+        <calcite-button slot="footer-leading" color="light" scale="s" icon-start='circle'></calcite-button>
         <div slot="footer-trailing">
-          <calcite-button scale="s" color="light" id="card-icon-test-2" icon='circle'></calcite-button>
-          <calcite-button scale="s" color="light" id="card-icon-test-1" icon='circle'></calcite-button>
+          <calcite-button scale="s" color="light" id="card-icon-test-2" icon-start='circle'></calcite-button>
+          <calcite-button scale="s" color="light" id="card-icon-test-1" icon-start='circle'></calcite-button>
           <calcite-dropdown>
-            <calcite-button id="card-icon-test-5" slot="dropdown-trigger" scale="s" color="light" icon='circle'></calcite-button>
+            <calcite-button id="card-icon-test-5" slot="dropdown-trigger" scale="s" color="light" icon-start='circle'></calcite-button>
             <calcite-dropdown-group selection-mode="none">
               <calcite-dropdown-item>View details</calcite-dropdown-item>
               <calcite-dropdown-item>Duplicate</calcite-dropdown-item>
@@ -233,9 +233,9 @@ storiesOf("Card", module)
       <span slot="subtitle">Johnathan Smith</span>
       <span slot="footer-leading">Nov 25, 2018</span>
       <div slot="footer-trailing">
-        <calcite-button id="card-icon-test-6" scale="s" appearance="transparent" color="dark" icon='circle'>
+        <calcite-button id="card-icon-test-6" scale="s" appearance="transparent" color="dark" icon-start='circle'>
         </calcite-button>
-        <calcite-button id="card-icon-test-7" scale="s" appearance="transparent" color="dark" icon='circle'>
+        <calcite-button id="card-icon-test-7" scale="s" appearance="transparent" color="dark" icon-start='circle'>
         </calcite-button>
       </div>
     </calcite-card>
@@ -268,12 +268,12 @@ storiesOf("Card", module)
           <br />
           View Count: 0
         </div>
-        <calcite-button slot="footer-leading" color="dark" scale="s" icon='circle'></calcite-button>
+        <calcite-button slot="footer-leading" color="dark" scale="s" icon-start='circle'></calcite-button>
         <div slot="footer-trailing">
-          <calcite-button theme="dark" color="dark" scale="s" id="card-icon-test-2" icon='circle'></calcite-button>
-          <calcite-button theme="dark" color="dark" scale="s" id="card-icon-test-1" icon='circle'></calcite-button>
+          <calcite-button theme="dark" color="dark" scale="s" id="card-icon-test-2" icon-start='circle'></calcite-button>
+          <calcite-button theme="dark" color="dark" scale="s" id="card-icon-test-1" icon-start='circle'></calcite-button>
           <calcite-dropdown>
-            <calcite-button theme="dark" color="dark" id="card-icon-test-5" slot="dropdown-trigger" scale="s" icon='circle'></calcite-button>
+            <calcite-button theme="dark" color="dark" id="card-icon-test-5" slot="dropdown-trigger" scale="s" icon-start='circle'></calcite-button>
             <calcite-dropdown-group selection-mode="none">
               <calcite-dropdown-item>View details</calcite-dropdown-item>
               <calcite-dropdown-item>Duplicate</calcite-dropdown-item>
