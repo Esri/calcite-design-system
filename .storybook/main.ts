a378abb14da3d1e2902e9e94cec3c4ebf172dc15
Matt Driscoll <MDriscoll@esri.com>
2020-07-09 15:13:07 -0700

    get all stories showing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,8 @@ module.exports = {
     "@storybook/addon-backgrounds/register",
     "@storybook/addon-knobs/register",
     "@storybook/addon-notes/register-panel",
-    "@storybook/addon-a11y/register",
+    "@storybook/addon-a11y/register"
   ],
   presets: ["@storybook/addon-docs/preset"],
-  stories: ["../src/**/*.stories.(mdx|ts)"],
+  stories: ["../src/**/*.stories.(mdx|ts|js)"]
 };
