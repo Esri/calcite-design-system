259d615b4cb6cd0558af9ee2dd403986c126aaf2
Alan Sangma <asangma@esri.com>
2020-07-10 11:20:39 -0700

    Action. Update icon scales.

--- src/components/calcite-action/calcite-action.tsx
+++ src/components/calcite-action/calcite-action.tsx
@@ -149,7 +149,7 @@ export class CalciteAction {
 
   renderIconContainer(): VNode {
     const { loading, icon, scale, el } = this;
-    const iconScale = scale === "l" ? "l" : "m";
+    const iconScale = scale === "l" ? "m" : "s";
     const calciteLoaderNode = loading ? (
       <calcite-loader is-active inline />
     ) : null;
