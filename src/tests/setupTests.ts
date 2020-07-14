01ebf56bce59e093e790d0ad406241aa88a76816
Matt Driscoll <MDriscoll@esri.com>
2020-07-14 08:55:32 -0700

    update comment

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -1,4 +1,4 @@
-// Fails tests on console.error(): https://ionic.zendesk.com/hc/en-us/requests/31510
+// Fail tests on console.error(): https://github.com/Esri/calcite-components/pull/728
 let error = console.error;
 console.error = function (message: any, ...args: any[]) {
   error.call(console, message, ...args); // keep default behaviour
