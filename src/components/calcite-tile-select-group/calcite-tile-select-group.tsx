9a388ebeec1e05eba3878f09b3dbc300de7eb3ca
Erik Harper <eharperdicianno@esri.com>
2020-07-22 17:10:45 -0700

    calcite-tile-select-group wrapper component

---
+++ src/components/calcite-tile-select-group/calcite-tile-select-group.tsx
@@ -0,0 +1,16 @@
+import { Component, Host, h } from "@stencil/core";
+
+@Component({
+  tag: "calcite-tile-select-group",
+  styleUrl: "calcite-tile-select-group.scss",
+  shadow: true,
+})
+export class CalciteTileSelectGroup {
+  render() {
+    return (
+      <Host>
+        <slot></slot>
+      </Host>
+    );
+  }
+}
