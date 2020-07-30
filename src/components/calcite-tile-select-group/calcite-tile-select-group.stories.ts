c9c99ace39a4a4f8a52369c099d8caae0934909f
Erik Harper <eharperdicianno@esri.com>
2020-07-30 15:35:40 -0700

    docs(calcite-tile-select-group): story

--- src/components/calcite-tile-select-group/calcite-tile-select-group.stories.ts
+++ src/components/calcite-tile-select-group/calcite-tile-select-group.stories.ts
@@ -1,41 +1,108 @@
 import { storiesOf } from "@storybook/html";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, select } from "@storybook/addon-knobs";
+import { darkBackground } from "../../../.storybook/helpers";
 
 storiesOf("components|Tile Select Group", module)
   .addDecorator(withKnobs)
   .add(
-    "Simple",
+    "Light",
     () => `
     <calcite-tile-select-group>
-      <calcite-tile-select checked icon="layers" name="left-radio" value="one"
+      <calcite-tile-select
+        checked
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.  Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
         heading="Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
+        icon="layers"
+        name="light"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="one"
+      >
       </calcite-tile-select>
-      <calcite-tile-select checked icon="layers" name="left-radio" value="two"
+      <calcite-tile-select
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
         heading="Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
+        icon="layers"
+        name="light"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="two"
+      >
       </calcite-tile-select>
-      <calcite-tile-select icon="layers" name="left-radio" value="three"
+      <calcite-tile-select
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall. Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
         heading="Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall. Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
+        icon="layers"
+        name="light"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="three"
+      >
       </calcite-tile-select>
-      <calcite-tile-select icon="layers" name="left-radio" value="four" heading="Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
-      </calcite-tile-select>
-      <calcite-tile-select icon="layers" name="left-radio" value="five" heading="Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
-      </calcite-tile-select>
-      <calcite-tile-select icon="layers" name="left-radio" value="six" heading="Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
-      </calcite-tile-select>
-      <calcite-tile-select icon="layers" name="left-radio" value="seven"
-        heading="Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
-      </calcite-tile-select>
-      <calcite-tile-select icon="layers" name="left-radio" value="eight"
-        heading="Tile title lorem ipsum"
-        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.">
+      <calcite-tile-select
+        checked
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.  Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
+        heading="Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum"
+        icon="layers"
+        name="light"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="four"
+      >
       </calcite-tile-select>
     </calcite-tile-select-group>
   `
+  )
+  .add(
+    "Dark",
+    () => `
+    <calcite-tile-select-group>
+      <calcite-tile-select
+        checked
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.  Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
+        heading="Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum"
+        icon="layers"
+        name="dark"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        theme="dark"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="one"
+      >
+      </calcite-tile-select>
+      <calcite-tile-select
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
+        heading="Tile title lorem ipsum"
+        icon="layers"
+        name="dark"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        theme="dark"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="two"
+      >
+      </calcite-tile-select>
+      <calcite-tile-select
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall. Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
+        heading="Tile title lorem ipsum"
+        icon="layers"
+        name="dark"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        theme="dark"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="three"
+      >
+      </calcite-tile-select>
+      <calcite-tile-select
+        checked
+        description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall.  Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
+        heading="Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum Tile title lorem ipsum"
+        icon="layers"
+        name="dark"
+        show-input="${select("show-input", ["left", "right", "none"], "left")}"
+        theme="dark"
+        type="${select("type", ["radio", "checkbox"], "radio")}"
+        value="four"
+      >
+    </calcite-tile-select-group>
+  `,
+    { backgrounds: darkBackground }
   );
