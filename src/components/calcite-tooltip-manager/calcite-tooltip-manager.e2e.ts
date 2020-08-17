2e6472b530fabf0a06fc30fb2f386b73e13354b3
Matt Driscoll <MDriscoll@esri.com>
2020-08-17 16:05:06 -0700

    feat(tooltip): Dismiss calcite-tooltip via ESC key #877

---
+++ src/components/calcite-tooltip-manager/calcite-tooltip-manager.e2e.ts
@@ -0,0 +1,120 @@
+import { newE2EPage } from "@stencil/core/testing";
+import { TOOLTIP_REFERENCE } from "../calcite-tooltip/resources";
+import { defaults, hidden, renders } from "../../tests/commonTests";
+
+describe("calcite-tooltip-manager", () => {
+  it("renders", async () => renders(`<calcite-tooltip-manager></calcite-tooltip-manager>`));
+
+  it("honors hidden attribute", async () => hidden("calcite-tooltip-manager"));
+
+  it("has property defaults", async () =>
+    defaults("calcite-tooltip-manager", [
+      {
+        propertyName: "selector",
+        defaultValue: `[${TOOLTIP_REFERENCE}]`
+      }
+    ]));
+
+  it("should honor tooltips on mouseover/mouseout", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      `
+      <button id="test">test</button>
+      <calcite-tooltip-manager>
+        <calcite-tooltip reference-element="ref">Content</calcite-tooltip>
+        <button id="ref">Button</button>
+      <calcite-tooltip-manager>
+      `
+    );
+
+    await page.waitForChanges();
+
+    const tooltip = await page.find("calcite-tooltip");
+
+    expect(await tooltip.getProperty("open")).toBe(false);
+
+    const referenceElement = await page.find("#ref");
+
+    await referenceElement.hover();
+
+    await page.waitForChanges();
+
+    expect(await tooltip.getProperty("open")).toBe(true);
+
+    const testElement = await page.find("#test");
+
+    await testElement.hover();
+
+    await page.waitForChanges();
+
+    expect(await tooltip.getProperty("open")).toBe(false);
+  });
+
+  it("should honor tooltips on focus/blur", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      `
+      <button id="test">test</button>
+      <calcite-tooltip-manager>
+        <calcite-tooltip reference-element="ref">Content</calcite-tooltip>
+        <button id="ref">Button</button>
+      <calcite-tooltip-manager>
+      `
+    );
+
+    await page.waitForChanges();
+
+    const tooltip = await page.find("calcite-tooltip");
+
+    expect(await tooltip.getProperty("open")).toBe(false);
+
+    const referenceElement = await page.find("#ref");
+
+    await referenceElement.focus();
+
+    await page.waitForChanges();
+
+    expect(await tooltip.getProperty("open")).toBe(true);
+
+    const testElement = await page.find("#test");
+
+    await testElement.focus();
+
+    await page.waitForChanges();
+
+    expect(await tooltip.getProperty("open")).toBe(false);
+  });
+
+  it("should honor tooltips closing with ESC key", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      `
+      <calcite-tooltip-manager>
+        <calcite-tooltip reference-element="ref">Content</calcite-tooltip>
+        <button id="ref">Button</button>
+      <calcite-tooltip-manager>
+      `
+    );
+
+    await page.waitForChanges();
+
+    const tooltip = await page.find("calcite-tooltip");
+
+    expect(await tooltip.getProperty("open")).toBe(false);
+
+    const referenceElement = await page.find("#ref");
+
+    await referenceElement.focus();
+
+    await page.waitForChanges();
+
+    expect(await tooltip.getProperty("open")).toBe(true);
+
+    await referenceElement.press("Escape");
+
+    expect(await tooltip.getProperty("open")).toBe(false);
+  });
+});
