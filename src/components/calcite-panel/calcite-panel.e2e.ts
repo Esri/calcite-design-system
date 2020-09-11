1c0455fcacd8e76610b3f7502302c394c374e5ef
Alan Sangma <asangma@esri.com>
2020-09-11 13:33:13 -0700

    adds some additional e2e tests for panel. (#891)

--- src/components/calcite-panel/calcite-panel.e2e.ts
+++ src/components/calcite-panel/calcite-panel.e2e.ts
@@ -94,4 +94,126 @@ describe("calcite-panel", () => {
 
     expect(scrollSpy).toHaveReceivedEventTimes(1);
   });
+
+  it("should not render a header if there are no actions or content", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-panel>test</<calcite-panel>");
+
+    const header = await page.find(`calcite-panel >>> .${CSS.header}`);
+
+    expect(header).toBeNull();
+  });
+
+  it("should not render a footer if there are no actions or content", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-panel>test</<calcite-panel>");
+
+    const footer = await page.find(`calcite-panel >>> .${CSS.footer}`);
+
+    expect(footer).toBeNull();
+  });
+  
+  it("should not render menu nodes when there are no menu actions", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-panel></calcite-panel>");
+
+    const menuButton = await page.find(`calcite-panel >>> .${CSS.menuButton}`);
+    const menuContainer = await page.find(`calcite-panel >>> .${CSS.menuContainer}`);
+
+    expect(menuButton).toBeNull();
+    expect(menuContainer).toBeNull();
+  });
+
+  it("menuOpen should show/hide when toggled", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      `<calcite-panel>
+        <calcite-action slot="${SLOTS.headerMenuActions}" text="hello"></calcite-action>
+        <calcite-action slot="${SLOTS.headerMenuActions}" text="hello2"></calcite-action>
+      </calcite-panel>`
+    );
+
+    await page.waitForChanges();
+
+    const element = await page.find("calcite-panel");
+
+    expect(element.getAttribute("menuOpen")).toBeNull();
+
+    element.setProperty("menuOpen", true);
+
+    await page.waitForChanges();
+
+    const menu = await page.find(`calcite-panel >>> .${CSS.menu}`);
+
+    expect(menu).not.toBeNull();
+
+    const menuVisible = await menu.isVisible();
+
+    expect(menuVisible).toBe(true);
+
+    const menuButton = await page.find(`calcite-panel >>> .${CSS.menuButton}`);
+
+    expect(menuButton).not.toBeNull();
+
+    const menuButtonVisible = await menuButton.isVisible();
+
+    expect(menuButtonVisible).toBe(true);
+  });
+
+  it("should not render start or end actions containers when there are no start or end actions", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-panel></calcite-panel>");
+
+    const actionsContainerStart = await page.find(`calcite-panel >>> .${CSS.headerActionsStart}`);
+    const actionsContainerEnd = await page.find(`calcite-panel >>> .${CSS.headerActionsEnd}`);
+
+    expect(actionsContainerStart).toBeNull();
+    expect(actionsContainerEnd).toBeNull();
+  });
+
+  it("showBackButton", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent("<calcite-panel></calcite-panel>");
+
+    const element = await page.find("calcite-panel");
+
+    const showBackButton = await element.getProperty("showBackButton");
+
+    expect(showBackButton).toBe(false);
+
+    const panelNode = await page.find("calcite-panel >>> calcite-panel");
+    const backButton = await panelNode.find(`.${CSS.backButton}`);
+
+    expect(backButton).toBeNull();
+
+    element.setProperty("showBackButton", true);
+
+    await page.waitForChanges();
+
+    const showBackButtonNew = await element.getProperty("showBackButton");
+
+    expect(showBackButtonNew).toBe(true);
+
+    const panelNodeNew = await page.find("calcite-panel >>> calcite-panel");
+    const backButtonNew = await panelNodeNew.find(`.${CSS.backButton}`);
+
+    expect(backButtonNew).not.toBeNull();
+
+    expect(await backButtonNew.isVisible()).toBe(true);
+
+    const eventSpy = await page.spyOnEvent("calcitePanelBackClick", "window");
+
+    await page.$eval("calcite-panel", (elm: HTMLElement) => {
+      const nativeBackButton = elm.shadowRoot.querySelector(`calcite-action`);
+      nativeBackButton.click();
+    });
+
+    expect(eventSpy).toHaveReceivedEvent();
+  });
 });
