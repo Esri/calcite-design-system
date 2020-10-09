717ec95edc712311a1c70fe0beaaf8c4a2ef3f98
Paul C Pederson <paul.c.pederson@gmail.com>
2020-10-09 12:30:37 -0700

    test(date): work around missing shadow piercing element in e2e test

--- src/components/calcite-date/calcite-date.e2e.ts
+++ src/components/calcite-date/calcite-date.e2e.ts
@@ -7,25 +7,24 @@ describe("calcite-date", () => {
   it("fires a calciteDateChange event on change", async () => {
     const page = await newE2EPage();
     await page.setContent("<calcite-date></calcite-date>");
-    const input = await page.find("calcite-date >>> calcite-input");
+    await page.keyboard.press("Tab");
     const changedEvent = await page.spyOnEvent("calciteDateChange");
-    await input.callMethod("setFocus");
     // have to wait for transition
     await new Promise((res) => setTimeout(() => res(true), 200));
     const wrapper = await page.find("calcite-date >>> .calendar-picker-wrapper");
     const visible = await wrapper.isVisible();
     expect(visible).toBe(true);
-    await input.press("3");
-    await input.press("/");
-    await input.press("7");
-    await input.press("/");
-    await input.press("2");
+    await page.keyboard.press("3");
+    await page.keyboard.press("/");
+    await page.keyboard.press("7");
+    await page.keyboard.press("/");
+    await page.keyboard.press("2");
     expect(changedEvent).toHaveReceivedEventTimes(1);
-    await input.press("0");
+    await page.keyboard.press("0");
     expect(changedEvent).toHaveReceivedEventTimes(2);
-    await input.press("2");
+    await page.keyboard.press("2");
     expect(changedEvent).toHaveReceivedEventTimes(3);
-    await input.press("0");
+    await page.keyboard.press("0");
     expect(changedEvent).toHaveReceivedEventTimes(4);
   });
 
