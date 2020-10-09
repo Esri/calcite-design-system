9bc5f673144b7b35f6c41c7b190bf75f9f54a88e
Paul C Pederson <paul.c.pederson@gmail.com>
2020-10-09 14:09:17 -0700

    come on travis, give me a pass 🎲 🎲 ✨

--- src/components/calcite-date/calcite-date.e2e.ts
+++ src/components/calcite-date/calcite-date.e2e.ts
@@ -7,7 +7,12 @@ describe("calcite-date", () => {
   it("fires a calciteDateChange event on change", async () => {
     const page = await newE2EPage();
     await page.setContent("<calcite-date></calcite-date>");
-    await page.keyboard.press("Tab");
+    const input = (
+      await page.waitForFunction(() =>
+        document.querySelector("calcite-date").shadowRoot.querySelector("calcite-input input")
+      )
+    ).asElement();
+    await input.focus();
     const changedEvent = await page.spyOnEvent("calciteDateChange");
     // have to wait for transition
     await new Promise((res) => setTimeout(() => res(true), 200));
@@ -17,21 +22,18 @@ describe("calcite-date", () => {
       )
     ).asElement();
     expect(await wrapper.isIntersectingViewport()).toBe(true);
-    await page.keyboard.press("3");
-    await page.keyboard.press("/");
-    await page.keyboard.press("7");
-    await page.keyboard.press("/");
-    await page.keyboard.press("2");
-    await page.waitForChanges();
+
+    await input.press("3");
+    await input.press("/");
+    await input.press("7");
+    await input.press("/");
+    await input.press("2");
     expect(changedEvent).toHaveReceivedEventTimes(1);
-    await page.keyboard.press("0");
-    await page.waitForChanges();
+    await input.press("0");
     expect(changedEvent).toHaveReceivedEventTimes(2);
-    await page.keyboard.press("2");
-    await page.waitForChanges();
+    await input.press("2");
     expect(changedEvent).toHaveReceivedEventTimes(3);
-    await page.keyboard.press("0");
-    await page.waitForChanges();
+    await input.press("0");
     expect(changedEvent).toHaveReceivedEventTimes(4);
   });
 
