9671e0d29160252a0f2ff717b2cc3cc65d9f1c11
Alan Sangma <asangma@esri.com>
2020-10-22 15:36:32 -0700

    adds spacing values to tailwind config. refactored deprecated spacing css vars using theme (#1165)

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,6 +84,34 @@ module.exports = {
       'l': '1024px',
       'xl': '1440px'
     },
+    spacing: {
+      0: "0px",
+      "px": "1px",
+      "05": "0.125rem",
+      /* Increase by 0.25rem */
+      1: "0.25rem",     // 4px
+      2: "0.5rem",      // 8px
+      3: "0.75rem",     // 12px
+      4: "1rem",        // 16px
+      5: "1.25rem",     // 20px
+      6: "1.5rem",      // 24px
+      7: "1.75rem",     // 28px
+      8: "2rem",        // 32px
+      9: "2.25rem",     // 36px
+      10: "2.5rem",     // 40px
+      /* Increase by 0.5rem */
+      12: "3rem",       // 48px
+      /* Increase by 1rem */
+      16: "4rem",       // 64px
+      20: "5rem",       // 80px
+      24: "6rem",       // 96px
+      /* Increase by 2rem */
+      32: "8rem",       // 128px
+      40: "10rem",      // 160px
+      48: "12rem",      // 192px
+      56: "14rem",      // 224px
+      64: "16rem"       // 256px
+    },
     textColor: theme => ({
       1: theme("colors.text.1"),
       2: theme("colors.text.2"),
