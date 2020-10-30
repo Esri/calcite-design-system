e425d9c578c6b56c36e2d6c46f237a4f3ef1b212
Alan Sangma <asangma@esri.com>
2020-10-30 15:30:22 -0700

    some tailwind config cleanup. updates font config to use vars. fixes timing function. renames some stuff in global and refactors mixins to extensions. (#1165)

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,26 +16,10 @@ module.exports = {
       'none': 'none',
     },
     colors: {
-      blue: {
-        1: "var(--calcite-ui-blue-1)",
-        2: "var(--calcite-ui-blue-2)",
-        3: "var(--calcite-ui-blue-3)",
-      },
-      green: {
-        1: "var(--calcite-ui-green-1)",
-        2: "var(--calcite-ui-green-2)",
-        3: "var(--calcite-ui-green-3)",
-      },
-      yellow: {
-        1: "var(--calcite-ui-yellow-1)",
-        2: "var(--calcite-ui-yellow-2)",
-        3: "var(--calcite-ui-yellow-3)",
-      },
-      red: {
-        1: "var(--calcite-ui-red-1)",
-        2: "var(--calcite-ui-red-2)",
-        3: "var(--calcite-ui-red-3)",
-      },
+      blue: "var(--calcite-ui-blue-3)",
+      green: "var(--calcite-ui-green-3)",
+      yellow: "var(--calcite-ui-yellow-3)",
+      red: "var(--calcite-ui-red-3)",
       background: "var(--calcite-ui-background)",
       foreground: {
         1: "var(--calcite-ui-foreground-1)",
@@ -53,11 +37,13 @@ module.exports = {
         3: "var(--calcite-ui-border-3)",
         4: "var(--calcite-ui-border-4)",
         5: "var(--calcite-ui-border-5)",
-      }
+      },
+      transparent: "transparent"
     },
     fontFamily: {
-      sans: ["Avenir Next", "Avenir", "Helvetica Neue", "sans-serif"],
-      mono: ["Consolas", "Andale Mono", "Lucida Console", "Monaco", "monospace"],
+      sans: "var(--calcite-sans-family)",
+      mono: "var(--calcite-code-family)",
+      inherit: "inherit"
     },
     fontSize: {
       "-3": "var(--calcite-font-size--3)",  // 10px
@@ -85,53 +71,22 @@ module.exports = {
       'l': '1024px',
       'xl': '1440px'
     },
-    spacing: {
-      0: "0px",
-      "px": "1px",
-      "05": "0.125rem",
-      /* Increase by 0.25rem */
-      1: "0.25rem",     // 4px
-      2: "0.5rem",      // 8px
-      3: "0.75rem",     // 12px
-      4: "1rem",        // 16px
-      5: "1.25rem",     // 20px
-      6: "1.5rem",      // 24px
-      7: "1.75rem",     // 28px
-      8: "2rem",        // 32px
-      9: "2.25rem",     // 36px
-      10: "2.5rem",     // 40px
-      /* Increase by 0.5rem */
-      12: "3rem",       // 48px
-      /* Increase by 1rem */
-      16: "4rem",       // 64px
-      20: "5rem",       // 80px
-      24: "6rem",       // 96px
-      /* Increase by 2rem */
-      32: "8rem",       // 128px
-      40: "10rem",      // 160px
-      48: "12rem",      // 192px
-      56: "14rem",      // 224px
-      64: "16rem"       // 256px
-    },
-    textColor: theme => ({
-      "color-1": theme("colors.text.1"),
-      "color-2": theme("colors.text.2"),
-      "color-3": theme("colors.text.3"),
-      blue: {
-        1: theme("colors.blue.1"),
-        2: theme("colors.blue.2"),
-        3: theme("colors.blue.3"),
-      },
-      white: theme("colors.background")
-    }),
-    transitionDuration: {
-      75: "75ms",
-      150: "150ms",
-      300: "300ms",
-      500: "500ms"
-    },
-    transitionTimingFunction: "cubic-bezier(0.215, 0.440, 0.420, 0.880)"
+    // textColor: theme => ({
+    //   "color-1": theme("colors.text.1"),
+    //   "color-2": theme("colors.text.2"),
+    //   "color-3": theme("colors.text.3"),
+    //   blue: {
+    //     1: theme("colors.blue.1"),
+    //     2: theme("colors.blue.2"),
+    //     3: theme("colors.blue.3"),
+    //   },
+    //   white: theme("colors.background")
+    // }),
+    transitionTimingFunction: {
+      cubic: "cubic-bezier(0.215, 0.440, 0.420, 0.880)"
+    }
   },
+  backgroundColor: theme => theme("colors"),
   future: {
     removeDeprecatedGapUtilities: true,
   },
