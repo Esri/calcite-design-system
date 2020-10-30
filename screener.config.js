379bd5a54def18bb9be421711c1ad96f5865d522
Erik Harper <eharperdicianno@esri.com>
2020-10-29 17:11:29 -0700

    first commit to test out screener-storyboook

---
+++ screener.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  projectRepo: 'Esri/calcite-components',
+  storybookConfigDir: '.storybook',
+  storybookStaticDir: './__docs-temp__',
+  apiKey: '1817834e-8791-4606-a9f0-6c164f21b1ec',
+  resolution: '1024x768',
+  browsers: [
+    {
+      browserName: 'chrome'
+    },
+    {
+      browserName: 'firefox'
+    },
+    {
+      browserName: 'internet explorer',
+      version: '11'
+    }
+  ]
+};
