4a220d5950c1420a4830f013815e25995256d0f7
Erik Harper <eharperdicianno@esri.com>
2020-11-06 11:48:47 -0800

    storing screen api key in travis environment variable

--- screener.config.js
+++ screener.config.js
@@ -2,7 +2,7 @@ module.exports = {
   projectRepo: 'Esri/calcite-components',
   storybookConfigDir: '.storybook',
   storybookStaticDir: './__docs-temp__',
-  apiKey: '1817834e-8791-4606-a9f0-6c164f21b1ec',
+  apiKey: process.env.SCREENER_API_KEY,
   resolution: '1024x768',
   browsers: [
     {
