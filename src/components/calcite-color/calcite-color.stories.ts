79133b3a01ad7a9b05e0f3204d0393d9c465179b
JC Franco <jfranco@esri.com>
2020-12-18 10:46:08 -0800

    chore(storybook): migrate stories to CSF

--- src/components/calcite-color/calcite-color.stories.ts
+++ src/components/calcite-color/calcite-color.stories.ts
@@ -1,37 +1,41 @@
-import { storiesOf } from "@storybook/html";
 import { select } from "@storybook/addon-knobs";
 import { darkBackground } from "../../../.storybook/utils";
 import { boolean } from "../../../.storybook/helpers";
 import colorReadme from "./readme.md";
 import colorSwatchReadme from "../calcite-color-swatch/readme.md";
 import hexInputReadme from "../calcite-color-hex-input/readme.md";
+import { html } from "../../tests/utils";
 
-storiesOf("Components/Color", module)
-  .addParameters({ notes: [colorReadme, colorSwatchReadme, hexInputReadme] })
-  .add(
-    "Simple",
-    (): string => `
-    <calcite-color
-      ${boolean("hide hex", false)}
-      ${boolean("hide channels", false)}
-      ${boolean("hide saved", false)}
-      theme="light"
-      scale="${select("scale", ["s", "m", "l"], "m")}"
-      value="#beefee"
-    ></calcite-color>
-  `
-  )
-  .add(
-    "Dark Mode",
-    (): string => `
-      <calcite-color
-      hide-channels=${boolean("hide channels", false)}
-      hide-hex=${boolean("hide hex", false)}
-      hide-saved=${boolean("hide saved", false)}
-      theme="dark"
-      scale="${select("scale", ["s", "m", "l"], "m")}"
-      value="#beefee"
-    ></calcite-color>
-    `,
-    { backgrounds: darkBackground }
-  );
+export default {
+  title: "Components/Color",
+
+  parameters: {
+    notes: [colorReadme, colorSwatchReadme, hexInputReadme]
+  }
+};
+
+export const Simple = (): string => html`
+  <calcite-color
+    ${boolean("hide hex", false)}
+    ${boolean("hide channels", false)}
+    ${boolean("hide saved", false)}
+    theme="light"
+    scale="${select("scale", ["s", "m", "l"], "m")}"
+    value="#beefee"
+  ></calcite-color>
+`;
+
+export const DarkMode = (): string => html`
+  <calcite-color
+    hide-channels=${boolean("hide channels", false)}
+    hide-hex=${boolean("hide hex", false)}
+    hide-saved=${boolean("hide saved", false)}
+    theme="dark"
+    scale="${select("scale", ["s", "m", "l"], "m")}"
+    value="#beefee"
+  ></calcite-color>
+`;
+
+DarkMode.story = {
+  parameters: { backgrounds: darkBackground }
+};
--- src/components/calcite-notice/calcite-notice.stories.ts
+++ src/components/calcite-notice/calcite-notice.stories.ts
@@ -1,92 +1,97 @@
-import { storiesOf } from "@storybook/html";
 import { select } from "@storybook/addon-knobs";
 import { boolean, iconNames } from "../../../.storybook/helpers";
 import { darkBackground } from "../../../.storybook/utils";
 import readme from "./readme.md";
+import { html } from "../../tests/utils";
 
-storiesOf("Components/Notice", module)
-  .addParameters({ notes: readme })
-  .add(
-    "Simple",
-    (): string => `
-    <div style="width:600px;max-width:100%;text-align:center;">
+export default {
+  title: "Components/Notice",
+
+  parameters: {
+    notes: readme
+  }
+};
+
+export const Simple = (): string => html`
+  <div style="width:600px;max-width:100%;text-align:center;">
     <calcite-notice
-    theme="light"
-    ${boolean("icon", true)}
-    ${boolean("active", true)}
-    ${boolean("dismissible", true)}
-    scale="${select("scale", ["s", "m", "l"], "m")}"
-    width="${select("width", ["auto", "half", "full"], "auto")}"
-    color="${select("color", ["green", "red", "yellow", "blue"], "blue")}">
-    <div slot="notice-title">Your settings area has changed</div>
-    <div slot="notice-message">
-      Look around and let us know what you think
-    </div>
+      theme="light"
+      ${boolean("icon", true)}
+      ${boolean("active", true)}
+      ${boolean("dismissible", true)}
+      scale="${select("scale", ["s", "m", "l"], "m")}"
+      width="${select("width", ["auto", "half", "full"], "auto")}"
+      color="${select("color", ["green", "red", "yellow", "blue"], "blue")}"
+    >
+      <div slot="notice-title">Your settings area has changed</div>
+      <div slot="notice-message">Look around and let us know what you think</div>
       <calcite-link slot="notice-link" title="my action">Learn more</calcite-link>
     </calcite-notice>
-    </div>
-  `
-  )
-  .add(
-    "Custom icon",
-    (): string => `
-    <div style="width:600px;max-width:100%;text-align:center;">
+  </div>
+`;
+
+export const CustomIcon = (): string => html`
+  <div style="width:600px;max-width:100%;text-align:center;">
     <calcite-notice
-    theme="light"
-    icon="${select("icon", iconNames, iconNames[0])}"
-    ${boolean("active", true)}
-    ${boolean("dismissible", true)}
-    scale="${select("scale", ["s", "m", "l"], "m")}"
-    width="${select("width", ["auto", "half", "full"], "auto")}"
-    color="${select("color", ["green", "red", "yellow", "blue"], "blue")}">
-    <div slot="notice-title">Your settings area has changed</div>
-    <div slot="notice-message">
-      Look around and let us know what you think
-    </div>
+      theme="light"
+      icon="${select("icon", iconNames, iconNames[0])}"
+      ${boolean("active", true)}
+      ${boolean("dismissible", true)}
+      scale="${select("scale", ["s", "m", "l"], "m")}"
+      width="${select("width", ["auto", "half", "full"], "auto")}"
+      color="${select("color", ["green", "red", "yellow", "blue"], "blue")}"
+    >
+      <div slot="notice-title">Your settings area has changed</div>
+      <div slot="notice-message">Look around and let us know what you think</div>
       <calcite-link slot="notice-link" title="my action">Learn more</calcite-link>
     </calcite-notice>
-    </div>
-  `
-  )
-  .add(
-    "Dark Mode",
-    (): string => `
-    <div style="width:600px;max-width:100%;text-align:center;">
+  </div>
+`;
+
+CustomIcon.story = {
+  name: "Custom icon"
+};
+
+export const DarkMode = (): string => html`
+  <div style="width:600px;max-width:100%;text-align:center;">
     <calcite-notice
-    theme="dark"
-    ${boolean("icon", true)}
-    ${boolean("active", true)}
-    ${boolean("dismissible", false)}
-    scale="${select("scale", ["s", "m", "l"], "m")}"
-    width="${select("width", ["auto", "half", "full"], "auto")}"
-    color="${select("color", ["green", "red", "yellow", "blue"], "red")}">
-    <div slot="notice-title">This is a destructive action</div>
-    <div slot="notice-message">
-     Be sure you know what you are doin, folks.
-    </div>
+      theme="dark"
+      ${boolean("icon", true)}
+      ${boolean("active", true)}
+      ${boolean("dismissible", false)}
+      scale="${select("scale", ["s", "m", "l"], "m")}"
+      width="${select("width", ["auto", "half", "full"], "auto")}"
+      color="${select("color", ["green", "red", "yellow", "blue"], "red")}"
+    >
+      <div slot="notice-title">This is a destructive action</div>
+      <div slot="notice-message">Be sure you know what you are doin, folks.</div>
     </calcite-notice>
-    </div>
-    `,
-    { backgrounds: darkBackground }
-  )
-  .add(
-    "RTL",
-    (): string => `
-      <div dir="rtl" style="width:600px;max-width:100%;text-align:center;">
-      <calcite-notice
+  </div>
+`;
+
+DarkMode.story = {
+  parameters: { backgrounds: darkBackground }
+};
+
+export const Rtl = (): string => html`
+  <div dir="rtl" style="width:600px;max-width:100%;text-align:center;">
+    <calcite-notice
       theme="light"
       ${boolean("icon", true)}
       ${boolean("active", true)}
       ${boolean("dismissible", true)}
       width="${select("width", ["auto", "half", "full"], "auto")}"
       scale="${select("scale", ["s", "m", "l"], "m")}"
-      color="${select("color", ["green", "red", "yellow", "blue"], "blue")}" active>
+      color="${select("color", ["green", "red", "yellow", "blue"], "blue")}"
+      active
+    >
       <div slot="notice-title">Your settings area has changed</div>
-      <div slot="notice-message">
-        Look around and let us know what you think
-      </div>
-        <calcite-link slot="notice-link" title="my action">Learn more</calcite-link>
-      </calcite-notice>
-      </div>
-      `
-  );
+      <div slot="notice-message">Look around and let us know what you think</div>
+      <calcite-link slot="notice-link" title="my action">Learn more</calcite-link>
+    </calcite-notice>
+  </div>
+`;
+
+Rtl.story = {
+  name: "RTL"
+};
