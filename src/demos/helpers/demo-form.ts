ee39b6e90366c97cc8f58baad72f157fd40c1fd1
JC Franco <jfranco@esri.com>
2021-01-21 09:00:23 -0800

    chore: exclude unrelated component code from dist

--- src/demos/helpers/demo-form.ts
+++ src/demos/helpers/demo-form.ts
@@ -3,21 +3,24 @@ class DemoForm extends HTMLElement {
     this.addEventListener("submit", this.onFormSubmit);
     this.addEventListener("formdata", this.onFormData);
   }
+
   disconnectedCallback() {
     this.removeEventListener("submit", this.onFormSubmit);
     this.removeEventListener("formdata", this.onFormData);
   }
+
   onFormSubmit(event) {
     event.preventDefault();
     new FormData(event.target);
   }
+
   onFormData(event) {
-    let data = {};
-    for (var pair of event.formData.entries()) {
+    const data = {};
+    for (const pair of event.formData.entries()) {
       data[pair[0]] = pair[1];
     }
     console.log(data);
-    alert(JSON.stringify(data, null, 2))
+    alert(JSON.stringify(data, null, 2));
   }
 }
 customElements.define("demo-form", DemoForm);
