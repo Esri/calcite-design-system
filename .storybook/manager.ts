356252c9789e2c52b0abc4e431900c00f3c98619
JC Franco <jfranco@esri.com>
2021-02-03 19:23:41 -0800

    docs(storybook): prevent internal attributes from being rendered in HTML preview

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,5 +1,16 @@
 import { addons } from "@storybook/addons";
 import theme from "./theme";
+const cheerio = require("cheerio");
+
+// override
+addons.register("@whitespace/storybook-addon-html", (api) => {
+  // intercept HTML-preview event and remove internal-attrs
+  api.on("html/htmlReceived", (eventData) => {
+    const $ = cheerio.load(eventData.html, null, false);
+    $("[calcite-hydrated]", "");
+    eventData.html = $.html();
+  });
+});
 
 addons.setConfig({
   panelPosition: "right",
