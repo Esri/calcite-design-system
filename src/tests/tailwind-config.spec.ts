26eb5ec247fe49a01fec051e2c0bf60f50caa498
Cari Palazzolo <cpalazzolo@esri.com>
2021-02-24 12:38:21 -0800

    test(tailwind.config): add tailwind stub and unit test (#1537)

---
+++ src/tests/tailwind-config.spec.ts
@@ -0,0 +1,16 @@
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../tailwind.config";
+import themeConfig from "./tailwind-config.stub";
+
+const fullConfig = resolveConfig(tailwindConfig);
+
+describe("TailwindCSS config", () => {
+  it("should match the stubbed theme", () => {
+    expect(fullConfig.theme).toEqual(themeConfig);
+  });
+
+  // TODO: move to component-level test (confirm theme vars are overridable)
+  it("should include defined CSS variables", () => {
+    expect(themeConfig.colors.brand).toEqual("var(--calcite-ui-brand)");
+  });
+});
