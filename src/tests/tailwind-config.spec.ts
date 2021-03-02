44b25c1c57f11778e58f794744ce98196417958a
Cari Palazzolo <cpalazzolo@esri.com>
2021-03-01 19:41:30 -0800

    update stub to latest

--- src/tests/tailwind-config.spec.ts
+++ src/tests/tailwind-config.spec.ts
@@ -8,9 +8,4 @@ describe("TailwindCSS config", () => {
   it("should match the stubbed theme", () => {
     expect(fullConfig.theme).toEqual(themeConfig);
   });
-
-  // TODO: move to component-level test (confirm theme vars are overridable)
-  it("should include defined CSS variables", () => {
-    expect(themeConfig.colors.brand).toEqual("var(--calcite-ui-brand)");
-  });
 });
