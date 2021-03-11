20913b1af982ecb7222922b84a41d0294520deed
Erik Harper <eharperdicianno@esri.com>
2021-03-10 17:31:43 -0800

    reconciling unrelated diffs on master

--- src/components/calcite-combobox/interfaces.ts
+++ src/components/calcite-combobox/interfaces.ts
@@ -6,3 +6,4 @@ export interface listItem {
 export type ComboboxSelectionMode = "single" | "multi" | "ancestors";
 
 export type ComboboxChildElement = HTMLCalciteComboboxItemElement | HTMLCalciteComboboxItemGroupElement;
+
