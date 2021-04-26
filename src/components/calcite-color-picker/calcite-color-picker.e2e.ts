0c2c27d5915f6e9f51f9431214cb3fe86ead611e
JC Franco <jfranco@esri.com>
2021-04-26 10:55:31 -0700

    use page.waitForChanges on initially empty color picker

--- src/components/calcite-color-picker/calcite-color-picker.e2e.ts
+++ src/components/calcite-color-picker/calcite-color-picker.e2e.ts
@@ -731,6 +731,7 @@ describe("calcite-color-picker", () => {
           const page = await newE2EPage({
             html: "<calcite-color-picker allow-empty value=''></calcite-color-picker>"
           });
+          await page.waitForChanges();
 
           const hexInput = await page.find(`calcite-color-picker >>> calcite-color-picker-hex-input`);
 
