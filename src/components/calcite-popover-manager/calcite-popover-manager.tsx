5974e7359a4bb406bcc1a7a4337c235224faf6ba
Matt Driscoll <mdriscoll@esri.com>
2021-04-27 10:18:30 -0700

    docs(popover-manager, tooltip-manager): Document the default slot. (#2061)

--- src/components/calcite-popover-manager/calcite-popover-manager.tsx
+++ src/components/calcite-popover-manager/calcite-popover-manager.tsx
@@ -2,6 +2,9 @@ import { Component, Element, Host, h, Listen, Prop, VNode } from "@stencil/core"
 import { POPOVER_REFERENCE } from "../calcite-popover/resources";
 import { getElementById, getRootNode } from "../../utils/dom";
 
+/**
+ * @slot - A slot for adding elements that reference a 'calcite-popover' by the 'selector' property.
+ */
 @Component({
   tag: "calcite-popover-manager"
 })
@@ -37,7 +40,11 @@ export class CalcitePopoverManager {
   // --------------------------------------------------------------------------
 
   render(): VNode {
-    return <Host />;
+    return (
+      <Host>
+        <slot />
+      </Host>
+    );
   }
 
   //--------------------------------------------------------------------------
