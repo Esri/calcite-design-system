c5f3d6bfd3eae16d495cd83052047e4aea0a9db6
Matt Driscoll <mdriscoll@esri.com>
2021-05-03 09:07:09 -0700

    fix(tab-title): Remove setting 'dir' attribute in light DOM elements. #1831 (#2047)

--- src/components/calcite-tab-title/calcite-tab-title.tsx
+++ src/components/calcite-tab-title/calcite-tab-title.tsx
@@ -130,7 +130,6 @@ export class CalciteTabTitle {
       <Host
         aria-controls={this.controls}
         aria-expanded={this.active.toString()}
-        dir={dir}
         hasText={this.hasText}
         id={id}
         role="tab"
