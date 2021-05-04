85d6ac86d8cc8e48d111395ce833fcc8ce7ef5a6
Erik Harper <eharperdicianno@esri.com>
2021-05-04 15:57:19 -0700

    giving parseNumberString test vars more meaning

--- src/utils/number.spec.ts
+++ src/utils/number.spec.ts
@@ -20,25 +20,36 @@ describe("isValidNumber", () => {
 
 describe("parseNumberString", () => {
   it("returns null for string values that can't compute to a number", () => {
-    expect(parseNumberString("undefined")).toBe(null);
     expect(parseNumberString()).toBe(null);
     expect(parseNumberString("")).toBe(null);
     expect(parseNumberString(undefined)).toBe(null);
     expect(parseNumberString(null)).toBe(null);
-    expect(parseNumberString("null")).toBe(null);
-    expect(parseNumberString("not a number")).toBe(null);
-    expect(parseNumberString("kjas;lkjwo;aiej(*&,asd;flkj-")).toBe(null);
-    expect(parseNumberString("-ASDF(*^LKJihsdf*&^")).toBe(null);
+    expect(parseNumberString("text only")).toBe(null);
+
+    const lettersAndSymbols = "kjas;lkjwo;aiej(*&,asd;flkj-";
+    const lettersAndSymbolsWithLeadingNegativeSign = "-ASDF(*^LKJihsdf*&^";
+
+    expect(parseNumberString(lettersAndSymbols)).toBe(null);
+    expect(parseNumberString(lettersAndSymbolsWithLeadingNegativeSign)).toBe(null);
   });
 
   it("returns valid number string for string values that compute to a valid number", () => {
-    expect(parseNumberString(";lkj2323")).toBe("2323");
-    expect(parseNumberString("1 2345,67")).toBe("1234567");
-    expect(parseNumberString("123345345")).toBe("123345345");
-    expect(parseNumberString("123sdfa34345klndfsi8*&(^asdf5345")).toBe("1233434585345");
-    expect(parseNumberString("123123.234234")).toBe("123123.234234");
-    expect(parseNumberString("12asdfas$%@$3123.23asdf2a4234")).toBe("123123.2324234");
-    expect(parseNumberString("12a-sdfas$%@$3123.23asdf2a4234")).toBe("123123.2324234");
-    expect(parseNumberString("-12a-sdfas$%@$3123.23asdf2a4234")).toBe("-123123.2324234");
+    const stringWithLettersAndDigits = "lkj2323lkj";
+    const frenchNumber = "1 2345,67";
+    const positiveInteger = "123345345";
+    const stringWithLettersDigitsAndSymbols = "123sdfa34345klndfsi8*&(^asdf5345";
+    const decimal = "123123.234234";
+    const stringWithLettersAndDecimal = "12asdfas$%@$3123.23asdf2a4234";
+    const stringWithLettersDecimalAndNonLeadingNegativeSign = "12a-sdfas$%@$3123.23asdf2a4234";
+    const stringWithLettersDecimalAndLeadingNegativeSign = "-12a-sdfas$%@$3123.23asdf2a4234";
+
+    expect(parseNumberString(stringWithLettersAndDigits)).toBe("2323");
+    expect(parseNumberString(frenchNumber)).toBe("1234567");
+    expect(parseNumberString(positiveInteger)).toBe(positiveInteger);
+    expect(parseNumberString(stringWithLettersDigitsAndSymbols)).toBe("1233434585345");
+    expect(parseNumberString(decimal)).toBe(decimal);
+    expect(parseNumberString(stringWithLettersAndDecimal)).toBe("123123.2324234");
+    expect(parseNumberString(stringWithLettersDecimalAndNonLeadingNegativeSign)).toBe("123123.2324234");
+    expect(parseNumberString(stringWithLettersDecimalAndLeadingNegativeSign)).toBe("-123123.2324234");
   });
 });
