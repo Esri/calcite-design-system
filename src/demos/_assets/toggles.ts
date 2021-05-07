2ff41d9ddbb67d5ee30e9840cca24a91b77ae662
Matt Driscoll <mdriscoll@esri.com>
2021-05-07 09:12:01 -0700

    chore: Update demo toggles to use action-pad and add toggling of document dir and body theme. (#2111)
    * chore: Update demo toggles to use action-pad and add toggling of document dir and body theme.
    
    Removes other body theme file and removes IE11 check.
    
    * cleanup demos

--- src/demos/_assets/toggles.ts
+++ src/demos/_assets/toggles.ts
@@ -1,50 +1,53 @@
 ((): void => {
-  const IS_IE11 = /Trident.*rv[ :]*11\./.test(navigator.userAgent);
-  if (!IS_IE11) {
-    const DEMO_ROOT = "demos";
-    const ASSETS_PATH = "demos/_assets";
+  const DEMO_ROOT = "demos";
+  const ASSETS_PATH = "demos/_assets";
 
-    const parseTemplate = (text: string): HTMLTemplateElement => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(text, "text/html");
-      return doc.head.querySelector("template");
-    };
+  const parseTemplate = (text: string): HTMLTemplateElement => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/html");
+    return doc.head.querySelector("template");
+  };
 
-    let components: HTMLElement[] = null;
-    const excludedComponents = ["calcite-button"];
-    const toggleProperty = (property: string): void => {
-      components = components || Array.from(document.body.querySelectorAll("[calcite-hydrated]"));
+  let components: HTMLElement[] = null;
+  const excludedComponents = ["calcite-button"];
+  const toggleAction = (action: HTMLElement): void => {
+    const property = action.dataset.jsId;
+    (action as any).active = !(action as any).active;
+    if (property === "loading" || property === "disabled") {
+      components = components || Array.from(document.body.querySelectorAll("[calcite-hydrated]:not([data-excluded])"));
 
       components.forEach((component) => {
         if (!excludedComponents.includes(component.tagName.toLowerCase())) {
           component.toggleAttribute(property);
         }
       });
-    };
-
-    const attachHandlers = (): void => {
-      const buttons = document.querySelectorAll<HTMLButtonElement>(".toggles calcite-button");
-      buttons.forEach((button) =>
-        button.addEventListener("click", (event) => toggleProperty((event.target as HTMLElement).dataset.jsId))
-      );
-    };
-
-    const loadToggles = async (): Promise<void> => {
-      const root = window.location.pathname.split(DEMO_ROOT).shift();
-      const response = await window.fetch(`${root}${ASSETS_PATH}/toggles.template`);
-      const text = await response.text();
-      const template = parseTemplate(text);
-      const firstChild = document.body.firstChild;
-      firstChild && document.body.insertBefore(template.content, firstChild);
-      attachHandlers();
-    };
-
-    if (document.readyState === "loading") {
-      // Loading hasn't finished yet
-      document.addEventListener("DOMContentLoaded", loadToggles);
-    } else {
-      // `DOMContentLoaded` has already fired
-      loadToggles();
+    } else if (property === "dir") {
+      document.dir = document.dir === "rtl" ? "ltr" : "rtl";
+    } else if (property === "theme") {
+      document.body.setAttribute("theme", document.body.getAttribute("theme") === "dark" ? "light" : "dark");
     }
+  };
+
+  const attachHandlers = (): void => {
+    const actions = document.querySelectorAll<HTMLElement>(".toggles calcite-action");
+    actions.forEach((action) => action.addEventListener("click", (event) => toggleAction(event.target as HTMLElement)));
+  };
+
+  const loadToggles = async (): Promise<void> => {
+    const root = window.location.pathname.split(DEMO_ROOT).shift();
+    const response = await window.fetch(`${root}${ASSETS_PATH}/toggles.template`);
+    const text = await response.text();
+    const template = parseTemplate(text);
+    const firstChild = document.body.firstChild;
+    firstChild && document.body.insertBefore(template.content, firstChild);
+    attachHandlers();
+  };
+
+  if (document.readyState === "loading") {
+    // Loading hasn't finished yet
+    document.addEventListener("DOMContentLoaded", loadToggles);
+  } else {
+    // `DOMContentLoaded` has already fired
+    loadToggles();
   }
 })();
