a8fe10e3eb1b5b82938d2e2348a17793aeaf924d
Matt Driscoll <MDriscoll@esri.com>
2021-05-14 14:15:31 -0700

    WIP

--- src/utils/dom.e2e.ts
+++ src/utils/dom.e2e.ts
@@ -1,30 +1,39 @@
-import { newE2EPage } from "@stencil/core/testing";
+import { E2EPage, newE2EPage } from "@stencil/core/testing";
 import { queryElementRelativeTo, getRootNode, getHost } from "./dom";
 
 describe("utils/dom", () => {
-  it("queryElementRelativeTo: shadow dom element", async () => {
-    const page = await newE2EPage({
-      html: "<button>Light</button>"
+  let page: E2EPage;
+
+  beforeEach(async () => {
+    page = await newE2EPage({
+      html: "<span>Test</span><button>Outside Host</button>"
     });
 
-    await page.addScriptTag({
-      content: `${getRootNode} ${getHost} ${queryElementRelativeTo}`
-    });
-    await page.waitForFunction(() => (window as any).queryElementRelativeTo);
-
-    const text = await page.evaluate((): string => {
-      console.log(document.head.innerHTML);
+    function setupTestComponent() {
       class TestComponent extends HTMLElement {
         constructor() {
           super();
           const shadow = this.attachShadow({ mode: "open" });
-          shadow.innerHTML = "<div><button>Shadow</button></div>";
+          shadow.innerHTML = "<div><button>Not queryable</button></div>";
         }
       }
 
       customElements.define("test-component", TestComponent);
+    }
+
+    await page.addScriptTag({
+      content: `${getRootNode} ${getHost} ${queryElementRelativeTo} ${setupTestComponent}`
+    });
+
+    await page.waitForFunction(() => (window as any).queryElementRelativeTo);
+  });
+
+  it("should query from inside host element", async () => {
+    const text = await page.evaluate((): string => {
+      (window as any).setupTestComponent();
 
       const testComponent = document.createElement("test-component");
+      testComponent.innerHTML = "<button>Inside Host</button>";
       document.body.appendChild(testComponent);
 
       const queryEl = testComponent.shadowRoot.querySelector("div");
@@ -33,6 +42,23 @@ describe("utils/dom", () => {
       return resultEl?.textContent;
     });
 
-    expect(text).toBe("Shadow");
+    expect(text).toBe("Inside Host");
+  });
+
+  it("should query from outside host element", async () => {
+    const text = await page.evaluate((): string => {
+      (window as any).setupTestComponent();
+
+      const testComponent = document.createElement("test-component");
+      testComponent.innerHTML = "<button>Inside Host</button>";
+      document.body.appendChild(testComponent);
+
+      const queryEl = document.body.querySelector("span");
+      const resultEl = (window as any).queryElementRelativeTo("button", queryEl);
+
+      return resultEl?.textContent;
+    });
+
+    expect(text).toBe("Outside Host");
   });
 });
