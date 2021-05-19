65b45c1dc766235d24a339f5344ecb958548a988
Erik Harper <eharperdicianno@esri.com>
2021-05-19 16:56:21 -0700

    adding track range dragging tests

--- src/components/calcite-slider/calcite-slider.e2e.ts
+++ src/components/calcite-slider/calcite-slider.e2e.ts
@@ -186,7 +186,7 @@ describe("calcite-slider", () => {
       expect(changeEvent).toHaveReceivedEventTimes(1);
     });
 
-    it("single handle: clicking the track and dragging the mouse changes and emits the value", async () => {
+    it("single handle: clicking and dragging the track changes and emits the value", async () => {
       const page = await newE2EPage({
         html: `
           <style>body { margin: 0; }</style>
@@ -242,6 +242,35 @@ describe("calcite-slider", () => {
       expect(changeEvent).toHaveReceivedEventTimes(1);
     });
 
+    it("range: clicking and dragging the track to the left of the min handle changes minValue and emits", async () => {
+      const page = await newE2EPage({
+        html: `
+          <style>body { margin: 0; }</style>
+          <div style="width:100px">
+            <calcite-slider min-value="50" max-value="75" snap></calcite-slider>
+          </div>
+        `
+      });
+      const slider = await page.find("calcite-slider");
+      const changeEvent = await slider.spyOnEvent("calciteSliderChange");
+
+      expect(await slider.getProperty("minValue")).toBe(50);
+      expect(await slider.getProperty("maxValue")).toBe(75);
+
+      await page.mouse.move(20, 8);
+      await page.mouse.down();
+      await page.mouse.move(21, 8);
+      await page.mouse.move(22, 8);
+      await page.mouse.move(23, 8);
+      await page.mouse.move(24, 8);
+      await page.mouse.move(25, 8);
+      await page.waitForChanges();
+
+      expect(await slider.getProperty("minValue")).toBe(25);
+      expect(await slider.getProperty("maxValue")).toBe(75);
+      expect(changeEvent).toHaveReceivedEventTimes(5);
+    });
+
     it("range: clicking the track to the right of the max handle changes maxValue on mousedown, emits on mouseup", async () => {
       const page = await newE2EPage({
         html: `
@@ -269,6 +298,35 @@ describe("calcite-slider", () => {
       await page.waitForChanges();
 
       expect(changeEvent).toHaveReceivedEventTimes(1);
+    });
+
+    it("range: clicking and dragging the track to the right of the max handle changes maxValue on mousedown, emits on mouseup", async () => {
+      const page = await newE2EPage({
+        html: `
+          <style>body { margin: 0; }</style>
+          <div style="width:100px">
+            <calcite-slider min-value="25" max-value="50" snap></calcite-slider>
+          </div>
+        `
+      });
+      const slider = await page.find("calcite-slider");
+      const changeEvent = await slider.spyOnEvent("calciteSliderChange");
+
+      expect(await slider.getProperty("minValue")).toBe(25);
+      expect(await slider.getProperty("maxValue")).toBe(50);
+
+      await page.mouse.move(70, 8);
+      await page.mouse.down();
+      await page.mouse.move(71, 8);
+      await page.mouse.move(72, 8);
+      await page.mouse.move(73, 8);
+      await page.mouse.move(74, 8);
+      await page.mouse.move(75, 8);
+      await page.waitForChanges();
+
+      expect(await slider.getProperty("minValue")).toBe(25);
+      expect(await slider.getProperty("maxValue")).toBe(75);
+      expect(changeEvent).toHaveReceivedEventTimes(5);
     });
   });
 });
