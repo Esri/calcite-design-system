89d00173b7219df3ee9918c59a306da719e8f42c
Matt Driscoll <mdriscoll@esri.com>
2021-05-21 10:59:38 -0700

    test: Fix radio-button test. (#2192)
    * tests: Fix radio-button test.
    
    * test: fix radio button test.

--- src/components/calcite-radio-button/calcite-radio-button.e2e.ts
+++ src/components/calcite-radio-button/calcite-radio-button.e2e.ts
@@ -343,6 +343,7 @@ describe("calcite-radio-button", () => {
       </script>
     `);
 
+    await page.waitForChanges();
     const radios = await page.findAll("div >>> calcite-radio-button");
     const inputs = await page.findAll("div >>> calcite-radio-button >>> input");
 
