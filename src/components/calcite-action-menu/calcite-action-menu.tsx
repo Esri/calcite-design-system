95f13d855c3d577241e58936e28baf6553c6e83a
Alan Sangma <asangma@esri.com>
2021-06-07 17:17:32 -0700

    feat(action-menu): update styling and keyboard behavior
    * refactor action-menu with tailwind.
    
    * disable pointer (#1906)
    
    * initial focus fix
    
    * removes focus ring from menu. adds space for Action focus ring to show (#1906)

--- src/components/calcite-action-menu/calcite-action-menu.tsx
+++ src/components/calcite-action-menu/calcite-action-menu.tsx
@@ -217,6 +217,7 @@ export class CalciteActionMenu {
 
     return (
       <calcite-popover
+        disablePointer
         label={label}
         open={open}
         overlayPositioning={overlayPositioning}
