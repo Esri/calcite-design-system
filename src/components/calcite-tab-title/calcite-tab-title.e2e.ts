d5e62800086a3ee4c73a934648644155b2a56ed5
Cari Palazzolo <cpalazzolo@esri.com>
2021-06-11 15:53:34 -0700

    fix brittle tests

--- src/components/calcite-tab-title/calcite-tab-title.e2e.ts
+++ src/components/calcite-tab-title/calcite-tab-title.e2e.ts
@@ -41,7 +41,7 @@ describe("calcite-tab-title", () => {
 
   describe("when parent element is tab-nav", () => {
     describe("when position is above, default", () => {
-      it("should render with top border on hover", async () => {
+      it("should render with bottom border on hover", async () => {
         const page = await newE2EPage({
           html: `
           <calcite-tab-nav>
@@ -55,12 +55,12 @@ describe("calcite-tab-title", () => {
         const linkTag = await page.find("#for-hover >>> a");
         const linkStyles = await linkTag.getComputedStyle();
         expect(linkStyles["border-top-width"]).toEqual("0px");
-        expect(linkStyles["border-bottom-width"]).toEqual("2px");
+        expect(linkStyles["border-bottom-width"]).not.toEqual("0px");
       });
     });
 
     describe("when position is below", () => {
-      it("should render with bottom border on hover", async () => {
+      it("should render with top border on hover", async () => {
         const page = await newE2EPage({
           html: `
           <calcite-tab-nav position="below">
@@ -74,7 +74,7 @@ describe("calcite-tab-title", () => {
 
         const linkTag = await page.find("#for-hover >>> a");
         const linkStyles = await linkTag.getComputedStyle();
-        expect(linkStyles["border-top-width"]).toEqual("2px");
+        expect(linkStyles["border-top-width"]).not.toEqual("0px");
         expect(linkStyles["border-bottom-width"]).toEqual("0px");
       });
     });
@@ -89,9 +89,10 @@ describe("calcite-tab-title", () => {
         });
         const element = await page.find("calcite-tab-title");
         const linkTag = await page.find("calcite-tab-title >>> a");
+        const linkStyles = await linkTag.getComputedStyle();
         expect(element).toEqualAttribute("scale", "s");
-        expect(await (await linkTag.getComputedStyle())["font-size"]).toEqual("12px");
-        expect(await (await linkTag.getComputedStyle())["line-height"]).toEqual("16px"); // 1rem
+        expect(linkStyles.fontSize).toEqual("12px");
+        expect(linkStyles.lineHeight).toEqual("16px"); // 1rem
       });
 
       it("should inherit medium scale from tab-nav", async () => {
@@ -103,9 +104,10 @@ describe("calcite-tab-title", () => {
         });
         const element = await page.find("calcite-tab-title");
         const linkTag = await page.find("calcite-tab-title >>> a");
+        const linkStyles = await linkTag.getComputedStyle();
         expect(element).toEqualAttribute("scale", "m");
-        expect(await (await linkTag.getComputedStyle())["font-size"]).toEqual("14px");
-        expect(await (await linkTag.getComputedStyle())["line-height"]).toEqual("16px"); // 1rem
+        expect(linkStyles.fontSize).toEqual("14px");
+        expect(linkStyles.lineHeight).toEqual("16px"); // 1rem
       });
 
       it("should inherit large scale from tab-nav", async () => {
@@ -117,9 +119,10 @@ describe("calcite-tab-title", () => {
         });
         const element = await page.find("calcite-tab-title");
         const linkTag = await page.find("calcite-tab-title >>> a");
+        const linkStyles = await linkTag.getComputedStyle();
         expect(element).toEqualAttribute("scale", "l");
-        expect(await (await linkTag.getComputedStyle())["font-size"]).toEqual("16px");
-        expect(await (await linkTag.getComputedStyle())["line-height"]).toEqual("20px"); // 1.25rem
+        expect(linkStyles.fontSize).toEqual("16px");
+        expect(linkStyles.lineHeight).toEqual("20px"); // 1.25rem
       });
     });
   });
@@ -191,7 +194,7 @@ describe("calcite-tab-title", () => {
               .shadowRoot.querySelector(".tab-nav-active-indicator") as HTMLDivElement
           ).style.left;
         })
-      ).toEqual("85px");
+      ).not.toEqual("0px");
 
       // toggle new active tab-title
       await tabTitle2.removeAttribute("active");
