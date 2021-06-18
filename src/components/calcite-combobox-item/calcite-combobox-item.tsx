c4b76b999f4eed70634c994dbe3c08ffdb9ee470
Matt Driscoll <mdriscoll@esri.com>
2021-06-18 15:21:49 -0700

    feat(combobox); Allow for greater nesting of calcite-combobox-items within calcite-combobox #2337

--- src/components/calcite-combobox-item/calcite-combobox-item.tsx
+++ src/components/calcite-combobox-item/calcite-combobox-item.tsx
@@ -141,11 +141,12 @@ export class CalciteComboboxItem {
   // --------------------------------------------------------------------------
 
   renderIcon(isSingle: boolean): VNode {
-    const { icon, el, disabled, selected } = this;
-    const level = `${CSS.icon}--indent-${getDepth(el)}`;
+    const { icon, disabled, selected } = this;
+    const level = `${CSS.icon}--indent`;
     const defaultIcon = isSingle ? "dot" : "check";
     const iconPath = disabled ? "circle-disallowed" : defaultIcon;
     const showDot = isSingle && !icon && !disabled;
+
     return showDot ? (
       <span
         class={{
@@ -189,10 +190,14 @@ export class CalciteComboboxItem {
       [CSS.active]: this.active,
       [CSS.single]: isSingleSelect
     };
+    const depth = getDepth(this.el);
 
     return (
       <Host aria-hidden="true">
-        <div class={`scale--${this.scale}`}>
+        <div
+          class={`scale--${this.scale}`}
+          style={{ "--calcite-combobox-item-spacing-indent-multiplier": `${depth}` }}
+        >
           <li class={classes} id={this.guid} onClick={this.itemClickHandler}>
             {this.renderIcon(isSingleSelect)}
             <span class={CSS.title}>{this.textLabel}</span>
