e7a3217a29a226fd039e3d9ceb5f84c3e68a896d
JC Franco <jfranco@esri.com>
2021-06-21 17:15:34 -0700

    fix(dropdown): fix initial processing of groups and items (#2308)
     #2268 #2026

--- src/components/calcite-dropdown/interfaces.ts
+++ src/components/calcite-dropdown/interfaces.ts
@@ -1,24 +1,7 @@
 import { PopperPlacement } from "../../utils/popper";
 
-export interface ItemRegistration {
-  position: number;
-}
-
 export interface ItemKeyboardEvent {
   keyboardEvent: KeyboardEvent;
-}
-
-export interface GroupRegistration {
-  items: HTMLCalciteDropdownItemElement[];
-  position: number;
-  group: HTMLCalciteDropdownGroupElement;
-  titleEl: HTMLSpanElement;
-  separatorEl: HTMLDivElement;
-}
-
-export interface RegisteredItem {
-  item: HTMLCalciteDropdownItemElement;
-  position: number;
 }
 
 export type DropdownPlacement = Extract<
