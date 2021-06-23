a48aff2a461fa5270bee459117e85c4c8c06a8f7
Matt Driscoll <mdriscoll@esri.com>
2021-06-23 16:01:57 -0700

    chore: Change screener config to TS file

---
+++ screener.config.ts
@@ -0,0 +1,14 @@
+export default {
+  projectRepo: "Esri/calcite-components",
+  storybookConfigDir: ".storybook",
+  storybookStaticDir: "./__docs-temp__",
+  apiKey: process.env.SCREENER_API_KEY,
+  resolution: "1024x768",
+  baseBranch: "master",
+  browsers: [
+    {
+      browserName: "chrome"
+    }
+  ],
+  failureExitCode: 0
+};
