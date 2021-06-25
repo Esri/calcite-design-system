7572fc452b93d085ac86ee06250d74cb01414267
Alan Sangma <asangma@esri.com>
2021-06-25 14:35:53 -0700

    add test for content node. fix render logic for content node.

--- src/components/calcite-list-item/calcite-list-item.e2e.ts
+++ src/components/calcite-list-item/calcite-list-item.e2e.ts
@@ -1,5 +1,7 @@
+import { newE2EPage } from "@stencil/core/testing";
 import { hidden, renders } from "../../tests/commonTests";
 import { defaults } from "../../tests/commonTests";
+import { CSS } from "./resources";
 
 describe("calcite-list-item", () => {
   it("renders", async () => renders("calcite-list-item"));
@@ -25,4 +27,28 @@ describe("calcite-list-item", () => {
         defaultValue: undefined
       }
     ]));
+
+  it("renders content node when label is provided", async () => {
+    const page = await newE2EPage({ html: `<calcite-list-item label="test"></calcite-list-item>` });
+
+    const contentNode = await page.find(`calcite-list-item >>> .${CSS.content}`);
+
+    expect(contentNode).not.toBeNull();
+  });
+
+  it("renders content node when description is provided", async () => {
+    const page = await newE2EPage({ html: `<calcite-list-item description="test"></calcite-list-item>` });
+
+    const contentNode = await page.find(`calcite-list-item >>> .${CSS.content}`);
+
+    expect(contentNode).not.toBeNull();
+  });
+
+  it("does not render content node when description and label is not provided", async () => {
+    const page = await newE2EPage({ html: `<calcite-list-item></calcite-list-item>` });
+
+    const contentNode = await page.find(`calcite-list-item >>> .${CSS.content}`);
+
+    expect(contentNode).toBeNull();
+  });
 });
--- src/components/calcite-list-item/calcite-list-item.tsx
+++ src/components/calcite-list-item/calcite-list-item.tsx
@@ -113,7 +113,7 @@ export class CalciteListItem {
   renderContent(): VNode {
     const { label, description } = this;
 
-    return !!label && !!description ? (
+    return !!label || !!description ? (
       <div class={CSS.content}>
         {label ? <div class={CSS.label}>{label}</div> : null}
         {description ? <div class={CSS.description}>{description}</div> : null}
