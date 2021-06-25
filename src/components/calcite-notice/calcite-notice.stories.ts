1e8bdb773278ba11cefbdac1c2b5b678920506da
JC Franco <jfranco@esri.com>
2021-06-25 16:20:08 -0700

    update demo & story to sync scales with slotted actions

--- src/components/calcite-notice/calcite-notice.stories.ts
+++ src/components/calcite-notice/calcite-notice.stories.ts
@@ -64,7 +64,12 @@ export const WithAction = (): string => html`
     >
       <div slot="title">Notice with action</div>
       <div slot="message">This shows a notice with a custom action</div>
-      <calcite-action label="Retry" icon="reset" slot="actions-end"></calcite-action>
+      <calcite-action
+        label="Retry"
+        icon="reset"
+        scale="${select("scale", ["s", "m", "l"], "m")}"
+        slot="actions-end"
+      ></calcite-action>
     </calcite-notice>
   </div>
 `;
