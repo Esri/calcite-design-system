5335bb05b7617b7d0a0636ff4c14a79d7e455438
Matt Driscoll <mdriscoll@esri.com>
2021-07-07 17:48:53 -0700

    Revert "fix selection mode."
    This reverts commit b6579f2b1963be4eafe13c6db6eef535bfd1a356.

--- src/components/calcite-dropdown-item/calcite-dropdown-item.tsx
+++ src/components/calcite-dropdown-item/calcite-dropdown-item.tsx
@@ -180,12 +180,8 @@ export class CalciteDropdownItem {
             [CSS.containerNone]: this.selectionMode === "none"
           }}
         >
-          {this.selectionMode !== "none" ? (
-            <calcite-icon
-              class="dropdown-item-icon"
-              icon={this.selectionMode === "multi" ? "check" : "circle"}
-              scale="s"
-            />
+          {this.selectionMode === "multi" ? (
+            <calcite-icon class="dropdown-item-check-icon" icon="check" scale="s" />
           ) : null}
           {contentEl}
         </div>
