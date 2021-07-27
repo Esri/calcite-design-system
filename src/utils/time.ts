17fb49bc0703d5ba37f59e19e18daeb01cd1976d
Erik Harper <eharperdicianno@esri.com>
2021-07-26 17:17:19 -0700

    first commit: breaking out time format logic into a new isValidTime util and setting up demo page to show all locales

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -16,6 +16,31 @@ export type TimeFocusId = "hour" | MinuteOrSecond | "meridiem";
 
 export const maxTenthForMinuteAndSecond = 5;
 
+function createLocaleDateTimeFormatter(locale: string): Intl.DateTimeFormat {
+  return new Intl.DateTimeFormat(locale);
+}
+
+export function formatTimePart(number: number): string {
+  const numberAsString = number.toString();
+  return number >= 0 && number <= 9 ? numberAsString.padStart(2, "0") : numberAsString;
+}
+
+export function formatTimeString(value: string): string {
+  if (!isValidTime(value)) {
+    return null;
+  }
+  const splitValue = value.split(":");
+  const [hour, minute, second] = splitValue;
+  const hourAsNumber = parseInt(hour);
+  const minuteAsNumber = parseInt(minute);
+  let newValue = `${formatTimePart(hourAsNumber)}:${formatTimePart(minuteAsNumber)}`;
+  if (second) {
+    const secondAsNumber = parseInt(second);
+    newValue = `${newValue}:${formatTimePart(secondAsNumber)}`;
+  }
+  return newValue;
+}
+
 export function getMeridiem(hour: string): Meridiem {
   if (!isValidNumber(hour)) {
     return null;
@@ -35,22 +60,12 @@ export function getMeridiemHour(hour: string): string {
   return hourAsNumber > 12 ? formatTimePart(hourAsNumber - 12) : hour;
 }
 
-export function parseTimeString(value: string): Time {
-  const timeString = formatTimeString(value);
-  const [hour, minute, second] = timeString ? timeString.split(":") : [null, null, null];
-  return {
-    hour,
-    minute,
-    second: second || (hour && minute ? "00" : null)
-  };
-}
-
-export function formatTimeString(value: string): string {
-  if (!value || value.endsWith(":") || value.startsWith(":")) {
-    return null;
+export function isValidTime(value: string): boolean {
+  if (!value || value.startsWith(":") || value.endsWith(":")) {
+    return false;
   }
   const splitValue = value.split(":");
-  if (splitValue.length > 1) {
+  if (splitValue.length > 1 && splitValue.length < 4) {
     const [hour, minute, second] = splitValue;
     const hourAsNumber = parseInt(splitValue[0]);
     const minuteAsNumber = parseInt(splitValue[1]);
@@ -59,17 +74,28 @@ export function formatTimeString(value: string): string {
     const minuteValid = isValidNumber(minute) && minuteAsNumber >= 0 && minuteAsNumber < 60;
     const secondValid = isValidNumber(second) && secondAsNumber >= 0 && secondAsNumber < 60;
     if ((hourValid && minuteValid && !second) || (hourValid && minuteValid && secondValid)) {
-      let newValue = `${formatTimePart(hourAsNumber)}:${formatTimePart(minuteAsNumber)}`;
-      if (secondValid) {
-        newValue = `${newValue}:${formatTimePart(secondAsNumber)}`;
-      }
-      return newValue;
+      return true;
     }
   }
-  return null;
+  return false;
 }
 
-export function formatTimePart(number: number): string {
-  const numberAsString = number.toString();
-  return number >= 0 && number <= 9 ? numberAsString.padStart(2, "0") : numberAsString;
+export function localizeTimeString(timeString: string, locale: string): string {
+  const dateFromTimeString = new Date(timeString);
+  if (dateFromTimeString) {
+    const formatter = createLocaleDateTimeFormatter(locale);
+    const parts = formatter.formatToParts(dateFromTimeString);
+    console.log(parts);
+  }
+  return timeString;
+}
+
+export function parseTimeString(value: string): Time {
+  const timeString = formatTimeString(value);
+  const [hour, minute, second] = timeString ? timeString.split(":") : [null, null, null];
+  return {
+    hour,
+    minute,
+    second: second || (hour && minute ? "00" : null)
+  };
 }
