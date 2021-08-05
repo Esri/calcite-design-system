121452a75f9543672be3593fb0ac2f17b2621df8
Matt Driscoll <mdriscoll@esri.com>
2021-08-05 09:58:24 -0700

    chore: run prettier

--- src/components/calcite-pick-list-item/calcite-pick-list-item.tsx
+++ src/components/calcite-pick-list-item/calcite-pick-list-item.tsx
@@ -49,7 +49,7 @@ export class CalcitePickListItem {
    * When false, the item cannot be deselected by user interaction.
    */
   @Prop() disableDeselect = false;
-  
+
   /**
    * @internal When true, the item cannot be selected by user interaction.
    */
@@ -209,7 +209,7 @@ export class CalcitePickListItem {
   pickListKeyDownHandler = (event: KeyboardEvent): void => {
     if (event.key === " ") {
       event.preventDefault();
-      if (this.disableDeselect && this.selected || this.nonInteractive) {
+      if ((this.disableDeselect && this.selected) || this.nonInteractive) {
         return;
       }
       this.selected = !this.selected;
