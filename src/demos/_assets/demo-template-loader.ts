370b91cbe6a6bcc61462de596b800873259ba65b
Matt Driscoll <mdriscoll@esri.com>
2021-08-11 16:48:59 -0700

    for @alan0002

--- src/demos/_assets/demo-template-loader.ts
+++ src/demos/_assets/demo-template-loader.ts
@@ -1,6 +1,7 @@
 ((): void => {
   const DEMO_ROOT = "demos";
   const ASSETS_PATH = "demos/_assets";
+  const DISABLE_HEADER_URL_PARAM = "disable-header";
 
   const parseTemplate = (text: string): HTMLTemplateElement => {
     const parser = new DOMParser();
@@ -8,7 +9,13 @@
     return doc.head.querySelector("template");
   };
 
-  const loadToggles = async (): Promise<void> => {
+  const loadHeader = async (): Promise<void> => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has(DISABLE_HEADER_URL_PARAM)) {
+      return;
+    }
+
     const root = window.location.pathname.split(DEMO_ROOT).shift();
     const response = await window.fetch(`${root}${ASSETS_PATH}/demo-template.html`);
     const text = await response.text();
@@ -19,9 +26,9 @@
 
   if (document.readyState === "loading") {
     // Loading hasn't finished yet
-    document.addEventListener("DOMContentLoaded", loadToggles);
+    document.addEventListener("DOMContentLoaded", loadHeader);
   } else {
     // `DOMContentLoaded` has already fired
-    loadToggles();
+    loadHeader();
   }
 })();
