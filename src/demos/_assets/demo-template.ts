e8b71d50fca3a4a5f827f5523b84fe104f564e65
Matt Driscoll <mdriscoll@esri.com>
2021-08-12 09:34:49 -0700

    cleanup demo code

--- src/demos/_assets/demo-template.ts
+++ src/demos/_assets/demo-template.ts
@@ -6,8 +6,10 @@ const toggleTheme = (): void => {
   document.body.classList.toggle("calcite-theme-dark");
 };
 
-window.onload = () => {
+const loadDemoToggles = () => {
   document.getElementById("demo-heading").textContent = document.title;
   document.getElementById("toggle-dir").addEventListener("calciteSwitchChange", toggleDir);
   document.getElementById("toggle-theme").addEventListener("calciteSwitchChange", toggleTheme);
 };
+
+document.readyState === "loading" ? document.addEventListener("DOMContentLoaded", loadDemoToggles) : loadDemoToggles();
