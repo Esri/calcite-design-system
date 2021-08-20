7cb687113fc6684cd1e87efe2fc8a6b90eaa5b38
Matt Driscoll <mdriscoll@esri.com>
2021-08-20 11:13:08 -0700

    disable reactDoggen
    https://github.com/styleguidist/react-docgen-typescript/issues/356#issuecomment-850400428

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,5 +6,8 @@ module.exports = {
     "@storybook/addon-a11y",
     "@whitespace/storybook-addon-html"
   ],
-  stories: ["../src/**/*.stories.@(mdx|ts)"]
+  stories: ["../src/**/*.stories.@(mdx|ts)"],
+  typescript: {
+    reactDocgen: false
+  }
 };
