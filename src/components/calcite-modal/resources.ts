ebebb3f706e4ef2c21f0cb1fbab64d323c115bac
Ben Elan <belan@esri.com>
2021-08-26 10:07:42 -0700

    refactor(modal): use resources file

---
+++ src/components/calcite-modal/resources.ts
@@ -0,0 +1,27 @@
+export const CSS = {
+  title: "title",
+  header: "header",
+  footer: "footer",
+  scrim: "scrim",
+  back: "back",
+  close: "close",
+  secondary: "secondary",
+  primary: "primary",
+  overflowHidden: "overflow-hidden"
+};
+
+export const ICONS = {
+  close: "x"
+};
+
+export const SLOTS = {
+  content: "content",
+  header: "header",
+  back: "back",
+  secondary: "secondary",
+  primary: "primary"
+};
+
+export const TEXT = {
+  close: "Close"
+};
