1957d222407cd050506598d0fc325cdbd1018fe3
Erik Harper <eharperdicianno@esri.com>
2021-09-27 12:11:09 -0700

    refactoring action story to use stepStory while retaining original setup and adding in missing props to the action storybook

--- src/components/calcite-action/calcite-action.stories.ts
+++ src/components/calcite-action/calcite-action.stories.ts
@@ -1,7 +1,14 @@
-import { boolean as storybookBoolean, select, text } from "@storybook/addon-knobs";
-import { html } from "../../tests/utils";
-import { boolean, createSteps, stepStory, setTheme, setKnobs } from "../../../.storybook/helpers";
+import { boolean, select, text } from "@storybook/addon-knobs";
+import {
+  Attribute,
+  filterComponentAttributes,
+  Attributes,
+  createComponentHTML as create,
+  themesDarkDefault
+} from "../../../.storybook/utils";
 import readme from "./readme.md";
+import { html } from "../../tests/utils";
+import { createSteps, stepStory, setTheme, setKnobs } from "../../../.storybook/helpers";
 import { ATTRIBUTES } from "../../../.storybook/resources";
 
 export default {
@@ -11,24 +18,114 @@ export default {
   }
 };
 
-const { appearance, scale } = ATTRIBUTES;
+const createAttributes: (options?: { exceptions: string[] }) => Attributes = ({ exceptions } = { exceptions: [] }) => {
+  const { alignment, appearance, scale } = ATTRIBUTES;
+
+  return filterComponentAttributes(
+    [
+      {
+        name: "active",
+        commit(): Attribute {
+          this.value = boolean("active", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "alignment",
+        commit(): Attribute {
+          this.value = select("alignment", alignment.values, alignment.defaultValue);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "appearance",
+        commit(): Attribute {
+          this.value = select("appearance", appearance.values, appearance.defaultValue);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "compact",
+        commit(): Attribute {
+          this.value = boolean("compact", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "disabled",
+        commit(): Attribute {
+          this.value = boolean("disabled", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "icon",
+        commit(): Attribute {
+          this.value = text("icon", "beaker");
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "indicator",
+        commit(): Attribute {
+          this.value = boolean("indicator", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "label",
+        commit(): Attribute {
+          this.value = text("label", "Label");
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "loading",
+        commit(): Attribute {
+          this.value = boolean("loading", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "scale",
+        commit(): Attribute {
+          this.value = select("scale", scale.values, scale.defaultValue);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "text",
+        commit(): Attribute {
+          this.value = text("text", "Text");
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "text-enabled",
+        commit(): Attribute {
+          this.value = boolean("textEnabled", true);
+          delete this.build;
+          return this;
+        }
+      }
+    ],
+    exceptions
+  );
+};
 
 export const Default = stepStory(
-  (): string => html`
-    <calcite-action
-      active="${boolean("active", false)}"
-      disabled="${boolean("disabled", false)}"
-      icon="${text("icon", "beaker")}"
-      indicator="${boolean("indicator", false)}"
-      label="${text("label", "Label")}"
-      loading="${storybookBoolean("loading", false)}"
-      placement="${select("placement", appearance.values, appearance.defaultValue)}"
-      scale="${select("scale", scale.values, scale.defaultValue)}"
-      text="${text("text", "Text")}"
-      text-enabled="${boolean("text-enabled", false)}"
-    >
-    </calcite-action>
-  `,
+  (): string => html`${create("calcite-action", createAttributes())}`,
   createSteps("calcite-action")
     // Default
     .snapshot("Default")
