3f518feb8190045ae6d16e07e83269601407f75a
Erik Harper <eharperdicianno@esri.com>
2021-09-27 16:42:02 -0700

    adding snapshots for each action prop value while resetting previous values, except for alignment, which requires text-enabled to be false

--- src/components/calcite-action/calcite-action.stories.ts
+++ src/components/calcite-action/calcite-action.stories.ts
@@ -3,13 +3,13 @@ import {
   Attribute,
   filterComponentAttributes,
   Attributes,
-  createComponentHTML as create,
-  themesDarkDefault
+  createComponentHTML as create
 } from "../../../.storybook/utils";
 import readme from "./readme.md";
 import { html } from "../../tests/utils";
 import { createSteps, stepStory, setTheme, setKnobs } from "../../../.storybook/helpers";
 import { ATTRIBUTES } from "../../../.storybook/resources";
+const { alignment, appearance, scale } = ATTRIBUTES;
 
 export default {
   title: "Components/Buttons/Action",
@@ -19,8 +19,6 @@ export default {
 };
 
 const createAttributes: (options?: { exceptions: string[] }) => Attributes = ({ exceptions } = { exceptions: [] }) => {
-  const { alignment, appearance, scale } = ATTRIBUTES;
-
   return filterComponentAttributes(
     [
       {
@@ -129,4 +127,321 @@ export const Default = stepStory(
   createSteps("calcite-action")
     // Default
     .snapshot("Default")
+
+    // Default Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Default Dark")
+
+    // Default RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Default RTL")
+
+    // Default Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Default Dark RTL")
+
+    // Active
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({ story: "components-buttons-action--default", knobs: [{ name: "active", value: "true" }] })
+    )
+    .snapshot("Active")
+
+    // Active Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Active Dark")
+
+    // Active RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Active RTL")
+
+    // Active Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Active Dark RTL")
+
+    // Appearance Clear
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "active", value: "false" },
+          { name: "appearance", value: "clear" }
+        ]
+      })
+    )
+    .snapshot("Appearance Clear")
+
+    // Appearance Clear Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Appearance Clear Dark")
+
+    // Appearance Clear RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Appearance Clear RTL")
+
+    // Appearance Clear Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Appearance Clear Dark RTL")
+
+    // Appearance Outline
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({ story: "components-buttons-action--default", knobs: [{ name: "appearance", value: "outline" }] })
+    )
+    .snapshot("Appearance Outline")
+
+    // Appearance Outline Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Appearance Outline Dark")
+
+    // Appearance Outline RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Appearance Outline RTL")
+
+    // Appearance Outline Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Appearance Outline Dark RTL")
+
+    // Compact
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "appearance", value: appearance.defaultValue },
+          { name: "compact", value: "true" }
+        ]
+      })
+    )
+    .snapshot("Compact")
+
+    // Compact Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Compact Dark")
+
+    // Compact RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Compact RTL")
+
+    // Compact Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Compact Dark RTL")
+
+    // Disabled
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "compact", value: "false" },
+          { name: "disabled", value: "true" }
+        ]
+      })
+    )
+    .snapshot("Disabled")
+
+    // Disabled Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Disabled Dark")
+
+    // Disabled RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Disabled RTL")
+
+    // Disabled Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Disabled Dark RTL")
+
+    // Indicator
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "disabled", value: "false" },
+          { name: "indicator", value: "true" }
+        ]
+      })
+    )
+    .snapshot("Indicator")
+
+    // Indicator Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Indicator Dark")
+
+    // Indicator RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Indicator RTL")
+
+    // Indicator Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Indicator Dark RTL")
+
+    // Loading
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "indicator", value: "false" },
+          { name: "loading", value: "true" }
+        ]
+      })
+    )
+    .snapshot("Loading")
+
+    // Loading Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Loading Dark")
+
+    // Loading RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Loading RTL")
+
+    // Loading Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Loading Dark RTL")
+
+    // Scale Small
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "loading", value: "false" },
+          { name: "scale", value: "s" }
+        ]
+      })
+    )
+    .snapshot("Scale Small")
+
+    // Scale Small Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Scale Small Dark")
+
+    // Scale Small RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Scale Small RTL")
+
+    // Scale Small Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Scale Small Dark RTL")
+
+    // Scale Large
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(setKnobs({ story: "components-buttons-action--default", knobs: [{ name: "scale", value: "l" }] }))
+    .snapshot("Scale Large")
+
+    // Scale Large Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Scale Large Dark")
+
+    // Scale Large RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Scale Large RTL")
+
+    // Scale Large Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Scale Large Dark RTL")
+
+    // Text Not Enabled
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "scale", value: scale.defaultValue },
+          { name: "text-enabled", value: "false" }
+        ]
+      })
+    )
+    .snapshot("Text Not Enabled")
+
+    // Text Not Enabled Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Text Not Enabled Dark")
+
+    // Text Not Enabled RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Text Not Enabled RTL")
+
+    // Text Not Enabled Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Text Not Enabled Dark RTL")
+
+    // Alignment Center
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "alignment", value: "center" },
+          { name: "text-enabled", value: "false" }
+        ]
+      })
+    )
+    .snapshot("Alignment Center")
+
+    // Alignment Center Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Alignment Center Dark")
+
+    // Alignment Center RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Alignment Center RTL")
+
+    // Alignment Center Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Alignment Center Dark RTL")
+
+    // Alignment End
+    .ltr()
+    .executeScript(setTheme("light"))
+    .executeScript(
+      setKnobs({
+        story: "components-buttons-action--default",
+        knobs: [
+          { name: "alignment", value: "end" },
+          { name: "text-enabled", value: "false" }
+        ]
+      })
+    )
+    .snapshot("Alignment End")
+
+    // Alignment End Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Alignment End Dark")
+
+    // Alignment End RTL
+    .rtl()
+    .executeScript(setTheme("light"))
+    .snapshot("Alignment End RTL")
+
+    // Alignment End Dark RTL
+    .executeScript(setTheme("dark"))
+    .snapshot("Alignment End Dark RTL")
 );
