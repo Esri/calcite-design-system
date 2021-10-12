b455c397840c68cbdcf72f993269f0746c21a73c
Anveshreddy mekala <anv11827@esri.com>
2021-10-11 20:38:36 -0500

    fix(dropdown): dropdown content no longer cut off when expanded inside tab (#3182)
    * fix(dropdown): remove relative position on host
    
    * add e2e test
    
    * stabilize e2e test
    
    * feedback changes

--- src/components/calcite-dropdown/calcite-dropdown.e2e.ts
+++ src/components/calcite-dropdown/calcite-dropdown.e2e.ts
@@ -999,4 +999,37 @@ describe("calcite-dropdown", () => {
 
     expect(finalSelectedItem).toBe("item-3");
   });
+
+  it("dropdown should not overflow when wrapped inside a tab #3007", async () => {
+    const page = await newE2EPage({
+      html: html` <calcite-tabs>
+        <calcite-tab-nav slot="tab-nav">
+          <calcite-tab-title is-active>First tab</calcite-tab-title>
+        </calcite-tab-nav>
+        <calcite-tab is-active>
+          <calcite-dropdown>
+            <calcite-button slot="dropdown-trigger" class="dropdown">Dropdown</calcite-button>
+            <calcite-dropdown-group group-title="Select one">
+              <calcite-dropdown-item>First</calcite-dropdown-item>
+              <calcite-dropdown-item>Second</calcite-dropdown-item>
+            </calcite-dropdown-group>
+          </calcite-dropdown>
+        </calcite-tab>
+      </calcite-tabs>`
+    });
+    await page.waitForChanges();
+
+    const button = await page.find("calcite-button");
+
+    await button.click();
+    await page.waitForChanges();
+
+    expect(
+      await page.$eval("calcite-dropdown", (dropdown) => {
+        // check whether the element is overflown, ref :https://stackoverflow.com/questions/9333379/check-if-an-elements-content-is-overflowing
+        const { clientWidth, clientHeight, scrollWidth, scrollHeight } = dropdown;
+        return scrollHeight > clientHeight || scrollWidth > clientWidth;
+      })
+    ).toBe(false);
+  });
 });
