f6a0e5c765271c15b61dd9dc7f352f8ea03993e1
JC Franco <jfranco@esri.com>
2021-10-12 17:33:21 -0700

    update preamble

--- stencil.config.ts
+++ stencil.config.ts
@@ -120,7 +120,7 @@ export const create: () => Config = () => ({
     selector: "attribute",
     name: "calcite-hydrated"
   },
-  preamble: `Copyright 2021 Esri\n \n Licensed under the Apache License Version 2.0 (the "License");\n you may not use this file except in compliance with the License.\n You may obtain a copy of the License at\n \n     http://www.apache.org/licenses/LICENSE-2.0\n \n Unless required by applicable law or agreed to in writing, software\n distributed under the License is distributed on an "AS IS" BASIS,\n WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n See the License for the specific language governing permissions and\n limitations under the License.\n`,
+  preamble: `All material copyright ESRI, All Rights Reserved, unless otherwise specified.\nSee https://github.com/Esri/calcite-components/blob/master/LICENSE.md for details.`,
   extras: {
     scriptDataOpts: true
   }
