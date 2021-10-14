1c8f8c5149c0999a24722e6573163c742161e828
Yona N <04l.yona@gmail.com>
2021-10-14 10:26:37 -0700

    fix(combobox): add chevron icon at end of input regardless of selecti… (#3143)
    * fix(combobox): add chevron icon at end of input regardless of selection mode (#3055)
    
    * fix(combobox): align the chevron icon to be centered vertically on multiple chips select mode (#3055)
    
    * fix(action-bar): get up-to-date master changes
    
    * feat(combobox): add additional story for multiple with preselected inputs
    
    * fix(combobox): make all the combobox height size consistent scale

--- src/components/calcite-combobox/calcite-combobox.tsx
+++ src/components/calcite-combobox/calcite-combobox.tsx
@@ -969,12 +969,14 @@ export class CalciteCombobox implements LabelableComponent {
           ref={this.setReferenceEl}
           role="combobox"
         >
-          {this.renderIconStart()}
-          {!single && this.renderChips()}
-          <label class="screen-readers-only" htmlFor={`${guid}-input`} id={labelId}>
-            {label}
-          </label>
-          {this.renderInput()}
+          <div class="grid-input">
+            {this.renderIconStart()}
+            {!single && this.renderChips()}
+            <label class="screen-readers-only" htmlFor={`${guid}-input`} id={labelId}>
+              {label}
+            </label>
+            {this.renderInput()}
+          </div>
           {this.renderIconEnd()}
         </div>
         <ul
