f45a1d96765bdbd592efd9072ea93b2b9cd02168
Erik Harper <eharperdicianno@esri.com>
2021-10-14 17:19:57 -0700

    restoring original storybook helpers file because the experimental changes are no longer needed

--- .storybook/helpers.ts
+++ .storybook/helpers.ts
@@ -23,9 +23,9 @@ export interface Story {
   decorators?: ((Story: Story) => DocumentFragment)[];
 }
 
-export const setKnobs = ({ story, knobs }: { story: string; knobs: { name: string; value?: string }[] }) => {
+export const setKnobs = ({ story, knobs }: { story: string; knobs: { name: string; value: string }[] }) => {
   return `window.location.href = "?path=/story/${story}${knobs
-    .map(({ name, value }) => `&knob-${name}=${value && value}`)
+    .map(({ name, value }) => `&knob-${name}=${value}`)
     .join("")}"`;
 };
 
