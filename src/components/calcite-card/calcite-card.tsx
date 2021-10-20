44938867d8f2cfe83ba69922f1ce733f462855ac
Matt Driscoll <mdriscoll@esri.com>
2021-10-20 15:55:22 -0700

    cleanup

--- src/components/calcite-card/calcite-card.tsx
+++ src/components/calcite-card/calcite-card.tsx
@@ -141,15 +141,14 @@ export class CalciteCard {
     const checkboxLabel = this.selected ? this.intlDeselect : this.intlSelect;
 
     return (
-      <label
-        aria-label={checkboxLabel}
+      <div
         class={CSS.checkboxWrapper}
         onClick={this.cardSelectClick}
         onKeyDown={this.cardSelectKeyDown}
         title={checkboxLabel}
       >
-        <calcite-checkbox checked={this.selected} />
-      </label>
+        <calcite-checkbox checked={this.selected} label={checkboxLabel} />
+      </div>
     );
   }
 
