0e927295c0cc6bbe68331b12ab2463264be352f1
Matt Driscoll <mdriscoll@esri.com>
2021-10-21 11:07:16 -0700

    remove some tests

--- src/components/calcite-checkbox/calcite-checkbox.e2e.ts
+++ src/components/calcite-checkbox/calcite-checkbox.e2e.ts
@@ -24,37 +24,20 @@ describe("calcite-checkbox", () => {
     expect(calciteCheckbox).not.toHaveAttribute("indeterminate");
   });
 
-  it("correctly creates a hidden checkbox input", async () => {
-    const testName = "test-name";
-    const testValue = "test-value";
-    const page = await newE2EPage();
-    await page.setContent(`<calcite-checkbox checked name="${testName}" value="${testValue}"></calcite-checkbox>`);
-
-    const input = await page.find("calcite-checkbox input");
-
-    expect(input).toHaveProperty("type", "hidden");
-    expect(input).toHaveProperty("name", testName);
-    expect(input).toHaveProperty("value", testValue);
-    expect(input).toHaveProperty("checked", true);
-  });
-
   it("toggles the checked attributes appropriately when clicked", async () => {
     const page = await newE2EPage();
     await page.setContent("<calcite-checkbox></calcite-checkbox>");
 
     const calciteCheckbox = await page.find("calcite-checkbox");
-    const input = await page.find("calcite-checkbox input");
 
     expect(calciteCheckbox).not.toHaveAttribute("checked");
     expect(await calciteCheckbox.getProperty("checked")).toBe(false);
-    expect(await input.getProperty("checked")).toBe(false);
 
     await calciteCheckbox.click();
     await page.waitForChanges();
 
     expect(calciteCheckbox).toHaveAttribute("checked");
     expect(await calciteCheckbox.getProperty("checked")).toBe(true);
-    expect(await input.getProperty("checked")).toBe(true);
   });
 
   it("appropriately triggers the custom change event", async () => {
@@ -90,17 +73,14 @@ describe("calcite-checkbox", () => {
     await page.setContent("<calcite-checkbox disabled></calcite-checkbox>");
 
     const calciteCheckbox = await page.find("calcite-checkbox");
-    const input = await page.find("calcite-checkbox input");
 
     expect(calciteCheckbox).not.toHaveAttribute("checked");
-    expect(input).toHaveProperty("checked", false);
 
     await calciteCheckbox.click();
 
     await page.waitForChanges();
 
     expect(calciteCheckbox).not.toHaveAttribute("checked");
-    expect(input).toHaveProperty("checked", false);
   });
 
   it("removes the indeterminate attribute when clicked", async () => {
@@ -116,34 +96,14 @@ describe("calcite-checkbox", () => {
     expect(calciteCheckbox).not.toHaveAttribute("indeterminate");
   });
 
-  it("removing a checkbox also removes the hidden <input type=checkbox> element", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <calcite-checkbox name="checky" id="first" value="one"></calcite-checkbox>
-    `);
-
-    let input = await page.find("calcite-checkbox input");
-    expect(input).toBeTruthy();
-
-    await page.evaluate(() => {
-      const checkbox = document.querySelector("calcite-checkbox");
-      checkbox.parentNode.removeChild(checkbox);
-    });
-    await page.waitForChanges();
-
-    input = await page.find("calcite-checkbox input");
-
-    expect(input).toBeFalsy();
-  });
-
   it("behaves as expected when wrapped in a calcite-label with inline layout mode", async () => {
     const page = await newE2EPage();
     await page.setContent(`
       <calcite-label layout="inline"><calcite-checkbox></calcite-checkbox>Label</calcite-label>
     `);
 
-    const inputs = await page.findAll("calcite-checkbox input");
-    expect(inputs.length).toEqual(1);
+    const checkboxes = await page.findAll("calcite-checkbox");
+    expect(checkboxes.length).toEqual(1);
   });
 
   it("behaves as expected when wrapped in a calcite-label with inline-space-between layout mode", async () => {
@@ -152,8 +112,8 @@ describe("calcite-checkbox", () => {
       <calcite-label layout="inline-space-between"><calcite-checkbox></calcite-checkbox>Label</calcite-label>
     `);
 
-    const inputs = await page.findAll("calcite-checkbox input");
-    expect(inputs.length).toEqual(1);
+    const checkboxes = await page.findAll("calcite-checkbox");
+    expect(checkboxes.length).toEqual(1);
   });
 
   it("resets to initial value when form reset event is triggered", async () => {
