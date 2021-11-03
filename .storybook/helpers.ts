d5b265d3f5c4c96636b28a4097394dc1963adfe2
Matt Driscoll <mdriscoll@esri.com>
2021-11-03 14:42:20 -0700

    docs(storybook): fix storybook error.

--- .storybook/helpers.ts
+++ .storybook/helpers.ts
@@ -6,8 +6,8 @@ import { ThemeName } from "../src/components/interfaces";
 
 // we can get all unique icon names from all size 16 non-filled icons.
 export const iconNames = Object.keys(icons)
-  .filter((iconName) => iconName.endsWith("16"))
-  .map((iconName) => iconName.replace("16", ""));
+  .filter((iconName) => iconName?.endsWith("16"))
+  .map((iconName) => iconName?.replace("16", ""));
 
 // custom boolean will start up a knob but only add the prop if it is true
 // if you'd insead like `attr="true|false" set the standalone option to false
@@ -24,14 +24,20 @@ export interface Story {
 }
 
 export const setKnobs = ({ story, knobs }: { story: string; knobs: { name: string; value: string }[] }) => {
-  return `window.location.href = "?path=/story/${story}${knobs
-    .map(({ name, value }) => `&knob-${name}=${value}`)
-    .join("")}"`;
+  return knobs
+    ? `window.location.href = "?path=/story/${story}${knobs
+        .map(({ name, value }) => `&knob-${name}=${value}`)
+        .join("")}"`
+    : "";
 };
 
-export const setTheme = (value: ThemeName) => `${THEMES.map(
-  (theme) => `document.body.classList.toggle('${theme.className}', ${(theme.name === value).toString()});`
-).join("")}
+export const setTheme = (value: ThemeName) => `${
+  THEMES
+    ? THEMES.map(
+        (theme) => `document.body.classList.toggle('${theme.className}', ${(theme.name === value).toString()});`
+      ).join("")
+    : ""
+}
 `;
 
 export const createSteps = (componentSelector: string): Steps => {
