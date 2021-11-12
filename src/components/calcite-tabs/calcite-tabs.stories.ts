e46b214d28e422a9b33223ab89d567791bfbfd67
Yona Nagayama <yon97701@esri.com>
2021-11-12 11:07:46 -0800

    add snapshot after click of id

--- src/components/calcite-tabs/calcite-tabs.stories.ts
+++ src/components/calcite-tabs/calcite-tabs.stories.ts
@@ -24,7 +24,7 @@ export const Simple = stepStory(
     >
       <calcite-tab-nav slot="tab-nav">
         <calcite-tab-title active>Tab 1 Title</calcite-tab-title>
-        <calcite-tab-title reference-element="reference-element">Tab 2 Title</calcite-tab-title>
+        <calcite-tab-title id="reference-element">Tab 2 Title</calcite-tab-title>
         <calcite-tab-title>Tab 3 Title</calcite-tab-title>
         <calcite-tab-title>Tab 4 Title</calcite-tab-title>
       </calcite-tab-nav>
@@ -42,7 +42,7 @@ export const Simple = stepStory(
       <calcite-tab><p>Tab 4 Content</p></calcite-tab>
     </calcite-tabs>
   `,
-  createSteps("calcite-tabs").click("#reference-element").snapshot("simple")
+  createSteps("calcite-tabs").snapshot("simple").click("#reference-element").snapshot("horizontal scroll")
 );
 
 // export const Simple = (): string => html`
