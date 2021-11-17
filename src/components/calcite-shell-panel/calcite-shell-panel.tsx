3bee4247a75753bf419fd8f8e5fff66a64a9bdd5
Matt Driscoll <mdriscoll@esri.com>
2021-11-16 17:11:39 -0800

    WIP

--- src/components/calcite-shell-panel/calcite-shell-panel.tsx
+++ src/components/calcite-shell-panel/calcite-shell-panel.tsx
@@ -1,7 +1,18 @@
-import { Component, Element, Event, EventEmitter, Prop, Watch, h, VNode } from "@stencil/core";
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  Prop,
+  Watch,
+  h,
+  VNode,
+  State
+} from "@stencil/core";
 import { CSS, SLOTS } from "./resources";
 import { Position, Scale } from "../interfaces";
 import { getSlotted } from "../../utils/dom";
+import { getKey } from "../../utils/key";
 
 /**
  * @slot - A slot for adding content to the shell panel.
@@ -50,12 +61,37 @@ export class CalciteShellPanel {
    */
   @Prop({ reflect: true }) position: Position;
 
+  //--------------------------------------------------------------------------
+  //
+  //  Lifecycle
+  //
+  //--------------------------------------------------------------------------
+
+  disconnectedCallback(): void {
+    this.disconnectSeparator();
+  }
+
   // --------------------------------------------------------------------------
   //
   //  Private Properties
   //
   // --------------------------------------------------------------------------
+
   @Element() el: HTMLCalciteShellPanelElement;
+
+  @State() value: number;
+
+  contentEl: HTMLDivElement;
+
+  separatorEl: HTMLDivElement;
+
+  max = 1000; // todo
+
+  min = 50; // todo
+
+  step = 1;
+
+  stepMultiplier = 10;
 
   // --------------------------------------------------------------------------
   //
@@ -86,10 +122,15 @@ export class CalciteShellPanel {
   }
 
   render(): VNode {
-    const { collapsed, detached, position } = this;
+    const { collapsed, detached, position, value } = this;
 
     const contentNode = (
-      <div class={{ [CSS.content]: true, [CSS.contentDetached]: detached }} hidden={collapsed}>
+      <div
+        class={{ [CSS.content]: true, [CSS.contentDetached]: detached }}
+        hidden={collapsed}
+        ref={(contentEl) => (this.contentEl = contentEl)}
+        style={{ width: `${value}px;` }}
+      >
         {this.renderHeader()}
         <div class={CSS.contentBody}>
           <slot />
@@ -97,9 +138,25 @@ export class CalciteShellPanel {
       </div>
     );
 
+    const separatorNode = (
+      <div
+        aria-label="todo"
+        aria-orientation="vertical"
+        aria-valuemax={this.max}
+        aria-valuemin={this.min}
+        aria-valuenow={this.value}
+        class={CSS.separator}
+        onKeyDown={this.separatorKeyDown}
+        ref={this.connectSeparator}
+        role="separator"
+        tabIndex={0}
+        touch-action="none"
+      />
+    );
+
     const actionBarNode = <slot name={SLOTS.actionBar} />;
 
-    const mainNodes = [actionBarNode, contentNode];
+    const mainNodes = [actionBarNode, contentNode, separatorNode];
 
     if (position === "end") {
       mainNodes.reverse();
@@ -107,4 +164,128 @@ export class CalciteShellPanel {
 
     return <div class={{ [CSS.container]: true }}>{mainNodes}</div>;
   }
+
+  // --------------------------------------------------------------------------
+  //
+  //  private Methods
+  //
+  // --------------------------------------------------------------------------
+
+  getKeyvalue = (event: KeyboardEvent): number => {
+    const key = getKey(event.key);
+    const { value, step, stepMultiplier, min, max } = this;
+    const multipliedStep = step * stepMultiplier;
+
+    const MOVEMENT_KEYS = [
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+      "Home",
+      "End",
+      "PageUp",
+      "PageDown"
+    ];
+
+    if (MOVEMENT_KEYS.indexOf(key) > -1) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    const increaseKeys = key === "ArrowDown" || key === "ArrowRight";
+
+    if (increaseKeys) {
+      const stepValue = event.shiftKey ? multipliedStep : step;
+
+      return value + stepValue;
+    }
+
+    const decreaseKeys = key === "ArrowUp" || key === "ArrowLeft";
+
+    if (decreaseKeys) {
+      const stepValue = event.shiftKey ? multipliedStep : step;
+
+      return value - stepValue;
+    }
+
+    if (key === "Home") {
+      return min;
+    }
+
+    if (key === "End") {
+      return max;
+    }
+
+    if (key === "PageDown") {
+      return value + multipliedStep;
+    }
+
+    if (key === "PageUp") {
+      return value - multipliedStep;
+    }
+
+    return null;
+  };
+
+  separatorKeyDown = (event: KeyboardEvent): void => {
+    const value = this.getKeyvalue(event);
+
+    if (typeof value === "number") {
+      this.value = value;
+    }
+  };
+
+  // private calculatePointerOffset = (event: PointerEvent): void => {
+  //   const target = event.target as HTMLElement;
+  //   const containerSize = target.clientHeight / 2;
+
+  //   const rect = target.getBoundingClientRect();
+  //   const offsetY = event.clientY - rect.top;
+
+  //   this.pointerOffset = offsetY - containerSize;
+  // };
+
+  // todo: fix
+  separatorPointerMove = (event: PointerEvent): void => {
+    event.preventDefault();
+
+    const { contentEl } = this;
+    //const { clientY } = event;
+    const { width } = contentEl.getBoundingClientRect();
+
+    console.log(event);
+
+    const value = width + event.offsetX;
+
+    console.log(value);
+
+    this.value = value;
+  };
+
+  private separatorPointerUp = (event: PointerEvent): void => {
+    event.preventDefault();
+
+    document.removeEventListener("pointerup", this.separatorPointerUp);
+    document.removeEventListener("pointermove", this.separatorPointerMove);
+  };
+
+  separatorPointerDown = (event: PointerEvent): void => {
+    event.preventDefault();
+    this.separatorEl && document.activeElement !== this.separatorEl && this.separatorEl.focus();
+
+    //this.calculatePointerOffset(event);
+    document.addEventListener("pointerup", this.separatorPointerUp);
+    document.addEventListener("pointermove", this.separatorPointerMove);
+  };
+
+  connectSeparator = (separatorEl: HTMLDivElement): void => {
+    this.disconnectSeparator();
+
+    this.separatorEl = separatorEl;
+    separatorEl.addEventListener("pointerdown", this.separatorPointerDown);
+  };
+
+  disconnectSeparator = (): void => {
+    this.separatorEl?.removeEventListener("pointerdown", this.separatorPointerDown);
+  };
 }
--- src/components/calcite-shell-panel/resources.ts
+++ src/components/calcite-shell-panel/resources.ts
@@ -3,7 +3,8 @@ export const CSS = {
   content: "content",
   contentHeader: "content__header",
   contentBody: "content__body",
-  contentDetached: "content--detached"
+  contentDetached: "content--detached",
+  separator: "separator"
 };
 
 export const SLOTS = {
