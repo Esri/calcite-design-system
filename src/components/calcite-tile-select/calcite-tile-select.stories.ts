37c6832b770b975f6796561d7ac64cbb708aa9ef
Erik Harper <eharperdicianno@esri.com>
2021-11-24 10:47:40 -0800

    first commit trying to get focus states working

--- src/components/calcite-tile-select/calcite-tile-select.stories.ts
+++ src/components/calcite-tile-select/calcite-tile-select.stories.ts
@@ -1,76 +1,146 @@
-import { select, text } from "@storybook/addon-knobs";
-import { iconNames, boolean } from "../../../.storybook/helpers";
-import { themesDarkDefault } from "../../../.storybook/utils";
+import { boolean, select, text } from "@storybook/addon-knobs";
+import { iconNames, setKnobs, setTheme } from "../../../.storybook/helpers";
+import {
+  Attribute,
+  Attributes,
+  createComponentHTML as create,
+  filterComponentAttributes,
+  themesDarkDefault
+} from "../../../.storybook/utils";
 import readme from "./readme.md";
 import { html } from "../../tests/utils";
+import { createSteps, stepStory } from "../../../.storybook/helpers";
 
 export default {
   title: "Components/Tiles/Tile Select",
-
   parameters: {
     notes: readme
   }
 };
 
-export const Light = (): string => html`
-  <calcite-tile-select
-    ${boolean("checked", true)}
-    description="${text(
-      "description",
-      "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
-    )}"
-    ${boolean("disabled", false)}
-    heading="${text("heading", "Tile heading lorem ipsum")}"
-    ${boolean("hidden", false)}
-    icon="${select("icon", iconNames, "layer")}"
-    ${boolean("input-enabled", true)}
-    input-alignment="${select("input-alignment", ["start", "end"], "end")}"
-    type="${select("type", ["radio", "checkbox"], "radio")}"
-    value="${text("value", "one")}"
-  >
-  </calcite-tile-select>
-`;
+const createAttributes: (options?: { exceptions: string[] }) => Attributes = ({ exceptions } = { exceptions: [] }) => {
+  return filterComponentAttributes(
+    [
+      {
+        name: "checked",
+        commit(): Attribute {
+          this.value = boolean("checked", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "description",
+        commit(): Attribute {
+          this.value = text(
+            "description",
+            "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
+          );
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "disabled",
+        commit(): Attribute {
+          this.value = boolean("disabled", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "heading",
+        commit(): Attribute {
+          this.value = text("heading", "Tile heading lorem ipsum");
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "hidden",
+        commit(): Attribute {
+          this.value = boolean("hidden", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "icon",
+        commit(): Attribute {
+          this.value = select("icon", ["", ...iconNames], "");
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "input-alignment",
+        commit(): Attribute {
+          this.value = select("input-alignment", ["start", "end"], "start");
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "input-enabled",
+        commit(): Attribute {
+          this.value = boolean("input-enabled", false);
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "type",
+        commit(): Attribute {
+          this.value = select("type", ["radio", "checkbox"], "radio");
+          delete this.build;
+          return this;
+        }
+      },
+      {
+        name: "value",
+        commit(): Attribute {
+          this.value = text("value", "one");
+          delete this.build;
+          return this;
+        }
+      }
+    ],
+    exceptions
+  );
+};
 
-export const Dark = (): string => html`
-  <calcite-tile-select
-    ${boolean("checked", true)}
-    description="${text(
-      "description",
-      "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
-    )}"
-    ${boolean("disabled", false)}
-    heading="${text("heading", "Tile heading lorem ipsum")}"
-    ${boolean("hidden", false)}
-    icon="${select("icon", iconNames, "layer")}"
-    name="${text("name", "tile-select-demo")}"
-    ${boolean("input-enabled", true)}
-    input-alignment="${select("input-alignment", ["start", "end"], "end")}"
-    class="calcite-theme-dark"
-    type="${select("type", ["radio", "checkbox"], "radio")}"
-    value="${text("value", "one")}"
-  >
-  </calcite-tile-select>
-`;
+export const Default = stepStory(
+  (): string => html`${create("calcite-tile-select", createAttributes())}`,
+  createSteps("calcite-tile-select")
+    // Input Disabled No Icon
+    .snapshot("Input Disabled No Icon")
 
-Dark.parameters = { themes: themesDarkDefault };
+    // Input Disabled No Icon Hover
+    .hover("calcite-tile-select")
+    .snapshot("Input Disabled No Icon Hover")
 
-export const RTL = (): string => html`
-  <calcite-tile-select
-    ${boolean("checked", true)}
-    description="${text(
-      "description",
-      "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
-    )}"
-    ${boolean("disabled", false)}
-    heading="${text("heading", "Tile heading lorem ipsum!")}"
-    ${boolean("hidden", false)}
-    icon="${select("icon", iconNames, "layer")}"
-    name="${text("name", "tile-select-demo")}"
-    ${boolean("input-enabled", true)}
-    input-alignment="${select("input-alignment", ["start", "end"], "end")}"
-    type="${select("type", ["radio", "checkbox"], "radio")}"
-    value="${text("value", "one")}"
-    dir="rtl"
-  >
-  </calcite-tile-select>
-`;
+    // Input Disabled No Icon Focus
+    // .keys("calcite-tile-select", "Tab")
+    // .snapshot("Input Disabled No Icon Focus")
+
+    // Input Disabled No Icon Click
+    .click("calcite-tile-select")
+    .snapshot("Input Disabled No Icon Click")
+    .click("body")
+
+    // Input Disabled No Icon Dark
+    .executeScript(setTheme("dark"))
+    .snapshot("Input Disabled No Icon Dark")
+
+    // Input Disabled No Icon Hover Dark
+    .hover("calcite-tile-select")
+    .snapshot("Input Disabled No Icon Hover Dark")
+
+    // Input Disabled No Icon Focus Dark
+    .keys("calcite-tile-select", "Tab")
+    .snapshot("Input Disabled No Icon Focus Dark")
+
+    // Input Disabled No Icon Click Dark
+    .click("calcite-tile-select")
+    .snapshot("Input Disabled No Icon Click Dark")
+);
