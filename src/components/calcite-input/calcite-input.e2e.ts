470f34711480dc8d48224f66d7b1274f93c3e271
Eliza Khachatryan <elijbet@gmail.com>
2021-11-30 11:01:54 -0800

    fix(input): style read-only and disabled inputs per new design reqs and hide spinners for read-only inputs (#3582)
    * WIP
    
    * fix(input): style readonly and disabled inputs per new design reqs and hide spinners for read only inputs
    
    * bring disabled boolean back
    
    * remove aria-disabled attr in favor of disabled and add 2 tests to ensure buttons don't render in vertical or horizontal alignment when read-only
    
    * typo
    
    Co-authored-by: Eliza Khachatryan <eli97736@esri.com>

--- src/components/calcite-input/calcite-input.e2e.ts
+++ src/components/calcite-input/calcite-input.e2e.ts
@@ -172,6 +172,32 @@ describe("calcite-input", () => {
     expect(numberHorizontalItemUp).not.toBeNull();
   });
 
+  it("does not render number buttons in default vertical alignment when type=number and read-only", async () => {
+    const page = await newE2EPage();
+    await page.setContent(html` <calcite-input type="number" read-only></calcite-input> `);
+
+    const numberVerticalWrapper = await page.find("calcite-input >>> .number-button-wrapper");
+
+    expect(numberVerticalWrapper).toBeNull();
+  });
+
+  it("does not render number buttons in horizontal alignment when type=number, number button type is horizontal, and read-only", async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      html` <calcite-input type="number" number-button-type="horizontal" read-only></calcite-input> `
+    );
+
+    const numberHorizontalItemDown = await page.find(
+      "calcite-input >>> .number-button-item--horizontal[data-adjustment='down']"
+    );
+    const numberHorizontalItemUp = await page.find(
+      "calcite-input >>> .number-button-item--horizontal[data-adjustment='up']"
+    );
+
+    expect(numberHorizontalItemDown).toBeNull();
+    expect(numberHorizontalItemUp).toBeNull();
+  });
+
   it("renders no buttons in type=number and number button type is none", async () => {
     const page = await newE2EPage();
     await page.setContent(`
