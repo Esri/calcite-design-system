cfc8e6f745fd9987661f45730f9957a1c46469a8
Yona Nagayama <yon97701@esri.com>
2021-12-13 15:04:28 -0800

    add additional story for tab panel with set width

--- src/components/calcite-tabs/calcite-tabs.stories.ts
+++ src/components/calcite-tabs/calcite-tabs.stories.ts
@@ -118,6 +118,35 @@ export const WithIcons = (): string => html`
 
 WithIcons.storyName = "With icons";
 
+export const SetWidth = (): string => html`
+  <div style="width: 400px;">
+    <calcite-tabs
+    layout="${select("layout", ["inline", "center"], "inline")}"
+    position="${select("position", ["above", "below"], "above")}"
+    scale="${select("scale", ["s", "m", "l"], "m")}"
+    >
+    <calcite-tab-nav slot="tab-nav">
+      <calcite-tab-title active>Tab 1 Title</calcite-tab-title>
+      <calcite-tab-title id="reference-element">Tab 2 Title</calcite-tab-title>
+      <calcite-tab-title>Tab 3 Title</calcite-tab-title>
+      <calcite-tab-title>Tab 4 Title</calcite-tab-title>
+    </calcite-tab-nav>
+
+    <calcite-tab active>
+      <p>Tab 1 Content</p><br />
+    </calcite-tab>
+    <calcite-tab><p>Tab 2 Content</p>
+    <img src="${placeholderImage({
+      width: 1000,
+      height: 200
+    })}"></img>
+    </calcite-tab>
+    <calcite-tab><p>Tab 3 Content</p></calcite-tab>
+    <calcite-tab><p>Tab 4 Content</p></calcite-tab>
+    </calcite-tabs>
+  </div>
+`;
+
 export const JustTabNav = (): string => html`
   <calcite-tab-nav
     position="${select("position", ["above", "below"], "below")}"
