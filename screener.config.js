cdbffd82b6757b26ee1ceb97feb4cf2a9d4a77b3
Erik Harper <eharperdicianno@esri.com>
2021-12-29 12:23:10 -0800

    ci(screener): integrating SauceLabs account with Screener for finer-grained browser control

--- screener.config.js
+++ screener.config.js
@@ -8,11 +8,17 @@ module.exports = {
   baseBranch: "master",
   browsers: [
     {
-      browserName: "chrome"
+      browserName: "chrome",
+      version: "87.0"
     }
   ],
   diffOptions: {
     minLayoutDimension: 1,
     minLayoutPosition: 1
+  },
+  sauce: {
+    username: "calcite-admin",
+    accessKey: process.env.SAUCE_ACCESS_KEY,
+    maxConcurrent: 10
   }
 };
