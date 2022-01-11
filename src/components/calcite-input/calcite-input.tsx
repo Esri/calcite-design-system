2f08b4594098d5338c3b88f6b3b7e05ddf6a97c1
Eliza Khachatryan <eli97736@esri.com>
2022-01-11 11:20:29 -0800

    fix(input): number input no longer increments by itself indefinitely when up/down buttons are toggled fast or pressed at the same time; disable incrementing altogether when up/down buttons are pressed at the same time

--- src/components/calcite-input/calcite-input.tsx
+++ src/components/calcite-input/calcite-input.tsx
@@ -265,6 +265,11 @@ export class CalciteInput implements LabelableComponent, FormComponent {
 
   private nudgeNumberValueIntervalId;
 
+  private keysPressed = {
+    up: false,
+    down: false
+  };
+
   //--------------------------------------------------------------------------
   //
   //  State
@@ -480,10 +485,12 @@ export class CalciteInput implements LabelableComponent, FormComponent {
       return;
     }
     if (event.key === "ArrowUp") {
+      this.keysPressed.up = true;
       this.nudgeNumberValue("up", event);
       return;
     }
     if (event.key === "ArrowDown") {
+      this.keysPressed.down = true;
       this.nudgeNumberValue("down", event);
       return;
     }
@@ -548,15 +555,23 @@ export class CalciteInput implements LabelableComponent, FormComponent {
     const inputMin = this.minString ? parseFloat(this.minString) : null;
     const valueNudgeDelayInMs = 100;
 
+    if (this.keysPressed.up && this.keysPressed.down) {
+      return;
+    }
     this.incrementOrDecrementNumberValue(direction, inputMax, inputMin, nativeEvent);
 
+    if (this.nudgeNumberValueIntervalId) {
+      window.clearInterval(this.nudgeNumberValueIntervalId);
+    }
     let firstValueNudge = true;
     this.nudgeNumberValueIntervalId = window.setInterval(() => {
       if (firstValueNudge) {
         firstValueNudge = false;
         return;
       }
-
+      if (this.keysPressed.up && this.keysPressed.down) {
+        return;
+      }
       this.incrementOrDecrementNumberValue(direction, inputMax, inputMin, nativeEvent);
     }, valueNudgeDelayInMs);
   };
@@ -645,7 +660,13 @@ export class CalciteInput implements LabelableComponent, FormComponent {
     }
   };
 
-  private inputKeyUpHandler = (): void => {
+  private inputKeyUpHandler = (event): void => {
+    if (event.key === "ArrowUp") {
+      this.keysPressed.up = false;
+    }
+    if (event.key === "ArrowDown") {
+      this.keysPressed.down = false;
+    }
     window.clearInterval(this.nudgeNumberValueIntervalId);
   };
 
