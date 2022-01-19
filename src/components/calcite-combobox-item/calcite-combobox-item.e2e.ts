d37423e5ef96ecae2673175cafca6aec417df7fb
JC Franco <jfranco@esri.com>
2022-01-19 15:46:01 -0800

    wire up default slot test for combobox item

--- src/components/calcite-combobox-item/calcite-combobox-item.e2e.ts
+++ src/components/calcite-combobox-item/calcite-combobox-item.e2e.ts
@@ -1,10 +1,9 @@
-import { hidden, renders } from "../../tests/commonTests";
+import { hidden, renders, slots } from "../../tests/commonTests";
 
 describe("calcite-combobox-item", () => {
   it("renders", async () => renders("calcite-combobox-item", { display: "flex" }));
 
   it("honors hidden attribute", async () => hidden("calcite-combobox-item"));
 
-  // todo: need the test utility to be able to check for default slotted elements.
-  //it("has slots", () => slots("calcite-combobox-item", SLOTS));
+  it("has slots", () => slots("calcite-combobox-item", [], true));
 });
