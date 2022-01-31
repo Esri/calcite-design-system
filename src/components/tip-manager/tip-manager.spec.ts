6f6ca52bf44c06b7cda58d851533b17764b660ea
Ben Elan <belan@esri.com>
2022-01-31 10:32:21 -0800

    cleanup

--- src/components/tip-manager/tip-manager.spec.ts
+++ src/components/tip-manager/tip-manager.spec.ts
@@ -1,9 +1,9 @@
 // TODO: Uncomment this test when there's a resolution to this bug. https://github.com/ionic-team/stencil/issues/1669
 // import { TipManager } from "./calcite-tip-manager";
 
-describe.skip("CalciteTipManager", () => {
+describe.skip("TipManager", () => {
   it("should increment/decrement the selectedIndex when the public next/prev methods are called", () => {
-    // const tipManager = new CalciteTipManager();
+    // const tipManager = new TipManager();
     // tipManager.total = 2; //needed to
     // expect(tipManager.selectedIndex).toBe(0);
     // tipManager.nextTip();
