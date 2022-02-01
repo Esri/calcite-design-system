628e9c2357c57dcef8eb506f2a2b3c3e7a192895
Ben Elan <benelan@users.noreply.github.com>
2022-01-31 18:10:28 -0800

    refactor: remove calcite prefix from file names (#3804)
    * refactor: remove calcite prefix from file names
    
    * fix asset paths
    
    * remove prefixes in doc
    
    * remove prefix from class names
    
    * more class names
    
    * cleanup
    
    * fix merge conflict changes
    
    * remove prefix from interface
    
    * cleanup
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/components/list/list.tsx
@@ -0,0 +1,64 @@
+import { Component, Element, h, VNode, Host, Prop, Method } from "@stencil/core";
+import { CSS } from "./resources";
+import { HeadingLevel } from "../functional/Heading";
+
+/**
+ * A general purpose list that enables users to construct list items that conform to Calcite styling.
+ * @slot - A slot for adding `calcite-list-item` elements.
+ */
+@Component({
+  tag: "calcite-list",
+  styleUrl: "list.scss",
+  shadow: true
+})
+export class List {
+  // --------------------------------------------------------------------------
+  //
+  //  Properties
+  //
+  // --------------------------------------------------------------------------
+
+  /**
+   * Number at which section headings should start for this component.
+   */
+  @Prop() headingLevel: HeadingLevel;
+
+  // --------------------------------------------------------------------------
+  //
+  //  Private Properties
+  //
+  // --------------------------------------------------------------------------
+
+  @Element() el: HTMLCalciteListElement;
+
+  // --------------------------------------------------------------------------
+  //
+  //  Public Methods
+  //
+  // --------------------------------------------------------------------------
+
+  /** Sets focus on the component. */
+  @Method()
+  async setFocus(): Promise<void> {
+    const firstListItem: HTMLCalciteListItemElement = this.el.querySelector(
+      `calcite-list-item:not([non-interactive])`
+    );
+    firstListItem?.setFocus();
+  }
+
+  // --------------------------------------------------------------------------
+  //
+  //  Render Methods
+  //
+  // --------------------------------------------------------------------------
+
+  render(): VNode {
+    return (
+      <Host role="list">
+        <div class={CSS.container}>
+          <slot />
+        </div>
+      </Host>
+    );
+  }
+}
