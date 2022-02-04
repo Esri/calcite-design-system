a1d0f7e7bf287f3fd1407f1d49a381b31619b8d7
Matt Driscoll <mdriscoll@esri.com>
2022-02-03 16:29:57 -0800

    add test

--- src/components/combobox/combobox.stories.ts
+++ src/components/combobox/combobox.stories.ts
@@ -1,5 +1,5 @@
 import { select, number, text } from "@storybook/addon-knobs";
-import { boolean } from "../../../.storybook/helpers";
+import { boolean, createSteps, stepStory } from "../../../.storybook/helpers";
 
 import { themesDarkDefault } from "../../../.storybook/utils";
 import readme1 from "./readme.md";
@@ -217,3 +217,31 @@ export const Rtl = (): string => html`
 `;
 
 Rtl.storyName = "RTL";
+
+export const FlipPositioning = stepStory(
+  (): string => html`
+    <div style="overflow: auto; height: 600px; width: 500px;">
+      <div style="height: 400px;"></div>
+      <calcite-combobox
+        placeholder="${text("placeholder", "placeholder")}"
+        label="${text("label (for screen readers)", "demo")}"
+        selection-mode="${select("selection-mode", ["multi", "single", "ancestors"], "multi")}"
+        scale="${select("scale", ["s", "m", "l"], "m")}"
+        ${boolean("disabled", false)}
+        ${boolean("allow-custom-values", false)}
+      >
+        <calcite-combobox-item value="Trees" text-label="Trees">
+          <calcite-combobox-item value="Pine" text-label="Pine"></calcite-combobox-item>
+          <calcite-combobox-item value="Sequoia" disabled text-label="Sequoia"></calcite-combobox-item>
+          <calcite-combobox-item value="Douglas Fir" text-label="Douglas Fir"></calcite-combobox-item>
+        </calcite-combobox-item>
+        <calcite-combobox-item value="Flowers" text-label="Flowers">
+          <calcite-combobox-item value="Daffodil" text-label="Daffodil"></calcite-combobox-item>
+          <calcite-combobox-item value="Black Eyed Susan" text-label="Black Eyed Susan"></calcite-combobox-item>
+          <calcite-combobox-item value="Nasturtium" text-label="Nasturtium"></calcite-combobox-item>
+        </calcite-combobox-item>
+      </calcite-combobox>
+    </div>
+  `,
+  createSteps("calcite-combobox").snapshot("Default").click("calcite-combobox").snapshot("Open")
+);
