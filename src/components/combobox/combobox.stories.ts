631c49fde2a42b19c337d5b59982293331f4f96c
Matt Driscoll <mdriscoll@esri.com>
2022-02-04 11:07:32 -0800

    fix(combobox): Align with dropdown logic and repositioning (#4059)
    * refactor(combobox): Align with dropdown logic and repositioning. #3868
    
    * add test
    
    * add test and cleanup
    
    * cleanup story

--- src/components/combobox/combobox.stories.ts
+++ src/components/combobox/combobox.stories.ts
@@ -1,5 +1,5 @@
 import { select, number, text } from "@storybook/addon-knobs";
-import { boolean } from "../../../.storybook/helpers";
+import { boolean, createSteps, stepStory } from "../../../.storybook/helpers";
 
 import { themesDarkDefault } from "../../../.storybook/utils";
 import readme1 from "./readme.md";
@@ -217,3 +217,42 @@ export const Rtl = (): string => html`
 `;
 
 Rtl.storyName = "RTL";
+
+export const FlipPositioning = stepStory(
+  (): string => html`
+    <div style="position: absolute; bottom: 10px; left: 10px;">
+      <calcite-combobox
+        max-items="${number("max-items", 6)}"
+        placeholder="${text("placeholder", "placeholder")}"
+        label="${text("label (for screen readers)", "demo")}"
+        selection-mode="${select("selection-mode", ["multi", "single", "ancestors"], "multi")}"
+        scale="${select("scale", ["s", "m", "l"], "m")}"
+        ${boolean("disabled", false)}
+        ${boolean("allow-custom-values", false)}
+      >
+        <calcite-combobox-item value="Trees" text-label="Trees">
+          <calcite-combobox-item value="Pine" text-label="Pine"></calcite-combobox-item>
+          <calcite-combobox-item value="Sequoia" disabled text-label="Sequoia"></calcite-combobox-item>
+          <calcite-combobox-item value="Douglas Fir" text-label="Douglas Fir"></calcite-combobox-item>
+        </calcite-combobox-item>
+        <calcite-combobox-item value="Flowers" text-label="Flowers">
+          <calcite-combobox-item value="Daffodil" text-label="Daffodil"></calcite-combobox-item>
+          <calcite-combobox-item value="Black Eyed Susan" text-label="Black Eyed Susan"></calcite-combobox-item>
+          <calcite-combobox-item value="Nasturtium" text-label="Nasturtium"></calcite-combobox-item>
+        </calcite-combobox-item>
+        <calcite-combobox-item value="Animals" text-label="Animals">
+          <calcite-combobox-item value="Birds" text-label="Birds"></calcite-combobox-item>
+          <calcite-combobox-item value="Reptiles" text-label="Reptiles"></calcite-combobox-item>
+          <calcite-combobox-item value="Amphibians" text-label="Amphibians"></calcite-combobox-item>
+        </calcite-combobox-item>
+        <calcite-combobox-item value="Rocks" text-label="Rocks"></calcite-combobox-item>
+        <calcite-combobox-item value="Insects" text-label="Insects"></calcite-combobox-item>
+        <calcite-combobox-item value="Rivers" text-label="Rivers"></calcite-combobox-item>
+      </calcite-combobox>
+    </div>
+  `,
+  createSteps("calcite-combobox").snapshot("Default").click("calcite-combobox").snapshot("Open")
+);
+FlipPositioning.parameters = {
+  layout: "fullscreen"
+};
