030ce65e3310e353a9348b8629dd6aab1b5666c8
JC Franco <jfranco@esri.com>
2022-02-10 10:05:01 -0800

    update accordion roles/attributes

--- src/components/accordion-item/accordion-item.e2e.ts
+++ src/components/accordion-item/accordion-item.e2e.ts
@@ -2,5 +2,5 @@ import { accessible, renders } from "../../tests/commonTests";
 
 describe("calcite-accordion-item", () => {
   it("renders", async () => renders("calcite-accordion-item", { display: "flex" }));
-  it("is accessible", async () => accessible("calcite-accordion-item"));
+  it("is accessible", async () => accessible(`<calcite-accordion-item item-title="test"></calcite-accordion-item>`));
 });
