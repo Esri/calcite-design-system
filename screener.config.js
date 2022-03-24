19a8fb2ce0ba2b5e2a09c32238d35eb612d88893
Erik Harper <eharperdicianno@esri.com>
2022-03-24 14:32:27 -0700

    ci(screener): integrating sauce labs without build failures

--- screener.config.js
+++ screener.config.js
@@ -4,17 +4,24 @@ module.exports = {
   storybookStaticDir: "./__docs-temp__",
   apiKey: process.env.SCREENER_API_KEY,
   commit: process.env.COMMIT_SHA,
-  resolution: "1024x768",
   baseBranch: "master",
   browsers: [
     {
-      browserName: "chrome"
+      browserName: "chrome",
+      version: "97.0"
     }
   ],
   diffOptions: {
     minLayoutDimension: 1,
     minLayoutPosition: 1
   },
+  sauce: {
+    username: process.env.SAUCE_ACCESS_NAME,
+    accessKey: process.env.SAUCE_ACCESS_KEY,
+    maxConcurrent: 10
+  },
   excludeRules: [/^Overview/],
-  resolution: "1920x1440"
+  resolution: "1024x768",
+  failureExitCode: 0,
+  failOnNewStates: false
 };
