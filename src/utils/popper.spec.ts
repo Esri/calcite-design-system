128724c6a1a635d6bd991d886e947923c035f5d8
Matt Driscoll <mdriscoll@esri.com>
2022-04-01 13:33:28 -0700

    revert

--- src/utils/popper.spec.ts
+++ src/utils/popper.spec.ts
@@ -60,5 +60,7 @@ it("should correctly calculate hypotenuse", () => {
 });
 
 it("should filter computed placements", () => {
-  expect(filterComputedPlacements(popperPlacements, document.createElement("div"))).toEqual(popperComputedPlacements);
+  expect(new Set(filterComputedPlacements(popperPlacements, document.createElement("div")))).toEqual(
+    new Set(popperComputedPlacements)
+  );
 });
