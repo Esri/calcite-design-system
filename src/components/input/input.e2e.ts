d16dc3c9c6353de6d6d7f21fdff26514ad10c8b2
Ben Elan <benelan@users.noreply.github.com>
2022-04-12 20:25:03 -0700

    cleanup tests

--- src/components/input/input.e2e.ts
+++ src/components/input/input.e2e.ts
@@ -1075,18 +1075,6 @@ describe("calcite-input", () => {
       });
   });
 
-  it(`allows negative, decimal numbers for et locale`, async () => {
-    const value = "-1,5";
-    const page = await newE2EPage();
-    await page.setContent(html`<calcite-input locale="et" type="number"></calcite-input>`);
-    const element = await page.find("calcite-input");
-    await element.callMethod("setFocus");
-    await typeNumberValue(page, value);
-    await page.waitForChanges();
-    await page.keyboard.press("Tab");
-    expect(await element.getProperty("value")).toBe("-1.5");
-  });
-
   it(`allows negative, decimal numbers for ar locale`, async () => {
     const value = "-0001.0001";
     const page = await newE2EPage();
--- src/utils/locale.spec.ts
+++ src/utils/locale.spec.ts
@@ -1,6 +1,6 @@
 import { delocalizeNumberString, localizeNumberString, locales } from "./locale";
 
-const localesWithIssues = ["ar"];
+const localesWithIssues = ["ar"]; // arabic has different numeral characters
 
 describe("localizeNumberString and delocalizeNumberString", () => {
   locales
@@ -22,6 +22,13 @@ describe("localizeNumberString and delocalizeNumberString", () => {
 
       it(`floating point numbers localize and delocalize in "${locale}"`, () => {
         const numberString = "1.05";
+        const localizedNumberString = localizeNumberString(numberString, locale);
+        const delocalizedNumberString = delocalizeNumberString(localizedNumberString, locale);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
+
+      it(`exponential numbers localize and delocalize in "${locale}"`, () => {
+        const numberString = "2.5e-3";
         const localizedNumberString = localizeNumberString(numberString, locale);
         const delocalizedNumberString = delocalizeNumberString(localizedNumberString, locale);
         expect(delocalizedNumberString).toBe(numberString);
