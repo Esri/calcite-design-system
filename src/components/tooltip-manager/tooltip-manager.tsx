6e5d13f256b44be9b5367cae7309ae7be55c78d7
Matt Driscoll <mdriscoll@esri.com>
2022-04-20 08:56:25 -0700

    feat(tooltip): Simplify tooltip usage by deprecating tooltip-manager component. (#4422)
    * refactor(tooltip): Deprecate tooltip manager component.
    
    * cleanup
    
    * cleanup
    
    * cleanup

--- src/components/tooltip-manager/tooltip-manager.tsx
+++ src/components/tooltip-manager/tooltip-manager.tsx
@@ -1,9 +1,7 @@
-import { Component, h, Listen, Prop, VNode, Element } from "@stencil/core";
-import { TOOLTIP_REFERENCE, TOOLTIP_DELAY_MS } from "../tooltip/resources";
-import { queryElementRoots } from "../../utils/dom";
-
+import { Component, h, Prop, VNode } from "@stencil/core";
 /**
  * @slot - A slot for adding elements that reference a 'calcite-tooltip' by the 'selector' property.
+ * @deprecated No longer required for tooltip usage.
  */
 @Component({
   tag: "calcite-tooltip-manager",
@@ -11,20 +9,6 @@ import { queryElementRoots } from "../../utils/dom";
   shadow: true
 })
 export class TooltipManager {
-  // --------------------------------------------------------------------------
-  //
-  //  Variables
-  //
-  // --------------------------------------------------------------------------
-
-  @Element() el: HTMLCalciteTooltipManagerElement;
-
-  tooltipEl: HTMLCalciteTooltipElement;
-
-  hoverTimeouts: WeakMap<HTMLCalciteTooltipElement, number> = new WeakMap();
-
-  clickedTooltip: HTMLCalciteTooltipElement;
-
   // --------------------------------------------------------------------------
   //
   //  Properties
@@ -35,149 +19,7 @@ export class TooltipManager {
    * CSS Selector to match reference elements for tooltips. Reference elements will be identified by this selector in order to open their associated tooltip.
    * @default `[data-calcite-tooltip-reference]`
    */
-  @Prop() selector = `[${TOOLTIP_REFERENCE}]`;
-
-  // --------------------------------------------------------------------------
-  //
-  //  Private Methods
-  //
-  // --------------------------------------------------------------------------
-
-  queryTooltip = (composedPath: EventTarget[]): HTMLCalciteTooltipElement => {
-    const { el } = this;
-
-    if (!composedPath.includes(el)) {
-      return null;
-    }
-
-    const referenceElement = (composedPath as HTMLElement[]).find((pathEl) =>
-      pathEl?.hasAttribute?.(TOOLTIP_REFERENCE)
-    );
-
-    if (!referenceElement) {
-      return null;
-    }
-
-    const id = referenceElement.getAttribute(TOOLTIP_REFERENCE);
-
-    return queryElementRoots(el, { id }) as HTMLCalciteTooltipElement;
-  };
-
-  clearHoverTimeout = (tooltip: HTMLCalciteTooltipElement): void => {
-    const { hoverTimeouts } = this;
-
-    if (hoverTimeouts.has(tooltip)) {
-      window.clearTimeout(hoverTimeouts.get(tooltip));
-      hoverTimeouts.delete(tooltip);
-    }
-  };
-
-  closeExistingTooltip = (): void => {
-    const { tooltipEl } = this;
-
-    if (tooltipEl) {
-      this.toggleTooltip(tooltipEl, false);
-    }
-  };
-
-  focusTooltip = ({
-    tooltip,
-    value
-  }: {
-    tooltip: HTMLCalciteTooltipElement;
-    value: boolean;
-  }): void => {
-    this.closeExistingTooltip();
-
-    if (value) {
-      this.clearHoverTimeout(tooltip);
-    }
-
-    this.toggleTooltip(tooltip, value);
-  };
-
-  toggleTooltip = (tooltip: HTMLCalciteTooltipElement, value: boolean): void => {
-    tooltip.open = value;
-
-    if (value) {
-      this.tooltipEl = tooltip;
-    }
-  };
-
-  hoverToggle = ({
-    tooltip,
-    value
-  }: {
-    tooltip: HTMLCalciteTooltipElement;
-    value: boolean;
-  }): void => {
-    const { hoverTimeouts } = this;
-
-    hoverTimeouts.delete(tooltip);
-
-    if (value) {
-      this.closeExistingTooltip();
-    }
-
-    this.toggleTooltip(tooltip, value);
-  };
-
-  hoverTooltip = ({
-    tooltip,
-    value
-  }: {
-    tooltip: HTMLCalciteTooltipElement;
-    value: boolean;
-  }): void => {
-    this.clearHoverTimeout(tooltip);
-
-    const { hoverTimeouts } = this;
-
-    const timeoutId = window.setTimeout(
-      () => this.hoverToggle({ tooltip, value }),
-      TOOLTIP_DELAY_MS || 0
-    );
-
-    hoverTimeouts.set(tooltip, timeoutId);
-  };
-
-  activeTooltipHover = (event: MouseEvent): void => {
-    const { tooltipEl, hoverTimeouts } = this;
-    const { type } = event;
-
-    if (!tooltipEl) {
-      return;
-    }
-
-    if (type === "mouseover" && event.composedPath().includes(tooltipEl)) {
-      this.clearHoverTimeout(tooltipEl);
-    } else if (type === "mouseout" && !hoverTimeouts.has(tooltipEl)) {
-      this.hoverTooltip({ tooltip: tooltipEl, value: false });
-    }
-  };
-
-  hoverEvent = (event: MouseEvent, value: boolean): void => {
-    const tooltip = this.queryTooltip(event.composedPath());
-
-    this.activeTooltipHover(event);
-
-    if (!tooltip) {
-      return;
-    }
-
-    this.hoverTooltip({ tooltip, value });
-  };
-
-  focusEvent = (event: FocusEvent, value: boolean): void => {
-    const tooltip = this.queryTooltip(event.composedPath());
-
-    if (!tooltip || tooltip === this.clickedTooltip) {
-      this.clickedTooltip = null;
-      return;
-    }
-
-    this.focusTooltip({ tooltip, value });
-  };
+  @Prop() selector = "[data-calcite-tooltip-reference]";
 
   // --------------------------------------------------------------------------
   //
@@ -187,48 +29,5 @@ export class TooltipManager {
 
   render(): VNode {
     return <slot />;
-  }
-
-  //--------------------------------------------------------------------------
-  //
-  //  Event Listeners
-  //
-  //--------------------------------------------------------------------------
-
-  @Listen("keyup", { target: "document" })
-  keyUpHandler(event: KeyboardEvent): void {
-    if (event.key === "Escape") {
-      const { tooltipEl } = this;
-
-      if (tooltipEl) {
-        this.clearHoverTimeout(tooltipEl);
-        this.toggleTooltip(tooltipEl, false);
-      }
-    }
-  }
-
-  @Listen("mouseover", { capture: true })
-  mouseEnterShow(event: MouseEvent): void {
-    this.hoverEvent(event, true);
-  }
-
-  @Listen("mouseout", { capture: true })
-  mouseLeaveHide(event: MouseEvent): void {
-    this.hoverEvent(event, false);
-  }
-
-  @Listen("click", { capture: true })
-  clickHandler(event: MouseEvent): void {
-    this.clickedTooltip = this.queryTooltip(event.composedPath());
-  }
-
-  @Listen("focus", { capture: true })
-  focusShow(event: FocusEvent): void {
-    this.focusEvent(event, true);
-  }
-
-  @Listen("blur", { capture: true })
-  blurHide(event: FocusEvent): void {
-    this.focusEvent(event, false);
   }
 }
