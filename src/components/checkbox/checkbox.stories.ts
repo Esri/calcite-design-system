a8f894c5584dab0c586fffc1567ec8967a2fa145
anveshmekala <anv11827@esri.com>
2022-04-29 16:44:21 -0500

    rename misspelled component

--- src/components/checkbox/checkbox.stories.ts
+++ src/components/checkbox/checkbox.stories.ts
@@ -76,7 +76,7 @@ export const RTL = (): string => html`
 
 export const TestFocusRing = stepStory(
   (): string => html`${create("calcite-checkbox", createAttributes())}`,
-  createSteps("calcite-component").keys("calcite-checkbox", ["Tab", "Enter"])
+  createSteps("calcite-checkbox").keys("calcite-checkbox", ["Tab", "Enter"])
 );
 
 export const disabled = (): string => html`<calcite-checkbox checked disabled></calcite-checkbox>`;
