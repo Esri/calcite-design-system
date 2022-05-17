b44e4df95bd6156ac1845533d3ab743987643864
Matt Driscoll <mdriscoll@esri.com>
2022-05-17 16:50:16 -0700

    fix(stepper): Stepper methods should replace active content correctly. (#4567)

--- src/components/stepper/stepper.tsx
+++ src/components/stepper/stepper.tsx
@@ -152,7 +152,7 @@ export class Stepper {
       this.requestedContent = content;
     }
 
-    this.itemMap.set(item, position);
+    this.itemMap.set(item, { position, content });
     this.items = this.sortItems();
     this.enabledItems = this.filterItems();
     event.stopPropagation();
@@ -185,7 +185,7 @@ export class Stepper {
       return;
     }
 
-    this.emitChangedItem(enabledStepIndex);
+    this.updateStep(enabledStepIndex);
   }
 
   /** set the previous step as active */
@@ -197,7 +197,7 @@ export class Stepper {
       return;
     }
 
-    this.emitChangedItem(enabledStepIndex);
+    this.updateStep(enabledStepIndex);
   }
 
   /**
@@ -210,7 +210,7 @@ export class Stepper {
     const position = step - 1;
 
     if (this.currentPosition !== position) {
-      this.emitChangedItem(position);
+      this.updateStep(position);
     }
   }
 
@@ -223,7 +223,7 @@ export class Stepper {
       return;
     }
 
-    this.emitChangedItem(enabledStepIndex);
+    this.updateStep(enabledStepIndex);
   }
 
   /** set the last step as active */
@@ -235,7 +235,7 @@ export class Stepper {
       return;
     }
 
-    this.emitChangedItem(enabledStepIndex);
+    this.updateStep(enabledStepIndex);
   }
 
   //--------------------------------------------------------------------------
@@ -244,7 +244,7 @@ export class Stepper {
   //
   //--------------------------------------------------------------------------
 
-  private itemMap = new Map<HTMLCalciteStepperItemElement, number>();
+  private itemMap = new Map<HTMLCalciteStepperItemElement, { position: number; content: Node[] }>();
 
   /** list of sorted Stepper items */
   private items: HTMLCalciteStepperItemElement[] = [];
@@ -287,8 +287,16 @@ export class Stepper {
     this.el.insertAdjacentElement("beforeend", this.stepperContentContainer);
   }
 
-  private emitChangedItem(position: number): void {
+  private updateStep(position: number): void {
+    const { itemMap, items } = this;
+
     this.currentPosition = position;
+
+    const content = itemMap.get(items[position])?.content;
+
+    if (content) {
+      this.requestedContent = content;
+    }
 
     this.calciteInternalStepperItemChange.emit({
       position
@@ -329,7 +337,9 @@ export class Stepper {
   private sortItems(): HTMLCalciteStepperItemElement[] {
     const { itemMap } = this;
 
-    return Array.from(itemMap.keys()).sort((a, b) => itemMap.get(a) - itemMap.get(b));
+    return Array.from(itemMap.keys()).sort(
+      (a, b) => itemMap.get(a).position - itemMap.get(b).position
+    );
   }
 
   private filterItems(): HTMLCalciteStepperItemElement[] {
