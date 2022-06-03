0bb94ac160b44480c59a5fc6f5c78bc207a77b31
Matt Driscoll <mdriscoll@esri.com>
2022-06-03 10:35:50 -0700

    feat(tooltip): Add property to close tooltip when referenceElement is clicked. (#4399)

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -32,6 +32,9 @@ export class Tooltip {
   //
   // --------------------------------------------------------------------------
 
+  /** Closes the tooltip when the referenceElement is clicked */
+  @Prop() closeOnClick = false;
+
   /** Accessible name for the component */
   @Prop() label!: string;
 
