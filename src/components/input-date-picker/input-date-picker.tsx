f2be377e6ad3db8f6a9622f07bad23054bc96eb7
Matt Driscoll <mdriscoll@esri.com>
2022-07-19 15:43:18 -0700

    feat(input-date-picker): deprecate active property in favor of open

--- src/components/input-date-picker/input-date-picker.tsx
+++ src/components/input-date-picker/input-date-picker.tsx
@@ -87,7 +87,7 @@ export class InputDatePicker
   @Watch("readOnly")
   handleDisabledAndReadOnlyChange(value: boolean): void {
     if (!value) {
-      this.active = false;
+      this.open = false;
     }
   }
 
@@ -169,17 +169,29 @@ export class InputDatePicker
     }
   }
 
-  /** Expand or collapse when calendar does not have input */
+  /**
+   * Expand or collapse when calendar does not have input
+   *
+   * @deprecated use open instead
+   */
   @Prop({ mutable: true, reflect: true }) active = false;
 
   @Watch("active")
-  activeHandler(): void {
+  activeHandler(value: boolean): void {
+    this.open = value;
+  }
+
+  /** Expand or collapse when calendar does not have input */
+  @Prop({ mutable: true, reflect: true }) open = false;
+
+  @Watch("open")
+  openHandler(): void {
     if (!this.disabled || !this.readOnly) {
       this.reposition();
       return;
     }
 
-    this.active = false;
+    this.open = false;
   }
 
   /**
@@ -272,7 +284,7 @@ export class InputDatePicker
       return;
     }
 
-    this.active = false;
+    this.open = false;
   }
 
   //--------------------------------------------------------------------------
@@ -347,6 +359,7 @@ export class InputDatePicker
   // --------------------------------------------------------------------------
 
   connectedCallback(): void {
+    this.open = this.active;
     if (Array.isArray(this.value)) {
       this.valueAsDate = this.value.map((v) => dateFromISO(v));
       this.start = this.value[0];
@@ -417,11 +430,7 @@ export class InputDatePicker
         role="application"
       >
         {this.localeData && (
-          <div
-            aria-expanded={toAriaBoolean(this.active)}
-            class="input-container"
-            role="application"
-          >
+          <div aria-expanded={toAriaBoolean(this.open)} class="input-container" role="application">
             {
               <div class="input-wrapper" ref={this.setStartWrapper}>
                 <calcite-input
@@ -445,10 +454,10 @@ export class InputDatePicker
               </div>
             }
             <div
-              aria-hidden={toAriaBoolean(!this.active)}
+              aria-hidden={toAriaBoolean(!this.open)}
               class={{
                 [CSS.menu]: true,
-                [CSS.menuActive]: this.active
+                [CSS.menuActive]: this.open
               }}
               ref={this.setMenuEl}
             >
@@ -457,7 +466,7 @@ export class InputDatePicker
                   ["calendar-picker-wrapper"]: true,
                   ["calendar-picker-wrapper--end"]: this.focusedInput === "end",
                   [PopperCSS.animation]: true,
-                  [PopperCSS.animationActive]: this.active
+                  [PopperCSS.animationActive]: this.open
                 }}
                 onTransitionEnd={this.transitionEnd}
                 ref={this.setContainerEl}
@@ -615,13 +624,13 @@ export class InputDatePicker
 
   transitionStartHandler = (event: TransitionEvent): void => {
     if (event.propertyName === this.activeTransitionProp && event.target === this.containerEl) {
-      this.active ? this.onBeforeOpen() : this.onBeforeClose();
+      this.open ? this.onBeforeOpen() : this.onBeforeClose();
     }
   };
 
   transitionEnd = (event: TransitionEvent): void => {
     if (event.propertyName === this.activeTransitionProp && event.target === this.containerEl) {
-      this.active ? this.onOpen() : this.onClose();
+      this.open ? this.onOpen() : this.onClose();
     }
   };
 
@@ -634,7 +643,7 @@ export class InputDatePicker
   };
 
   deactivate = (): void => {
-    this.active = false;
+    this.open = false;
   };
 
   keyDownHandler = (event: KeyboardEvent): void => {
@@ -645,7 +654,7 @@ export class InputDatePicker
 
   keyUpHandler = (e: KeyboardEvent): void => {
     if (e.key === "Escape") {
-      this.active = false;
+      this.open = false;
     }
   };
 
@@ -655,14 +664,14 @@ export class InputDatePicker
 
   startInputFocus = (): void => {
     if (!this.readOnly) {
-      this.active = true;
+      this.open = true;
     }
     this.focusedInput = "start";
   };
 
   endInputFocus = (): void => {
     if (!this.readOnly) {
-      this.active = true;
+      this.open = true;
     }
     this.focusedInput = "end";
   };
@@ -700,7 +709,7 @@ export class InputDatePicker
 
     const eventListenerModifier: Partial<StrictModifiers> = {
       name: "eventListeners",
-      enabled: this.active
+      enabled: this.open
     };
 
     return [flipModifier, eventListenerModifier];
