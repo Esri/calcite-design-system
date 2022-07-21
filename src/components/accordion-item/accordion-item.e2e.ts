86dc89fdc5fd86ac648137accfa5ce6eb85ce995
Matt Driscoll <mdriscoll@esri.com>
2022-07-21 13:01:13 -0700

    docs(accordion, accordion-item): Update docs to use heading and description props. (#4982)
    * feat(accordion-item): add actions-end and actions-start slots. (#1786)
    
    * cleanup
    
    * tests
    
    * review fixes
    
    * revert html
    
    * review fixes
    
    * fix bug
    
    * update accordion doc
    
    * docs(accordion, accordion-item): Update docs to use heading and description props.

--- src/components/accordion-item/accordion-item.e2e.ts
+++ src/components/accordion-item/accordion-item.e2e.ts
@@ -3,6 +3,6 @@ import { SLOTS } from "./resources";
 
 describe("calcite-accordion-item", () => {
   it("renders", async () => renders("calcite-accordion-item", { display: "flex" }));
-  it("is accessible", async () => accessible(`<calcite-accordion-item item-title="test"></calcite-accordion-item>`));
+  it("is accessible", async () => accessible(`<calcite-accordion-item heading="My Heading"></calcite-accordion-item>`));
   it("has slots", () => slots("calcite-accordion-item", SLOTS));
 });
