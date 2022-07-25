17ae96279fa3f43f5bf74c0d4e378d4047bcc401
Matt Driscoll <mdriscoll@esri.com>
2022-07-25 11:14:47 -0700

    Revert "rename slots"
    This reverts commit b5679cef0aab7de183058dbc5386ff6c5bd3b0ab.

--- src/components/shell/shell.e2e.ts
+++ src/components/shell/shell.e2e.ts
@@ -34,10 +34,10 @@ describe("calcite-shell", () => {
   it("should be accessible", async () =>
     accessible(`
     <calcite-shell>
-      <calcite-shell-panel slot="${SLOTS.shellPanelStart}" position="start">
+      <calcite-shell-panel slot="${SLOTS.panelStart}" position="start">
         <p>Primary Content</p>
       </calcite-shell-panel>
-      <calcite-shell-panel slot="${SLOTS.shellPanelEnd}" position="end">
+      <calcite-shell-panel slot="${SLOTS.panelEnd}" position="end">
         <p>Primary Content</p>
       </calcite-shell-panel>
     </calcite-shell>
@@ -85,10 +85,10 @@ describe("calcite-shell", () => {
     const page = await newE2EPage();
 
     await page.setContent(`<calcite-shell content-behind>
-    <calcite-shell-panel slot="${SLOTS.shellPanelStart}" position="end">
+    <calcite-shell-panel slot="${SLOTS.panelStart}" position="end">
       <p>Primary Content</p>
     </calcite-shell-panel>
-    <calcite-shell-panel slot="${SLOTS.shellPanelEnd}" position="start">
+    <calcite-shell-panel slot="${SLOTS.panelEnd}" position="start">
       <p>Primary Content</p>
     </calcite-shell-panel>
   </calcite-shell>`);
@@ -104,10 +104,10 @@ describe("calcite-shell", () => {
     const page = await newE2EPage();
 
     await page.setContent(`<calcite-shell>
-    <calcite-shell-panel slot="${SLOTS.shellPanelStart}" position="end">
+    <calcite-shell-panel slot="${SLOTS.panelStart}" position="end">
       <p>Primary Content</p>
     </calcite-shell-panel>
-    <calcite-shell-panel slot="${SLOTS.shellPanelEnd}" position="start">
+    <calcite-shell-panel slot="${SLOTS.panelEnd}" position="start">
       <p>Primary Content</p>
     </calcite-shell-panel>
   </calcite-shell>`);
@@ -123,10 +123,10 @@ describe("calcite-shell", () => {
     const page = await newE2EPage();
 
     await page.setContent(`<calcite-shell>
-    <calcite-shell-panel slot="${SLOTS.shellPanelStart}" position="end">
+    <calcite-shell-panel slot="${SLOTS.panelStart}" position="end">
       <p>Primary Content</p>
     </calcite-shell-panel>
-    <calcite-shell-panel slot="${SLOTS.shellPanelEnd}" position="start">
+    <calcite-shell-panel slot="${SLOTS.panelEnd}" position="start">
       <p>Primary Content</p>
     </calcite-shell-panel>
     <p>Main content</p>
@@ -147,10 +147,10 @@ describe("calcite-shell", () => {
     const page = await newE2EPage();
 
     await page.setContent(`<calcite-shell content-behind>
-    <calcite-shell-panel slot="${SLOTS.shellPanelStart}" position="end">
+    <calcite-shell-panel slot="${SLOTS.panelStart}" position="end">
       <p>Primary Content</p>
     </calcite-shell-panel>
-    <calcite-shell-panel slot="${SLOTS.shellPanelEnd}" position="start">
+    <calcite-shell-panel slot="${SLOTS.panelEnd}" position="start">
       <p>Primary Content</p>
     </calcite-shell-panel>
     <p>Main content</p>
