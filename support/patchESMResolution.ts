4e96b1537aba6400187b4f210e63849e34fc95fd
Ben Elan <benelan@users.noreply.github.com>
2022-08-12 13:50:24 -0700

    cleanup

--- support/patchESMResolution.ts
+++ support/patchESMResolution.ts
@@ -1,20 +1,24 @@
 (async function () {
   const {
-    promises: { readFile, writeFile }
+    promises: { readFile, readdir, writeFile }
   } = await import("fs");
   const { dirname, normalize } = await import("path");
+  const { quote } = await import("shell-quote");
   const { fileURLToPath } = await import("url");
-
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
 
   // patch needed due to switching package.json's type to "module"
   // https://github.com/Esri/calcite-components/issues/5141
   try {
-    const filePath = normalize(`${__dirname}/../dist/components/index.js`);
     const importedModule = "@stencil/core/internal/client";
-    const contents = await readFile(filePath, { encoding: "utf8" });
-    await writeFile(filePath, contents.replace(importedModule, importedModule + ".js"));
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const componentsOutput = quote([normalize(`${__dirname}/../dist/components/`)]);
+    const files = await readdir(componentsOutput);
+    for (const file of files) {
+      const filePath = quote([normalize(`${componentsOutput}/${file}`)]);
+      const contents = await readFile(filePath, { encoding: "utf8" });
+      await writeFile(filePath, contents.replace(importedModule, importedModule + "/index.js"));
+    }
   } catch (err) {
     console.error(err);
     process.exit(1);
