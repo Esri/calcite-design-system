2320e1f2a40736dbaf31725e967502629ffe7f00
JC Franco <jfranco@esri.com>
2022-08-15 18:25:43 -0700

    rename type generating script and tidy up

--- support/generateT9nTypes.ts
+++ support/generateT9nTypes.ts
@@ -5,23 +5,17 @@ import pify from "pify";
 import { readFile, writeFile } from "fs";
 
 (async () => {
-  const rootBundleExtension = "en.json";
-  const rootBundlePattern = `src/components/**/${rootBundleExtension}`;
+  const rootBundleFile = "en.json";
+  const rootBundlePattern = `src/components/**/t9n/${rootBundleFile}`;
 
   const rootBundles = await globby([rootBundlePattern]);
 
-  console.log("starting generation of i18n string typings...", rootBundlePattern);
+  console.log("starting generation of t9n string typings...", rootBundlePattern);
 
   const paths = await Promise.all(
     rootBundles.map(async (bundle) => {
-      const typeName = `${bundle.split("/").pop().replace(rootBundleExtension, "")}-strings`;
-      let jsonContents = await pify(readFile)(bundle, { encoding: "utf-8" });
-
-      // workaround for https://github.com/quicktype/quicktype/issues/1479
-      const temp = JSON.parse(jsonContents);
-      const stopgapKey = "__stopgap__";
-      temp[stopgapKey] = 0;
-      jsonContents = JSON.stringify(temp);
+      const typeName = `${bundle.split("/").pop().replace(rootBundleFile, "")}-strings`;
+      const jsonContents = await pify(readFile)(bundle, { encoding: "utf-8" });
 
       const jsonInput = jsonInputForTargetLanguage("typescript");
       await jsonInput.addSource({ name: typeName, samples: [jsonContents] });
@@ -33,19 +27,15 @@ import { readFile, writeFile } from "fs";
         await quicktype({
           inputData,
           lang: "typescript",
+          inferMaps: false,
           rendererOptions: {
             "just-types": "true"
           }
         })
-      ).lines
-        .filter((line) => !line.includes(stopgapKey))
-        .join("\n")
-        // export types to avoid issues when typing the string bundles
-        // see https://github.com/microsoft/TypeScript/issues/15300
-        .replace(/interface (.+) \{/g, "type $1 = {");
+      ).lines.join("\n");
 
       /* Note: using `.d.ts` file extension will exclude it from the output build */
-      const declarationFile = bundle.replace(rootBundleExtension, "index.d.ts");
+      const declarationFile = bundle.replace(rootBundleFile, "index.d.ts");
 
       await pify(writeFile)(
         declarationFile,
@@ -53,12 +43,12 @@ import { readFile, writeFile } from "fs";
           filepath: declarationFile
         })
       );
-      const i18nPath = `${bundle.split("/t9n")[0]}/t9n`;
-      return i18nPath.replace(/\//g, "\\");
+      const t9nPath = `${bundle.split("/t9n")[0]}/t9n`;
+      return t9nPath.replace(/\//g, "\\");
     })
   );
 
-  console.log("finished generating i18n string typings");
+  console.log("finished generating t9n string typings");
   const manifestFileContents = paths.sort().join("\n");
   await pify(writeFile)("manifest.txt", manifestFileContents);
   console.log("finished writing manifest");
