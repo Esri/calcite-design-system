78d2987c3ce6021f0eab3f2a3608c40ac33d257b
anveshmekala <anv11827@esri.com>
2022-08-24 18:13:48 -0500

    sync en.json with en-US.json bundle

---
+++ support/syncEnT9nBundles.ts
@@ -0,0 +1,27 @@
+import { readFile, copyFile } from "fs";
+
+function syncEnT9nFiles() {
+  const t9nManifestPath = "./manifest.txt";
+  readFile(t9nManifestPath, async (err, data) => {
+    if (err) {
+      return (err.code = "ENOENT") ? console.error(`${t9nManifestPath} file not found`) : console.error(err);
+    }
+
+    const arr = data.toString().replace(/\r\n/g, "\n").split("\n");
+
+    for (const i of arr) {
+      const path = i.replaceAll("\\", "/");
+      const component = path.split("/")[2];
+      console.log(`syncronizing t9 en files for ${component}`);
+
+      copyFile(`${path}/en.json`, `${path}/en-US.json`, (err) => {
+        if (err) {
+          return console.log("err", err);
+        }
+        console.log(`created en-US.json file for ${component} `);
+      });
+    }
+  });
+}
+
+syncEnT9nFiles();
