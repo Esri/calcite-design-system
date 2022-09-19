2da17fffdcacb45742f5a20850ba13c20f0bb4e2
Eliza Khachatryan <eli97736@esri.com>
2022-09-19 09:08:43 -0700

    incorporate screener into darkThemeRTL_TestOnly instead of adding a dedicated story

--- src/components/progress/progress.stories.ts
+++ src/components/progress/progress.stories.ts
@@ -26,19 +26,8 @@ export const darkThemeRTL_TestOnly = (): string => html`
     class="calcite-theme-dark"
     type="${select("type", { determinate: "determinate", indeterminate: "indeterminate" }, "indeterminate")}"
     value="${number("value", 0.2, { range: true, min: 0, max: 1, step: 0.01 })}"
-    text="${text("text", "")}"
-  ></calcite-progress>
-`;
-
-darkThemeRTL_TestOnly.parameters = { themes: themesDarkDefault };
-
-export const darkThemeText = (): string => html`
-  <calcite-progress
-    class="calcite-theme-dark"
-    type="${select("type", { determinate: "determinate", indeterminate: "indeterminate" }, "determinate")}"
-    value="${number("value", 0.2, { range: true, min: 0, max: 1, step: 0.01 })}"
     text="${text("text", "% Complete (optional text)")}"
   ></calcite-progress>
 `;
 
-darkThemeText.parameters = { themes: themesDarkDefault };
+darkThemeRTL_TestOnly.parameters = { themes: themesDarkDefault };
