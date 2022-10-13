1a12a2ebae56e4c881a1746ee9ad5e1766986cc8
Ben Elan <benelan@users.noreply.github.com>
2022-10-13 16:14:48 -0700

    Revert "ci(prepReleaseCommit): skip git sanity checks for next releases"
    This reverts commit 7cc2d8b2ea8a75bbf85258f1309ab85d5581e075.

--- support/prepReleaseCommit.ts
+++ support/prepReleaseCommit.ts
@@ -22,21 +22,19 @@ import yargs from "yargs";
   const changelogPath = quote([normalize(`${__dirname}/../CHANGELOG.md`)]);
   const readmePath = quote([normalize(`${__dirname}/../readme.md`)]);
 
-  const { next } = yargs(process.argv.slice(2))
-    .options({ next: { type: "boolean", default: false } })
-    .parseSync();
-
   if (
-    !next && // next releases already have sanity checks in `isNextDeployable.ts`
-    ((await exec("git rev-parse --abbrev-ref HEAD")).stdout.trim() !== "master" ||
-      (await exec("git rev-parse master")).stdout.trim() !==
-        (await exec("git rev-parse origin/master")).stdout.trim() ||
-      (await exec("git status --porcelain=v1 2>/dev/null | wc -l")).stdout.trim() !== "0")
+    (await exec("git rev-parse --abbrev-ref HEAD")).stdout.trim() !== "master" ||
+    (await exec("git rev-parse master")).stdout.trim() !== (await exec("git rev-parse origin/master")).stdout.trim() ||
+    (await exec("git status --porcelain=v1 2>/dev/null | wc -l")).stdout.trim() !== "0"
   ) {
     throw new Error(
       "Make sure the master branch is checked out, in sync with origin, and that there are no uncommitted changes."
     );
   }
+
+  const { next } = yargs(process.argv.slice(2))
+    .options({ next: { type: "boolean", default: false } })
+    .parseSync();
 
   // deepen the history when fetching tags due to shallow clone
   await exec("git fetch --deepen=250 --tags");
