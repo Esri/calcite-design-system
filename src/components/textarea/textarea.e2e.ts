1f07b3a991cf525243b921cf6d459d79ef2a377d
anveshmekala <anv11827@esri.com>
2022-10-31 11:30:20 -0500

    fix e2e

--- src/components/textarea/textarea.e2e.ts
+++ src/components/textarea/textarea.e2e.ts
@@ -1,4 +1,5 @@
 import { newE2EPage } from "@stencil/core/testing";
+import { HYDRATED_ATTR } from "../../tests/commonTests";
 
 describe("calcite-textarea", () => {
   it("renders", async () => {
@@ -6,6 +7,6 @@ describe("calcite-textarea", () => {
     await page.setContent("<calcite-textarea></calcite-textarea>");
 
     const element = await page.find("calcite-textarea");
-    expect(element).toHaveClass("hydrated");
+    expect(element).toHaveAttribute(HYDRATED_ATTR);
   });
 });
