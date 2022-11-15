4a4d7767e7cc39cc1561432c74d99d0783d3997a
Matt Driscoll <mdriscoll@esri.com>
2022-11-15 11:16:24 -0800

    fix(combobox): Wrap and break text on long items (#5672)
    * fix(combobox): Wrap and break text on long items. (#5419)
    
    * update test

--- src/components/combobox/combobox.stories.ts
+++ src/components/combobox/combobox.stories.ts
@@ -145,6 +145,21 @@ export const nestedItems = (): string => html`
   </div>
 `;
 
+export const longItems_TestOnly = (): string => html`<style>
+    calcite-combobox {
+      width: 260px;
+    }
+    calcite-combobox-item {
+      width: 260px;
+    }
+  </style>
+  <calcite-combobox open>
+    <calcite-combobox-item text-label="Layers">
+      <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
+      <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item>
+    </calcite-combobox-item>
+  </calcite-combobox>`;
+
 export const disabled_TestOnly = (): string => html`<calcite-combobox disabled>
   <calcite-combobox-item value="Trees" text-label="Trees">
     <calcite-combobox-item value="Pine" text-label="Pine"></calcite-combobox-item>
