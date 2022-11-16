bdb0c2874645d068f90e52c7e0e13d53b9ad85ea
Ben Elan <belan@esri.com>
2022-11-16 10:57:43 -0800

    test: create locale/numberingSystem describe blocks

--- src/utils/locale.spec.ts
+++ src/utils/locale.spec.ts
@@ -35,93 +35,97 @@ describe("NumberStringFormat", () => {
     expect(numberStringFormatter.numberFormatOptions.locale).toBe(defaultLocale);
   });
 
-  locales.forEach((locale) => {
-    it(`locale: integers localize and delocalize in "${locale}"`, () => {
-      const numberString = "555";
-      numberStringFormatter.numberFormatOptions = {
-        locale,
-        numberingSystem: "latn",
-        useGrouping: false
-      };
-      const localizedNumberString = numberStringFormatter.localize(numberString);
-      const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
-      expect(delocalizedNumberString).toBe(numberString);
-    });
+  describe("locales", () => {
+    locales.forEach((locale) => {
+      it(`integers localize and delocalize in "${locale}"`, () => {
+        const numberString = "555";
+        numberStringFormatter.numberFormatOptions = {
+          locale,
+          numberingSystem: "latn",
+          useGrouping: false
+        };
+        const localizedNumberString = numberStringFormatter.localize(numberString);
+        const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
 
-    it(`locale: negative numbers localize and delocalize in "${locale}"`, () => {
-      const numberString = "-123";
-      numberStringFormatter.numberFormatOptions = {
-        locale,
-        numberingSystem: "latn",
-        useGrouping: false
-      };
-      const localizedNumberString = numberStringFormatter.localize(numberString);
-      const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
-      expect(delocalizedNumberString).toBe(numberString);
-    });
+      it(`negative numbers localize and delocalize in "${locale}"`, () => {
+        const numberString = "-123";
+        numberStringFormatter.numberFormatOptions = {
+          locale,
+          numberingSystem: "latn",
+          useGrouping: false
+        };
+        const localizedNumberString = numberStringFormatter.localize(numberString);
+        const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
 
-    it(`locale: floating point numbers localize and delocalize in "${locale}"`, () => {
-      const numberString = "4.321";
-      numberStringFormatter.numberFormatOptions = {
-        locale,
-        numberingSystem: "latn",
-        useGrouping: false
-      };
-      const localizedNumberString = numberStringFormatter.localize(numberString);
-      const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
-      expect(delocalizedNumberString).toBe(numberString);
-    });
+      it(`locale: floating point numbers localize and delocalize in "${locale}"`, () => {
+        const numberString = "4.321";
+        numberStringFormatter.numberFormatOptions = {
+          locale,
+          numberingSystem: "latn",
+          useGrouping: false
+        };
+        const localizedNumberString = numberStringFormatter.localize(numberString);
+        const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
 
-    it(`locale: exponential numbers localize and delocalize in "${locale}"`, () => {
-      const numberString = "2.5e-3";
-      numberStringFormatter.numberFormatOptions = {
-        locale,
-        numberingSystem: "latn",
-        useGrouping: false
-      };
-      const localizedNumberString = numberStringFormatter.localize(numberString);
-      const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
-      expect(delocalizedNumberString).toBe(numberString);
-    });
+      it(`exponential numbers localize and delocalize in "${locale}"`, () => {
+        const numberString = "2.5e-3";
+        numberStringFormatter.numberFormatOptions = {
+          locale,
+          numberingSystem: "latn",
+          useGrouping: false
+        };
+        const localizedNumberString = numberStringFormatter.localize(numberString);
+        const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
 
-    it(`locale: numbers with group separators localize and delocalize in "${locale}"`, () => {
-      const numberString = "1234567890";
-      numberStringFormatter.numberFormatOptions = {
-        locale,
-        // the group separator is different in arabic depending on the numberingSystem
-        numberingSystem: locale === "ar" ? "arab" : "latn",
-        useGrouping: true
-      };
-      const localizedNumberString = numberStringFormatter.localize(numberString);
-      const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
-      expect(delocalizedNumberString).toBe(numberString);
-    });
+      it(`numbers with group separators localize and delocalize in "${locale}"`, () => {
+        const numberString = "1234567890";
+        numberStringFormatter.numberFormatOptions = {
+          locale,
+          // the group separator is different in arabic depending on the numberingSystem
+          numberingSystem: locale === "ar" ? "arab" : "latn",
+          useGrouping: true
+        };
+        const localizedNumberString = numberStringFormatter.localize(numberString);
+        const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
 
-    it(`locale: floating point numbers with group separators localize and delocalize in "${locale}"`, () => {
-      const numberString = "12345678.0123456789";
-      numberStringFormatter.numberFormatOptions = {
-        locale,
-        // the group separator is different in arabic depending on the numberingSystem
-        numberingSystem: locale === "ar" ? "arab" : "latn",
-        useGrouping: true
-      };
-      const localizedNumberString = numberStringFormatter.localize(numberString);
-      const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
-      expect(delocalizedNumberString).toBe(numberString);
+      it(`floating point numbers with group separators localize and delocalize in "${locale}"`, () => {
+        const numberString = "12345678.0123456789";
+        numberStringFormatter.numberFormatOptions = {
+          locale,
+          // the group separator is different in arabic depending on the numberingSystem
+          numberingSystem: locale === "ar" ? "arab" : "latn",
+          useGrouping: true
+        };
+        const localizedNumberString = numberStringFormatter.localize(numberString);
+        const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
     });
   });
 
-  numberingSystems.forEach((numberingSystem) => {
-    const numberString = "0.0123456789";
-    it(`numberingSystem: floating point numbers with group separators localize and delocalize in "${numberingSystem}"`, () => {
-      numberStringFormatter.numberFormatOptions = {
-        locale: numberingSystem === "arab" ? "ar" : "en",
-        numberingSystem,
-        useGrouping: true
-      };
-      const localizedNumberString = numberStringFormatter.localize(numberString);
-      const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
-      expect(delocalizedNumberString).toBe(numberString);
+  describe("numberingSystems", () => {
+    numberingSystems.forEach((numberingSystem) => {
+      const numberString = "0.0123456789";
+      it(`floating point numbers with group separators localize and delocalize in "${numberingSystem}"`, () => {
+        numberStringFormatter.numberFormatOptions = {
+          locale: numberingSystem === "arab" ? "ar" : "en",
+          numberingSystem,
+          useGrouping: true
+        };
+        const localizedNumberString = numberStringFormatter.localize(numberString);
+        const delocalizedNumberString = numberStringFormatter.delocalize(localizedNumberString);
+        expect(delocalizedNumberString).toBe(numberString);
+      });
     });
   });
 });
