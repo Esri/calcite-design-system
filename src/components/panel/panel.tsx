38ca639010b8bd1d1fe32c9cf9b54dfc38cf9877
Matt Driscoll <mdriscoll@esri.com>
2022-11-16 09:58:34 -0800

    fix(block, date-picker, list-item-group, panel, pick-list-group, popover, tip, tip-manager)!: Set default internal heading to a div. (#5728)
    * fix: Set default heading to be a div to accommodate more audiences #5099
    
    Applies to block, date-picker, list-item-group, panel, pick-list-group, popover, tip, and tip-manager
    
    * revert change to file

--- src/components/panel/panel.tsx
+++ src/components/panel/panel.tsx
@@ -11,7 +11,7 @@ import {
   Fragment,
   State
 } from "@stencil/core";
-import { CSS, HEADING_LEVEL, ICONS, SLOTS, TEXT } from "./resources";
+import { CSS, ICONS, SLOTS, TEXT } from "./resources";
 import { getElementDir, toAriaBoolean } from "../../utils/dom";
 import { Scale } from "../interfaces";
 import { HeadingLevel, Heading } from "../functional/Heading";
@@ -457,7 +457,7 @@ export class Panel implements InteractiveComponent {
   renderHeaderContent(): VNode {
     const { heading, headingLevel, summary, description, hasHeaderContent } = this;
     const headingNode = heading ? (
-      <Heading class={CSS.heading} level={headingLevel || HEADING_LEVEL}>
+      <Heading class={CSS.heading} level={headingLevel}>
         {heading}
       </Heading>
     ) : null;
