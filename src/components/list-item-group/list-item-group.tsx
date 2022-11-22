f489c57095ec21df1f427176d2d635675eea95d3
Matt Driscoll <mdriscoll@esri.com>
2022-11-21 16:01:01 -0800

    feat(list, list-item, list-item-group)!: Adds support for selecting and filtering list items. Improves accessibility by using aria "treegrid" role. (#4527)
    BREAKING CHANGE:  To know when `calcite-list-item` content is selected, listen to the event `calciteListItemSelect` instead of `click`.
    
    - `List`
      - Adds `label` property to specify an accessible name for the component.
      - Adds `loading` property to show a busy indicator.
      - Adds `selectionMode` and `selectionAppearance` properties to handle configuration of selection.
      - Adds `filterEnabled`, `filteredData`, `filteredItems`, `filterText`, and `filterPlaceholder` properties to support filtering.
      - Adds `calciteListFilter` event to notify when a filter has changed.
      - Deprecates `headingLevel` property.
    -  `ListItemGroup`
        - Adds `disabled` property to prevent user interaction.
        - Deprecates `headingLevel` property.
    - `ListItem`
      - Adds `calciteListItemSelect` event to notify when list item content is selected.
      - Adds `selected` and `value` properties to handle selection.
      - Adds `open` property to show child components.
      - Deprecates `nonInteractive` property.

--- src/components/list-item-group/list-item-group.tsx
+++ src/components/list-item-group/list-item-group.tsx
@@ -1,7 +1,9 @@
-import { Component, Element, Prop, h, VNode, Host } from "@stencil/core";
+import { Component, Prop, h, VNode, Host, Element, State } from "@stencil/core";
+import { InteractiveComponent, updateHostInteraction } from "../../utils/interactive";
 import { CSS } from "./resources";
-import { HeadingLevel, Heading, constrainHeadingLevel } from "../functional/Heading";
-
+import { MAX_COLUMNS } from "../list-item/resources";
+import { HeadingLevel } from "../functional/Heading";
+import { getDepth } from "../list-item/utils";
 /**
  * @slot - A slot for adding `calcite-list-item` and `calcite-list-item-group` elements.
  */
@@ -10,12 +12,17 @@ import { HeadingLevel, Heading, constrainHeadingLevel } from "../functional/Head
   styleUrl: "list-item-group.scss",
   shadow: true
 })
-export class ListItemGroup {
+export class ListItemGroup implements InteractiveComponent {
   // --------------------------------------------------------------------------
   //
   //  Properties
   //
   // --------------------------------------------------------------------------
+
+  /**
+   * When true, interaction is prevented and the component is displayed with lower opacity.
+   */
+  @Prop({ reflect: true }) disabled = false;
 
   /**
    * The header text for all nested `calcite-list-item` rows.
@@ -25,8 +32,25 @@ export class ListItemGroup {
 
   /**
    * Specifies the number at which section headings should start.
+   *
+   * @deprecated no longer necessary.
    */
   @Prop({ reflect: true }) headingLevel: HeadingLevel;
+
+  // --------------------------------------------------------------------------
+  //
+  //  Lifecycle
+  //
+  // --------------------------------------------------------------------------
+
+  connectedCallback(): void {
+    const { el } = this;
+    this.visualLevel = getDepth(el, true);
+  }
+
+  componentDidRender(): void {
+    updateHostInteraction(this);
+  }
 
   // --------------------------------------------------------------------------
   //
@@ -36,6 +60,8 @@ export class ListItemGroup {
 
   @Element() el: HTMLCalciteListItemGroupElement;
 
+  @State() visualLevel: number = null;
+
   // --------------------------------------------------------------------------
   //
   //  Render Methods
@@ -43,24 +69,18 @@ export class ListItemGroup {
   // --------------------------------------------------------------------------
 
   render(): VNode {
-    const { el, heading, headingLevel } = this;
-
-    const parentLevel = el.closest<HTMLCalciteListElement | HTMLCalciteListItemGroupElement>(
-      "calcite-list, calcite-list-item-group"
-    )?.headingLevel;
-    const relativeLevel = parentLevel ? constrainHeadingLevel(parentLevel + 1) : null;
-    const level = headingLevel || relativeLevel;
-
+    const { heading, visualLevel } = this;
     return (
       <Host>
-        {heading ? (
-          <Heading class={CSS.heading} level={level}>
+        <tr
+          class={CSS.container}
+          style={{ "--calcite-list-item-spacing-indent-multiplier": `${visualLevel}` }}
+        >
+          <td class={CSS.heading} colSpan={MAX_COLUMNS}>
             {heading}
-          </Heading>
-        ) : null}
-        <div class={CSS.container} role="group">
-          <slot />
-        </div>
+          </td>
+        </tr>
+        <slot />
       </Host>
     );
   }
