ea93555c3e9a78b1ff3efb2865e1821a4d340f6d
Erik Harper <eharperdicianno@esri.com>
2022-11-23 10:51:10 -0800

    fix(input-date-picker): display updated valueAsDate in the two range inputs (#5758)
    **Related Issue:** #5207 
    
    ## Summary
    
    This PR fixes an issue where the input boxes are completely empty when
    the `valueAsDate` property is directly changed with javascript when
    using the `range` prop.
    
    <!--
    
    Please make sure the PR title and/or commit message adheres to the
    https://www.conventionalcommits.org/en/v1.0.0/ specification.
    
    Note: If your PR only has one commit and it is NOT semantic, you will
    need to either
    
    a. add another commit and wait for the check to update
    b. proceed to squash merge, but make sure the commit message is the same
    as the title.
    
    This is because of the way GitHub handles single-commit squash merges
    (see https://github.com/zeke/semantic-pull-requests/issues/17)
    
    If this is component-related, please verify that:
    
    - [ ] feature or fix has a corresponding test
    - [ ] changes have been tested with demo page in Edge
    
    ---
    
    If this is skipping an unstable test:
    
    - include info about the test failure
    - submit an unstable-test issue by
    [choosing](https://github.com/Esri/calcite-components/issues/new/choose)
    the unstable test template and filling it out
    
    -->
    
    Co-authored-by: Erik Harper <eriklharper@pm.me>

--- src/components/input-date-picker/input-date-picker.e2e.ts
+++ src/components/input-date-picker/input-date-picker.e2e.ts
@@ -414,6 +414,68 @@ describe("calcite-input-date-picker", () => {
     });
   });
 
+  it("should update this.value and input value when valueAsDate is set", async () => {
+    const page = await newE2EPage();
+    await page.setContent(html` <calcite-input-date-picker></calcite-input-date-picker>`);
+
+    const inputDatePickerEl = await page.find("calcite-input-date-picker");
+
+    await page.$eval("calcite-input-date-picker", (element: any) => {
+      element.valueAsDate = new Date("2022-10-1");
+    });
+
+    const expectedValue = "2022-10-01";
+    const expectedInputValue = "10/1/2022";
+
+    expect(await inputDatePickerEl.getProperty("value")).toEqual(expectedValue);
+
+    const inputValue = await page.evaluate(() => {
+      const inputDatePicker = document.querySelector("calcite-input-date-picker");
+      const calciteInput = inputDatePicker.shadowRoot.querySelector("calcite-input");
+      const input = calciteInput.shadowRoot.querySelector("input");
+      return input.value;
+    });
+
+    expect(inputValue).toEqual(expectedInputValue);
+  });
+
+  it("should update this.value and both input values when valueAsDate is set for range", async () => {
+    const page = await newE2EPage();
+    await page.setContent(html` <calcite-input-date-picker range></calcite-input-date-picker>`);
+
+    const inputDatePickerEl = await page.find("calcite-input-date-picker");
+
+    await page.$eval("calcite-input-date-picker", (element: any) => {
+      element.valueAsDate = [new Date("2022-10-1"), new Date("2022-10-2")];
+    });
+
+    const expectedStartDateValue = "2022-10-01";
+    const expectedEndDateValue = "2022-10-02";
+    const expectedValue = [expectedStartDateValue, expectedEndDateValue];
+
+    expect(await inputDatePickerEl.getProperty("value")).toEqual(expectedValue);
+
+    const expectedStartDateInputValue = "10/1/2022";
+    const expectedEndDateInputValue = "10/2/2022";
+
+    const startDateInputValue = await page.evaluate(() => {
+      const inputDatePicker = document.querySelector("calcite-input-date-picker");
+      const calciteInput = inputDatePicker.shadowRoot.querySelector("calcite-input");
+      const input = calciteInput.shadowRoot.querySelector("input");
+      return input.value;
+    });
+
+    const endDateInputValue = await page.evaluate(() => {
+      const inputDatePicker = document.querySelector("calcite-input-date-picker");
+      const calciteInputs = inputDatePicker.shadowRoot.querySelectorAll("calcite-input");
+      const input = calciteInputs[1].shadowRoot.querySelector("input");
+      return input.value;
+    });
+
+    expect(startDateInputValue).toEqual(expectedStartDateInputValue);
+    expect(endDateInputValue).toEqual(expectedEndDateInputValue);
+  });
+
   it("should return endDate time as 23:59:999 when valueAsDate property is parsed", async () => {
     const page = await newE2EPage();
     await page.setContent(html` <calcite-input-date-picker layout="horizontal" range />`);
