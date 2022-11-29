005246bf0b62ec0e386515a8247ee57cbba94275
Matt Driscoll <mdriscoll@esri.com>
2022-11-28 17:56:38 -0800

    refactor(tip-manager)!: Remove deprecated `calciteTipManagerClose` event. #5798

--- src/components/tip-manager/tip-manager.e2e.ts
+++ src/components/tip-manager/tip-manager.e2e.ts
@@ -65,7 +65,6 @@ describe("calcite-tip-manager", () => {
       let isVisible = await container.isVisible();
       expect(isVisible).toBe(true);
 
-      const toggleEventSpy = await page.spyOnEvent("calciteTipManagerToggle", "window");
       const closeEventSpy = await page.spyOnEvent("calciteTipManagerClose", "window");
 
       const closeButton = await page.find(`calcite-tip-manager >>> .${CSS.close}`);
