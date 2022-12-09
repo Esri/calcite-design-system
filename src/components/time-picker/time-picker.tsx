a288c265cefe4b399efe4b991e8fff23271d1d74
Erik Harper <eriklharper@pm.me>
2022-12-08 16:29:47 -0800

    refactor(time-picker)!: Remove deprecated locale property

--- src/components/time-picker/time-picker.tsx
+++ src/components/time-picker/time-picker.tsx
@@ -154,20 +154,6 @@ export class TimePicker implements LocalizedComponent, LoadableComponent {
    */
   @Prop() intlSecondUp = TEXT.secondUp;
 
-  /**
-   * BCP 47 language tag for desired language and country format.
-   *
-   * @internal
-   * @deprecated set the global `lang` attribute on the element instead.
-   * @mdn [lang](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/lang)
-   */
-  @Prop({ mutable: true }) locale: string;
-
-  @Watch("locale")
-  localeChanged(): void {
-    updateEffectiveLocale(this);
-  }
-
   /** Specifies the size of the component. */
   @Prop({ reflect: true }) scale: Scale = "m";
 
