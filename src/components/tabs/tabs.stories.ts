1c72d8209147feac9e2e886e8afebd5b9da20c25
Ben Elan <belan@esri.com>
2022-12-13 13:24:43 -0800

    ci(screener): remove (#6008)
    **Related Issue:** #4825
    
    ## Summary
    Completely remove screener

--- src/components/tabs/tabs.stories.ts
+++ src/components/tabs/tabs.stories.ts
@@ -1,5 +1,5 @@
 import { select, optionsKnob } from "@storybook/addon-knobs";
-import { createSteps, iconNames, stepStory, storyFilters } from "../../../.storybook/helpers";
+import { iconNames, storyFilters } from "../../../.storybook/helpers";
 import { themesDarkDefault } from "../../../.storybook/utils";
 import { placeholderImage } from "../../../.storybook/placeholderImage";
 import readme1 from "./readme.md";
@@ -15,36 +15,6 @@ export default {
   },
   ...storyFilters()
 };
-
-export const simple = stepStory(
-  (): string => html`
-    <calcite-tabs
-      layout="${select("layout", ["inline", "center"], "inline")}"
-      position="${select("position", ["top", "bottom"], "top")}"
-      scale="${select("scale", ["s", "m", "l"], "m")}"
-    >
-      <calcite-tab-nav slot="tab-nav">
-        <calcite-tab-title selected>Tab 1 Title</calcite-tab-title>
-        <calcite-tab-title id="reference-element">Tab 2 Title</calcite-tab-title>
-        <calcite-tab-title disabled>Disabled Tab</calcite-tab-title>
-        <calcite-tab-title>Tab 4 Title</calcite-tab-title>
-      </calcite-tab-nav>
-
-      <calcite-tab selected>
-        <p>Tab 1 Content</p><br />
-      </calcite-tab>
-      <calcite-tab><p>Tab 2 Content</p>
-      <img src="${placeholderImage({
-        width: 1000,
-        height: 200
-      })}"></img>
-      </calcite-tab>
-      <calcite-tab><p>Tab 3 Content</p></calcite-tab>
-      <calcite-tab><p>Tab 4 Content</p></calcite-tab>
-    </calcite-tabs>
-  `,
-  createSteps("calcite-tabs").snapshot("simple").click("#reference-element").snapshot("horizontal scroll")
-);
 
 export const simpleDarkThemeRTL_TestOnly = (): string => html`
   <calcite-tabs
@@ -213,47 +183,6 @@ export const disabledTabs_TestOnly = (): string => {
       </calcite-tabs>
     `;
 };
-
-export const layoutCenterAndBorderedCenter_TestOnly = stepStory(
-  (): string => html`<calcite-tabs layout="center">
-    <calcite-tab-nav slot="tab-nav">
-      <calcite-tab-title>Tab 1 Title</calcite-tab-title>
-      <calcite-tab-title>Tab 2 Title</calcite-tab-title>
-      <calcite-tab-title>Tab 3 Title</calcite-tab-title>
-      <calcite-tab-title>Tab 4 Title</calcite-tab-title>
-    </calcite-tab-nav>
-  </calcite-tabs>`,
-  createSteps("calcite-tabs")
-    .snapshot("Center Layout.")
-    .executeScript(`document.querySelector("calcite-tabs").bordered = "true"`)
-    .snapshot("Bordered state when layout is center.")
-);
-
-export const WithIconStart_TestOnly = stepStory(
-  (): string => html`
-    <calcite-tabs>
-      <calcite-tab-nav slot="tab-nav">
-        <calcite-tab-title selected id="tab-title">Boats</calcite-tab-title>
-        <calcite-tab-title>Ships</calcite-tab-title>
-        <calcite-tab-title>Yachts</calcite-tab-title>
-      </calcite-tab-nav>
-      <calcite-tab selected><calcite-button id="btn">Add indicator</calcite-button></calcite-tab>
-      <calcite-tab>Tab 2 content</calcite-tab>
-      <calcite-tab>Tab 3 content</calcite-tab>
-    </calcite-tabs>
-  `,
-  createSteps("calcite-tabs")
-    .executeScript(
-      `const tabtitle = document.getElementById("tab-title");
-      const button = document.getElementById("btn");
-      button.addEventListener("click", () => {
-      tabtitle.iconStart = "car";
-      });
-      `
-    )
-    .click("calcite-button")
-    .snapshot("WithIconStart")
-);
 
 export const TabChilrenWithPercentageHeights = (): string => html`
   <calcite-tabs style="height: 250px;">
