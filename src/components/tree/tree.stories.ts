688a0b589666349ba87e5a1a8e300cfbf5b20332
Eliza Khachatryan <eli97736@esri.com>
2022-12-12 17:00:23 -0800

    WIP: stories to demo use case actionsEndSlottedDropdown

--- src/components/tree/tree.stories.ts
+++ src/components/tree/tree.stories.ts
@@ -41,6 +41,20 @@ const treeItems = `
   </calcite-tree-item>
 `;
 
+const slottedDropdown = `
+<calcite-dropdown slot="actions-end">
+  <calcite-action slot='dropdown-trigger' icon='ellipsis'></calcite-action>
+  <calcite-dropdown-group group-title="Settings" selection-mode="multi">
+    <calcite-dropdown-item>Group elements</calcite-dropdown-item>
+  </calcite-dropdown-group>
+  <calcite-dropdown-group group-title="Display mode" selection-mode="single">
+    <calcite-dropdown-item selected>Row</calcite-dropdown-item>
+    <calcite-dropdown-item>Column</calcite-dropdown-item>
+    <calcite-dropdown-item>Tabs</calcite-dropdown-item>
+  </calcite-dropdown-group>
+</calcite-dropdown>
+`;
+
 export default {
   title: "Components/Tree",
   parameters: {
@@ -61,11 +75,48 @@ export const simple = (): string => html`
   </calcite-tree>
 `;
 
-export const selectionModeNone = (): string => html`<calcite-tree
+export const actionsEndSlottedDropdown = (): string => html`<calcite-tree
   ${boolean("lines", false)}
   selection-mode="${select("selection-mode", selectionModes, "none")}"
 >
-  ${treeItems}
+  <calcite-tree-item>
+    <a>Child 1</a>
+    ${slottedDropdown}
+  </calcite-tree-item>
+  <calcite-tree-item>
+    <a>Child 2</a>
+    <calcite-tree slot="children">
+      <calcite-tree-item>
+        <a>Grandchild 1</a>
+        ${slottedDropdown}
+      </calcite-tree-item>
+      <calcite-tree-item>
+        <a>Grandchild 2</a>
+        <calcite-tree slot="children">
+          <calcite-tree-item>
+            <a>Great-Grandchild 1</a>
+            ${slottedDropdown}
+          </calcite-tree-item>
+          <calcite-tree-item>
+            <a>Great-Grandchild 2</a>
+          </calcite-tree-item>
+        </calcite-tree>
+      </calcite-tree-item>
+    </calcite-tree>
+  </calcite-tree-item>
+  <calcite-tree-item>
+    <a>Child 3</a>
+    ${slottedDropdown}
+    <calcite-tree slot="children">
+      <calcite-tree-item>
+        <a>Grandchild 1</a>
+      </calcite-tree-item>
+      <calcite-tree-item>
+        <a>Grandchild 2</a>
+        ${slottedDropdown}
+      </calcite-tree-item>
+    </calcite-tree>
+  </calcite-tree-item>
 </calcite-tree>`;
 
 export const darkThemeRTL_TestOnly = (): string => html`
