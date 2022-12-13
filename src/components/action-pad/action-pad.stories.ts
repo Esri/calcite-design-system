c98c1b19cd9852fa156ed0062e1c0462b772dbdd
anveshmekala <anv11827@esri.com>
2022-12-13 13:11:31 -0600

    refactor action-pad stories

--- src/components/action-pad/action-pad.stories.ts
+++ src/components/action-pad/action-pad.stories.ts
@@ -127,43 +127,25 @@ withTooltip_NoTest.parameters = {
 };
 
 export const hebrewLocale_TestOnly = (): string => html`<div style="width:400px">
-  <calcite-action-pad expanded lang="he" layout="horizontal">
-    <calcite-action text="Information" icon="information"></calcite-action>
-    <calcite-action text="Feedback" slot="bottom-actions" icon="mega-phone"></calcite-action>
-  </calcite-action-pad>
+  <calcite-action-pad expanded lang="he" layout="horizontal"> </calcite-action-pad>
 </div>`;
 
 export const norwegianLocale_TestOnly = (): string => html`<div style="width:400px">
-  <calcite-action-pad expanded lang="nb" layout="horizontal">
-    <calcite-action text="Information" icon="information"></calcite-action>
-    <calcite-action text="Feedback" slot="bottom-actions" icon="mega-phone"></calcite-action>
-  </calcite-action-pad>
+  <calcite-action-pad expanded lang="nb" layout="horizontal"> </calcite-action-pad>
 </div>`;
 
 export const spanishLocale_TestOnly = (): string => html`<div style="width:400px">
-  <calcite-action-pad expanded lang="es" layout="horizontal">
-    <calcite-action text="Information" icon="information"></calcite-action>
-    <calcite-action text="Feedback" slot="bottom-actions" icon="mega-phone"></calcite-action>
-  </calcite-action-pad>
+  <calcite-action-pad expanded lang="es" layout="horizontal"> </calcite-action-pad>
 </div>`;
 
 export const taiwanLocale_TestOnly = (): string => html`<div style="width:400px">
-  <calcite-action-pad expanded lang="zh-TW" layout="horizontal">
-    <calcite-action text="Information" icon="information"></calcite-action>
-    <calcite-action text="Feedback" slot="bottom-actions" icon="mega-phone"></calcite-action>
-  </calcite-action-pad>
+  <calcite-action-pad expanded lang="zh-TW" layout="horizontal"> </calcite-action-pad>
 </div>`;
 
 export const russianLocale_TestOnly = (): string => html`<div style="width:400px">
-  <calcite-action-pad expanded lang="ru" layout="horizontal">
-    <calcite-action text="Information" icon="information"></calcite-action>
-    <calcite-action text="Feedback" slot="bottom-actions" icon="mega-phone"></calcite-action>
-  </calcite-action-pad>
+  <calcite-action-pad expanded lang="ru" layout="horizontal"> </calcite-action-pad>
 </div>`;
 
 export const romanianMoldovaLocale_TestOnly = (): string => html`<div style="width:400px">
-  <calcite-action-pad expanded lang="ro-mo" layout="horizontal">
-    <calcite-action text="Information" icon="information"></calcite-action>
-    <calcite-action text="Feedback" slot="bottom-actions" icon="mega-phone"></calcite-action>
-  </calcite-action-pad>
+  <calcite-action-pad expanded lang="ro-mo" layout="horizontal"> </calcite-action-pad>
 </div>`;
