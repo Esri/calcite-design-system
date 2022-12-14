d454f590b482b0de420d11bb8074191fe1aa0f87
Matt Driscoll <mdriscoll@esri.com>
2022-12-14 12:50:10 -0800

    refactor(popover, dropdown, input-time-picker, modal, pick-list-item, value-list-item) rename `disable*` properties. #4688

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -75,24 +75,24 @@ export class Modal
   beforeClose: (el: HTMLElement) => Promise<void> = () => Promise.resolve();
 
   /** When `true`, disables the component's close button. */
-  @Prop({ reflect: true }) disableCloseButton = false;
+  @Prop({ reflect: true }) closeButtonDisabled = false;
 
   /**
    * When `true`, prevents focus trapping.
    */
-  @Prop({ reflect: true }) disableFocusTrap = false;
+  @Prop({ reflect: true }) focusTrapDisabled = false;
 
-  @Watch("disableFocusTrap")
-  handleDisableFocusTrap(disableFocusTrap: boolean): void {
+  @Watch("focusTrapDisabled")
+  handlefocusTrapDisabled(focusTrapDisabled: boolean): void {
     if (!this.open) {
       return;
     }
 
-    disableFocusTrap ? deactivateFocusTrap(this) : activateFocusTrap(this);
+    focusTrapDisabled ? deactivateFocusTrap(this) : activateFocusTrap(this);
   }
 
   /** When `true`, disables the closing of the component when clicked outside. */
-  @Prop({ reflect: true }) disableOutsideClose = false;
+  @Prop({ reflect: true }) outsideCloseDisabled = false;
 
   /** Accessible name for the component's close button. */
   @Prop() intlClose = TEXT.close;
@@ -101,7 +101,7 @@ export class Modal
   @Prop({ reflect: true }) docked: boolean;
 
   /** When `true`, disables the default close on escape behavior. */
-  @Prop({ reflect: true }) disableEscape = false;
+  @Prop({ reflect: true }) escapeDisabled = false;
 
   /** Specifies the size of the component. */
   @Prop({ reflect: true }) scale: Scale = "m";
@@ -208,7 +208,7 @@ export class Modal
   }
 
   renderCloseButton(): VNode {
-    return !this.disableCloseButton ? (
+    return !this.closeButtonDisabled ? (
       <button
         aria-label={this.intlClose}
         class={CSS.close}
@@ -296,7 +296,7 @@ export class Modal
 
   @Listen("keydown", { target: "window" })
   handleEscape(event: KeyboardEvent): void {
-    if (this.open && !this.disableEscape && event.key === "Escape" && !event.defaultPrevented) {
+    if (this.open && !this.escapeDisabled && event.key === "Escape" && !event.defaultPrevented) {
       this.close();
       event.preventDefault();
     }
@@ -430,7 +430,7 @@ export class Modal
   }
 
   handleOutsideClose = (): void => {
-    if (this.disableOutsideClose) {
+    if (this.outsideCloseDisabled) {
       return;
     }
 
