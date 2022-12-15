f8c05ce228d2e9b70e449e6414c01c83c1c00ffb
anveshmekala <anv11827@esri.com>
2022-12-14 18:43:22 -0600

    refactor(input): remove deprecated intl properties

--- src/components/input-number/input-number.tsx
+++ src/components/input-number/input-number.tsx
@@ -135,21 +135,6 @@ export class InputNumber
    */
   @Prop({ reflect: true }) icon: string | boolean;
 
-  /**
-   * A text label that will appear on the clear button for screen readers.
-   *
-   * @deprecated – translations are now built-in, if you need to override a string, please use `messageOverrides`.
-   */
-  @Prop() intlClear: string;
-
-  /**
-   * Accessible name that will appear while loading.
-   *
-   * @deprecated – translations are now built-in, if you need to override a string, please use `messageOverrides`.
-   * @default "Loading"
-   */
-  @Prop() intlLoading?: string;
-
   /** When `true`, the icon will be flipped when the element direction is right-to-left (`"rtl"`). */
   @Prop({ reflect: true }) iconFlipRtl = false;
 
@@ -301,8 +286,6 @@ export class InputNumber
    */
   @Prop({ mutable: true }) messageOverrides: Partial<Messages>;
 
-  @Watch("intlClear")
-  @Watch("intlLoading")
   @Watch("messageOverrides")
   onMessagesChange(): void {
     /* wired up by t9n util */
