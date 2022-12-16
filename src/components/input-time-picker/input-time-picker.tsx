7a8d31c5b52b9b79f01f4baf16c1b58be6403a35
anveshmekala <anv11827@esri.com>
2022-12-16 14:17:08 -0600

    refactor(input-time-picker): remove deprecated locale property

--- src/components/input-time-picker/input-time-picker.tsx
+++ src/components/input-time-picker/input-time-picker.tsx
@@ -111,20 +111,6 @@ export class InputTimePicker
    */
   @Prop() messagesOverrides: Partial<Messages>;
 
-  /**
-   * BCP 47 language tag for desired language and country format.
-   *
-   * @internal
-   * @deprecated set the global `lang` attribute on the element instead.
-   * @mdn [lang](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/lang)
-   */
-  @Prop({ mutable: true }) locale: string;
-
-  @Watch("locale")
-  localeChanged(): void {
-    updateEffectiveLocale(this);
-  }
-
   /** Specifies the name of the component on form submission. */
   @Prop() name: string;
 
