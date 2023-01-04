b79db358d4f9d31988e66c4b5b27403aa8ea1d58
Adam Tirella <macandcheese@users.noreply.github.com>
2023-01-04 11:51:23 -0800

    refactor(panel, shell-panel, flow-item)!: Remove properties, update available css variables (#6173)
    BREAKING CHANGE: Update available css variables
    
    - Removes `heightScale` and `widthScale` properties from Panel and Flow
    Item
    - Removes css variables for Panel - the component will now fill
    available width of parent component.
    - Documents public css variables for Shell Panel.
    
    Co-authored-by: Ben Elan <belan@esri.com>

--- src/components/flow-item/flow-item.tsx
+++ src/components/flow-item/flow-item.tsx
@@ -13,7 +13,6 @@ import {
 } from "@stencil/core";
 import { getElementDir } from "../../utils/dom";
 import { HeadingLevel } from "../functional/Heading";
-import { Scale } from "../interfaces";
 import { CSS, ICONS, SLOTS } from "./resources";
 import { SLOTS as PANEL_SLOTS } from "../panel/resources";
 import { InteractiveComponent, updateHostInteraction } from "../../utils/interactive";
@@ -88,11 +87,6 @@ export class FlowItem
   @Prop({ reflect: true }) headingLevel: HeadingLevel;
 
   /**
-   * Specifies the maximum height of the component.
-   */
-  @Prop({ reflect: true }) heightScale: Scale;
-
-  /**
    * When `true`, a busy indicator is displayed.
    */
   @Prop({ reflect: true }) loading = false;
@@ -125,11 +119,6 @@ export class FlowItem
    * @internal
    */
   @Prop() showBackButton = false;
-
-  /**
-   * Specifies the width of the component.
-   */
-  @Prop({ reflect: true }) widthScale: Scale;
 
   //--------------------------------------------------------------------------
   //
@@ -307,11 +296,9 @@ export class FlowItem
       disabled,
       heading,
       headingLevel,
-      heightScale,
       loading,
       menuOpen,
       messages,
-      widthScale,
       backButtonEl
     } = this;
     const label = messages.back;
@@ -324,14 +311,12 @@ export class FlowItem
           disabled={disabled}
           heading={heading}
           headingLevel={headingLevel}
-          heightScale={heightScale}
           loading={loading}
           menuOpen={menuOpen}
           messageOverrides={messages}
           onCalcitePanelClose={this.handlePanelClose}
           onCalcitePanelScroll={this.handlePanelScroll}
           ref={this.setContainerRef}
-          widthScale={widthScale}
         >
           {this.renderBackButton()}
           <slot name={SLOTS.headerActionsStart} slot={PANEL_SLOTS.headerActionsStart} />
