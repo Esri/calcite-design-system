694c49d82ca77f369c3e4d3beef7e4baf6bdc072
Kitty Hurley <khurley@esri.com>
2023-01-06 09:54:56 -0600

    docs: add context to  for components with the  interface

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -51,7 +51,7 @@ export class Link implements InteractiveComponent, LoadableComponent {
   /** Specifies an icon to display at the end of the component. */
   @Prop({ reflect: true }) iconEnd: string;
 
-  /** When `true`, the icon will be flipped when the element direction is right-to-left (`"rtl"`). */
+  /** Displays the `iconStart` and/or `iconEnd` as flipped when the element direction is right-to-left (`"rtl"`). */
   @Prop({ reflect: true }) iconFlipRtl: FlipContext;
 
   /** Specifies an icon to display at the start of the component. */
