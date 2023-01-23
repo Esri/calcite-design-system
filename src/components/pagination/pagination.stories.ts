2d695c4037836360200e337c0475f4b0452cd608
Anveshreddy mekala <anv11827@esri.com>
2023-01-23 15:29:21 -0600

    refactor(pagination)!: rename properties. (#6286)
    **Related Issue:** #6285 
    
    BREAKING CHANGE: Rename properties.
    
    - Removed the property `num`, use `pageSize` instead.
    - Removed the property `total`, use `totalItems` instead.
    - Removed the property `start`, use `startItem` instead.

--- src/components/pagination/pagination.stories.ts
+++ src/components/pagination/pagination.stories.ts
@@ -19,21 +19,21 @@ export default {
 export const simple = (): string => html`
   <calcite-pagination
     scale="${select("scale", ["s", "m", "l"], "m")}"
-    start="${number("start", 1)}"
+    start-item="${number("start-item", 1)}"
     lang="${select("lang", locales, "en")}"
-    total="${number("total", 123456789)}"
-    num="${number("num", 10)}"
+    total-items="${number("total-items", 123456789)}"
+    page-size="${number("page-size", 10)}"
   >
   </calcite-pagination>
 `;
 
 export const darkModeFrenchLocale_TestOnly = (): string => html`<calcite-pagination
   class="calcite-mode-dark"
-  start="1"
+  start-item="1"
   lang="fr"
   group-separator
-  total="123456789"
-  num="10"
+  total-items="123456789"
+  page-size="10"
 >
 </calcite-pagination>`;
 
@@ -41,11 +41,11 @@ darkModeFrenchLocale_TestOnly.parameters = { modes: modesDarkDefault };
 
 export const arabicNumberingSystemAndRTL_TestOnly = (): string => html`<calcite-pagination
   dir="rtl"
-  numbering-system="arab"
-  start="1"
+  page-sizebering-system="arab"
+  start-item="1"
   lang="fr"
-  total="123456789"
-  num="10"
+  total-items="123456789"
+  page-size="10"
 >
 </calcite-pagination>`;
 
