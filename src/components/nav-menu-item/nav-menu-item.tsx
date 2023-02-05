97efe99d1aa29b68d8d2363fa63fdfd8be512210
Adam <adam@tirel.la>
2023-02-05 15:53:28 -0800

    Wip - nav updates

--- src/components/nav-menu-item/nav-menu-item.tsx
+++ src/components/nav-menu-item/nav-menu-item.tsx
@@ -12,7 +12,7 @@ import {
   VNode
 } from "@stencil/core";
 import { FlipContext } from "../interfaces";
-import { filterDirectChildren, getSlotted } from "../../utils/dom";
+import { filterDirectChildren, getElementDir, getSlotted } from "../../utils/dom";
 
 @Component({
   tag: "calcite-nav-menu-item",
@@ -30,8 +30,6 @@ export class CalciteNavMenuItem {
 
   @Prop({ reflect: true, mutable: true }) editable: boolean;
 
-  @Prop({ reflect: true, mutable: true }) isTitle: boolean;
-
   @Prop({ reflect: true, mutable: true }) iconStart?: string;
 
   @Prop({ reflect: true, mutable: true }) iconEnd?: string;
@@ -44,7 +42,8 @@ export class CalciteNavMenuItem {
 
   @Prop({ reflect: true, mutable: true }) subText?: string;
 
-  @Prop({ reflect: true, mutable: true }) layout?: "horizontal" | "vertical" = "horizontal";
+  // remove reflect and move style to class
+  @Prop({ mutable: true, reflect: true }) layout?: "horizontal" | "vertical" = "horizontal";
 
   //--------------------------------------------------------------------------
   //
@@ -54,6 +53,7 @@ export class CalciteNavMenuItem {
   @Listen("click", { target: "window" })
   handleClickOut(event: Event): void {
     if (
+      this.nestedParentType !== "vertical" &&
       this.hasSlottedDropdownMenuItems &&
       this.slottedDropdownMenuItemsOpen &&
       !this.el.contains(event.target as Element)
@@ -65,7 +65,7 @@ export class CalciteNavMenuItem {
   // todo events
   @Event() calciteMenuItemSelected: EventEmitter<any>;
 
-  @Prop() editingActive = false;
+  @Prop({ mutable: true }) editingActive = false;
 
   @State() hasSlottedDropdownMenuItems: boolean;
 
@@ -74,9 +74,14 @@ export class CalciteNavMenuItem {
   // for multiple levels of nesting
   @State() isNestedDropdown: boolean;
 
+  // need to track the parent type of layout to determine if the nested dropdown position absolutely (horizontal parent),
+  // or position downward and expand relative (vertical parent)
+  @State() nestedParentType: "vertical" | "horizontal";
+
   @State() dropdownMenuItems?: HTMLCalciteNavMenuItemElement[];
 
   connectedCallback() {
+    this.active = this.active || this.editable;
     // todo - make this reactive to slot change, remove use of getSlotted
     // todo just get any nav items in the default slot?
     this.hasSlottedDropdownMenuItems = !!getSlotted(this.el, "menu-item-dropdown");
@@ -85,7 +90,10 @@ export class CalciteNavMenuItem {
       matches: "calcite-nav-menu-item"
     }) as HTMLCalciteNavMenuItemElement[];
     // for now to detect nesting only working two level for demo.. need to just check if it has any parent originating at top lvel
-    this.isNestedDropdown = this.el.parentElement.slot === "";
+    this.isNestedDropdown =
+      this.el.parentElement?.slot === "" || this.el.parentElement?.slot === "menu-item-dropdown";
+
+    this.nestedParentType = this.el.closest("calcite-nav-menu")?.layout;
 
     // todo determine indentation level to support fly out
     // ensure any items slotted as dropdown menu children are vertical mode
@@ -97,7 +105,7 @@ export class CalciteNavMenuItem {
   renderIconElStart(): VNode {
     return (
       <calcite-icon
-        class="icon-start"
+        class="icon icon-start"
         flipRtl={this.iconFlipRtl === "start" || this.iconFlipRtl === "both"}
         icon={this.iconStart}
         scale="s"
@@ -108,7 +116,7 @@ export class CalciteNavMenuItem {
   renderIconElEnd(): VNode {
     return (
       <calcite-icon
-        class="icon-end"
+        class="icon icon-end"
         flipRtl={this.iconFlipRtl === "end" || this.iconFlipRtl === "both"}
         icon={this.iconEnd}
         scale="s"
@@ -119,7 +127,7 @@ export class CalciteNavMenuItem {
   renderEditIcon(): VNode {
     return (
       <calcite-icon
-        class="icon-end"
+        class="icon icon-end"
         flipRtl={this.iconFlipRtl === "end" || this.iconFlipRtl === "both"}
         icon={"pencil"}
         scale="s"
@@ -130,10 +138,10 @@ export class CalciteNavMenuItem {
   renderEditSaveButton(): VNode {
     return (
       <calcite-button
-        color="neutral"
+        appearance="outline-fill"
+        style={{ ["--calcite-ui-icon-color"]: "var(--calcite-ui-brand)" }}
         icon-start="save"
-        onClick={() => (this.editingActive = !this.editingActive)}
-        slot="action"
+        onClick={() => (this.editingActive = false)}
       />
     );
   }
@@ -142,24 +150,80 @@ export class CalciteNavMenuItem {
     return (
       <calcite-button
         appearance="transparent"
-        color="neutral"
         icon-start="trash"
-        onClick={() => (this.editingActive = !this.editingActive)}
-        slot="action"
-        style={{ "--calcite-ui-icon-color": "var(--calcite-ui-text-3)" }}
+        style={{ ["--calcite-ui-icon-color"]: "var(--calcite-ui-border-input)" }}
+        onClick={() => (this.editingActive = false)}
       />
     );
   }
 
   renderDropdownMenuItems(): VNode {
+    const dir = getElementDir(this.el);
     return (
       <div
         class={`dropdown-menu-items ${this.slottedDropdownMenuItemsOpen ? "open" : ""} ${
           this.isNestedDropdown ? "nested" : ""
+        }${dir === "rtl" ? " is-rtl" : ""}${
+          this.nestedParentType === "vertical" ? " is-vertical-dropdown-type" : ""
         }`}
       >
         <slot name="menu-item-dropdown" />
       </div>
+    );
+  }
+
+  renderBreadcrumbChevron(): VNode {
+    const dir = getElementDir(this.el);
+    const dirChevron = dir === "rtl" ? "chevron-left" : "chevron-right";
+    return <calcite-icon class="icon icon-breadcrumb" icon={dirChevron} scale="s" />;
+  }
+
+  renderDirChevron(): VNode {
+    const dir = getElementDir(this.el);
+    const dirChevron = dir === "rtl" ? "chevron-left" : "chevron-right";
+    return (
+      <calcite-icon
+        class="icon icon-dropdown"
+        icon={
+          this.slottedDropdownMenuItemsOpen &&
+          (!this.isNestedDropdown || this.nestedParentType === "vertical")
+            ? "chevron-up"
+            : !this.isNestedDropdown || this.nestedParentType === "vertical"
+            ? "chevron-down"
+            : // and not vertical
+            this.isNestedDropdown
+            ? dirChevron
+            : dirChevron
+        }
+        scale="s"
+      />
+    );
+  }
+
+  renderDirDropdownAction(): VNode {
+    const dir = getElementDir(this.el);
+    const dirChevron = dir === "rtl" ? "chevron-left" : "chevron-right";
+    return (
+      <calcite-action
+        icon={
+          this.slottedDropdownMenuItemsOpen &&
+          (!this.isNestedDropdown || this.nestedParentType === "vertical")
+            ? "chevron-up"
+            : !this.isNestedDropdown || this.nestedParentType === "vertical"
+            ? "chevron-down"
+            : // and not vertical
+            this.isNestedDropdown
+            ? dirChevron
+            : dirChevron
+        }
+        text="open-dropdown"
+        class="dropdown-with-href-toggle"
+        onClick={() => {
+          if (this.hasSlottedDropdownMenuItems) {
+            this.slottedDropdownMenuItemsOpen = !this.slottedDropdownMenuItemsOpen;
+          }
+        }}
+      />
     );
   }
 
@@ -169,94 +233,66 @@ export class CalciteNavMenuItem {
     const itemContent = (
       <Fragment>
         {this.iconStart && this.renderIconElStart()}
-        {!this.editingActive && this.text && this.textEnabled && this.text && this.isTitle
-          ? this.text
-          : this.text && this.textEnabled && this.text
-          ? this.text
-          : null}
+        <div class="text-container">
+          <span contenteditable={this.editingActive ? true : undefined}>
+            {this.text && this.textEnabled && this.text ? this.text : null}
+          </span>
+          {this.href && this.isNestedDropdown && this.hasSlottedDropdownMenuItems ? (
+            <calcite-icon class="icon icon-link-when-dropdown" icon={"chevrons-right"} scale="s" />
+          ) : null}
+        </div>
+        {this.editingActive ? (
+          <div class="editable-content">
+            {this.renderEditCancelButton()}
+            {this.renderEditSaveButton()}
+          </div>
+        ) : null}
         {/* handle subtext display */}
         {this.iconEnd && !this.editingActive && this.renderIconElEnd()}
         {this.editable && !this.editingActive && this.renderEditIcon()}
-        {!this.href && this.hasSlottedDropdownMenuItems ? (
-          <calcite-icon
-            class="icon icon-dropdown"
-            icon={
-              this.slottedDropdownMenuItemsOpen && !this.isNestedDropdown
-                ? "chevron-up"
-                : !this.isNestedDropdown
-                ? "chevron-down"
-                : // and not vertical
-                this.isNestedDropdown
-                ? "chevron-right"
-                : "chevron-right"
-            }
-            scale="s"
-          />
-        ) : null}
-        {this.breadcrumb ? (
-          <calcite-icon class="icon icon-breadcrumb" icon="chevron-right" scale="s" />
-        ) : null}
+        {!this.href && this.hasSlottedDropdownMenuItems ? this.renderDirChevron() : null}
+        {this.breadcrumb ? this.renderBreadcrumbChevron() : null}
       </Fragment>
     );
 
     return (
       <Host tabindex={0}>
-        {this.text && this.editable && this.editingActive ? (
-          <div class="editable-content">
-            <calcite-input clearable placeholder={this.text} scale="l" value={this.text}>
-              {this.renderEditCancelButton()}
-              {this.renderEditSaveButton()}
-            </calcite-input>
-          </div>
-        ) : (
-          <Fragment>
-            <a
-              tabindex={-1}
-              href={this.href ? this.href : null}
-              onClick={() => {
-                if (!this.editable && !this.hasSlottedDropdownMenuItems) {
-                  return;
-                } else if (this.editable && !this.hasSlottedDropdownMenuItems) {
-                  this.editingActive = true;
-                } else if (!this.href && this.hasSlottedDropdownMenuItems) {
-                  this.slottedDropdownMenuItemsOpen = !this.slottedDropdownMenuItemsOpen;
-                }
-              }}
-              onKeyDown={(event) => {
-                if (
-                  (event.key === " " || event.key === "enter") &&
-                  !this.href &&
-                  this.hasSlottedDropdownMenuItems
-                )
-                  this.slottedDropdownMenuItemsOpen = !this.slottedDropdownMenuItemsOpen;
-              }}
-            >
-              {itemContent}
-            </a>
-            {this.href && this.hasSlottedDropdownMenuItems ? (
-              <calcite-action
-                icon={
-                  this.slottedDropdownMenuItemsOpen && !this.isNestedDropdown
-                    ? "chevron-up"
-                    : !this.isNestedDropdown
-                    ? "chevron-down"
-                    : // and not vertical
-                    this.isNestedDropdown
-                    ? "chevron-right"
-                    : "chevron-right"
-                }
-                text="open-dropdown"
-                class="dropdown-with-href-toggle"
-                onClick={() => {
-                  if (this.hasSlottedDropdownMenuItems) {
+        <div
+          class={`container ${
+            this.nestedParentType === "vertical" ? "nav-item-vertical-parent" : ""
+          }`}
+        >
+          <div class="item-content">
+            <Fragment>
+              <a
+                tabindex={-1}
+                href={this.href ? this.href : null}
+                onClick={(ev) => {
+                  if (!this.editable && !this.hasSlottedDropdownMenuItems) {
+                    return;
+                  } else if (this.editable && !this.hasSlottedDropdownMenuItems) {
+                    this.editingActive = true;
+                  } else if (!this.href && this.hasSlottedDropdownMenuItems) {
                     this.slottedDropdownMenuItemsOpen = !this.slottedDropdownMenuItemsOpen;
                   }
                 }}
-              />
-            ) : null}
-          </Fragment>
-        )}
-        {this.hasSlottedDropdownMenuItems ? this.renderDropdownMenuItems() : null}
+                onKeyDown={(event) => {
+                  if (
+                    (event.key === " " || event.key === "enter") &&
+                    !this.href &&
+                    this.hasSlottedDropdownMenuItems
+                  )
+                    this.slottedDropdownMenuItemsOpen = !this.slottedDropdownMenuItemsOpen;
+                }}
+              >
+                {itemContent}
+              </a>
+            </Fragment>
+
+            {this.href && this.hasSlottedDropdownMenuItems ? this.renderDirDropdownAction() : null}
+          </div>
+          {this.hasSlottedDropdownMenuItems ? this.renderDropdownMenuItems() : null}
+        </div>
       </Host>
     );
   }
--- src/components/nav-menu/nav-menu.tsx
+++ src/components/nav-menu/nav-menu.tsx
@@ -1,4 +1,5 @@
-import { Component, Element, h, Host, Prop } from "@stencil/core";
+import { Component, Element, h, Host, Prop, State } from "@stencil/core";
+import { getSlotted } from "../../utils/dom";
 
 @Component({
   tag: "calcite-nav-menu",
@@ -22,19 +23,45 @@ export class CalciteNavMenu {
   // todo evaluate slotted content and determine if it is a nav menu item, then limit # rendered when auto-collapsing based on width of parent
   @Prop({ mutable: true }) minCollapsedItems?;
 
+  @Prop({ reflect: true }) layout?: "horizontal" | "vertical";
+
+  @State() childNavMenuItems?: HTMLCalciteNavMenuItemElement[];
+
+  @State() overflowedNavMenuItems?: HTMLCalciteNavMenuItemElement[] = [];
+
+  private overflowMenuItem: HTMLCalciteNavMenuItemElement;
+
   // --------------------------------------------------------------------------
   //
   //  Render Methods
   //
   // --------------------------------------------------------------------------
+  // todo - shell prevent double border like shell panel when in shell header slot
+
+  connectedCallback() {
+    this.childNavMenuItems = getSlotted(this.el, "", {
+      all: true,
+      matches: "calcite-nav-menu-item"
+    }) as HTMLCalciteNavMenuItemElement[];
+
+    // todo determine indentation level to support fly out
+    // ensure any items slotted as dropdown menu children are vertical mode
+    this.childNavMenuItems.map((el: HTMLCalciteNavMenuItemElement) => {
+      el.layout = this.layout;
+    });
+  }
 
   render() {
     return (
       <Host>
         <slot />
-        <div class="overflow-temp">
-          <slot name="overflow-nav-items" />
-        </div>
+        {this.overflowedNavMenuItems.length > 0 && (
+          <calcite-nav-menu-item
+            ref={(el) => (this.overflowMenuItem = el)}
+            icon-start="ellipsis"
+            title="overflow-items"
+          />
+        )}
       </Host>
     );
   }
