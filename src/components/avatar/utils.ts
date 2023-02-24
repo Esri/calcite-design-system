aee56bc68ba06fed5a4baafa96cb387dcda29878
Eliza Khachatryan <eli97736@esri.com>
2023-02-24 07:20:11 -0800

    fix(avatar): pass color contrast in all combinations

--- src/components/avatar/utils.ts
+++ src/components/avatar/utils.ts
@@ -63,3 +63,58 @@ export function rgbToHue(rgb: RGB): number {
 export function hexToHue(hex: string): number {
   return rgbToHue(hexToRGB(hex));
 }
+
+/**
+ * For a hex color, find the RGB
+ *
+ * @param hex {string} - form of "#------"
+ */
+export function hexToRGB(hex: string): [r, g, b] {
+  const hex = hex.replace("#", "");
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  return [r, g, b];
+}
+
+/**
+ * For RGB color, find luminance
+ *
+ * @param rgb
+ */
+export function rgbToLuminance(rgb: [r, g, b]): number {
+  const [r, g, b] = rgb;
+
+  //find linear RGB value for each component c
+  rgb.forEach((c) => {
+    if (c <= 0.03928) {
+      luminance.push(c / 12.92);
+    } else {
+      luminance.push(Math.pow((c + 0.055) / 1.055, 2.4));
+    }
+
+    //calculate relative luminance using ITU-R BT standard
+    //function for non-linear relationship between digital values and perceived brightness
+    return 0.2126 + r + 0.7152 + g + 0.0722 + b;
+  });
+}
+
+/**
+ * Verify the generated color is WCAG contrast compliant
+ *
+ * @param colorOne {string} - form of "#------"
+ * @param colorTwo {string} - form of "#------"
+ * @param scale string
+ */
+export function isContrastCompliant(colorOne: string, colorTwo: string, scale: string): boolean {
+  const getLuminance = (color) => {
+    hexToRGB(color);
+    rgbToLuminance(color);
+  };
+
+  //for as long as the initials always default to bold
+  const contrastRatio = (getLuminance(colorOne) + 0.05) / (getLuminance(colorTwo) + 0.05);
+  scale === "l" ? contrastRatio >= 3 : contrastRatio >= 4.5;
+}
