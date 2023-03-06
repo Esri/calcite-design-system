803f03e71f56f1c190fb30548013f7fdcc20b7cc
Eliza Khachatryan <eli97736@esri.com>
2023-03-06 15:44:43 -0800

    WIP: bring in color library

--- src/components/avatar/utils.ts
+++ src/components/avatar/utils.ts
@@ -1,5 +1,6 @@
 import { RGB } from "../color-picker/interfaces";
 import { hexToRGB } from "../color-picker/utils";
+import Color from "color";
 
 /**
  * Convert a string to a valid hex by hashing its contents
@@ -9,30 +10,22 @@ import { hexToRGB } from "../color-picker/utils";
  * @param theme {string}
  * @param scale {string}
  */
-let multi = 8; //temporary var, testing recursive regeneration of hex
 export function stringToHex(name: string, theme?: string, scale?: string): string {
-  const { constName, constTheme, constScale } = { constName: name, constTheme: theme, constScale: scale };
+  let hash = 0;
+  for (let i = 0; i < name.length; i++) {
+    hash = name.charCodeAt(i) + ((hash << 5) - hash);
+  }
 
-  const generateNewHexValue = () => {
-    let hash = 0;
-    for (let i = 0; i < constName.length; i++) {
-      hash = constName.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    let hex = "#";
-    for (let j = 0; j < 3; j++) {
-      const value = (hash >> (j * multi)) & 0xff;
-      hex += ("00" + value.toString(16)).substr(-2);
-    }
-    multi++;
+  let hex = "#";
+  for (let j = 0; j < 3; j++) {
+    const value = (hash >> (j * 8)) & 0xff;
+    hex += ("00" + value.toString(16)).substr(-2);
+  }
+
+  if (isContrastCompliant(hex, theme, scale)) {
     return hex;
-  };
-
-  const newHexValue: string = generateNewHexValue();
-
-  if (isContrastCompliant(newHexValue, constTheme, constScale)) {
-    return newHexValue;
   } else {
-    stringToHex(constName, constTheme, constScale);
+    console.log("====Not compliant====");
   }
 }
 
@@ -82,29 +75,12 @@ export function hexToHue(hex: string): number {
 /**
  * For RGB color, find luminance
  *
- * @param colorObject { r: number; g: number; b: number }
- * @param colorObject.r
- * @param colorObject.g
- * @param colorObject.b
+ * @param colorObject {RGB}
  */
-export function rgbToLuminance(colorObject: { r: number; g: number; b: number }): number {
-  const { r, g, b } = colorObject;
-  const linearRGB = [];
-  let luminance: number;
-
-  //find linear RGB value for each channel c
-  [r, g, b].forEach((c) => {
-    if (c <= 0.03928) {
-      linearRGB.push(c / 12.92);
-    } else {
-      linearRGB.push(Math.pow((c + 0.055) / 1.055, 2.4));
-    }
-
-    //calculate relative luminance using ITU-R BT standard
-    //function for non-linear relationship between digital values and perceived brightness
-    luminance = 0.2126 + r + 0.7152 + g + 0.0722 + b;
-  });
-  return luminance;
+export function rgbToLuminosity(colorObject: RGB): number {
+  const color = Color(colorObject);
+  const luminosity = color.luminosity();
+  return luminosity;
 }
 
 /**
@@ -117,11 +93,12 @@ export function rgbToLuminance(colorObject: { r: number; g: number; b: number })
 export function isContrastCompliant(hex: string, theme: string, scale: string): boolean {
   const getLuminance = (hex) => {
     const rgbObj = hexToRGB(hex);
-    return rgbToLuminance(rgbObj);
+    return rgbToLuminosity(rgbObj);
   };
   const themeRGB = theme === "Light" ? "#9F9F9F" : "#6A6A6A";
 
   //as long as the initials always default to bold
   const contrastRatio = (getLuminance(hex) + 0.05) / (getLuminance(themeRGB) + 0.05);
+  console.log("contrastRatio", contrastRatio);
   return scale === "l" ? contrastRatio >= 3 : contrastRatio >= 4.5;
 }
