12eac0a5443654569e47af07cb388ea59102dac9
Kitty Hurley <khurley@esri.com>
2023-04-03 13:54:43 -0500

    docs(tree): specify selectionMode types

--- src/components/tree/tree.tsx
+++ src/components/tree/tree.tsx
@@ -49,7 +49,14 @@ export class Tree {
   @Prop({ mutable: true, reflect: true }) scale: Scale = "m";
 
   /**
-   * Customize how the component's selection works.
+   * Specifies the selection mode, where
+   * `"ancestors"` displays with a checkbox and allows any number of selections from corresponding parent and child selections,
+   * `"children"` allows any number of selections from one parent from corresponding parent and child selections,
+   * `"multichildren"` allows any number of selections from corresponding parent and child selections,
+   * `"multiple"` allows any number of selections,
+   * `"none"` allows no selections,
+   * `"single"` allows one selection, or
+   * `"single-persist"` allows and requires one selection.
    *
    * @default "single"
    */
