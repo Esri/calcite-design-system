1032db899bccb0505d44287472f82fda54480b86
Matt Driscoll <mdriscoll@esri.com>
2023-04-21 14:57:41 -0700

    cleanup

--- src/components/shell/shell.stories.ts
+++ src/components/shell/shell.stories.ts
@@ -725,22 +725,25 @@ height:400px;
 position:relative;
 "
 >
-  <calcite-shell-panel slot="panel-start" position="start" collapsed>
+  <calcite-shell-panel slot="panel-start" position="start">
     <calcite-action-bar slot="action-bar">
       <calcite-tooltip open slot="expand-tooltip">Expand</calcite-tooltip>
-      <calcite-action-group>
-        <calcite-action text="Add" icon="plus"></calcite-action>
-        <calcite-action text="Save" disabled icon="save"></calcite-action>
-        <calcite-action text="Layers" active indicator icon="layers"></calcite-action>
-      </calcite-action-group>
-      <calcite-action-group>
-        <calcite-action text="Add" icon="plus"></calcite-action>
-        <calcite-action text="Layers" indicator icon="layers"></calcite-action>
-      </calcite-action-group>
     </calcite-action-bar>
   </calcite-shell-panel>
-  <calcite-shell-center-row detached>
-    <div style="background-color:black; height: 100%; width: 600px;"></div>
+  <calcite-shell-center-row slot="panel-bottom">
+    <div style="height: 100%; width: 600px;">
+      <div
+        style="
+      width:100%;
+      height:100%;
+      background-image: linear-gradient(45deg, #ccc 25%, transparent 25%),
+      linear-gradient(-45deg, #ccc 25%, transparent 25%),
+      linear-gradient(45deg, transparent 75%, #ccc 75%),
+      linear-gradient(-45deg, transparent 75%, #ccc 75%);
+      background-size: 20px 20px;
+      background-position: 0 0, 0 10px, 10px -10px, -10px 0px;"
+      ></div>
+    </div>
   </calcite-shell-center-row>
 </calcite-shell>`;
 
