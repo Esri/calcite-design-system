e182a58978579cb3c227c52ef92a5de51a163722
Matt Driscoll <mdriscoll@esri.com>
2023-04-21 14:59:18 -0700

    cleanup

--- src/components/shell/shell.stories.ts
+++ src/components/shell/shell.stories.ts
@@ -731,19 +731,7 @@ position:relative;
     </calcite-action-bar>
   </calcite-shell-panel>
   <calcite-shell-center-row slot="panel-bottom">
-    <div style="height: 100%; width: 600px;">
-      <div
-        style="
-      width:100%;
-      height:100%;
-      background-image: linear-gradient(45deg, #ccc 25%, transparent 25%),
-      linear-gradient(-45deg, #ccc 25%, transparent 25%),
-      linear-gradient(45deg, transparent 75%, #ccc 75%),
-      linear-gradient(-45deg, transparent 75%, #ccc 75%);
-      background-size: 20px 20px;
-      background-position: 0 0, 0 10px, 10px -10px, -10px 0px;"
-      ></div>
-    </div>
+    <div style="height: 100%; width: 600px; background-color: black;"></div>
   </calcite-shell-center-row>
 </calcite-shell>`;
 
