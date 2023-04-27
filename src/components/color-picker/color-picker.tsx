50e50d297ec7c153e41c1c3ab7dc20e9d5fece3f
Kitty Hurley <khurley@esri.com>
2023-04-27 11:23:45 -0500

    docs(color-picker): update allowEmpty prop description

--- src/components/color-picker/color-picker.tsx
+++ src/components/color-picker/color-picker.tsx
@@ -81,8 +81,6 @@ export class ColorPicker
   //--------------------------------------------------------------------------
 
   /**
-   * When `false`, an empty color (`null`) will be allowed as a `value`. Otherwise, a color value is enforced on the component.
-   *
    * When `true`, a color value is enforced, and clearing the input or blurring will restore the last valid `value`. When `false`, an empty color (`null`) will be allowed as a `value`.
    */
   @Prop({ reflect: true }) allowEmpty = false;
