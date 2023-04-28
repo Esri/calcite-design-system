8aed351a8ea0cce04fee11976c36f530e6d39f7f
Adam <adam@tirel.la>
2023-04-28 15:30:05 -0700

    Update stories

--- src/components/nav-user/nav-user.stories.ts
+++ src/components/nav-user/nav-user.stories.ts
@@ -2,6 +2,7 @@ import { boolean, storyFilters } from "../../../.storybook/helpers";
 import { text } from "@storybook/addon-knobs";
 import { html } from "../../../support/formatting";
 import readme from "./readme.md";
+import { placeholderImage } from "../../../.storybook/placeholderImage";
 
 export default {
   title: "Components/Nav/Nav User",
@@ -18,7 +19,49 @@ export const simple = (): string =>
       full-name="${text("full-name", "Edward Abbey")}"
       username="${text("username", "eabbey_123")}"
       thumbnail="${text("thumbnail", "")}"
+      user-id="${text("user-id", "")}"
       ${boolean("hide-text", false)}
       ${boolean("active", true)}
     />
   </calcite-nav>`;
+
+export const fullName = (): string => html` <calcite-shell>
+  <calcite-nav slot="header">
+    <calcite-nav-user slot="logo" full-name="Edward Abbey" />
+  </calcite-nav>
+</calcite-shell>`;
+
+export const username_TestOnly = (): string => html` <calcite-shell>
+  <calcite-nav slot="header">
+    <calcite-nav-user slot="logo" username="eabbey_123" />
+  </calcite-nav>
+</calcite-shell>`;
+
+export const thumbnail_TestOnly = (): string => html` <calcite-shell>
+  <calcite-nav slot="header">
+    <calcite-nav-user slot="logo" thumbnail="${placeholderImage({ width: 50, height: 50 })}" />
+  </calcite-nav>
+</calcite-shell>`;
+
+export const fullNameAndThumbnail_TestOnly = (): string => html` <calcite-shell>
+  <calcite-nav slot="header">
+    <calcite-nav-user slot="logo" full-name="Edward Abbey" thumbnail="${placeholderImage({ width: 50, height: 50 })}" />
+  </calcite-nav>
+</calcite-shell>`;
+
+export const usernameAndThumbnail_TestOnly = (): string => html` <calcite-shell>
+  <calcite-nav slot="header">
+    <calcite-nav-user slot="logo" username="eabbey_123" thumbnail="${placeholderImage({ width: 50, height: 50 })}" />
+  </calcite-nav>
+</calcite-shell>`;
+
+export const All_TestOnly = (): string => html` <calcite-shell>
+  <calcite-nav slot="header">
+    <calcite-nav-user
+      slot="logo"
+      full-name="Edward Abbey"
+      username="eabbey_123"
+      thumbnail="${placeholderImage({ width: 50, height: 50 })}"
+    />
+  </calcite-nav>
+</calcite-shell>`;
--- src/components/nav/nav.stories.ts
+++ src/components/nav/nav.stories.ts
@@ -23,38 +23,70 @@ export const simple = (): string =>
     </calcite-shell>
   `;
 
-export const allSlots = (): string =>
+export const primarySlots_TestOnly = (): string =>
   html`
     <calcite-shell>
       <calcite-nav slot="header">
-        <calcite-menu slot="primary-content-start">
-          <calcite-menu-item text="Example nav item 1" text-enabled></calcite-menu-item>
-          <calcite-menu-item text="Example nav item 2" text-enabled active></calcite-menu-item>
-        </calcite-menu>
-        <calcite-menu slot="primary-content-center">
-          <calcite-menu-item text="Example nav item 1" text-enabled></calcite-menu-item>
-          <calcite-menu-item text="Example nav item 2" text-enabled active></calcite-menu-item>
-        </calcite-menu>
-        <calcite-menu slot="primary-content-end">
-          <calcite-menu-item text="Example nav item 1" text-enabled></calcite-menu-item>
-          <calcite-menu-item text="Example nav item 2" text-enabled active></calcite-menu-item>
-        </calcite-menu>
-        <calcite-menu slot="secondary-content-start">
-          <calcite-menu-item text="Example nav item 1" text-enabled></calcite-menu-item>
-          <calcite-menu-item text="Example nav item 2" text-enabled active></calcite-menu-item>
-        </calcite-menu>
-        <calcite-menu slot="secondary-content-end">
-          <calcite-menu-item text="Example nav item 1" text-enabled></calcite-menu-item>
-          <calcite-menu-item text="Example nav item 2" text-enabled active></calcite-menu-item>
-        </calcite-menu>
-        <calcite-menu slot="tertiary-content-start">
-          <calcite-menu-item text="Example nav item 1" text-enabled></calcite-menu-item>
-          <calcite-menu-item text="Example nav item 2" text-enabled active></calcite-menu-item>
-        </calcite-menu>
-        <calcite-menu slot="tertiary-content-end">
-          <calcite-menu-item text="Example nav item 1" text-enabled></calcite-menu-item>
-          <calcite-menu-item text="Example nav item 2" text-enabled active></calcite-menu-item>
-        </calcite-menu>
+        <calcite-chip slot="logo"> logo </calcite-chip>
+        <calcite-chip slot="primary-content-start"> content </calcite-chip>
+        <calcite-chip slot="primary-content-center"> content </calcite-chip>
+        <calcite-chip slot="primary-content-end"> content </calcite-chip>
+        <calcite-chip slot="user"> user </calcite-chip>
+        <calcite-chip slot="secondary-content-start"> content </calcite-chip>
+        <calcite-chip slot="secondary-content-center"> content </calcite-chip>
+        <calcite-chip slot="tertiary-content-start"> content </calcite-chip>
+        <calcite-chip slot="tertiary-content-center"> content </calcite-chip>
       </calcite-nav>
     </calcite-shell>
   `;
+
+export const primaryAndSecondarySlots_TestOnly = (): string =>
+  html`
+    <calcite-shell>
+      <calcite-nav slot="header">
+        <calcite-chip slot="logo"> logo </calcite-chip>
+        <calcite-chip slot="primary-content-start"> content </calcite-chip>
+        <calcite-chip slot="primary-content-center"> content </calcite-chip>
+        <calcite-chip slot="primary-content-end"> content </calcite-chip>
+        <calcite-chip slot="user"> user </calcite-chip>
+        <calcite-chip slot="secondary-content-start"> content </calcite-chip>
+        <calcite-chip slot="secondary-content-center"> content </calcite-chip>
+      </calcite-nav>
+    </calcite-shell>
+  `;
+
+export const allSlots_TestOnly = (): string =>
+  html`
+    <calcite-shell>
+      <calcite-nav slot="header">
+        <calcite-chip slot="logo"> logo </calcite-chip>
+        <calcite-chip slot="primary-content-start"> content </calcite-chip>
+        <calcite-chip slot="primary-content-center"> content </calcite-chip>
+        <calcite-chip slot="primary-content-end"> content </calcite-chip>
+        <calcite-chip slot="user"> user </calcite-chip>
+        <calcite-chip slot="secondary-content-start"> content </calcite-chip>
+        <calcite-chip slot="secondary-content-center"> content </calcite-chip>
+        <calcite-chip slot="tertiary-content-start"> content </calcite-chip>
+        <calcite-chip slot="tertiary-content-center"> content </calcite-chip>
+      </calcite-nav>
+    </calcite-shell>
+  `;
+
+export const allSlots_darkModeRTL_TestOnly = (): string =>
+  html`
+      <div style="class="calcite-mode-dark" dir="rtl">
+        <calcite-shell>
+          <calcite-nav slot="header">
+            <calcite-chip slot="logo"> logo </calcite-chip>
+            <calcite-chip slot="primary-content-start"> content </calcite-chip>
+            <calcite-chip slot="primary-content-center"> content </calcite-chip>
+            <calcite-chip slot="primary-content-end"> content </calcite-chip>
+            <calcite-chip slot="user"> user </calcite-chip>
+            <calcite-chip slot="secondary-content-start"> content </calcite-chip>
+            <calcite-chip slot="secondary-content-center"> content </calcite-chip>
+            <calcite-chip slot="tertiary-content-start"> content </calcite-chip>
+            <calcite-chip slot="tertiary-content-center"> content </calcite-chip>
+          </calcite-nav>
+        </calcite-shell>
+      </div>
+    `;
