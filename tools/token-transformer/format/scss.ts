46a8802228d68ab8eff3725a59ecf39d4e695a0b
Ali Stump <astump@esri.com>
2023-05-08 16:59:05 -0700

    chore: resolve issue with generated tokens

--- tools/token-transformer/format/scss.ts
+++ tools/token-transformer/format/scss.ts
@@ -1,5 +1,5 @@
 import { pascalCase, sentenceCase } from "change-case";
-import sd, { Dictionary, File, Platform, Options } from "style-dictionary";
+import sd, { Dictionary, File, Platform, Options, TransformedToken } from "style-dictionary";
 
 const { formatHelpers } = sd;
 
@@ -28,11 +28,25 @@ export function formatSCSS(fileInfo: {
       .split(" ")
       .filter((n) => !regexThemeGroup.test(n))
       .join(" ")
-    ).toLowerCase();
-  return (
-    formatHelpers.fileHeader({ file }) +
-    `@mixin calcite-theme-${themeName}() {\n` +
-    formatHelpers.formattedVariables({ format: "css", dictionary, outputReferences }) +
-    `\n}\n`
-  );
+  ).toLowerCase();
+  const coreTokens: TransformedToken[] = dictionary.allTokens.reduce((acc, token) => {
+    if (token.filePath.includes("core")) {
+      const path = token.path.filter((p) => !/(core|default|font$)/.test(p));
+      token.name = token.type === "color" ? path.slice(-1).join("-") : path.join("-");
+      token.value = token.value.includes(" ") ? `"${token.value}"` : token.value === "Demi" ? 600 : token.value;
+
+      acc.push(token);
+    }
+    return acc;
+  }, []);
+  return formatHelpers.fileHeader({ file }) + file.destination.includes("headless")
+    ? formatHelpers.formattedVariables({
+        format: "sass",
+        dictionary: { ...dictionary, allTokens: coreTokens },
+        outputReferences
+      }) + "\n"
+    : "" +
+        `@mixin calcite-theme-${themeName}() {\n` +
+        formatHelpers.formattedVariables({ format: "css", dictionary, outputReferences }) +
+        `\n}\n`;
 }
