0387676079860f2c7accd350a30d11ba83a511a5
Matt Driscoll <mdriscoll@esri.com>
2023-05-15 10:22:26 -0700

    Update stack.e2e.ts

--- src/components/stack/stack.e2e.ts
+++ src/components/stack/stack.e2e.ts
@@ -1,5 +1,5 @@
 import { html } from "../../../support/formatting";
-import { hidden, renders, slots, defaults, accessible } from "../../tests/commonTests";
+import { hidden, renders, slots, defaults } from "../../tests/commonTests";
 import { SLOTS } from "./resources";
 
 describe("calcite-stack", () => {
@@ -15,17 +15,9 @@ describe("calcite-stack", () => {
     renders("calcite-stack", { display: "flex" });
   });
 
-  describe("accessible when disabled", () => {
-    accessible(html`<calcite-stack disabled>
-      <calcite-action appearance="transparent" text="banana" icon="banana" slot="actions-start"></calcite-action>
-      Hello World
-      <calcite-avatar slot="content-end" thumbnail="http://placekitten.com/105/105" scale="s"> </calcite-avatar>
-      <calcite-chip slot="content-start" value="chip" scale="s" appearance="outline">My great chip</calcite-chip>
-      <calcite-action appearance="transparent" text="Close" icon="x" slot="actions-end"></calcite-action>
-    </calcite-stack>`);
+   describe("honors hidden attribute", () => {
+    hidden("calcite-stack");
   });
-
-  it("honors hidden attribute", async () => hidden("calcite-stack"));
 
   it("has slots", () => slots("calcite-stack", SLOTS));
 });
