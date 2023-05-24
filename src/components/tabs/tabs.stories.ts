3d9bb3b31c5c03a19c1fca3d2d422b7cd02d585a
eliza <eli97736@esri.com>
2023-05-24 16:52:09 -0700

    fix(tab-title): tabs center when set to layout='center'

--- src/components/tabs/tabs.stories.ts
+++ src/components/tabs/tabs.stories.ts
@@ -184,6 +184,19 @@ export const disabledTabsAndMediumIconsForLargeTabsTitle_TestOnly = (): string =
   </calcite-tabs>
 `;
 
+export const centered_TestOnly = (): string => html`
+  <calcite-tabs layout="center">
+    <calcite-tab-nav slot="title-group">
+      <calcite-tab-title icon-start="arrow-left">Tab 1 Title</calcite-tab-title>
+      <calcite-tab-title icon-start="arrow-left">Tab 2 Title</calcite-tab-title>
+      <calcite-tab-title icon-start="arrow-left" selected>Tab 3 Title</calcite-tab-title>
+    </calcite-tab-nav>
+    <calcite-tab><p>Tab 1 Content</p></calcite-tab>
+    <calcite-tab><p>Tab 2 Content</p></calcite-tab>
+    <calcite-tab><p>Tab 3 Content</p></calcite-tab>
+  </calcite-tabs>
+`;
+
 export const TabChildrenWithPercentageHeights = (): string => html`
   <calcite-tabs style="height: 250px;">
     <calcite-tab-nav slot="title-group">
