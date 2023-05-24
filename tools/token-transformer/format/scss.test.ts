71ac422d1a0bcdc87f12809b5d34f71145a70889
Ali Stump <astump@esri.com>
2023-05-24 09:27:22 -0700

    test: add transformer tests

--- tools/token-transformer/format/scss.test.ts
+++ tools/token-transformer/format/scss.test.ts
@@ -1,9 +1,20 @@
+import { tokens as mockTokens } from "../../_mocks_/mockStyleDictionaryTokens";
+import * as platformMocks from "../../_mocks_/mockPlatformTokens";
+
 const createPropertyFormatterCallback = jest.fn((token) => `--${token.name}: blue;`);
 const formattedVariables = jest.fn((tokens) => tokens.dictionary.allTokens.map(createPropertyFormatterCallback));
 const fileHeader = jest.fn(() => "");
+const mockReturnValue = (format: "sass" | "css" | "scss") => (token) => {
+  if (token) {
+    return format === "css"
+      ? platformMocks.cssRoot.find((v) => v.includes(token.name))
+      : platformMocks.scssBase.find((v) => v.includes(token.name));
+  }
+  return format === "css" ? platformMocks.cssRoot : platformMocks.scssBase;
+};
 const createPropertyFormatter = jest.fn(
-  (obj: { outputReferences: Record<any, any>; dictionary: Dictionary; format: "sass" | "css" }) => {
-    const returnValue = obj.format === "css" ? jest.fn().mockReturnValue(cssRoot) : jest.fn().mockReturnValue(scssBase);
+  (obj: { outputReferences: Record<any, any>; dictionary: Dictionary; format: "sass" | "css" | "scss" }) => {
+    const returnValue = jest.fn(mockReturnValue(obj.format));
     // returnIndex++;
     return returnValue;
   }
@@ -23,8 +34,6 @@ jest.mock("style-dictionary", () => {
 });
 
 import { formatSCSS } from "./scss";
-import { tokens as mockTokens } from "../../_mocks_/mockStyleDictionaryTokens";
-import { cssRoot, scssBase, scssCSSRoot, scssMixins } from "../../_mocks_/mockPlatformTokens";
 import { Dictionary } from "style-dictionary";
 
 const mock = {
@@ -32,11 +41,11 @@ const mock = {
   file: {
     destination: "calciteLight.scss"
   },
-  formattedTokenSet: [...scssBase, "", ...scssMixins, "", ...scssCSSRoot],
+  formattedTokenSet: [...platformMocks.scssBase, "", ...platformMocks.scssMixins, "", ...platformMocks.scssCSSRoot],
   options: {}
 };
 
-describe("formatting CSS Variable output", () => {
+describe("formatting SCSS Variable output", () => {
   it("should format values", () => {
     const cssFile = formatSCSS({ dictionary: mock.dictionary, file: mock.file, options: mock.options });
     expect(cssFile).toContain(mock.formattedTokenSet[0]);
