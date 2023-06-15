017aba1366985bc2d0c50e00d4dc5597c6e3b7ec
Matt Driscoll <mdriscoll@esri.com>
2023-06-15 12:19:41 -0700

    Revert "feat(list): Add slots for filter actions (#7148)"
    This reverts commit c9215c05535c0d91582a73eabb9a587d39ba0422.

--- packages/calcite-components/src/components/list/list.tsx
+++ packages/calcite-components/src/components/list/list.tsx
@@ -12,15 +12,14 @@ import {
   Watch
 } from "@stencil/core";
 import { debounce } from "lodash-es";
-import { slotChangeHasAssignedElement, toAriaBoolean } from "../../utils/dom";
+import { toAriaBoolean } from "../../utils/dom";
 import { InteractiveComponent, updateHostInteraction } from "../../utils/interactive";
 import { createObserver } from "../../utils/observers";
 import { SelectionMode } from "../interfaces";
 import { ItemData } from "../list-item/interfaces";
 import { MAX_COLUMNS } from "../list-item/resources";
 import { getListItemChildren, updateListItemChildren } from "../list-item/utils";
-import { CSS, debounceTimeout, SelectionAppearance, SLOTS } from "./resources";
-import { SLOTS as STACK_SLOTS } from "../stack/resources";
+import { CSS, debounceTimeout, SelectionAppearance } from "./resources";
 
 const listItemSelector = "calcite-list-item";
 const parentSelector = "calcite-list-item-group, calcite-list-item";
@@ -36,8 +35,6 @@ import {
  * A general purpose list that enables users to construct list items that conform to Calcite styling.
  *
  * @slot - A slot for adding `calcite-list-item` elements.
- * @slot filter-actions-start - A slot for adding actionable `calcite-action` elements before the filter component.
- * @slot filter-actions-end - A slot for adding actionable `calcite-action` elements after the filter component.
  */
 @Component({
   tag: "calcite-list",
@@ -248,10 +245,6 @@ export class List implements InteractiveComponent, LoadableComponent {
 
   @State() dataForFilter: ItemData = [];
 
-  @State() hasFilterActionsStart = false;
-
-  @State() hasFilterActionsEnd = false;
-
   filterEl: HTMLCalciteFilterElement;
 
   // --------------------------------------------------------------------------
@@ -281,9 +274,7 @@ export class List implements InteractiveComponent, LoadableComponent {
       dataForFilter,
       filterEnabled,
       filterPlaceholder,
-      filterText,
-      hasFilterActionsStart,
-      hasFilterActionsEnd
+      filterText
     } = this;
     return (
       <div class={CSS.container}>
@@ -295,32 +286,20 @@ export class List implements InteractiveComponent, LoadableComponent {
           onKeyDown={this.handleListKeydown}
           role="treegrid"
         >
-          {filterEnabled || hasFilterActionsStart || hasFilterActionsEnd ? (
+          {filterEnabled ? (
             <thead>
               <tr class={{ [CSS.sticky]: true }}>
                 <th colSpan={MAX_COLUMNS}>
-                  <calcite-stack class={CSS.stack}>
-                    <slot
-                      name={SLOTS.filterActionsStart}
-                      onSlotchange={this.handleFilterActionsStartSlotChange}
-                      slot={STACK_SLOTS.actionsStart}
-                    />
-                    <calcite-filter
-                      aria-label={filterPlaceholder}
-                      disabled={loading || disabled}
-                      items={dataForFilter}
-                      onCalciteFilterChange={this.handleFilter}
-                      placeholder={filterPlaceholder}
-                      value={filterText}
-                      // eslint-disable-next-line react/jsx-sort-props
-                      ref={(el) => (this.filterEl = el)}
-                    />
-                    <slot
-                      name={SLOTS.filterActionsEnd}
-                      onSlotchange={this.handleFilterActionsEndSlotChange}
-                      slot={STACK_SLOTS.actionsEnd}
-                    />
-                  </calcite-stack>
+                  <calcite-filter
+                    aria-label={filterPlaceholder}
+                    disabled={loading || disabled}
+                    items={dataForFilter}
+                    onCalciteFilterChange={this.handleFilter}
+                    placeholder={filterPlaceholder}
+                    value={filterText}
+                    // eslint-disable-next-line react/jsx-sort-props
+                    ref={(el) => (this.filterEl = el)}
+                  />
                 </th>
               </tr>
             </thead>
@@ -341,14 +320,6 @@ export class List implements InteractiveComponent, LoadableComponent {
 
   handleDefaultSlotChange = (event: Event): void => {
     updateListItemChildren(getListItemChildren(event));
-  };
-
-  handleFilterActionsStartSlotChange = (event: Event): void => {
-    this.hasFilterActionsStart = slotChangeHasAssignedElement(event);
-  };
-
-  handleFilterActionsEndSlotChange = (event: Event): void => {
-    this.hasFilterActionsEnd = slotChangeHasAssignedElement(event);
   };
 
   setActiveListItem = (): void => {
