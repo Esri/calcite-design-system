235ca4249e141ecdf58a4d7acb6f721912d88d28
Matt Driscoll <mdriscoll@esri.com>
2023-06-20 17:20:30 -0700

    review fix cleanup

--- packages/calcite-components/src/components/list/list.tsx
+++ packages/calcite-components/src/components/list/list.tsx
@@ -185,12 +185,8 @@ export class List implements InteractiveComponent, LoadableComponent {
     const target = event.target as HTMLCalciteListItemElement;
     const { listItems, selectionMode } = this;
 
-    if (target.selected) {
-      listItems.forEach((listItem) => {
-        if (selectionMode === "single" || selectionMode === "single-persist") {
-          listItem.selected = listItem === target;
-        }
-      });
+    if (target.selected && (selectionMode === "single" || selectionMode === "single-persist")) {
+      listItems.forEach((listItem) => (listItem.selected = listItem === target));
     }
 
     this.updateSelectedItems();
