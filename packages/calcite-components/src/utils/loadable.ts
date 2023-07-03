40fe2ce060d7db146b2e3b85d5e8e62b67034b34
Matt Driscoll <mdriscoll@esri.com>
2023-07-03 16:25:02 -0700

    fix(action): correctly focus the button after rendering updates. (#7255)
    **Related Issue:** #6400 #5369
    
    ## Summary
    
    - `forceUpdate()` is called in `setFocus()` in order to make sure the
    internal button has been rendered.
    - The internal button is then focused after `requestAnimationFrame()` to
    ensure its been rendered.

--- packages/calcite-components/src/utils/loadable.ts
+++ packages/calcite-components/src/utils/loadable.ts
@@ -1,3 +1,5 @@
+import { Build, forceUpdate } from "@stencil/core";
+
 /**
  * This helper adds support for knowing when a component has been loaded.
  *
@@ -31,7 +33,7 @@
  *  //
  *  // --------------------------------------------------------------------------
  *
- *  async setFocus(): Promise<void> {
+ *  async myMethod(): Promise<void> {
  *    await componentLoaded(this);
  *  }
  * ```
@@ -93,14 +95,14 @@ export function setComponentLoaded(component: LoadableComponent): void {
 }
 
 /**
- * This helper util can be used to ensure a component has been loaded (The "componentDidLoad" stencil lifecycle method has been called).
+ * This helper util can be used to ensure a component has been loaded (The "componentDidLoad" Stencil lifecycle method has been called).
  *
- * Requires "setUpLoadableComponent" and "setComponentLoaded" to be called first.
+ * Requires requires `LoadableComponent` to be implemented.
  *
  * A component developer can await this method before proceeding with any logic that requires a component to be loaded first.
  *
  * ```
- * async setFocus(): Promise<void> {
+ * async myMethod(): Promise<void> {
  *   await componentLoaded(this);
  * }
  * ```
@@ -110,4 +112,32 @@ export function setComponentLoaded(component: LoadableComponent): void {
  */
 export function componentLoaded(component: LoadableComponent): Promise<void> {
   return promiseMap.get(component);
+}
+
+/**
+ * This helper util can be used to ensuring the component is loaded and rendered by the browser (The "componentDidLoad" Stencil lifecycle method has been called and any internal elements are focusable).
+ *
+ * Requires requires `LoadableComponent` to be implemented.
+ *
+ * A component developer can await this method before proceeding with any logic that requires a component to be loaded first and then an internal element be focused.
+ *
+ * ```
+ * async setFocus(): Promise<void> {
+ *   await componentFocusable(this);
+ *   this.internalElement?.focus();
+ * }
+ * ```
+ *
+ * @param component
+ * @returns Promise<void>
+ */
+export async function componentFocusable(component: LoadableComponent): Promise<void> {
+  await componentLoaded(component);
+
+  if (!Build.isBrowser) {
+    return;
+  }
+
+  forceUpdate(component);
+  return new Promise((resolve) => requestAnimationFrame(() => resolve()));
 }
