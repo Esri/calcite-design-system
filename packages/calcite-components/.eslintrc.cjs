588be40c068a337689671256d2347ea42a434b7c
JC Franco <jfranco@esri.com>
2023-07-27 23:11:39 -0700

    chore(linting): set up spelling linting rules

--- packages/calcite-components/.eslintrc.cjs
+++ packages/calcite-components/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "jest/globals": true
   },
   extends: [
+    "plugin:@cspell/recommended",
     "plugin:@esri/calcite-components/recommended",
     "plugin:@stencil-community/recommended",
     "plugin:@typescript-eslint/recommended",
