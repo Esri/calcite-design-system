cf96822c08a8b1eafe829965ff8937408cf9d66b
Ben Elan <no-reply@benelan.dev>
2023-08-14 17:16:37 -0700

    chore: add auto-define util

---
+++ packages/calcite-components-react/src/auto-define.ts
@@ -0,0 +1,12 @@
+const isBrowser = (): boolean =>
+  ![typeof window, typeof document, typeof location].includes("undefined") &&
+  [typeof process, typeof global].includes("undefined") &&
+  window.location === location &&
+  window.document === document;
+
+export function autoDefine(component: string) {
+  if (isBrowser()) {
+    return async () => (await import(`@esri/calcite-components/dist/components/${component}.js`)).defineCustomElement();
+  }
+  return undefined;
+}
