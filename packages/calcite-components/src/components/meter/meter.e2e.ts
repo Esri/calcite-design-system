2dd0644174cb0176060c9c2b6ccac42343dacc0a
Adam <adam@tirel.la>
2023-08-17 18:37:49 -0700

    Pr feedback

--- packages/calcite-components/src/components/meter/meter.e2e.ts
+++ packages/calcite-components/src/components/meter/meter.e2e.ts
@@ -1,3 +1,5 @@
+import { newE2EPage } from "@stencil/core/testing";
+import { html } from "../../../support/formatting";
 import { accessible, renders, hidden, t9n, defaults, reflects } from "../../tests/commonTests";
 
 describe("calcite-meter", () => {
@@ -97,5 +99,72 @@ describe("calcite-meter", () => {
 
   describe("translation support", () => {
     t9n("calcite-meter");
+  });
+
+  describe("correctly sets range and value properties", () => {
+    it("correctly sets range and value properties if not present", async () => {
+      const page = await newE2EPage({
+        html: html`<calcite-meter />`,
+      });
+      const meter = await page.find(`calcite-meter`);
+      page.waitForChanges();
+      expect(await meter.getProperty("min")).toBe(0);
+      expect(await meter.getProperty("max")).toBe(100);
+      expect(await meter.getProperty("low")).toBe(0);
+      expect(await meter.getProperty("high")).toBe(100);
+      expect(await meter.getProperty("value")).toBe(0);
+    });
+
+    it("correctly sets range and value properties if not present and non-default min / max set", async () => {
+      const page = await newE2EPage({
+        html: html`<calcite-meter min="2000" max="10000" />`,
+      });
+      const meter = await page.find(`calcite-meter`);
+      page.waitForChanges();
+      expect(await meter.getProperty("min")).toBe(2000);
+      expect(await meter.getProperty("max")).toBe(10000);
+      expect(await meter.getProperty("low")).toBe(2000);
+      expect(await meter.getProperty("high")).toBe(10000);
+      expect(await meter.getProperty("value")).toBe(2000);
+    });
+
+    it("correctly adjusts out of range low and high", async () => {
+      const page = await newE2EPage({
+        html: html`<calcite-meter min="10" low="200" high="30" max="25" />`,
+      });
+      const meter = await page.find(`calcite-meter`);
+      page.waitForChanges();
+      expect(await meter.getProperty("min")).toBe(10);
+      expect(await meter.getProperty("max")).toBe(25);
+      expect(await meter.getProperty("low")).toBe(10);
+      expect(await meter.getProperty("high")).toBe(25);
+      expect(await meter.getProperty("value")).toBe(10);
+    });
+
+    it("correctly adjusts out of range low and high - b", async () => {
+      const page = await newE2EPage({
+        html: html`<calcite-meter min="10" low="15" high="5" max="25" />`,
+      });
+      const meter = await page.find(`calcite-meter`);
+      page.waitForChanges();
+      expect(await meter.getProperty("min")).toBe(10);
+      expect(await meter.getProperty("max")).toBe(25);
+      expect(await meter.getProperty("low")).toBe(10);
+      expect(await meter.getProperty("high")).toBe(25);
+      expect(await meter.getProperty("value")).toBe(10);
+    });
+
+    it("correctly leaves out of range value", async () => {
+      const page = await newE2EPage({
+        html: html`<calcite-meter value="210" min="10" low="200" high="30" max="25" />`,
+      });
+      const meter = await page.find(`calcite-meter`);
+      page.waitForChanges();
+      expect(await meter.getProperty("min")).toBe(10);
+      expect(await meter.getProperty("max")).toBe(25);
+      expect(await meter.getProperty("low")).toBe(10);
+      expect(await meter.getProperty("high")).toBe(25);
+      expect(await meter.getProperty("value")).toBe(210);
+    });
   });
 });
