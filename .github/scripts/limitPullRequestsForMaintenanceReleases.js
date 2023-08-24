87cfd887d1cb04e3e8204d41c9a4cf0c5d78c673
Ben Elan <no-reply@benelan.dev>
2023-08-24 01:44:22 -0700

    chore: cleanup

--- .github/scripts/limitPullRequestsForMaintenanceReleases.js
+++ .github/scripts/limitPullRequestsForMaintenanceReleases.js
@@ -13,29 +13,48 @@ module.exports = async ({ github, context, core }) => {
     process.exit(0);
   }
 
-  const allowedLabels = ["low risk", "p - high", "p - critical", "regression"];
-
-  github.event.pull_request.labels.forEach((label) => {
-    if (allowedLabels.includes(label.name)) {
-      core.notice(`Pull request has the "${label.name}" label, which allows installs during Maintenance milestones.`);
-      process.exit(0);
-    }
+  const { data: issue } = await github.rest.issues.get({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
   });
 
+  const allowedLabels = ["low risk", "p - high", "p - critical", "regression"];
+
+  if (issue.labels.length) {
+    console.log("Pull request labels:", issue.labels);
+    issue.labels.forEach((label) => {
+      if (allowedLabels.includes(label.name)) {
+        core.notice(
+          `Pull request has the "${label.name}" label, which allows installs during Maintenance milestones. Ending run.`,
+        );
+        process.exit(0);
+      }
+    });
+  }
+
   const currentDate = new Date(Date.now());
-  for (const [index, milestone] of milestones.entries()) {
-    if (!milestone.due_on || new Date(milestone.due_on) > currentDate) {
+  for (const milestone of milestones) {
+    if (!milestone?.due_on || new Date(milestone?.due_on) < currentDate) {
+      console.log(
+        `Skipping open milestone "${milestone.title}" because it is past due or doesn't have a due date.`,
+      );
       continue;
     }
 
-    if (milestone[index - 1].title.match("Maintenance")) {
+    console.log(`The current milestone is "${milestone?.title}".`);
+    if (/Maintenance/i.test(milestone?.title)) {
       core.setFailed(
         `Installing this pull request is blocked until the Maintenance milestone ends (${
-          milestone[index - 1].due_one
-        }). Add one of the following labels to prevent this error: ${allowedLabels}.`
+          milestone?.due_on.split("T")[0]
+        }). Add one of the following labels to prevent this error: ${JSON.stringify(
+          allowedLabels,
+        )}.`,
       );
     } else {
-      core.notice("Current milestone is not a Maintenance release");
+      core.notice(
+        "The current milestone is not for a Maintenance release, ending run.",
+      );
       process.exit(0);
     }
   }
