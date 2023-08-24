f1baa65eea7b0f703cdeb6c6d5581d7558749a6c
Matt Driscoll <mdriscoll@esri.com>
2023-08-24 11:50:59 -0700

    test + bg color restore

--- packages/calcite-components/src/components/sheet/sheet.e2e.ts
+++ packages/calcite-components/src/components/sheet/sheet.e2e.ts
@@ -47,9 +47,17 @@ describe("calcite-sheet properties", () => {
   });
 
   describe("accessible", () => {
-    accessible(
-      `<calcite-sheet style="--calcite-sheet-scrim-background: #ffffff" open label="hello world">Hello everyone!</calcite-sheet>`
-    );
+    accessible(`<calcite-sheet open label="hello world">Hello everyone!</calcite-sheet>`);
+    accessible(`<calcite-sheet open label="hello world"><calcite-panel closable heading="Ultrices neque"
+    ><p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <calcite-button slot="footer" width="half" appearance="outline">tincidunt lobortis</calcite-button>
+    <calcite-button slot="footer" width="half" appearance="outline">amet porttitor</calcite-button>
+  </calcite-panel></calcite-sheet>`);
   });
 
   describe("setFocus", () => {
