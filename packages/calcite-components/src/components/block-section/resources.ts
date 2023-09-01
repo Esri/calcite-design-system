1f44f6b0e05bcaf708015432ef64c8cb660101bd
JC Franco <jfranco@esri.com>
2023-08-31 21:25:25 -0700

    fix(block, block-section): improve a11y (#7557)
    **Related Issue:** #5565 
    
    ## Summary
    
    Updated HTML to improve a11y.
    
    **Note**: this removes references to outdated `TEXT` constants, which
    were used before translations were built-in.

--- packages/calcite-components/src/components/block-section/resources.ts
+++ packages/calcite-components/src/components/block-section/resources.ts
@@ -1,19 +1,21 @@
+export const IDS = {
+  content: "content",
+  toggle: "toggle",
+};
+
 export const CSS = {
   content: "content",
+  focusGuard: "focus-guard",
   invalid: "invalid",
-  toggle: "toggle",
-  toggleSwitch: "toggle--switch",
-  toggleSwitchContent: "toggle--switch__content",
-  toggleSwitchText: "toggle--switch__text",
   sectionHeader: "section-header",
   sectionHeaderText: "section-header__text",
   statusIcon: "status-icon",
+  toggle: "toggle",
+  toggleSwitch: "toggle--switch",
+  toggleSwitchContainer: "toggle--switch-container",
+  toggleSwitchContent: "toggle--switch__content",
+  toggleSwitchText: "toggle--switch__text",
   valid: "valid",
-};
-
-export const TEXT = {
-  collapse: "Collapse",
-  expand: "Expand",
 };
 
 export const ICONS = {
