79849775be24f6db93ce979dfadb851bb0d548de
eliza <eli97736@esri.com>
2023-09-03 21:02:32 -0700

    revert unrelated eslint change

--- packages/calcite-components/src/components/action-bar/action-bar.tsx
+++ packages/calcite-components/src/components/action-bar/action-bar.tsx
@@ -407,8 +407,8 @@ export class ActionBar
     return (
       <calcite-action-group
         class={CSS.actionGroupEnd}
-        hidden={this.expandDisabled && !(this.hasActionsEnd || this.hasBottomActions)}
         label={actionsEndGroupLabel}
+        hidden={this.expandDisabled && !(this.hasActionsEnd || this.hasBottomActions)}
         layout={layout}
         scale={scale}
       >
