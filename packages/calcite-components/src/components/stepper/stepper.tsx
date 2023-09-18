b9c9dc4801abc664ae7d9bd00a93f7d6d694b70c
anveshmekala <anv11827@esri.com>
2023-09-18 11:36:32 -0500

    remove builtin translation

--- packages/calcite-components/src/components/stepper/stepper.tsx
+++ packages/calcite-components/src/components/stepper/stepper.tsx
@@ -8,23 +8,15 @@ import {
   Listen,
   Method,
   Prop,
-  State,
   VNode,
   Watch,
 } from "@stencil/core";
 
 import { focusElementInGroup, slotChangeGetAssignedElements } from "../../utils/dom";
-import {
-  connectLocalized,
-  disconnectLocalized,
-  LocalizedComponent,
-  NumberingSystem,
-} from "../../utils/locale";
+import { NumberingSystem } from "../../utils/locale";
 import { Layout, Scale } from "../interfaces";
 import { StepperItemChangeEventDetail, StepperItemKeyEventDetail } from "./interfaces";
 import { createObserver } from "../../utils/observers";
-import { connectMessages, disconnectMessages, T9nComponent, updateMessages } from "../../utils/t9n";
-import { StepperMessages } from "./assets/stepper/t9n";
 
 /**
  * @slot - A slot for adding `calcite-stepper-item` elements.
@@ -33,9 +25,8 @@ import { StepperMessages } from "./assets/stepper/t9n";
   tag: "calcite-stepper",
   styleUrl: "stepper.scss",
   shadow: true,
-  assetsDirs: ["assets"],
 })
-export class Stepper implements T9nComponent, LocalizedComponent {
+export class Stepper {
   //--------------------------------------------------------------------------
   //
   //  Public Properties
@@ -47,25 +38,6 @@ export class Stepper implements T9nComponent, LocalizedComponent {
 
   /** Defines the layout of the component. */
   @Prop({ reflect: true }) layout: Extract<"horizontal" | "vertical", Layout> = "horizontal";
-
-  /**
-   * Made into a prop for testing purposes only
-   *
-   * @internal
-   */
-  // eslint-disable-next-line @stencil-community/strict-mutable -- updated by t9n module
-  @Prop({ mutable: true }) messages: StepperMessages;
-
-  /**
-   * Use this property to override individual strings used by the component.
-   */
-  // eslint-disable-next-line @stencil-community/strict-mutable -- updated by t9n module
-  @Prop({ mutable: true }) messageOverrides: StepperMessages;
-
-  @Watch("messageOverrides")
-  onMessagesChange(): void {
-    // wired up by t9n util
-  }
 
   /** When `true`, displays the step number in the `calcite-stepper-item` heading. */
   @Prop({ reflect: true }) numbered = false;
@@ -128,8 +100,6 @@ export class Stepper implements T9nComponent, LocalizedComponent {
   connectedCallback(): void {
     this.mutationObserver?.observe(this.el, { childList: true });
     this.updateItems();
-    connectMessages(this);
-    connectLocalized(this);
   }
 
   componentDidLoad(): void {
@@ -141,14 +111,9 @@ export class Stepper implements T9nComponent, LocalizedComponent {
     }
   }
 
-  disconnectedCallback(): void {
-    disconnectMessages(this);
-    disconnectLocalized(this);
-  }
-
   render(): VNode {
     return (
-      <Host aria-label={this.messages?.label} role="region">
+      <Host aria-label={"Progress steps"} role="region">
         <slot onSlotchange={this.handleDefaultSlotChange} />
       </Host>
     );
@@ -289,15 +254,6 @@ export class Stepper implements T9nComponent, LocalizedComponent {
   //--------------------------------------------------------------------------
 
   @Element() el: HTMLCalciteStepperElement;
-
-  @State() defaultMessages: StepperMessages;
-
-  @State() effectiveLocale = "";
-
-  @Watch("effectiveLocale")
-  effectiveLocaleChange(): void {
-    updateMessages(this, this.effectiveLocale);
-  }
 
   private itemMap = new Map<HTMLCalciteStepperItemElement, { position: number; content: Node[] }>();
 
