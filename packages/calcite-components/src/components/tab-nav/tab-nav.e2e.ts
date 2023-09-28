6ad662c07b40fc4c97961f068f47206aca0c4dab
eliza <eli97736@esri.com>
2023-09-27 17:32:25 -0700

    WIP: e2e test for overflowScenarios = [right, left, both]

--- packages/calcite-components/src/components/tab-nav/tab-nav.e2e.ts
+++ packages/calcite-components/src/components/tab-nav/tab-nav.e2e.ts
@@ -1,6 +1,7 @@
-import { newE2EPage } from "@stencil/core/testing";
+import { newE2EPage, E2EPage } from "@stencil/core/testing";
 import { accessible, renders, hidden, t9n } from "../../tests/commonTests";
 import { html } from "../../../support/formatting";
+import { CSS } from "./resources";
 
 describe("calcite-tab-nav", () => {
   const tabNavHtml = "<calcite-tab-nav></calcite-tab-nav>";
@@ -97,11 +98,11 @@ describe("calcite-tab-nav", () => {
       it("should render with small scale", async () => {
         const page = await newE2EPage({
           html: `<calcite-tab-nav scale='s'>
-            <calcite-tab-title selected>Tab 1 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 2 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 3 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 4 Title</calcite-tab-title>
-          </calcite-tab-nav>`,
+              <calcite-tab-title selected>Tab 1 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 2 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 3 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 4 Title</calcite-tab-title>
+            </calcite-tab-nav>`,
         });
         const element = await page.find("calcite-tab-nav");
         expect(await (await element.getComputedStyle())["minHeight"]).toEqual("24px");
@@ -113,11 +114,11 @@ describe("calcite-tab-nav", () => {
       it("should render with medium scale", async () => {
         const page = await newE2EPage({
           html: `<calcite-tab-nav scale='m'>
-            <calcite-tab-title selected>Tab 1 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 2 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 3 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 4 Title</calcite-tab-title>
-          </calcite-tab-nav>`,
+              <calcite-tab-title selected>Tab 1 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 2 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 3 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 4 Title</calcite-tab-title>
+            </calcite-tab-nav>`,
         });
         const element = await page.find("calcite-tab-nav");
         expect(await (await element.getComputedStyle())["minHeight"]).toEqual("32px");
@@ -129,11 +130,11 @@ describe("calcite-tab-nav", () => {
       it("should render with medium scale", async () => {
         const page = await newE2EPage({
           html: `<calcite-tab-nav scale='l'>
-            <calcite-tab-title selected>Tab 1 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 2 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 3 Title</calcite-tab-title>
-            <calcite-tab-title>Tab 4 Title</calcite-tab-title>
-          </calcite-tab-nav>`,
+              <calcite-tab-title selected>Tab 1 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 2 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 3 Title</calcite-tab-title>
+              <calcite-tab-title>Tab 4 Title</calcite-tab-title>
+            </calcite-tab-nav>`,
         });
         const element = await page.find("calcite-tab-nav");
         expect(await (await element.getComputedStyle())["minHeight"]).toEqual("44px");
@@ -195,5 +196,110 @@ describe("calcite-tab-nav", () => {
 
     await page.keyboard.press("Home");
     expect(await page.evaluate(() => document.activeElement.id)).toBe("tab1");
+  });
+
+  const inlineTabsWithVariedTitleLength = html`
+    <calcite-tabs layout="inline" style="margin: 75px; width: 375px">
+      <calcite-tab-nav slot="title-group" id="testSubjectNav">
+        <calcite-tab-title selected icon-start="tabbed-view" icon-end="pen" closable> Tab 1 Title </calcite-tab-title>
+        <calcite-tab-title icon-start="tabbed-view">Tab 2 Title</calcite-tab-title>
+        <calcite-tab-title>An Ultramarathon of a Tab Title, why not.</calcite-tab-title>
+        <calcite-tab-title closable>Tab 4 Title</calcite-tab-title>
+        <calcite-tab-title>Tab 5 Title</calcite-tab-title>
+        <calcite-tab-title>Tab 6 Title</calcite-tab-title>
+      </calcite-tab-nav>
+      <calcite-tab selected>Tab 1 Content</calcite-tab>
+      <calcite-tab>Tab 2 Content</calcite-tab>
+      <calcite-tab>Tab 3 Content</calcite-tab>
+      <calcite-tab>Tab 4 Content</calcite-tab>
+      <calcite-tab>Tab 5 Content</calcite-tab>
+      <calcite-tab>Tab 6 Content</calcite-tab>
+    </calcite-tabs>
+  `;
+
+  describe("responsive tabs for inline layout", () => {
+    const overflowScenarios = ["right", "left", "both"];
+    let page: E2EPage;
+
+    beforeEach(async () => {
+      page = await newE2EPage();
+      await page.setContent(inlineTabsWithVariedTitleLength);
+    });
+
+    it("should overflow tab-titles that don't fit within the bounds of tab-nav", async () => {
+      const { tabTitlesTotalWidth, tabNavWidth } = await page.evaluate(() => {
+        const tabNav = document.getElementById("testSubjectNav") as HTMLCalciteTabNavElement;
+        const tabTitles = Array.from(document.querySelectorAll("calcite-tab-title")) as HTMLCalciteTabTitleElement[];
+
+        const tabNavWidth = tabNav.offsetWidth;
+        const tabTitlesTotalWidth = tabTitles.reduce((sum, tabTitle: HTMLElement) => {
+          return sum + tabTitle.offsetWidth;
+        }, 0);
+
+        return { tabTitlesTotalWidth, tabNavWidth };
+      });
+
+      expect(tabTitlesTotalWidth).toBeGreaterThan(tabNavWidth);
+    });
+
+    overflowScenarios.forEach(async (overflowScenario) => {
+      if (overflowScenario === "right") {
+        it("should show action buttons with correct chevrons for overflow to the right", async () => {
+          const isOverflowingRight = await page.evaluate(() => {
+            const tabNav = document.getElementById("testSubjectNav") as HTMLCalciteTabNavElement;
+            const tabTitles = Array.from(document.querySelectorAll("calcite-tab-title"));
+
+            tabNav.scrollLeft = 0;
+            const mobilePageWidth = tabNav.getBoundingClientRect().width;
+
+            const visibleTabTitles = tabTitles.filter((tabTitle) => {
+              const tabTitleRect = tabTitle.getBoundingClientRect();
+              return tabTitleRect.left >= 0 && tabTitleRect.right <= mobilePageWidth;
+            });
+            const firstRightOverflowItem = tabTitles[visibleTabTitles.length];
+
+            const isOverflowingRight = firstRightOverflowItem.getBoundingClientRect().right > mobilePageWidth;
+            return isOverflowingRight;
+          });
+          expect(isOverflowingRight).toBe(true);
+
+          expect(await page.find(`#testSubjectNav >>> .${CSS.arrowRight}`)).not.toBe(null);
+          expect(await page.find(`#testSubjectNav >>> .${CSS.arrowLeft}`)).toBe(null);
+        });
+      } else if (overflowScenario === "left") {
+        it("should show action buttons with correct chevrons for overflow to the left", async () => {
+          const { isOverflowingLeft } = await page.evaluate(async () => {
+            const tabNav = document.getElementById("testSubjectNav") as HTMLCalciteTabNavElement;
+            const tabTitles = Array.from(document.querySelectorAll("calcite-tab-title"));
+
+            const mobilePageWidth = tabNav.getBoundingClientRect().width;
+
+            tabNav.scrollLeft += tabTitles[tabTitles.length - 1].getBoundingClientRect().right;
+
+            const visibleTabTitles = tabTitles.filter((tabTitle) => {
+              const tabTitleRect = tabTitle.getBoundingClientRect();
+              return tabTitleRect.left >= 0 && tabTitleRect.right <= mobilePageWidth;
+            });
+
+            const firstLeftOverflowItem = tabTitles[visibleTabTitles.length];
+            const isOverflowingLeft = firstLeftOverflowItem.getBoundingClientRect().right < mobilePageWidth;
+
+            return { isOverflowingLeft };
+          });
+
+          expect(isOverflowingLeft).toBe(true);
+
+          expect(await page.find(`#testSubjectNav >>> .${CSS.arrowRight}`)).toBe(null);
+          expect(await page.find(`#testSubjectNav >>> .${CSS.arrowLeft}`)).not.toBe(null);
+        });
+      } else if (overflowScenario === "both") {
+        it("should show action buttons with correct chevrons for overflow to both sides", async () => {
+          await page.evaluate(() => {
+            const tabNav = document.querySelector("calcite-tab-nav") as HTMLElement;
+            tabNav.scrollLeft = tabNav.scrollWidth / 2;
+          });
+        });
+      }
+    });
   });
 });
