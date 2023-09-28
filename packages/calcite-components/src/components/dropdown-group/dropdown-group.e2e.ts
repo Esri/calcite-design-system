036b01d20f202abe184b9a450b59ddea5239beba
Matt Driscoll <mdriscoll@esri.com>
2023-09-28 11:44:12 -0700

    test

--- packages/calcite-components/src/components/dropdown-group/dropdown-group.e2e.ts
+++ packages/calcite-components/src/components/dropdown-group/dropdown-group.e2e.ts
@@ -1,4 +1,6 @@
+import { newE2EPage } from "@stencil/core/testing";
 import { defaults, hidden, reflects, renders } from "../../tests/commonTests";
+import { html } from "../../../support/formatting";
 
 describe("calcite-dropdown-group", () => {
   describe("defaults", () => {
@@ -25,5 +27,29 @@ describe("calcite-dropdown-group", () => {
 
   describe("honors hidden attribute", () => {
     hidden("calcite-dropdown-group");
+  });
+
+  it("sets selectionMode on slotted dropdown item children", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(html`<calcite-dropdown-group selection-mode="none">
+      <calcite-dropdown-item>Mountain</calcite-dropdown-item>
+      <calcite-dropdown-item>River</calcite-dropdown-item>
+    </calcite-dropdown-group>`);
+
+    await page.waitForChanges();
+
+    await page.evaluate(() => {
+      const dropdownGroup = document.querySelector("calcite-dropdown-group");
+      const newItem = document.createElement("calcite-dropdown-item");
+      newItem.innerText = "Lake";
+      dropdownGroup.appendChild(newItem);
+    });
+
+    const items = await page.findAll("calcite-dropdown-item");
+
+    items.forEach(async (item) => {
+      expect(await item.getProperty("selectionMode")).toBe("none");
+    });
   });
 });
