57e9336f4c77cda8786e1fd2430613d14a478d31
JC Franco <jfranco@esri.com>
2023-10-02 12:09:06 -0700

    add delay to all stories for consistent snapshots

--- packages/calcite-components/src/components/input-time-zone/input-time-zone.stories.ts
+++ packages/calcite-components/src/components/input-time-zone/input-time-zone.stories.ts
@@ -4,7 +4,7 @@ import { modesDarkDefault } from "../../../.storybook/utils";
 import { html } from "../../../support/formatting";
 import readme from "./readme.md";
 
-const openStoryDelayInMs = 250;
+const storyDelayInMs = 250;
 
 export default {
   title: "Components/Controls/InputTimeZone",
@@ -30,16 +30,24 @@ export const timeZoneNameMode_TestOnly = (): string => html`
 `;
 
 timeZoneNameMode_TestOnly.parameters = {
-  chromatic: { delay: openStoryDelayInMs },
+  chromatic: { delay: storyDelayInMs },
 };
 
 export const initialNameSelected_TestOnly = (): string => html`
   <calcite-input-time-zone mode="name" value="America/Ciudad_Juarez"></calcite-input-time-zone>
 `;
 
+initialNameSelected_TestOnly.parameters = {
+  chromatic: { delay: storyDelayInMs },
+};
+
 export const initialOffsetSelected_TestOnly = (): string => html`
   <calcite-input-time-zone value="-360"></calcite-input-time-zone>
 `;
+
+initialOffsetSelected_TestOnly.parameters = {
+  chromatic: { delay: storyDelayInMs },
+};
 
 export const offsetAndGroupLabelsAreLocalized_TestOnly = (): string => html`
   <calcite-input-time-zone lang="en"></calcite-input-time-zone>
@@ -48,18 +56,27 @@ export const offsetAndGroupLabelsAreLocalized_TestOnly = (): string => html`
   <calcite-input-time-zone lang="zh"></calcite-input-time-zone>
 `;
 
+offsetAndGroupLabelsAreLocalized_TestOnly.parameters = {
+  chromatic: { delay: storyDelayInMs },
+};
+
 export const offsetAndGroupLabelsBasedOnReferenceDate_TestOnly = (): string => html`
   <calcite-input-time-zone></calcite-input-time-zone>
   <calcite-input-time-zone reference-date="2023-11-28T06:31:19.129Z"></calcite-input-time-zone>
 `;
+
+offsetAndGroupLabelsBasedOnReferenceDate_TestOnly.parameters = {
+  chromatic: { delay: storyDelayInMs },
+};
 
 export const displayingTimeZoneOffsets_TestOnly = (): string => html`
   <div style="width: 450px; height: 500px;">
     <calcite-input-time-zone open></calcite-input-time-zone>
   </div>
 `;
+
 displayingTimeZoneOffsets_TestOnly.parameters = {
-  chromatic: { delay: openStoryDelayInMs },
+  chromatic: { delay: storyDelayInMs },
 };
 
 export const disabled_TestOnly = (): string => html`<calcite-input-time-zone disabled></calcite-input-time-zone>`;
@@ -67,4 +84,5 @@ export const disabled_TestOnly = (): string => html`<calcite-input-time-zone dis
 export const darkModeRTL_TestOnly = (): string => html`
   <calcite-input-time-zone dir="rtl" class="calcite-mode-dark"></calcite-input-time-zone>
 `;
+
 darkModeRTL_TestOnly.parameters = { modes: modesDarkDefault };
