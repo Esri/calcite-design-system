4be45c374cb7692faef3c34898afbc31ae50d2e2
Ali Stump <astump@esri.com>
2023-10-03 11:18:00 -0700

    fix: call correct themes file

--- packages/calcite-design-tokens/support/run.ts
+++ packages/calcite-design-tokens/support/run.ts
@@ -10,7 +10,7 @@ const __dirname = new URL(".", import.meta.url).pathname;
  * Get all themes defined int the src/$themes.json and generate a Style Dictionary output for each theme
  */
 const tokensDir = resolve(__dirname, "../src");
-const rawData = readFileSync(join(tokensDir, "themes.json"), { encoding: "utf-8" });
+const rawData = readFileSync(join(tokensDir, "$themes.json"), { encoding: "utf-8" });
 const data = JSON.parse(rawData);
 const files = await getThemes(data);
 Promise.all(files.map((theme) => run("src", "dist", theme)));
