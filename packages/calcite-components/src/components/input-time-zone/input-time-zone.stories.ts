16ef49507bf9d9215f8e906cc02c7cdce436fd7a
JC Franco <jfranco@esri.com>
2023-10-02 19:41:09 -0700

    bump storybook delay

--- packages/calcite-components/src/components/input-time-zone/input-time-zone.stories.ts
+++ packages/calcite-components/src/components/input-time-zone/input-time-zone.stories.ts
@@ -4,7 +4,7 @@ import { modesDarkDefault } from "../../../.storybook/utils";
 import { html } from "../../../support/formatting";
 import readme from "./readme.md";
 
-const storyDelayInMs = 1000;
+const storyDelayInMs = 1500;
 
 export default {
   title: "Components/Controls/InputTimeZone",
