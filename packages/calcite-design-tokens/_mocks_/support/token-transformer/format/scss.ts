8bb216e22dfa7182b75916af52edb432be4febe1
Ali Stump <astump@esri.com>
2023-10-04 10:11:56 -0700

    test: fix tests before monorepo merge
    fixes tests to align with refactors of token transformer

---
+++ packages/calcite-design-tokens/_mocks_/support/token-transformer/format/scss.ts
@@ -0,0 +1,35 @@
+import { Dictionary } from "style-dictionary/types/Dictionary";
+import * as platformMocks from "../../../mockPlatformTokens";
+/**
+ * Test Setup
+ */
+// const createPropertyFormatterCallback = jest.fn((token) => `--${token.name}: blue;`);
+const formattedVariables = jest.fn(({ dictionary, format, outputReferences }) => `${dictionary.allTokens.map(createPropertyFormatter({outputReferences, dictionary, format })).join(';\n')}`);
+// const formattedVariables = jest.fn((tokens) => tokens.dictionary.allTokens.map(createPropertyFormatterCallback).join('\n'));
+const fileHeader = jest.fn(() => "");
+const mockReturnValue = (format: "sass" | "css" | "scss") => (token) => {
+  if (token) {
+    return platformMocks.scssBase.find((v) => v.includes(token.name));
+  }
+  return format === "css" ? platformMocks.cssRoot : platformMocks.scssBase;
+};
+const createPropertyFormatter = jest.fn(
+  (obj: { outputReferences: boolean; dictionary: Dictionary; format: "sass" | "css" | "scss" }) => {
+    const returnValue = jest.fn(mockReturnValue(obj.format));
+    // returnIndex++;
+    return returnValue;
+  }
+);
+
+jest.mock("style-dictionary", () => {
+  const originalModule = jest.requireActual("style-dictionary");
+  return {
+    __esModule: false,
+    ...originalModule,
+    formatHelpers: {
+      createPropertyFormatter,
+      formattedVariables,
+      fileHeader
+    }
+  };
+});
--- packages/calcite-design-tokens/support/token-transformer/format/scss.test.ts
+++ packages/calcite-design-tokens/support/token-transformer/format/scss.test.ts
@@ -1,54 +1,19 @@
-import { tokens as mockTokens } from "../../../_mocks/mockStyleDictionaryTokens";
-import * as platformMocks from "../../../_mocks/mockPlatformTokens";
-/**
- * Test Setup
- */
-const createPropertyFormatterCallback = jest.fn((token) => `--${token.name}: blue;`);
-const formattedVariables = jest.fn((tokens) => tokens.dictionary.allTokens.map(createPropertyFormatterCallback));
-const fileHeader = jest.fn(() => "");
-const mockReturnValue = (format: "sass" | "css" | "scss") => (token) => {
-  if (token) {
-    return format === "css"
-      ? platformMocks.cssRoot.find((v) => v.includes(token.name))
-      : platformMocks.scssBase.find((v) => v.includes(token.name));
-  }
-  return format === "css" ? platformMocks.cssRoot : platformMocks.scssBase;
-};
-const createPropertyFormatter = jest.fn(
-  (obj: { outputReferences: Record<any, any>; dictionary: Dictionary; format: "sass" | "css" | "scss" }) => {
-    const returnValue = jest.fn(mockReturnValue(obj.format));
-    // returnIndex++;
-    return returnValue;
-  }
-);
-
-jest.mock("style-dictionary", () => {
-  const originalModule = jest.requireActual("style-dictionary");
-  return {
-    __esModule: false,
-    ...originalModule,
-    formatHelpers: {
-      createPropertyFormatter,
-      formattedVariables,
-      fileHeader
-    }
-  };
-});
-
+import '../../../_mocks_/support/token-transformer/format/scss';
+import { tokens as mockTokens } from "../../../_mocks_/mockStyleDictionaryTokens";
+import * as platformMocks from "../../../_mocks_/mockPlatformTokens";
 import { formatSCSS } from "./scss";
-import { Dictionary } from "style-dictionary";
 
 const mock = {
   dictionary: mockTokens,
   file: {
-    destination: "calciteLight.scss"
+    destination: "light.scss"
   },
   formattedTokenSet: [
     ...platformMocks.scssBase,
     "",
     ...platformMocks.scssMixins,
     "",
-    ...platformMocks.scssCSSRoot("calcite-theme-light")
+    ...platformMocks.scssCSSRoot("light")
   ],
   options: {}
 };
@@ -64,9 +29,5 @@ describe("formatting SCSS Variable output", () => {
   it("should transform token names and values into SCSS variables", () => {
     expect(scssFile).toContain(platformMocks.scssBase[0]);
     expect(scssFile).toContain(platformMocks.scssBase[1]);
-  });
-
-  it("should contain a mixin", () => {
-    expect(scssFile).toContain(platformMocks.scssMixins[0]);
   });
 });
