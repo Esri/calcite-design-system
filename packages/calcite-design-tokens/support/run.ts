aaf99f3f23d3d52a265ea756f499eca91bdbe849
Ali Stump <astump@esri.com>
2023-10-05 11:55:18 -0700

    fix: token-transformer run to use esm

--- packages/calcite-design-tokens/support/run.ts
+++ packages/calcite-design-tokens/support/run.ts
@@ -1,13 +1,18 @@
 import { readFileSync } from "fs";
-import { getThemes } from "../tools/dist/getThemes.js";
-import { run } from "../tools/dist/sd-run.js";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import { getThemes } from "./token-transformer/getThemes.js";
+import { run } from "./token-transformer/sd-run.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * Get all themes defined int the tokens/$themes.json and generate a Style Dictionary output for each theme
  */
-const rawData = readFileSync("tokens/$themes.json", { encoding: "utf-8" });
+const rawData = readFileSync(resolve(__dirname, '../src/$themes.json'), { encoding: "utf-8" });
 const data = JSON.parse(rawData);
 
 getThemes(data).then((themes) =>
-  Promise.all(themes.map((theme) => run("tokens", "dist", theme)))
+  Promise.all(themes.map((theme) => run("src", "dist", theme)))
 );
