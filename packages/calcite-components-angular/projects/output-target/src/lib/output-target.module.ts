b28349a51317b219dceecc57aa806ce2eed065d5
Ben Elan <no-reply@benelan.dev>
2023-10-26 14:20:25 -0700

    chore: WIP

---
+++ packages/calcite-components-angular/projects/output-target/src/lib/output-target.module.ts
@@ -0,0 +1,7 @@
+import { DIRECTIVES } from './stencil-generated';
+
+@NgModule({
+  declarations: [...DIRECTIVES],
+  exports: [...DIRECTIVES],
+})
+export class OutputTargetModule {}
---
+++ packages/calcite-components-angular/projects/output-target/src/public-api.ts
@@ -0,0 +1,3 @@
+export * from './lib/output-target.module';
+export { DIRECTIVES } from './lib/stencil-generated';
+export * from './lib/stencil-generated/components';
