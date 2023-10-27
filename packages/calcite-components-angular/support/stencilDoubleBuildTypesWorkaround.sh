3423fd4ca8e363d2b00f96ea41011989468eb89a
Ben Elan <no-reply@benelan.dev>
2023-10-27 12:53:26 -0700

    chore: more types stuff

--- packages/calcite-components-angular/support/stencilDoubleBuildTypesWorkaround.sh
+++ packages/calcite-components-angular/support/stencilDoubleBuildTypesWorkaround.sh
@@ -8,5 +8,9 @@
 # the Angular wrapper from throwing build errors.
 
 # Only build if the types are not generated correctly
-npm --workspace @esri/calcite-components run util:test-types &&
-    npm --workspace @esri/calcite-components run build
+! npm --workspace @esri/calcite-components run util:test-types &&
+    # Use --force to prevent reading the cache from the previous build,
+    # which will hit since there are (likely) no changes to the source code
+    npx turbo run build --filter=@esri/calcite-components --force ||
+    # Don't fail the build if calcite-components types were already generated correctly
+    true
