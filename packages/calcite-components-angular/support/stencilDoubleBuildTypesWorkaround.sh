d1675d1f7331a5b1a6225a5e20f09b08f94ad188
Ben Elan <no-reply@benelan.dev>
2023-10-31 04:20:37 -0700

    ci: prevent github action from displaying error annotations for the type test results

--- packages/calcite-components-angular/support/stencilDoubleBuildTypesWorkaround.sh
+++ packages/calcite-components-angular/support/stencilDoubleBuildTypesWorkaround.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
 
-# This script re-builds the stencil web components if the
-# types aren't generated correctly as a workaround for
-# https://github.com/ionic-team/stencil/issues/3239
+# This script re-builds the Stencil web components if the types aren't generated correctly.
+# Workaround for: https://github.com/ionic-team/stencil/issues/3239
 #
-# It runs in the prebuild NPM script hook to prevent
-# the Angular wrapper from throwing build errors.
+# The patch needs to run in the prebuild NPM script hook to prevent build errors for the Angular wrapper.
 
 # Only build if the types are not generated correctly
-! npm --workspace @esri/calcite-components run util:test-types &&
+if ! npm --workspace @esri/calcite-components run util:test-types >/dev/null 2>&1; then
+    echo "Re-building @esri/calcite-components due to missing or incorrect types"
     # Use --force to prevent reading the cache from the previous build,
     # which will hit since there are (likely) no changes to the source code
-    npx turbo run build --filter=@esri/calcite-components --force ||
-    # Don't fail the build if calcite-components types were already generated correctly
-    true
+    npx turbo run build --filter=@esri/calcite-components --force
+fi
