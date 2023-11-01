c256045976c937c5a24602c9434fcf1970e7a2f9
Anveshreddy mekala <anv11827@esri.com>
2023-11-01 12:43:45 -0500

    fix(input-number): prevent mutating value when pointerUp is emitted. (#8077)
    **Related Issue:** #7188
    
    ## Summary
    
    - Prevents increasing/decreasing the value in `calcite-input-number`
    when the pointer is moved away from the increment/decrement buttons .
    - Emitting pointerUp event also prevents increasing/decreasing of the
    value.

--- packages/calcite-components/src/components/input-number/input-number.e2e.ts
+++ packages/calcite-components/src/components/input-number/input-number.e2e.ts
@@ -12,7 +12,7 @@ import {
   renders,
   t9n,
 } from "../../tests/commonTests";
-import { getElementXY, selectText } from "../../tests/utils";
+import { getElementRect, getElementXY, selectText } from "../../tests/utils";
 import { letterKeys, numberKeys } from "../../utils/key";
 import { locales, numberStringFormatter } from "../../utils/locale";
 
@@ -1708,5 +1708,32 @@ describe("calcite-input-number", () => {
 
   describe("translation support", () => {
     t9n("calcite-input-number");
+  });
+
+  it("should stop increasing the value when pointer is moved away from the increment button", async () => {
+    const page = await newE2EPage();
+    await page.setContent("<calcite-input-number></calcite-input-number>");
+    const inputNumber = await page.find("calcite-input-number");
+    expect(await inputNumber.getProperty("value")).toBe("");
+
+    const incrementButtonRect = await getElementRect(page, "calcite-input-number", "button");
+    await page.mouse.move(
+      incrementButtonRect.left + incrementButtonRect.width / 2,
+      incrementButtonRect.top + incrementButtonRect.height / 2
+    );
+    await page.mouse.down();
+    await page.waitForChanges();
+    // timeout is used to simulate long press.
+    await page.waitForTimeout(3000);
+    expect(await inputNumber.getProperty("value")).not.toBe("");
+
+    const value = await inputNumber.getProperty("value");
+    await page.mouse.move(incrementButtonRect.x, 2 * incrementButtonRect.bottom);
+    await page.waitForChanges();
+    expect(await inputNumber.getProperty("value")).toEqual(value);
+
+    await page.mouse.up();
+    await page.waitForChanges();
+    expect(await inputNumber.getProperty("value")).toEqual(value);
   });
 });
