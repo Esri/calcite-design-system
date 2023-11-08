6aa6b8cdf7742d715ceb88d7aa748dd53af21e24
JC Franco <jfranco@esri.com>
2023-11-08 14:56:24 -0800

    update icon util spec test

--- packages/calcite-components/stencil.config.ts
+++ packages/calcite-components/stencil.config.ts
@@ -142,7 +142,6 @@ export const create: () => Config = () => ({
   testing: {
     watchPathIgnorePatterns: ["<rootDir>/../../node_modules", "<rootDir>/dist", "<rootDir>/www", "<rootDir>/hydrate"],
     moduleNameMapper: {
-      "^/assets/(.*)$": "<rootDir>/src/tests/iconPathDataStub.ts",
       "^lodash-es$": "lodash",
     },
     setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
