f0130b2eba2481a1e1415ebaea3c8f330751b223
JC Franco <jfranco@esri.com>
2023-11-14 23:50:28 -0800

    user proper text prop

--- packages/calcite-components/src/components/alert/alert.stories.ts
+++ packages/calcite-components/src/components/alert/alert.stories.ts
@@ -235,16 +235,16 @@ const breakpointsStoryTemplate = html`
   <calcite-alert icon open scale="{scale}">
     <div slot="title">title</div>
     <div slot="message">message</div>
-    <calcite-action scale="{scale}" slot="actions-end" text-enabled title="Tips" icon="lightbulb"></calcite-action>
-    <calcite-action scale="{scale}" slot="actions-end" text-enabled title="Get info" icon="attachment"></calcite-action>
-    <calcite-action scale="{scale}" slot="actions-end" text-enabled title="Contact us" icon="phone"></calcite-action>
+    <calcite-action scale="{scale}" slot="actions-end" text-enabled text="Tips" icon="lightbulb"></calcite-action>
+    <calcite-action scale="{scale}" slot="actions-end" text-enabled text="Get info" icon="attachment"></calcite-action>
+    <calcite-action scale="{scale}" slot="actions-end" text-enabled text="Contact us" icon="phone"></calcite-action>
   </calcite-alert>
   <calcite-alert icon scale="{scale}">
     <div slot="title">title</div>
     <div slot="message">message</div>
-    <calcite-action scale="{scale}" slot="actions-end" text-enabled title="Tips" icon="lightbulb"></calcite-action>
-    <calcite-action scale="{scale}" slot="actions-end" text-enabled title="Get info" icon="attachment"></calcite-action>
-    <calcite-action scale="{scale}" slot="actions-end" text-enabled title="Contact us" icon="phone"></calcite-action>
+    <calcite-action scale="{scale}" slot="actions-end" text-enabled text="Tips" icon="lightbulb"></calcite-action>
+    <calcite-action scale="{scale}" slot="actions-end" text-enabled text="Get info" icon="attachment"></calcite-action>
+    <calcite-action scale="{scale}" slot="actions-end" text-enabled text="Contact us" icon="phone"></calcite-action>
   </calcite-alert>
   <script>
     (async function () {
