5eaad557e96a0a900ef4be31b07ffb5c3757ab82
JC Franco <jfranco@esri.com>
2023-11-16 10:34:06 -0800

    style: fix linting violations

--- packages/eslint-plugin-calcite-components/tests/lib/rules/strict-boolean-attributes/strict-boolean-attributes.spec.ts
+++ packages/eslint-plugin-calcite-components/tests/lib/rules/strict-boolean-attributes/strict-boolean-attributes.spec.ts
@@ -8,22 +8,22 @@ const projectPath = path.resolve(__dirname, "../../../tsconfig.json");
 describe("stencil rules", () => {
   const files = {
     good: path.resolve(__dirname, "strict-boolean-attributes.good.tsx"),
-    wrong: path.resolve(__dirname, "strict-boolean-attributes.wrong.tsx")
+    wrong: path.resolve(__dirname, "strict-boolean-attributes.wrong.tsx"),
   };
   ruleTester(projectPath).run("strict-boolean-attributes", rule, {
     valid: [
       {
         code: fs.readFileSync(files.good, "utf8"),
-        filename: files.good
-      }
+        filename: files.good,
+      },
     ],
 
     invalid: [
       {
         code: fs.readFileSync(files.wrong, "utf8"),
         filename: files.wrong,
-        errors: 1
-      }
-    ]
+        errors: 1,
+      },
+    ],
   });
 });
