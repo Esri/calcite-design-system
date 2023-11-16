97fe56ba60bf42744a3183b6de4a4d733f723e45
JC Franco <jfranco@esri.com>
2023-11-16 10:55:35 -0800

    style: fix linting violations (#8189)
    **Related Issue:** N/A
    
    ## Summary
    
    Extracts linting fixes from
    https://github.com/Esri/calcite-design-system/pull/6958/files.

--- packages/eslint-plugin-calcite-components/jest.config.ts
+++ packages/eslint-plugin-calcite-components/jest.config.ts
@@ -8,12 +8,12 @@ const config = {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
+      statements: 80,
+    },
   },
   transform: {
-    "^.+\\.ts$": "ts-jest"
-  }
+    "^.+\\.ts$": "ts-jest",
+  },
 };
 
 export default config;
