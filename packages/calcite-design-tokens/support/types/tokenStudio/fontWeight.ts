e887df3d68dc2460b0cfe464c1e374376e4c7ddd
JC Franco <jfranco@esri.com>
2023-11-20 01:46:21 -0800

    set up fonts + custom dictionary to clean up cspell warnings

--- packages/calcite-design-tokens/support/types/tokenStudio/fontWeight.ts
+++ packages/calcite-design-tokens/support/types/tokenStudio/fontWeight.ts
@@ -5,41 +5,21 @@ export const FontWeight = {
   // This font weight name comes from token studio transformers
   // eslint-disable-next-line @cspell/spellchecker
   buch: 400,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   demi: 600,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   demibold: 600,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   dreiviertelfett: 700,
   extrabold: 800,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   extrafett: 900,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   extraleicht: 200,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   extralight: 200,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   fett: 800,
   hairline: 1,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   halbfett: 600,
   heavy: 900,
   // This font weight name comes from token studio transformers
   // eslint-disable-next-line @cspell/spellchecker
   kraeftig: 500,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   kräftig: 500,
-  // This font weight name comes from token studio transformers
-  // eslint-disable-next-line @cspell/spellchecker
   leicht: 300,
   light: 300,
   medium: 500,
