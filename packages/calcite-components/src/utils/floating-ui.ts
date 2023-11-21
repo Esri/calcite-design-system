25a9c3fed117bc772d964d581496428e95331ed1
Matt Driscoll <mdriscoll@esri.com>
2023-11-21 11:34:56 -0800

    cleanup

--- packages/calcite-components/src/utils/floating-ui.ts
+++ packages/calcite-components/src/utils/floating-ui.ts
@@ -27,7 +27,7 @@ import { offsetParent } from "composed-offset-position";
   }
 })();
 
-function roundByDPR(value) {
+function roundByDPR(value: number): number {
   const dpr = window.devicePixelRatio || 1;
   return Math.round(value * dpr) / dpr;
 }
