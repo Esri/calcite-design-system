f74ceae891bb5acc3d8f144b468a58915858cfe0
Anveshreddy mekala <anv11827@esri.com>
2023-11-28 17:36:21 -0600

    refactor(input,input-text,input-number): updates `alignmen`t property type (#8289)
    **Related Issue:** # N/A
    
    ## Summary
    
    Update `alignment` property type to use `Alignment` interface.

--- packages/calcite-components/src/components/input-number/input-number.tsx
+++ packages/calcite-components/src/components/input-number/input-number.tsx
@@ -17,7 +17,7 @@ import {
   isPrimaryPointerButton,
   setRequestedIcon,
 } from "../../utils/dom";
-import { Position, Scale, Status } from "../interfaces";
+import { Alignment, Scale, Status } from "../interfaces";
 
 import {
   connectForm,
@@ -93,7 +93,7 @@ export class InputNumber
   //--------------------------------------------------------------------------
 
   /** Specifies the text alignment of the component's value. */
-  @Prop({ reflect: true }) alignment: Position = "start";
+  @Prop({ reflect: true }) alignment: Extract<"start" | "end", Alignment> = "start";
 
   /**
    * When `true`, the component is focused on page load. Only one element can contain `autofocus`. If multiple elements have `autofocus`, the first element will receive focus.
