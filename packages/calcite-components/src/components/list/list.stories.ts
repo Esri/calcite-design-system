c704ac1a56ea05cff1f3da0bf31c86ae8bfe07f4
Matt Driscoll <mdriscoll@esri.com>
2023-11-28 12:15:26 -0800

    feat(list-item): Add dragDisabled property. #8284

--- packages/calcite-components/src/components/list/list.stories.ts
+++ packages/calcite-components/src/components/list/list.stories.ts
@@ -697,6 +697,9 @@ export const sortableList_TestOnly = (): string => html`<calcite-list
   <calcite-list-item disabled label="test4" value="test4" description="hello world 4">
     <calcite-icon icon="compass" slot="content-start" style="color: var(--calcite-ui-success)"></calcite-icon>
   </calcite-list-item>
+  <calcite-list-item drag-disabled label="test5" value="test5" description="hello world 55">
+    <calcite-icon icon="compass" slot="content-start" style="color: var(--calcite-ui-success)"></calcite-icon>
+  </calcite-list-item>
 </calcite-list>`;
 
 export const sortableNestedList_TestOnly = (): string => html`<calcite-list
