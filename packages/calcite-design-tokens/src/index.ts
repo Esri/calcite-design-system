7fc1513e700009e0b3427f19d75c43c4e846791a
Ali Stump <alisonailea@users.noreply.github.com>
2023-11-29 12:16:27 -0800

    refactor(component tokens): remove old component tokens (#8280)
    **Related Issue:** #8278
    
    ## Summary
    
    These component tokens are out of date and should not be used. To avoid
    confusion and reduce the size of the repo we should remove these files.

--- packages/calcite-design-tokens/src/index.ts
+++ packages/calcite-design-tokens/src/index.ts
@@ -1,7 +1,6 @@
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import { createCalciteTokenFiles } from "../support/token-transformer/helpers/createCalciteTokenFiles.js";
-import { getFiles } from "../support/token-transformer/helpers/getFiles.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,20 +15,5 @@ export const globalTokens = await createCalciteTokenFiles({
   path: resolve(__dirname, "semantic"),
   references: coreTokens.source,
 });
-
-export const componentTokens = await getFiles(resolve(__dirname, "component")).then((components) =>
-  Promise.all(
-    Object.entries(components).map(([name, path]) =>
-      createCalciteTokenFiles({
-        name,
-        path,
-        references: globalTokens.source,
-        options: {
-          outputReferences: true,
-        },
-      })
-    )
-  )
-);
 
 export { config } from "./$config.js";
