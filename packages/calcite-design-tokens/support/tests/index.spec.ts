236784a1a39305635b49fa29f021de8035bb4d1f
Ali Stump <alisonailea@users.noreply.github.com>
2023-11-30 12:56:31 -0800

    test(tokens): fix token platform output shape tests (#8298)
    **Related Issue:** n/a
    
    ## Summary
    Fixes the token tests for the breaking changes in RC

--- packages/calcite-design-tokens/support/tests/index.spec.ts
+++ packages/calcite-design-tokens/support/tests/index.spec.ts
@@ -1,20 +1,25 @@
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
-describe.skip("generated tokens", () => {
+describe("generated tokens", () => {
   describe("CSS", () => {
     it("global should match", () => {
-      let global = readFileSync(resolve(__dirname, "../../dist/css/calcite-headless.css"), "utf-8");
+      let global = readFileSync(resolve(__dirname, "../../dist/css/global.css"), "utf-8");
       global = global.slice(global.indexOf("*/") + 1);
       expect(global).toMatchSnapshot();
     });
+    it("core should match", () => {
+      let core = readFileSync(resolve(__dirname, "../../dist/css/core.css"), "utf-8");
+      core = core.slice(core.indexOf("*/") + 1);
+      expect(core).toMatchSnapshot();
+    });
     it("light should match", () => {
-      let light = readFileSync(resolve(__dirname, "../../dist/css/calcite-light.css"), "utf-8");
+      let light = readFileSync(resolve(__dirname, "../../dist/css/light.css"), "utf-8");
       light = light.slice(light.indexOf("*/") + 1);
       expect(light).toMatchSnapshot();
     });
     it("dark should match", () => {
-      let dark = readFileSync(resolve(__dirname, "../../dist/css/calcite-light.css"), "utf-8");
+      let dark = readFileSync(resolve(__dirname, "../../dist/css/dark.css"), "utf-8");
       dark = dark.slice(dark.indexOf("*/") + 1);
       expect(dark).toMatchSnapshot();
     });
@@ -22,92 +27,47 @@ describe.skip("generated tokens", () => {
 
   describe("SCSS", () => {
     it("global should match", () => {
-      let global = readFileSync(resolve(__dirname, "../../dist/scss/calcite-headless.scss"), "utf-8");
+      let global = readFileSync(resolve(__dirname, "../../dist/scss/global.scss"), "utf-8");
       global = global.slice(global.indexOf("*/") + 1);
-      expect(global).toMatchSnapshot();
-    });
-    it("light should match", () => {
-      let light = readFileSync(resolve(__dirname, "../../dist/scss/calcite-light.scss"), "utf-8");
-      light = light.slice(light.indexOf("*/") + 1);
-      expect(light).toMatchSnapshot();
-    });
-    it("dark should match", () => {
-      let dark = readFileSync(resolve(__dirname, "../../dist/scss/calcite-light.scss"), "utf-8");
-      dark = dark.slice(dark.indexOf("*/") + 1);
-      expect(dark).toMatchSnapshot();
-    });
-  });
-
-  describe("JS", () => {
-    it("global should match", () => {
-      let global = readFileSync(resolve(__dirname, "../../dist/js/calcite-headless.js"), "utf-8");
-      global = global.slice(global.indexOf("*/") + 1);
-      expect(global).toMatchSnapshot();
-    });
-    it("light should match", () => {
-      let light = readFileSync(resolve(__dirname, "../../dist/js/calcite-light.js"), "utf-8");
-      light = light.slice(light.indexOf("*/") + 1);
-      expect(light).toMatchSnapshot();
-    });
-    it("dark should match", () => {
-      let dark = readFileSync(resolve(__dirname, "../../dist/js/calcite-light.js"), "utf-8");
-      dark = dark.slice(dark.indexOf("*/") + 1);
-      expect(dark).toMatchSnapshot();
-    });
-    it("types should match", () => {
-      let global = readFileSync(resolve(__dirname, "../../dist/js/calcite-headless.d.ts"), "utf-8");
-      let light = readFileSync(resolve(__dirname, "../../dist/js/calcite-light.d.ts"), "utf-8");
-      let dark = readFileSync(resolve(__dirname, "../../dist/js/calcite-dark.d.ts"), "utf-8");
-
-      global = global.slice(global.indexOf("*/") + 1);
-      light = light.slice(light.indexOf("*/") + 1);
-      dark = dark.slice(dark.indexOf("*/") + 1);
-
-      expect(global).toMatchSnapshot();
-      expect(light).toMatchSnapshot();
-      expect(dark).toMatchSnapshot();
-    });
-  });
-
-  describe("JSON", () => {
-    it("global should match", () => {
-      const global = readFileSync(resolve(__dirname, "../../dist/js/global.json"), "utf-8");
       expect(global).toMatchSnapshot();
     });
     it("core should match", () => {
-      const core = readFileSync(resolve(__dirname, "../../dist/js/core.json"), "utf-8");
+      let core = readFileSync(resolve(__dirname, "../../dist/scss/core.scss"), "utf-8");
+      core = core.slice(core.indexOf("*/") + 1);
       expect(core).toMatchSnapshot();
+    });
+    it("light should match", () => {
+      let light = readFileSync(resolve(__dirname, "../../dist/scss/light.scss"), "utf-8");
+      light = light.slice(light.indexOf("*/") + 1);
+      expect(light).toMatchSnapshot();
+    });
+    it("dark should match", () => {
+      let dark = readFileSync(resolve(__dirname, "../../dist/scss/dark.scss"), "utf-8");
+      dark = dark.slice(dark.indexOf("*/") + 1);
+      expect(dark).toMatchSnapshot();
     });
   });
 
   describe("ES6", () => {
     it("global should match", () => {
-      let global = readFileSync(resolve(__dirname, "../../dist/es6/calcite-headless.js"), "utf-8");
+      let global = readFileSync(resolve(__dirname, "../../dist/es6/global.js"), "utf-8");
       global = global.slice(global.indexOf("*/") + 1);
       expect(global).toMatchSnapshot();
     });
-    it("light should match", () => {
-      let light = readFileSync(resolve(__dirname, "../../dist/es6/calcite-light.js"), "utf-8");
-      light = light.slice(light.indexOf("*/") + 1);
-      expect(light).toMatchSnapshot();
-    });
-    it("dark should match", () => {
-      let dark = readFileSync(resolve(__dirname, "../../dist/es6/calcite-light.js"), "utf-8");
-      dark = dark.slice(dark.indexOf("*/") + 1);
-      expect(dark).toMatchSnapshot();
+    it("core should match", () => {
+      let core = readFileSync(resolve(__dirname, "../../dist/es6/core.js"), "utf-8");
+      core = core.slice(core.indexOf("*/") + 1);
+      expect(core).toMatchSnapshot();
     });
     it("types should match", () => {
-      let light = readFileSync(resolve(__dirname, "../../dist/es6/calcite-light.d.ts"), "utf-8");
-      let dark = readFileSync(resolve(__dirname, "../../dist/es6/calcite-dark.d.ts"), "utf-8");
-      let global = readFileSync(resolve(__dirname, "../../dist/es6/calcite-headless.d.ts"), "utf-8");
+      let core = readFileSync(resolve(__dirname, "../../dist/es6/core.d.ts"), "utf-8");
+      let global = readFileSync(resolve(__dirname, "../../dist/es6/global.d.ts"), "utf-8");
 
       global = global.slice(global.indexOf("*/") + 1);
-      light = light.slice(light.indexOf("*/") + 1);
-      dark = dark.slice(dark.indexOf("*/") + 1);
+      core = core.slice(core.indexOf("*/") + 1);
 
       expect(global).toMatchSnapshot();
-      expect(light).toMatchSnapshot();
-      expect(dark).toMatchSnapshot();
+      expect(core).toMatchSnapshot();
     });
   });
 });
