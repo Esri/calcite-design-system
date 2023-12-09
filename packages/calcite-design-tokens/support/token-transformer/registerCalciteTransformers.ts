8ee543b8fcd224e9a98cbfeb3bc6d39d15477209
Ali Stump <astump@esri.com>
2023-12-08 18:01:33 -0800

    Revert "feat(token transformer): allow font family tokens to output fallbacks"
    This reverts commit bc2c8c922748cfcb826dfe4200d6b1358524aac8.

--- packages/calcite-design-tokens/support/token-transformer/registerCalciteTransformers.ts
+++ packages/calcite-design-tokens/support/token-transformer/registerCalciteTransformers.ts
@@ -18,7 +18,6 @@ import { registerValueRGBA } from "./styleDictionary/transformer/value/valueRGBA
 import { registerNameSpacePath } from "./styleDictionary/transformer/name/nameSpacePath.js";
 import { registerFormatterDocs } from "./styleDictionary/formatter/docs.js";
 import { registerValueToREM } from "./styleDictionary/transformer/value/valueToREM.js";
-import { registerValueFontFamilyWithFallbacks } from "./styleDictionary/transformer/value/valueFontFamilyFallbacks.js";
 
 export async function registerCalciteTransformers(sd: StyleDictionary): Promise<void> {
   // Here we are registering the Transforms provided by Token Studio however,
@@ -28,22 +27,21 @@ export async function registerCalciteTransformers(sd: StyleDictionary): Promise<
   await registerTransforms(sd, {
     expand: false,
   });
+  registerValueRGBA(sd);
+  registerValueEvaluateMath(sd);
   registerAttributePlatformNames(sd);
   registerCustomJSONParser(sd);
   registerFilterSource(sd);
   registerFormatterCss(sd);
-  registerFormatterDocs(sd);
-  registerFormatterJs(sd);
   registerFormatterScss(sd);
+  registerFormatterJs(sd);
   registerNameCamelCase(sd);
   registerNameJoinPath(sd);
   registerNameKebabCase(sd);
-  registerNameSpacePath(sd);
   registerValueAlignFontWeightAndStyles(sd);
   registerValueAssetToken(sd);
-  registerValueEvaluateMath(sd);
-  registerValueFontFamilyWithFallbacks(sd);
-  registerValueRGBA(sd);
   registerValueStringWrapper(sd);
+  registerNameSpacePath(sd);
+  registerFormatterDocs(sd);
   registerValueToREM(sd);
 }
