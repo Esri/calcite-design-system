b3bd289af4111a15abeb5f9894fe97f3e94f5960
Matt Driscoll <mdriscoll@esri.com>
2023-12-21 17:38:32 -0800

    tests

--- packages/calcite-components/src/components/list/list.e2e.ts
+++ packages/calcite-components/src/components/list/list.e2e.ts
@@ -927,7 +927,7 @@ describe("calcite-list", () => {
       await page.keyboard.press("Tab");
       await page.keyboard.press("Tab");
       await page.keyboard.press("Space");
-      expect(await handle.getProperty("activated")).toBe(true);
+      expect(await handle.getProperty("selected")).toBe(true);
       await page.waitForChanges();
 
       let totalMoves = 0;
@@ -1037,7 +1037,7 @@ describe("calcite-list", () => {
       );
 
       await page.keyboard.press("Space");
-      expect(await handle.getProperty("activated")).toBe(true);
+      expect(await handle.getProperty("selected")).toBe(true);
       await page.waitForChanges();
 
       expect(assistiveTextElement.textContent).toBe(
@@ -1051,7 +1051,7 @@ describe("calcite-list", () => {
 
       await page.keyboard.press("ArrowDown");
       await page.waitForChanges();
-      expect(await handle.getProperty("activated")).toBe(true);
+      expect(await handle.getProperty("selected")).toBe(true);
       await page.waitForTimeout(debounceTimeout);
 
       startIndex += 1;
@@ -1082,7 +1082,7 @@ describe("calcite-list", () => {
       await page.keyboard.press("ArrowUp");
       await page.keyboard.press("Space");
       await page.waitForChanges();
-      expect(await handle.getProperty("activated")).toBe(false);
+      expect(await handle.getProperty("selected")).toBe(false);
     });
   });
 });
