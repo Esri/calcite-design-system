72a1ce4dd8d94ed092c4c24c37e7209eff67e544
Ben Elan <no-reply@benelan.dev>
2024-01-08 17:27:57 -0800

    feat(checkbox, combobox, input-date-picker, input-time-picker, segmented-control, select): add required property (#8517)
    **Related Issue:** #8057
    
    ## Summary
    
    Remove the `@internal` tag from the `required` property on form
    components.
    
    ---------
    
    Co-authored-by: Kitty Hurley <khurley@esri.com>
    Co-authored-by: Matt Driscoll <mdriscoll@esri.com>

--- packages/calcite-components/src/components/checkbox/checkbox.tsx
+++ packages/calcite-components/src/components/checkbox/checkbox.tsx
@@ -91,11 +91,7 @@ export class Checkbox
    */
   @Prop({ reflect: true }) name: string;
 
-  /**
-   * When `true`, the component must have a value in order for the form to submit.
-   *
-   * @internal
-   */
+  /** When `true`, the component must have a value in order for the form to submit. */
   @Prop({ reflect: true }) required = false;
 
   /** Specifies the size of the component. */
