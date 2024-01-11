0bdf8381383802686f317f1f37216b26d9d67371
Ben Elan <no-reply@benelan.dev>
2024-01-11 13:29:54 -0800

    test(radio-button-group): separate validation-message into a NoTest story

--- packages/calcite-components/src/components/radio-button-group/radio-button-group.stories.ts
+++ packages/calcite-components/src/components/radio-button-group/radio-button-group.stories.ts
@@ -19,9 +19,40 @@ export const simple = (): string => html`
     ${boolean("hidden", false)}
     layout="${select("layout", ["horizontal", "vertical"], "horizontal")}"
     scale="${select("scale", ["s", "m", "l"], "m")}"
-    status="${select("status", ["idle", "invalid", "valid"], "idle")}"
+  >
+    <calcite-label layout="inline">
+      <calcite-radio-button value="react"></calcite-radio-button>
+      React
+    </calcite-label>
+    <calcite-label layout="inline">
+      <calcite-radio-button value="ember"></calcite-radio-button>
+      Ember
+    </calcite-label>
+    <calcite-label layout="inline">
+      <calcite-radio-button value="angular"></calcite-radio-button>
+      Angular
+    </calcite-label>
+    <calcite-label layout="inline">
+      <calcite-radio-button value="vue"></calcite-radio-button>
+      Vue
+    </calcite-label>
+  </calcite-radio-button-group>
+`;
+
+// We created a separate story for validation-message because it is not possible
+// to set a text knob's value to undefined. Unfortunately, this makes the CSS
+// attribute selector truthy, which sets "--calcite-label-margin-bottom: 0;",
+// causing a Chromatic diff. See calcite-radio-button-group.scss.
+export const validationMessage_NoTest = (): string => html`
+  <calcite-radio-button-group
+    name="simple"
+    ${boolean("disabled", false)}
+    ${boolean("hidden", false)}
+    layout="${select("layout", ["horizontal", "vertical"], "horizontal")}"
+    scale="${select("scale", ["s", "m", "l"], "m")}"
+    status="${select("status", ["idle", "invalid", "valid"], "invalid")}"
     validation-icon="${select("validation-icon", ["", ...iconNames], "")}"
-    validation-message="${text("validation-message", "")}"
+    validation-message="${text("validation-message", "Please fill out this field.")}"
   >
     <calcite-label layout="inline">
       <calcite-radio-button value="react"></calcite-radio-button>
