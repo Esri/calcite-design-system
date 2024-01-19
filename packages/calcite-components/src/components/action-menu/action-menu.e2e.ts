dd68da00bdf1922ceb410965c05dfd4cdbb775e5
Matt Driscoll <mdriscoll@esri.com>
2024-01-19 13:34:24 -0800

    Revert "feat(action-menu): Close menu on blur instead of on tab key. (#8577)"
    This reverts commit ccfbd0c69f3f83149808f16dfaa389dc463c2442.
    
    # Conflicts:
    #	packages/calcite-components/src/components/action-menu/action-menu.e2e.ts

--- packages/calcite-components/src/components/action-menu/action-menu.e2e.ts
+++ packages/calcite-components/src/components/action-menu/action-menu.e2e.ts
@@ -402,13 +402,13 @@ describe("calcite-action-menu", () => {
       expect(await trigger.getProperty("active")).toBe(false);
     });
 
-    it.skip("should close on blur", async () => {
+    it.skip("should handle TAB navigation", async () => {
       const page = await newE2EPage({
         html: html`<calcite-action-menu>
-            <calcite-action id="first" text="Add" icon="plus" text-enabled></calcite-action>
-            <calcite-action id="second" text="Add" icon="minus" text-enabled></calcite-action>
-            <calcite-action id="third" text="Add" icon="banana" text-enabled></calcite-action> </calcite-action-menu
-          ><button>test</button>`,
+          <calcite-action id="first" text="Add" icon="plus" text-enabled></calcite-action>
+          <calcite-action id="second" text="Add" icon="minus" text-enabled></calcite-action>
+          <calcite-action id="third" text="Add" icon="banana" text-enabled></calcite-action>
+        </calcite-action-menu> `,
       });
 
       await page.waitForChanges();
@@ -429,8 +429,8 @@ describe("calcite-action-menu", () => {
       expect(actions[1].getAttribute(activeAttr)).toBe(null);
       expect(actions[2].getAttribute(activeAttr)).toBe(null);
 
-      const button = await page.find("button");
-      await button.focus();
+      await page.keyboard.press("Tab");
+
       await page.waitForChanges();
 
       expect(await actionMenu.getProperty("open")).toBe(false);
