4a5eda513146eec5f386d9f4a21393b7cd953820
eliza <eli97736@esri.com>
2024-02-01 16:22:11 -0800

    cleanup

--- packages/calcite-components/src/components/block/block.tsx
+++ packages/calcite-components/src/components/block/block.tsx
@@ -400,6 +400,7 @@ export class Block
               class={CSS.content}
               hidden={!open}
               id={IDS.content}
+              // eslint-disable-next-line react/jsx-sort-props -- ref should be last so node attrs/props are in sync (see https://github.com/Esri/calcite-design-system/pull/6530)
               ref={this.setTransitionEl}
             >
               {this.renderScrim()}
