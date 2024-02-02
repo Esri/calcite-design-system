d4d486725ba27b24ceaad33e03b89225afab65f8
Erik Harper <eriklharper@pm.me>
2024-02-01 16:08:23 -0800

    removing tile-group in favor of #8687

--- packages/calcite-components/src/components.d.ts
+++ packages/calcite-components/src/components.d.ts
@@ -84,7 +84,6 @@ import { TableMessages } from "./components/table/assets/table/t9n";
 import { TableCellMessages } from "./components/table-cell/assets/table-cell/t9n";
 import { TableHeaderMessages } from "./components/table-header/assets/table-header/t9n";
 import { TextAreaMessages } from "./components/text-area/assets/text-area/t9n";
-import { TileGroupLayout } from "./components/tile-group/interfaces";
 import { TileSelectType } from "./components/tile-select/interfaces";
 import { TileSelectGroupLayout } from "./components/tile-select-group/interfaces";
 import { TipMessages } from "./components/tip/assets/tip/t9n";
@@ -171,7 +170,6 @@ export { TableMessages } from "./components/table/assets/table/t9n";
 export { TableCellMessages } from "./components/table-cell/assets/table-cell/t9n";
 export { TableHeaderMessages } from "./components/table-header/assets/table-header/t9n";
 export { TextAreaMessages } from "./components/text-area/assets/text-area/t9n";
-export { TileGroupLayout } from "./components/tile-group/interfaces";
 export { TileSelectType } from "./components/tile-select/interfaces";
 export { TileSelectGroupLayout } from "./components/tile-select-group/interfaces";
 export { TipMessages } from "./components/tip/assets/tip/t9n";
@@ -4978,20 +4976,6 @@ export namespace Components {
          */
         "scale": Scale;
     }
-    interface CalciteTileGroup {
-        /**
-          * When `true`, interaction is prevented and the component is displayed with lower opacity.
-         */
-        "disabled": boolean;
-        /**
-          * Defines the layout of the component.  Use `"horizontal"` for rows, and `"vertical"` for a single column.
-         */
-        "layout": TileGroupLayout;
-        /**
-          * Specifies the size of the component.
-         */
-        "scale": Scale;
-    }
     interface CalciteTileSelect {
         /**
           * When `true`, the component is checked.
@@ -7147,12 +7131,6 @@ declare global {
         prototype: HTMLCalciteTileElement;
         new (): HTMLCalciteTileElement;
     };
-    interface HTMLCalciteTileGroupElement extends Components.CalciteTileGroup, HTMLStencilElement {
-    }
-    var HTMLCalciteTileGroupElement: {
-        prototype: HTMLCalciteTileGroupElement;
-        new (): HTMLCalciteTileGroupElement;
-    };
     interface HTMLCalciteTileSelectElementEventMap {
         "calciteTileSelectChange": void;
     }
@@ -7434,7 +7412,6 @@ declare global {
         "calcite-tabs": HTMLCalciteTabsElement;
         "calcite-text-area": HTMLCalciteTextAreaElement;
         "calcite-tile": HTMLCalciteTileElement;
-        "calcite-tile-group": HTMLCalciteTileGroupElement;
         "calcite-tile-select": HTMLCalciteTileSelectElement;
         "calcite-tile-select-group": HTMLCalciteTileSelectGroupElement;
         "calcite-time-picker": HTMLCalciteTimePickerElement;
@@ -12495,20 +12472,6 @@ declare namespace LocalJSX {
          */
         "scale"?: Scale;
     }
-    interface CalciteTileGroup {
-        /**
-          * When `true`, interaction is prevented and the component is displayed with lower opacity.
-         */
-        "disabled"?: boolean;
-        /**
-          * Defines the layout of the component.  Use `"horizontal"` for rows, and `"vertical"` for a single column.
-         */
-        "layout"?: TileGroupLayout;
-        /**
-          * Specifies the size of the component.
-         */
-        "scale"?: Scale;
-    }
     interface CalciteTileSelect {
         /**
           * When `true`, the component is checked.
@@ -13011,7 +12974,6 @@ declare namespace LocalJSX {
         "calcite-tabs": CalciteTabs;
         "calcite-text-area": CalciteTextArea;
         "calcite-tile": CalciteTile;
-        "calcite-tile-group": CalciteTileGroup;
         "calcite-tile-select": CalciteTileSelect;
         "calcite-tile-select-group": CalciteTileSelectGroup;
         "calcite-time-picker": CalciteTimePicker;
@@ -13136,7 +13098,6 @@ declare module "@stencil/core" {
             "calcite-tabs": LocalJSX.CalciteTabs & JSXBase.HTMLAttributes<HTMLCalciteTabsElement>;
             "calcite-text-area": LocalJSX.CalciteTextArea & JSXBase.HTMLAttributes<HTMLCalciteTextAreaElement>;
             "calcite-tile": LocalJSX.CalciteTile & JSXBase.HTMLAttributes<HTMLCalciteTileElement>;
-            "calcite-tile-group": LocalJSX.CalciteTileGroup & JSXBase.HTMLAttributes<HTMLCalciteTileGroupElement>;
             "calcite-tile-select": LocalJSX.CalciteTileSelect & JSXBase.HTMLAttributes<HTMLCalciteTileSelectElement>;
             "calcite-tile-select-group": LocalJSX.CalciteTileSelectGroup & JSXBase.HTMLAttributes<HTMLCalciteTileSelectGroupElement>;
             "calcite-time-picker": LocalJSX.CalciteTimePicker & JSXBase.HTMLAttributes<HTMLCalciteTimePickerElement>;
