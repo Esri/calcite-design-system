123265b9b270da8f615d3b90bd6f9a1de3141574
Adam <adam@tirel.la>
2024-02-17 12:11:21 -0800

    feat(pagination): Add component tokens

--- packages/calcite-components/src/components/pagination/pagination.stories.ts
+++ packages/calcite-components/src/components/pagination/pagination.stories.ts
@@ -116,3 +116,25 @@ export const arabicNumberingSystemAndRTL_TestOnly = (): string =>
 arabicNumberingSystemAndRTL_TestOnly.parameters = {
   chromatic: { diffThreshold: 1 },
 };
+
+export const theming_TestOnly = (): string => html`
+  <calcite-pagination
+    total-items="1200"
+    page-size="100"
+    start-item="1"
+    style="
+  --calcite-pagination-text-color: green;
+  --calcite-pagination-text-color-hover: darkgreen;
+  --calcite-pagination-text-color-selected:teal;
+  --calcite-pagination-background-color: lightyellow;
+  --calcite-pagination-background-color-hover: yellow;
+  --calcite-pagination-border-color-selected: green;
+  --calcite-pagination-border-color-hover:orange;
+  --calcite-pagination-arrow-text-color: blue;
+  --calcite-pagination-arrow-text-color-hover: pink;
+  --calcite-pagination-arrow-background-color: lightyellow;
+  --calcite-pagination-arrow-background-color-hover: orange;
+  --calcite-pagination-arrow-background-color-active: gold;
+        
+  ></calcite-pagination>
+`;
