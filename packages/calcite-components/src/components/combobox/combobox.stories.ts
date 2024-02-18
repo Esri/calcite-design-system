f908bee021def12de560c66502fd3539f24c0a22
eliza <eli97736@esri.com>
2024-02-18 10:22:38 -0800

    stories styling

--- packages/calcite-components/src/components/combobox/combobox.stories.ts
+++ packages/calcite-components/src/components/combobox/combobox.stories.ts
@@ -346,41 +346,42 @@ const style = html`
 
 export const longItemsAllSelectionModes = (): string => html`
   ${style}
-  <calcite-combobox open selection-mode="single">
-    <calcite-combobox-item text-label="Layers">
-      <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
-      <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item>
-    </calcite-combobox-item>
-  </calcite-combobox>
+  <div style="display: flex; flex-direction: column;"> 
+    <div style="display: flex; flex-direction: row; margin-block-end: 160px;"> 
+      <calcite-combobox open selection-mode="single" style="margin-right: 20px;">
+        <calcite-combobox-item text-label="Layers">
+        <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
+        <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item>ß</calcite-combobox-item>
+      </calcite-combobox>
 
-  <calcite-combobox open selection-mode="single-persist">
-    <calcite-combobox-item text-label="Layers">
-      <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
-      <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item>
-    </calcite-combobox-item>
-  </calcite-combobox>
+      <calcite-combobox open selection-mode="single-persist">
+        <calcite-combobox-item text-label="Layers">
+        <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
+        <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item></calcite-combobox-item>
+      </calcite-combobox>
+    </div>
 
-  <calcite-combobox open selection-mode="multiple">
-    <calcite-combobox-item-group label="First item group">
-      <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
-    </calcite-combobox-item-group>
+    <div style="display: flex; flex-direction: row;"> 
+      <calcite-combobox open selection-mode="multiple" style="margin-right: 20px;">
+        <calcite-combobox-item-group label="First item group">
+          <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
+        </calcite-combobox-item-group>
+        <calcite-combobox-item-group label="Last item group">
+          <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item></calcite-combobox-item>
+        </calcite-combobox-item-group>
+      </calcite-combobox>
 
-    <calcite-combobox-item-group label="Last item group">
-        <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item>
-      </calcite-combobox-item>
-    </calcite-combobox-item-group>
-  </calcite-combobox>
-
-  <calcite-combobox open selection-mode="ancestors">
-    <calcite-combobox-item-group label="First item group">
-      <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
-    </calcite-combobox-item-group>
-
-    <calcite-combobox-item-group label="Last item group">
-        <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item>
-      </calcite-combobox-item>
-    </calcite-combobox-item-group>
-  </calcite-combobox>
+      <calcite-combobox open selection-mode="ancestors">
+        <calcite-combobox-item-group label="First item group">
+          <calcite-combobox-item text-label="Enriched USA Census Tract Areas Aug29"></calcite-combobox-item>
+        </calcite-combobox-item-group>
+        <calcite-combobox-item-group label="Last item group">
+          <calcite-combobox-item text-label="Viewer_Reservable_Equipments_Capacity_V2_WFL1"></calcite-combobox-item>
+          </calcite-combobox-item>
+        </calcite-combobox-item-group>
+      </calcite-combobox>
+    </div> 
+  <div> 
 `;
 
 export const disabled_TestOnly = (): string =>
