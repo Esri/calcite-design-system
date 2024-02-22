b621efdf859868546cd94e464c6b665c7a1db278
eliza <eli97736@esri.com>
2024-02-22 11:25:23 -0800

    state token refactors and stories

--- packages/calcite-components/src/components/handle/handle.stories.ts
+++ packages/calcite-components/src/components/handle/handle.stories.ts
@@ -14,3 +14,20 @@ export const simple = (): string => html` <calcite-handle></calcite-handle> `;
 export const activated_TestOnly = (): string => html` <calcite-handle activated></calcite-handle> `;
 
 export const disabled_TestOnly = (): string => html` <calcite-handle disabled></calcite-handle> `;
+
+export const focused_TestOnly = (): string => html` <calcite-handle focused></calcite-handle> `;
+
+export const themedStates_TestOnly = (): string => html`
+  <calcite-handle
+    style="
+    --calcite-handle-text-color: blue;
+    --calcite-handle-text-color-focus: pink;
+    --calcite-handle-text-color-hover: green;
+    --calcite-handle-text-color-selected: purple;
+
+    --calcite-handle-background-color: red;
+    --calcite-handle-background-color-hover: yellow;
+    --calcite-handle-background-color-selected: orange;
+  "
+  ></calcite-handle>
+`;
