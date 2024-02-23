202a1e1f02fcfffdbbfc87dc81e805fda3c7075c
eliza <eli97736@esri.com>
2024-02-22 17:49:21 -0800

    stories

--- packages/calcite-components/src/components/tabs/tabs.stories.ts
+++ packages/calcite-components/src/components/tabs/tabs.stories.ts
@@ -492,3 +492,24 @@ export const paddingPropOverrideAtElementLevel = (): string => html`
     </calcite-tabs>
   </calcite-tabs>
 `;
+
+export const themed_TestOnly = (): string => html`
+  <calcite-tabs
+    style="
+    --calcite-tabs-background-color: yellow;
+    --calcite-tabs-border-color: green;
+    "
+    bordered
+  >
+    <calcite-tab-nav slot="title-group">
+      <calcite-tab-title tab="tab1">Tab 1 Title</calcite-tab-title>
+      <calcite-tab-title tab="tab2">Tab 2 Title</calcite-tab-title>
+      <calcite-tab-title tab="tab3">Tab 3 Title</calcite-tab-title>
+      <calcite-tab-title tab="tab4" selected>Tab 4 Title</calcite-tab-title>
+    </calcite-tab-nav>
+    <calcite-tab tab="tab1">Tab 1 Content</calcite-tab>
+    <calcite-tab tab="tab2">Tab 2 Content</calcite-tab>
+    <calcite-tab tab="tab3">Tab 3 Content</calcite-tab>
+    <calcite-tab tab="tab4" selected>Tab 4 Content</calcite-tab>
+  </calcite-tabs>
+`;
