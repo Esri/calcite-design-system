f812519b71ad73bbf4ade61abfe8b58659f1c277
Matt Driscoll <mdriscoll@esri.com>
2024-03-05 10:37:00 -0800

    feat(shell-center-row): add component tokens (#8798)
    **Related Issue:** #7180
    
    ## Summary
    
    ```
     * @prop --calcite-shell-center-row-background-color: Specifies the background color of the component.
     * @prop --calcite-shell-center-row-border-color: Specifies the border color of the component.
     * @prop --calcite-shell-center-row-border-radius: Specifies the border radius of the component.
     * 
    ```

--- packages/calcite-components/src/components/shell/shell.stories.ts
+++ packages/calcite-components/src/components/shell/shell.stories.ts
@@ -1371,5 +1371,20 @@ export const theming_TestOnly = (): string =>
   <calcite-shell-panel slot="panel-start">${leadingPanelHTML}</calcite-shell-panel>
   ${contentHTML}
   ${centerRowAdvancedHTML}
+${footerHTML}
+</calcite-shell>`);
+
+export const centerRowTheming_TestOnly = (): string =>
+  html(`<calcite-shell content-behind>
+  ${headerHTML}
+  <calcite-shell-panel slot="panel-start">${leadingPanelHTML}</calcite-shell-panel>
+  ${contentHTML}
+  <calcite-shell-center-row style="
+    --calcite-panel-background-color: lightblue;
+    --calcite-shell-center-row-background-color: red;
+    --calcite-shell-center-row-border-color: cyan;
+    --calcite-shell-center-row-border-radius: 20px;
+  " detached slot="center-row">${actionBarStartHTML}<p>Hello World!</p></calcite-shell-center-row>
+  <calcite-shell-panel slot="panel-end">${trailingPanelHTML}</calcite-shell-panel>
   ${footerHTML}
 </calcite-shell>`);
