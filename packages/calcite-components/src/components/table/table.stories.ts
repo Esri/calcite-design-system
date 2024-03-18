eecaf3f2405aba871530dfec5842d079c149a6f5
Adam Tirella <adam@tirel.la>
2024-03-18 12:47:55 -0700

    feat(table): Add component tokens (#8940)
    **Related Issue:** #7180 
    
    ## Summary
    <img width="1409" alt="Screenshot 2024-03-14 at 3 51 21 PM"
    src="https://github.com/Esri/calcite-design-system/assets/4733155/a7b49bea-e8d0-4d32-9137-01189f8daf2a">

--- packages/calcite-components/src/components/table/table.stories.ts
+++ packages/calcite-components/src/components/table/table.stories.ts
@@ -1047,21 +1047,29 @@ export const tableCellCssBackgroundVariable_TestOnly = (): string =>
       <calcite-table-header heading="Heading" description="Description"></calcite-table-header>
     </calcite-table-row>
     <calcite-table-row>
-      <calcite-table-cell style="--calcite-table-cell-background: rgba(20, 200, 50, 0.15)">cell</calcite-table-cell>
+      <calcite-table-cell style="--calcite-table-cell-background-color: rgba(20, 200, 50, 0.15)"
+        >cell</calcite-table-cell
+      >
       <calcite-table-cell>cell</calcite-table-cell>
       <calcite-table-cell>cell</calcite-table-cell>
       <calcite-table-cell>cell</calcite-table-cell>
     </calcite-table-row>
     <calcite-table-row>
-      <calcite-table-cell style="--calcite-table-cell-background: rgba(200, 50, 20, 0.15)">cell</calcite-table-cell>
+      <calcite-table-cell style="--calcite-table-cell-background-color: rgba(200, 50, 20, 0.15)"
+        >cell</calcite-table-cell
+      >
       <calcite-table-cell>cell</calcite-table-cell>
-      <calcite-table-cell style="--calcite-table-cell-background: rgba(20, 200, 50, 0.15)">cell</calcite-table-cell>
+      <calcite-table-cell style="--calcite-table-cell-background-color: rgba(20, 200, 50, 0.15)"
+        >cell</calcite-table-cell
+      >
       <calcite-table-cell>cell</calcite-table-cell>
     </calcite-table-row>
     <calcite-table-row>
       <calcite-table-cell>cell</calcite-table-cell>
       <calcite-table-cell>cell</calcite-table-cell>
-      <calcite-table-cell style="--calcite-table-cell-background: rgba(200, 50, 20, 0.15)">cell</calcite-table-cell>
+      <calcite-table-cell style="--calcite-table-cell-background-color: rgba(200, 50, 20, 0.15)"
+        >cell</calcite-table-cell
+      >
       <calcite-table-cell>cell</calcite-table-cell>
     </calcite-table-row>
   </calcite-table>`;
@@ -1328,3 +1336,191 @@ export const darkModeRTLWithSelection_TestOnly = (): string =>
   </calcite-table>`;
 
 darkModeRTLWithSelection_TestOnly.parameters = { modes: modesDarkDefault };
+
+export const themingExample_TestOnly = (): string =>
+  html`<style>
+      calcite-table {
+        --calcite-table-corner-radius: var(--calcite-corner-radius-round);
+        --calcite-table-shadow: var(--calcite-shadow-sm);
+        --calcite-table-border-color: red;
+        --calcite-table-row-background-color: #e6f4e6;
+        --calcite-table-row-background-color-striped: green;
+        --calcite-table-row-background-color-selected: orange;
+        --calcite-table-border-color: green;
+        --calcite-table-row-border-color-selected: pink;
+        --calcite-table-row-selected-accent-color: red;
+        --calcite-table-row-background-color-selected: red;
+        --calcite-table-number-cell-background-color: purple;
+        --calcite-table-number-cell-text-color: red;
+        --calcite-table-selection-cell-background-color: lightgreen;
+        --calcite-table-selection-cell-background-color-selected: green;
+        --calcite-table-selection-cell-icon-color: blue;
+        --calcite-table-selection-cell-icon-color-selected: white;
+        --calcite-table-header-background-color: green;
+        --calcite-table-header-heading-color: lightgreen;
+        --calcite-table-header-description-color: lightyellow;
+        --calcite-table-selection-dismiss-button-background-color: red;
+        --calcite-table-selection-dismiss-button-border-color: red;
+        --calcite-table-selection-dismiss-button-corner-radius: 14px;
+        --calcite-table-selection-dismiss-button-shadow: var(--calcite-shadow-sm);
+        --calcite-table-selection-dismiss-button-text-color: pink;
+        --calcite-table-selection-dismiss-button-background-color-hover: pink;
+        --calcite-table-selection-dismiss-button-border-color-hover: pink;
+        --calcite-table-selection-dismiss-button-text-color-hover: red;
+        --calcite-table-selection-dismiss-button-background-color-active: red;
+        --calcite-table-selection-dismiss-button-border-color-active: red;
+        --calcite-table-selection-dismiss-button-text-color-active: red;
+        --calcite-table-selection-chip-background-color: red;
+        --calcite-table-selection-chip-border-color: red;
+        --calcite-table-selection-chip-corner-radius: red;
+        --calcite-table-selection-chip-shadow: var(--calcite-shadow-sm);
+        --calcite-table-selection-chip-text-color: green;
+        --calcite-table-selection-chip-selected-background-color: purple;
+        --calcite-table-selection-chip-selected-border-color: purple;
+        --calcite-table-selection-chip-selected-text-color: purple;
+        --calcite-table-selection-chip-background-color-selected: pink;
+        --calcite-table-selection-chip-border-color-selected: pink;
+        --calcite-table-selection-chip-text-color-selected: red;
+        --calcite-table-selection-out-of-view-chip-background-color: red;
+        --calcite-table-selection-out-of-view-chip-border-color: green;
+        --calcite-table-selection-out-of-view-chip-corner-radius: red;
+        --calcite-table-selection-out-of-view-chip-shadow: var(--calcite-shadow-sm);
+        --calcite-table-selection-out-of-view-chip-text-color: green;
+        --calcite-table-selection-out-of-view-chip-icon-color: blue;
+        --calcite-table-pagination-text-color: green;
+        --calcite-table-pagination-text-color-hover: darkgreen;
+        --calcite-table-pagination-text-color-selected: teal;
+        --calcite-table-pagination-background-color: lightyellow;
+        --calcite-table-pagination-background-color-hover: yellow;
+        --calcite-table-pagination-background-color-active: gold;
+        --calcite-table-pagination-border-color-selected: green;
+        --calcite-table-pagination-border-color-hover: orange;
+        --calcite-table-pagination-arrow-icon-color: blue;
+        --calcite-table-pagination-arrow-icon-color-hover: pink;
+        --calcite-table-pagination-arrow-icon-color-active: red;
+        --calcite-table-pagination-arrow-background-color: lightyellow;
+        --calcite-table-pagination-arrow-background-color-hover: orange;
+        --calcite-table-pagination-arrow-background-color-active: gold;
+      }
+
+      calcite-table-row:nth-of-type(3) {
+        --calcite-table-cell-background-color: purple;
+        --calcite-table-row-border-color: purple;
+      }
+
+      calcite-table-row:nth-of-type(3) calcite-table-cell:nth-of-type(3) {
+        --calcite-table-cell-background-color: yellow;
+        --calcite-table-cell-border-color: yellow;
+      }
+      calcite-table-row[slot="table-footer"] {
+        --calcite-table-cell-background-color: purple;
+        --calcite-table-number-cell-background-color: purple;
+        --calcite-table-selection-cell-background-color: purple;
+      }
+      calcite-table-row[slot="table-header"] {
+        --calcite-table-cell-background-color: purple;
+        --calcite-table-number-cell-background-color: green;
+        --calcite-table-selection-cell-background-color: pink;
+        --calcite-table-selection-cell-background-color-selected: red;
+        --calcite-table-selection-cell-icon-color: orange;
+        --calcite-table-selection-cell-icon-color-selected: purple;
+      }
+    </style>
+    <calcite-table striped numbered caption="Simple-striped table" selection-mode="multiple" page-size="5">
+      <calcite-table-row slot="table-header">
+        <calcite-table-header heading="Heading" description="Description"></calcite-table-header>
+        <calcite-table-header heading="Heading" description="Description"></calcite-table-header>
+        <calcite-table-header heading="Heading" description="Description"></calcite-table-header>
+        <calcite-table-header heading="Heading" description="Description"></calcite-table-header>
+      </calcite-table-row>
+      <calcite-table-row selected>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row selected>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row selected>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row selected>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row selected>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+      <calcite-table-row slot="table-footer">
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+        <calcite-table-cell>cell</calcite-table-cell>
+      </calcite-table-row>
+    </calcite-table>`;
