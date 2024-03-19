9cfbd719c820b2f71fed203570bf6e5c5f9c8104
Ben Elan <no-reply@benelan.dev>
2024-03-19 00:12:38 -0700

    refactor(tokens): minor transformer improvements related to Windows support (#8473)
    ## Summary
    
    Cleans up the tokens transformer to make sure EOL and path strings are
    operating system agnostic.
    
    ---------
    
    Co-authored-by: JC Franco <jfranco@esri.com>

--- packages/calcite-design-tokens/support/token-transformer/sd-run.ts
+++ packages/calcite-design-tokens/support/token-transformer/sd-run.ts
@@ -11,6 +11,7 @@ import { PlatformOptions } from "../types/styleDictionary/platform.js";
 import { File } from "../types/styleDictionary/file.js";
 import { transformations } from "./styleDictionary/transformer/utils.js";
 import { format } from "./styleDictionary/formatter/utils.js";
+import { normalize } from "path";
 
 const destination = (name: string, format: PlatformFormats) => `${name}${fileExtension[format]}`;
 
@@ -63,7 +64,7 @@ export const run = async ({
       const platformConfig: PlatformOptions = {
         options: { ...options, expandFiles, platforms: output.platforms },
         transforms: transformations[platform],
-        buildPath: `${output.dir}/${platform}/`,
+        buildPath: normalize(`${output.dir}/${platform}/`),
         files: files(platform, name),
       };
       acc[platform] = platformConfig;
