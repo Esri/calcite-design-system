Esri/calcite-design-system

commit a1bae56236f8fbe8733401fca9319521c1766ad1

fix(combobox): prevent spacebar from opening the menu while focusing on `chip`'s close button (#8909)

packages/calcite-components/src/components/combobox/combobox.e2e.ts
@@ -1919,6 +1919,54 @@ describe("calcite-combobox", () => {
     });
   });
 
+  it("should not open combobox menu with spacebar while focusing on chip's close button", async () => {
+    const page = await newE2EPage();
+    await page.setContent(html`
+      <calcite-combobox label="test" placeholder="placeholder" max-items="10" scale="m">
+        <calcite-combobox-item-group label="Pokemon">
+          <calcite-combobox-item value="Pikachu" text-label="Pikachu"></calcite-combobox-item>
+          <calcite-combobox-item value="Venusaur" text-label="Venusaur"></calcite-combobox-item>
+          <calcite-combobox-item value="Charizard" text-label="Charizard"></calcite-combobox-item>
+          <calcite-combobox-item-group label="Cutest Pokemon">
+            <calcite-combobox-item value="Bulbasaur" text-label="Bulbasaur"></calcite-combobox-item>
+            <calcite-combobox-item value="Squirtle1" text-label="Squirtle1">
+              <calcite-combobox-item value="Squirtle2" text-label="Squirtle2">
+                <calcite-combobox-item value="Squirtle3" text-label="Squirtle3">
+                  <calcite-combobox-item value="Squirtle4" text-label="Squirtle4"></calcite-combobox-item>
+                </calcite-combobox-item>
+              </calcite-combobox-item>
+            </calcite-combobox-item>
+          </calcite-combobox-item-group>
+        </calcite-combobox-item-group>
+      </calcite-combobox>
+    `);
+
+    const combobox = await page.find("calcite-combobox");
+    const openEvent = page.waitForEvent("calciteComboboxOpen");
+    await combobox.click();
+    await openEvent;
+
+    await (await combobox.find("calcite-combobox-item[value=Pikachu]")).click();
+    await (await combobox.find("calcite-combobox-item[value=Charizard]")).click();
+    await (await combobox.find("calcite-combobox-item[value=Squirtle3]")).click();
+
+    const chips = await page.findAll("calcite-combobox >>> calcite-chip");
+    expect(chips.length).toBe(3);
+
+    const closeEvent = page.waitForEvent("calciteComboboxClose");
+    await combobox.press("Tab");
+    await closeEvent;
+
+    const wrapper = await page.find("calcite-combobox >>> .wrapper");
+    const close = await wrapper.find("calcite-chip >>> .close");
+    await close.click();
+    await page.waitForChanges();
+
+    const remainingChips = await page.findAll("calcite-combobox >>> calcite-chip");
+    expect(remainingChips.length).toBe(2);
+    expect(await page.find("calcite-combobox")).not.toHaveAttribute("open");
+  });
+
   it("prevents toggling items when combobox is closed", async () => {
     const page = await newE2EPage();
     await page.setContent(html`

packages/calcite-components/src/components/combobox/combobox.tsx
@@ -693,6 +693,10 @@ export class Combobox
           }
           event.preventDefault();
         }
+
+        if (event.composedPath().find((el: HTMLElement) => el.tagName === "CALCITE-CHIP")) {
+          event.preventDefault();
+        }
         break;
       case "Home":
         if (!this.open) {

commit 1a20d0eb740dbe2e371aeff90b8b9f301ca8bf2a

fix(combobox): prevent spacebar from opening the menu when focused on chip's close button (#8990)
**Related Issue:** #6777

## Summary

Prevent spacebar from opening the menu when focused on chip's `close`
button.

Fixes a regression introduced in
https://github.com/Esri/calcite-design-system/pull/8909.

packages/calcite-components/src/components/combobox/combobox.e2e.ts
@@ -1959,7 +1959,7 @@ describe("calcite-combobox", () => {
 
     const wrapper = await page.find("calcite-combobox >>> .wrapper");
     const close = await wrapper.find("calcite-chip >>> .close");
-    await close.click();
+    await close.press(" ");
     await page.waitForChanges();
 
     const remainingChips = await page.findAll("calcite-combobox >>> calcite-chip");

packages/calcite-components/src/components/combobox/combobox.tsx
@@ -681,15 +681,11 @@ export class Combobox
         }
         break;
       case " ":
-        if (!this.textInput.value) {
+        if (!this.textInput.value && !event.defaultPrevented) {
           if (!this.open) {
             this.open = true;
             this.shiftActiveItemIndex(1);
           }
-          event.preventDefault();
-        }
-
-        if (event.composedPath().find((el: HTMLElement) => el.tagName === "CALCITE-CHIP")) {
           event.preventDefault();
         }
         break;