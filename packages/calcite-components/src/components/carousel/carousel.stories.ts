e4f8cf54a5b09b532cbc3155f3b62f1260700233
Adam <adam@tirel.la>
2024-04-01 16:53:20 -0700

    Add tokens

--- packages/calcite-components/src/components/carousel/carousel.stories.ts
+++ packages/calcite-components/src/components/carousel/carousel.stories.ts
@@ -153,6 +153,105 @@ export const carouselInPopover_TestOnly = (): string =>
     </calcite-popover>
   </div>`;
 
+export const themed_simple_TestOnly = (): string =>
+  html` <div style="width:600px;height:400px;">
+    <style>
+      .bg-image-example {
+        color: red;
+        background-image: url("https://placebear.com/3000/2000");
+        background-size: cover;
+        padding: 1rem;
+        height: 300px;
+        font-size: 32px;
+        font-weight: 600;
+        line-height: 32px;
+      }
+      calcite-carousel {
+        --calcite-carousel-slide-item-icon-color-hover: green;
+        --calcite-carousel-slide-item-icon-color-selected: blue;
+        --calcite-carousel-slide-item-icon-color-active: orange;
+        --calcite-carousel-slide-item-icon-color: red;
+      }
+    </style>
+    <calcite-carousel>
+      <calcite-carousel-item label="Carousel Item 1">
+        <calcite-card>
+          <span slot="title">Some kind of carousel item content</span>
+          <span slot="subtitle">In this case, in a card</span>
+          <calcite-icon scale="s" slot="footer-start" icon="number-circle-1"></calcite-icon>
+        </calcite-card>
+      </calcite-carousel-item>
+      <calcite-carousel-item label="Carousel Item 2">
+        <calcite-card>
+          <span slot="title">Some kind of carousel item content</span>
+          <span slot="subtitle">In this case, in a card</span>
+          <calcite-icon scale="s" slot="footer-start" icon="number-circle-2"></calcite-icon>
+        </calcite-card>
+      </calcite-carousel-item>
+      <calcite-carousel-item label="Carousel Item 3">
+        <calcite-card>
+          <span slot="title">Some kind of carousel item content</span>
+          <span slot="subtitle">In this case, in a card</span>
+          <calcite-icon scale="s" slot="footer-start" icon="number-circle-3"></calcite-icon>
+        </calcite-card>
+      </calcite-carousel-item>
+      <calcite-carousel-item label="Carousel Item 4">
+        <calcite-card>
+          <span slot="title">Some kind of carousel item content</span>
+          <span slot="subtitle">In this case, in a card</span>
+          <calcite-icon scale="s" slot="footer-start" icon="number-circle-4"></calcite-icon>
+        </calcite-card>
+      </calcite-carousel-item>
+      <calcite-carousel-item label="Carousel Item 5">
+        <calcite-card>
+          <span slot="title">Some kind of carousel item content</span>
+          <span slot="subtitle">In this case, in a card</span>
+          <calcite-icon scale="s" slot="footer-start" icon="number-circle-5"></calcite-icon>
+        </calcite-card>
+      </calcite-carousel-item>
+    </calcite-carousel>
+  </div>`;
+
+export const themed_carouselFullImageWithOverlay_TestOnly = (): string =>
+  html` <div style="width:600px;height:400px;">
+    <style>
+      .bg-image-example {
+        color: red;
+        background-image: url("https://placebear.com/3000/2000");
+        background-size: cover;
+        padding: 1rem;
+        height: 300px;
+        font-size: 32px;
+        font-weight: 600;
+        line-height: 32px;
+      }
+      calcite-carousel {
+        --calcite-carousel-slide-item-background-color-active: orange;
+        --calcite-carousel-slide-item-background-color-hover: yellow;
+        --calcite-carousel-slide-item-background-color-selected: red;
+        --calcite-carousel-slide-item-background-color: green;
+        --calcite-carousel-slide-item-icon-color-hover: white;
+        --calcite-carousel-slide-item-icon-color-selected: pink;
+        --calcite-carousel-slide-item-icon-color-active: white;
+        --calcite-carousel-slide-item-icon-color: lightgreen;
+      }
+    </style>
+    <calcite-carousel control-overlay arrow-type="edges">
+      <calcite-carousel-item label="Carousel Item 1">
+        <div class="bg-image-example">Some kind of rich content over a bg using overlay controls</div>
+      </calcite-carousel-item>
+      <calcite-carousel-item label="Carousel Item 2">
+        <div class="bg-image-example">
+          Some kind of rich content over a bg using overlay controls but longer than the other one
+        </div>
+      </calcite-carousel-item>
+      <calcite-carousel-item label="Carousel Item 3">
+        <div class="bg-image-example">
+          Some kind of rich content over a bg using overlay controls but longer than the other one
+        </div>
+      </calcite-carousel-item>
+  </div>`;
+
 export const darkModeRTL_TestOnly = (): string =>
   html` <div style="width:600px;height:400px;" dir="rtl">
     <calcite-carousel>
