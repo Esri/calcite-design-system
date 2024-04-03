9a3a5df4491e762f3cf0803e9a7f459d9576749d
JC Franco <jfranco@esri.com>
2024-04-03 12:35:50 -0700

    ensure icon name list is sorted alphabetically

--- packages/calcite-components/.storybook/helpers.ts
+++ packages/calcite-components/.storybook/helpers.ts
@@ -4,7 +4,13 @@ import { boolean as booleanKnob } from "./fake-knobs";
 // we can get all unique icon names from all size 16 non-filled icons.
 export const iconNames = Object.keys(icons)
   .filter((iconName) => iconName.endsWith("16"))
-  .map((iconName) => iconName.replace("16", ""));
+  .map((iconName) => iconName.replace("16", ""))
+  .sort((a, b) => {
+    const iPrefixedNumberIconNamePattern = /^i(\d)/;
+    return a
+      .replace(iPrefixedNumberIconNamePattern, "$1")
+      .localeCompare(b.replace(iPrefixedNumberIconNamePattern, "$1"));
+  });
 
 // custom boolean will start up a knob but only add the prop if it is true
 // if you'd instead like `attr="true|false" set the standalone option to false
