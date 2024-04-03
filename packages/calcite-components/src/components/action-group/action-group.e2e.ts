ed56b0c0ecc1f9edba0c873fe93e2e0133f7c30c
Ali Stump <astump@esri.com>
2024-04-03 14:49:56 -0700

    test(action-group): add theme tests

--- packages/calcite-components/src/components/action-group/action-group.e2e.ts
+++ packages/calcite-components/src/components/action-group/action-group.e2e.ts
@@ -1,5 +1,5 @@
 import { newE2EPage, E2EPage } from "@stencil/core/testing";
-import { accessible, defaults, focusable, hidden, renders, slots, t9n } from "../../tests/commonTests";
+import { accessible, defaults, focusable, hidden, renders, slots, t9n, themed } from "../../tests/commonTests";
 import { CSS, SLOTS } from "./resources";
 
 const actionGroupHTML = `<calcite-action-group scale="l">
@@ -65,7 +65,7 @@ describe("calcite-action-group", () => {
     <calcite-action id="banana" slot="menu-actions" text="Banana" icon="banana"></calcite-action>
     </calcite-action-group>`);
     await page.waitForChanges();
-    const container = await page.find(`calcite-action-group >>> .${CSS.container}`);
+    const container = await page.find(`calcite-action-group >>> ..${CSS.container}`);
     expect(await container.getProperty("ariaLabel")).toBe("test");
   });
 
@@ -121,6 +121,118 @@ describe("calcite-action-group", () => {
       debugger;
       expect(defaultStyle.backgroundColor).not.toBe(customTheme["--calcite-action-group-background-color"]);
       expect(styles.backgroundColor).toBe(customTheme["--calcite-action-group-background-color"]);
+    });
+  });
+
+  describe("theme", () => {
+    describe("default", () => {
+      const tokens = {
+        "--calcite-action-group-border-color": [
+          {
+            selector: "calcite-action-group",
+            targetProp: "border-left-color",
+          },
+          {
+            selector: "calcite-action-group",
+            shadowSelector:
+              ".container calcite-action-menu >>> .container calcite-popover >>> .calcite-floating-ui-anim",
+            targetProp: "border-left-color",
+          },
+        ],
+        "--calcite-action-group-trigger-background-color": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "backgroundColor",
+        },
+        "--calcite-action-group-trigger-icon-color": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> calcite-icon`,
+          targetProp: "color",
+        },
+        "--calcite-action-group-trigger-shadow": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "boxShadow",
+        },
+        "--calcite-action-group-trigger-text-color": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "color",
+        },
+        "--calcite-action-group-trigger-background-color-active": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "backgroundColor",
+          state: { press: { attribute: "class", value: CSS.expandToggle } },
+        },
+        "--calcite-action-group-trigger-background-color-focus": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "backgroundColor",
+          state: "focus",
+        },
+        "--calcite-action-group-trigger-background-color-hover": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "backgroundColor",
+          state: "hover",
+        },
+        "--calcite-action-group-trigger-icon-color-active": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "color",
+          state: { press: { attribute: "class", value: CSS.expandToggle } },
+        },
+        "--calcite-action-group-trigger-icon-color-focus": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "color",
+          state: "focus",
+        },
+        "--calcite-action-group-trigger-icon-color-hover": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "color",
+          state: "hover",
+        },
+        "--calcite-action-group-trigger-shadow-active": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "boxShadow",
+          state: { press: { attribute: "class", value: CSS.expandToggle } },
+        },
+        "--calcite-action-group-trigger-shadow-focus": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "boxShadow",
+          state: "focus",
+        },
+        "--calcite-action-group-trigger-shadow-hover": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "boxShadow",
+          state: "hover",
+        },
+        "--calcite-action-group-trigger-text-color-active": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "color",
+          state: { press: { attribute: "class", value: CSS.expandToggle } },
+        },
+        "--calcite-action-group-trigger-text-color-focus": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "color",
+          state: "focus",
+        },
+        "--calcite-action-group-trigger-text-color-hover": {
+          selector: "calcite-action-group",
+          shadowSelector: `calcite-action-menu calcite-action >>> .button`,
+          targetProp: "color",
+          state: "hover",
+        },
+      };
+      themed(actionGroupHTML, tokens);
     });
   });
 });
