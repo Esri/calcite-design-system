1280ec7519138aaa835945ae83d929991d608b5f
Ditwan Price <price.ditwan@gmail.com>
2024-04-03 11:04:16 -0700

    docs: fix the description of headingLevel property (#9045)
    **Related Issue:** #9044 
    
    ## Summary
    Updating the description of `headingLevel` prop to eliminate confusion
    on what it's purpose is.
    
    ---------
    
    Co-authored-by: Kitty Hurley <khurley@esri.com>

--- packages/calcite-components/src/components/block/block.tsx
+++ packages/calcite-components/src/components/block/block.tsx
@@ -93,7 +93,7 @@ export class Block
   @Prop() heading!: string;
 
   /**
-   * Specifies the number at which section headings should start.
+   * Specifies the heading level of the component's `heading` for proper document structure, without affecting visual styling.
    */
   @Prop({ reflect: true }) headingLevel: HeadingLevel;
 
