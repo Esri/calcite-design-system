ec55c24870a9baec18c07b05bb10961adba441b4
Ali Stump <alisonailea@users.noreply.github.com>
2024-04-04 13:53:32 -0700

    test(action-group): remove extraneous theme-ing test and add "as const"

--- packages/calcite-components/src/components/action-group/action-group.e2e.ts
+++ packages/calcite-components/src/components/action-group/action-group.e2e.ts
@@ -72,58 +72,7 @@ describe("calcite-action-group", () => {
   describe("translation support", () => {
     t9n("calcite-action-group");
   });
-
-  describe("Theme-ing", () => {
-    let page: E2EPage;
-    const customTheme = {
-      "--calcite-action-group-border-color": "rgb(255, 0, 0)",
-      "--calcite-action-group-popover-background-color": "rgb(0, 255, 255)",
-      "--calcite-action-group-popover-border-color": "rgb(255, 0, 255)",
-      "--calcite-action-group-popover-corner-radius": "rgb(128, 0, 0)",
-      "--calcite-action-group-popover-shadow": "0 0 0 rgb(0, 128, 0)",
-      "--calcite-action-group-trigger-background-color-active": "rgb(0, 0, 128)",
-      "--calcite-action-group-trigger-background-color-focus": "rgb(128, 128, 0)",
-      "--calcite-action-group-trigger-background-color-hover": "rgb(0, 128, 128)",
-      "--calcite-action-group-trigger-background-color": "rgb(128, 0, 128)",
-      "--calcite-action-group-trigger-icon-color-active": "rgb(192, 192, 192)",
-      "--calcite-action-group-trigger-icon-color-focus": "rgb(192, 0, 0)",
-      "--calcite-action-group-trigger-icon-color-hover": "rgb(0, 192, 0)",
-      "--calcite-action-group-trigger-icon-color": "rgb(0, 0, 192)",
-      "--calcite-action-group-trigger-indicator-color": "rgb(192, 192, 0)",
-      "--calcite-action-group-trigger-loader-color": "rgb(0, 192, 192)",
-      "--calcite-action-group-trigger-shadow-active": "rgb(192, 0, 192)",
-      "--calcite-action-group-trigger-shadow-focus": "rgb(128, 128, 128)",
-      "--calcite-action-group-trigger-shadow-hover": "rgb(128, 64, 64)",
-      "--calcite-action-group-trigger-shadow": "rgb(64, 128, 64)",
-      "--calcite-action-group-trigger-text-color-active": "rgb(64, 64, 128)",
-      "--calcite-action-group-trigger-text-color-focus": "rgb(128, 64, 128)",
-      "--calcite-action-group-trigger-text-color-hover": "rgb(64, 128, 128)",
-      "--calcite-action-group-trigger-text-color": "rgb(128, 64, 64)",
-    };
-
-    beforeEach(async () => {
-      page = await newE2EPage({ html: actionGroupHTML });
-      await page.waitForChanges();
-    });
-
-    it("should allow theme-ing of the trigger", async () => {
-      const actionGroup = await page.find("calcite-action-group");
-      const defaultStyle = await actionGroup.getComputedStyle();
-
-      await actionGroup.setAttribute(
-        "style",
-        `${Object.entries(customTheme)
-          .map(([key, val]) => `${key}: ${val}`)
-          .join("; ")}`,
-      );
-      await page.waitForChanges();
-      const styles = await actionGroup.getComputedStyle();
-      debugger;
-      expect(defaultStyle.backgroundColor).not.toBe(customTheme["--calcite-action-group-background-color"]);
-      expect(styles.backgroundColor).toBe(customTheme["--calcite-action-group-background-color"]);
-    });
-  });
-
+  
   describe("theme", () => {
     describe("default", () => {
       const tokens = {
@@ -231,7 +180,7 @@ describe("calcite-action-group", () => {
           targetProp: "color",
           state: "hover",
         },
-      };
+      } as const;
       themed(actionGroupHTML, tokens);
     });
   });
