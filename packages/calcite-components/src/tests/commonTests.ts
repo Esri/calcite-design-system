ac4c62c33a479be31be38dc28d9b539f13d7094f
Ali Stump <astump@esri.com>
2024-04-09 14:53:02 -0700

    docs(common): easier copy paste for theme tests

--- packages/calcite-components/src/tests/commonTests.ts
+++ packages/calcite-components/src/tests/commonTests.ts
@@ -1846,37 +1846,37 @@ export function openClose(componentTagOrHTML: TagOrHTML, options?: OpenCloseOpti
  * Helper to test custom theming of a component's associated tokens.
  *
  * @example
- * describe("theme", () => {
- *   const tokens = {
- *     "--calcite-action-bar-trigger-background-color": [{
- *       selector: "calcite-action-bar",
- *       targetProp: "backgroundColor",
- *     }, {
- *       selector: "calcite-action-bar",
- *       shadowSelector: "calcite-action-group calcite-action >>> .button",
- *       targetProp: "backgroundColor",
- *     }],
- *     "--calcite-action-bar-trigger-background-color-active": {
- *       selector: "calcite-action-bar",
- *       shadowSelector: "calcite-action-group calcite-action >>> .button",
- *       targetProp: "backgroundColor",
- *       state: { press: { attribute: "class", value: CSS.expandToggle } },
- *     },
- *     "--calcite-action-bar-trigger-background-color-focus": {
- *       selector: "calcite-action-bar",
- *       shadowSelector: "calcite-action-group calcite-action >>> .button",
- *       targetProp: "backgroundColor",
- *       state: "focus",
- *     },
- *     "--calcite-action-bar-trigger-background-color-hover": {
- *       selector: "calcite-action-bar",
- *       shadowSelector: "calcite-action-group calcite-action >>> .button",
- *       targetProp: "backgroundColor",
- *       state: "hover",
- *     },
- *   } as const;
- *   themed(`calcite-action-bar`, tokens);
- * });
+// describe("theme", () => {
+//   const tokens = {
+//     "--calcite-action-bar-trigger-background-color": [{
+//       selector: "calcite-action-bar",
+//       targetProp: "backgroundColor",
+//     }, {
+//       selector: "calcite-action-bar",
+//       shadowSelector: "calcite-action-group calcite-action >>> .button",
+//       targetProp: "backgroundColor",
+//     }],
+//     "--calcite-action-bar-trigger-background-color-active": {
+//       selector: "calcite-action-bar",
+//       shadowSelector: "calcite-action-group calcite-action >>> .button",
+//       targetProp: "backgroundColor",
+//       state: { press: { attribute: "class", value: CSS.expandToggle } },
+//     },
+//     "--calcite-action-bar-trigger-background-color-focus": {
+//       selector: "calcite-action-bar",
+//       shadowSelector: "calcite-action-group calcite-action >>> .button",
+//       targetProp: "backgroundColor",
+//       state: "focus",
+//     },
+//     "--calcite-action-bar-trigger-background-color-hover": {
+//       selector: "calcite-action-bar",
+//       shadowSelector: "calcite-action-group calcite-action >>> .button",
+//       targetProp: "backgroundColor",
+//       state: "hover",
+//     },
+//   } as const;
+//   themed(`calcite-action-bar`, tokens);
+// });
  *
  * @param componentTagOrHTML  - The component tag or HTML markup to test against.
  * @param tokens - A record of token names and their associated selectors, shadow selectors, target props, and states.
