974397586843e5524429a3b3d97c175ba8282f52
eliza <eli97736@esri.com>
2024-04-10 13:06:01 -0700

    refactor(commonTests): split up helpers into individual modules

---
+++ packages/calcite-components/src/tests/commonTestUtils/essentialTests.ts
@@ -0,0 +1,205 @@
+/* eslint-disable jest/no-conditional-expect -- Using conditional logic in a confined test helper to handle specific scenarios, reducing duplication, balancing test readability and maintainability. **/
+/* eslint-disable jest/no-export -- Util functions are now imported to be used as `it` blocks within `describe` instead of assertions within `it` blocks. */
+import axe from "axe-core";
+import { toHaveNoViolations } from "jest-axe";
+import type { JSX } from "../../components";
+import { GlobalTestProps, skipAnimations } from "./../utils";
+import { ComponentTestSetup, getTagAndPage, HYDRATED_ATTR } from "./../commonTestUtils/setupForTests";
+
+expect.extend(toHaveNoViolations);
+
+type ComponentTag = keyof JSX.IntrinsicElements;
+type AxeOwningWindow = GlobalTestProps<{ axe: typeof axe }>;
+
+/**
+ * Helper for asserting that a component is accessible.
+ *
+ * Note that this helper should be used within a describe block.
+ *
+ * @example
+ * describe("accessible"), () => {
+ *    accessible(`<calcite-tree></calcite-tree>`);
+ * });
+ *
+ * @param {ComponentTestSetup} componentTestSetup - A component tag, html, or the tag and e2e page for setting up a test
+ */
+export function accessible(componentTestSetup: ComponentTestSetup): void {
+  it("is accessible", async () => {
+    const { page, tag } = await getTagAndPage(componentTestSetup);
+
+    await page.addScriptTag({ path: require.resolve("axe-core") });
+    await page.waitForFunction(() => (window as AxeOwningWindow).axe);
+
+    expect(
+      await page.evaluate(async (componentTag: ComponentTag) => (window as AxeOwningWindow).axe.run(componentTag), tag),
+    ).toHaveNoViolations();
+  });
+}
+
+/**
+ * Note that this helper should be used within a describe block.
+ *
+ * @example
+ * describe("renders", () => {
+ *    renders(`<calcite-tree></calcite-tree>`);
+ * });
+ * @param componentTestSetup
+ *
+ * @param {string} componentTagOrHTML - the component tag or HTML markup to test against
+ * @param {object} options - additional options to assert
+ * @param {string} options.visible - is the component visible
+ * @param {string} options.display - is the component's display "inline"
+ */
+export async function renders(
+  componentTestSetup: ComponentTestSetup,
+  options?: {
+    visible?: boolean;
+    display: string;
+  },
+): Promise<void> {
+  it(`renders`, async () => {
+    const { page, tag } = await getTagAndPage(componentTestSetup);
+    const element = await page.find(tag);
+
+    expect(element).toHaveAttribute(HYDRATED_ATTR);
+    expect(await element.isVisible()).toBe(options?.visible ?? true);
+    expect((await element.getComputedStyle()).display).toBe(options?.display ?? "inline");
+  });
+}
+
+/**
+ *
+ * Helper for asserting that a component reflects
+ *
+ * Note that this helper should be used within a describe block.
+ *
+ * @example
+ * describe("reflects", () => {
+ *    reflects("calcite-action-bar", [
+ *      {
+ *        propertyName: "expandDisabled",
+ *        value: true
+ *      },
+ *      {
+ *        propertyName: "expanded",
+ *        value: true
+ *      }
+ *    ])
+ * })
+ *
+ * @param {string} componentTagOrHTML - the component tag or HTML markup to test against
+ * @param componentTestSetup
+ * @param {object[]} propsToTest - the properties to test
+ * @param {string} propsToTest.propertyName - the property name
+ * @param {any} propsToTest.value - the property value (if boolean, needs to be `true` to ensure reflection)
+ */
+export function reflects(
+  componentTestSetup: ComponentTestSetup,
+  propsToTest: {
+    propertyName: string;
+    value: any;
+  }[],
+): void {
+  const cases = propsToTest.map(({ propertyName, value }) => [propertyName, value]);
+
+  it.each(cases)("%p", async (propertyName, value) => {
+    const { page, tag: componentTag } = await getTagAndPage(componentTestSetup);
+    await skipAnimations(page);
+    const element = await page.find(componentTag);
+
+    const attrName = propToAttr(propertyName);
+    const componentAttributeSelector = `${componentTag}[${attrName}]`;
+
+    element.setProperty(propertyName, value);
+    await page.waitForChanges();
+
+    expect(await page.find(componentAttributeSelector)).toBeTruthy();
+
+    if (typeof value === "boolean") {
+      const getExpectedValue = (propValue: boolean): string | null => (propValue ? "" : null);
+      const negated = !value;
+
+      element.setProperty(propertyName, negated);
+      await page.waitForChanges();
+
+      expect(element.getAttribute(attrName)).toBe(getExpectedValue(negated));
+
+      element.setProperty(propertyName, value);
+      await page.waitForChanges();
+
+      expect(element.getAttribute(attrName)).toBe(getExpectedValue(value));
+    }
+  });
+}
+
+function propToAttr(name: string): string {
+  return name.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
+}
+
+/**
+ * Helper for asserting that a property's value is its default
+ *
+ * Note that this helper should be used within a describe block.
+ *
+ * @example
+ * describe("defaults", () => {
+ *    defaults("calcite-action", [
+ *      {
+ *        propertyName: "active",
+ *        defaultValue: false
+ *      },
+ *      {
+ *        propertyName: "appearance",
+ *        defaultValue: "solid"
+ *      }
+ *    ])
+ * })
+ *
+ * @param {string} componentTagOrHTML - the component tag or HTML markup to test against
+ * @param componentTestSetup
+ * @param {object[]} propsToTest - the properties to test
+ * @param {string} propsToTest.propertyName - the property name
+ * @param {any} propsToTest.value - the property value
+ */
+export function defaults(
+  componentTestSetup: ComponentTestSetup,
+  propsToTest: {
+    propertyName: string;
+    defaultValue: any;
+  }[],
+): void {
+  it.each(propsToTest.map(({ propertyName, defaultValue }) => [propertyName, defaultValue]))(
+    "%p",
+    async (propertyName, defaultValue) => {
+      const { page, tag } = await getTagAndPage(componentTestSetup);
+      const element = await page.find(tag);
+      const prop = await element.getProperty(propertyName);
+      expect(prop).toEqual(defaultValue);
+    },
+  );
+}
+
+/**
+ * Helper for asserting that a component is not visible when hidden
+ *
+ * Note that this helper should be used within a describe block.
+ *
+ * @example
+ * @param componentTestSetup
+ * describe("honors hidden attribute", () => {
+ *    hidden("calcite-accordion")
+ * });
+ *
+ * @param {string} componentTagOrHTML - the component tag or HTML markup to test against
+ */
+export async function hidden(componentTestSetup: ComponentTestSetup): Promise<void> {
+  it("is hidden", async () => {
+    const { page, tag } = await getTagAndPage(componentTestSetup);
+    const element = await page.find(tag);
+
+    element.setAttribute("hidden", "");
+    await page.waitForChanges();
+
+    expect(await element.isVisible()).toBe(false);
+  });
+}
---
+++ packages/calcite-components/src/tests/commonTestUtils/labelable.ts
@@ -0,0 +1,308 @@
+/* eslint-disable jest/no-conditional-expect -- Using conditional logic in a confined test helper to handle specific scenarios, reducing duplication, balancing test readability and maintainability. **/
+/* eslint-disable jest/no-export -- Util functions are now imported to be used as `it` blocks within `describe` instead of assertions within `it` blocks. */
+import { E2EPage, newE2EPage } from "@stencil/core/testing";
+import { toHaveNoViolations } from "jest-axe";
+import { html } from "../../../support/formatting";
+import type { JSX } from "../../components";
+import { isElementFocused } from "./../utils";
+import {
+  TagOrHTMLWithBeforeContent,
+  isHTML,
+  getTag,
+  getTagOrHTMLWithBeforeContent,
+} from "./../commonTestUtils/setupForTests";
+import { FocusableOptions } from "./../commonTestUtils/focusable";
+
+expect.extend(toHaveNoViolations);
+
+type ComponentTag = keyof JSX.IntrinsicElements;
+type ComponentHTML = string;
+type TagOrHTML = ComponentTag | ComponentHTML;
+
+export async function assertLabelable({
+  page,
+  componentTag,
+  propertyToToggle,
+  focusTargetSelector = componentTag,
+  shadowFocusTargetSelector,
+}: {
+  page: E2EPage;
+  componentTag: string;
+  propertyToToggle?: string;
+  focusTargetSelector?: string;
+  shadowFocusTargetSelector?: string;
+}): Promise<void> {
+  const component = await page.find(componentTag);
+
+  if (propertyToToggle) {
+    await component.getProperty(propertyToToggle);
+  }
+
+  const label = await page.find("calcite-label");
+  await label.callMethod("click"); // we call the method to avoid clicking the child element
+  await page.waitForChanges();
+
+  expect(
+    await page.evaluate(
+      (focusTargetSelector: string): boolean => !!document.activeElement?.closest(focusTargetSelector),
+      focusTargetSelector,
+    ),
+  ).toBe(true);
+
+  if (shadowFocusTargetSelector) {
+    expect(
+      await page.$eval(
+        componentTag,
+        (element: Element, selector: string) => {
+          const shadowRoot = element.shadowRoot;
+          const activeElement = shadowRoot!.activeElement;
+          return activeElement ? activeElement.matches(selector) : false;
+
+          return false;
+        },
+        shadowFocusTargetSelector,
+      ),
+    ).toBe(true);
+  }
+
+  if (propertyToToggle) {
+    const initialPropertyValue: boolean = await component.getProperty(propertyToToggle);
+    const toggledPropertyValue = !initialPropertyValue;
+    expect(await component.getProperty(propertyToToggle)).toBe(toggledPropertyValue);
+
+    // assert that direct clicks on component toggle property correctly
+    component.setProperty(propertyToToggle, initialPropertyValue); // we reset as not all components toggle when clicked
+    await page.waitForChanges();
+    await component.click();
+    await page.waitForChanges();
+    expect(await component.getProperty(propertyToToggle)).toBe(toggledPropertyValue);
+  }
+
+  // assert clicking on labelable keeps focus
+  await component.callMethod("click");
+  await page.waitForChanges();
+
+  expect(await isElementFocused(page, focusTargetSelector)).toBe(true);
+}
+
+export interface LabelableOptions extends Pick<FocusableOptions, "focusTargetSelector" | "shadowFocusTargetSelector"> {
+  /**
+   * If clicking on a label toggles the labelable component, use this prop to specify the name of the toggled prop.
+   */
+  propertyToToggle?: string;
+}
+
+/**
+ * Helper for asserting label clicking functionality works.
+ *
+ * Note that this helper should be used within a describe block.
+ *
+ * @example
+ * describe("labelable", () => {
+ *    async () => labelable("calcite-button")
+ * })
+ *
+ * @param {string} componentTagOrHtml - The component tag or HTML used to test label support.
+ * @param {LabelableOptions} [options] - Labelable options.
+ */
+export function labelable(
+  componentTagOrHtml: TagOrHTML | TagOrHTMLWithBeforeContent,
+  options?: LabelableOptions,
+): void {
+  const id = "labelable-id";
+  const labelTitle = "My Component";
+  const propertyToToggle = options?.propertyToToggle;
+  const focusTargetSelector = options?.focusTargetSelector || `#${id}`;
+  const shadowFocusTargetSelector = options?.shadowFocusTargetSelector;
+  const { beforeContent, tagOrHTML } = getTagOrHTMLWithBeforeContent(componentTagOrHtml);
+  const componentTag = getTag(tagOrHTML);
+  const componentHtml = isHTML(tagOrHTML) ? ensureId(tagOrHTML) : `<${componentTag} id="${id}"></${componentTag}>`;
+
+  function ensureId(html: string): string {
+    return html.includes("id=") ? html : html.replace(componentTag, `${componentTag} id="${id}" `);
+  }
+
+  describe("label wraps labelables", () => {
+    it("is labelable when component is wrapped in a label", async () => {
+      const wrappedHtml = html`<calcite-label>${labelTitle} ${componentHtml}</calcite-label>`;
+      const wrappedPage: E2EPage = await newE2EPage();
+      beforeContent?.(wrappedPage);
+      await wrappedPage.setContent(wrappedHtml);
+      await wrappedPage.waitForChanges();
+
+      await assertLabelable({
+        page: wrappedPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector,
+        shadowFocusTargetSelector,
+      });
+    });
+
+    it("is labelable when wrapping label is set prior to component", async () => {
+      const labelFirstWrappedPage: E2EPage = await newE2EPage();
+      beforeContent?.(labelFirstWrappedPage);
+      await labelFirstWrappedPage.setContent(html`
+        <calcite-label></calcite-label>
+        <template>${componentHtml}</template>
+      `);
+      await labelFirstWrappedPage.waitForChanges();
+      await labelFirstWrappedPage.evaluate(() => {
+        const template = document.querySelector("template");
+        const labelEl = document.querySelector("calcite-label");
+
+        labelEl!.append(template!.content.cloneNode(true));
+      }, componentHtml);
+      await labelFirstWrappedPage.waitForChanges();
+
+      await assertLabelable({
+        page: labelFirstWrappedPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector,
+        shadowFocusTargetSelector,
+      });
+    });
+
+    it("is labelable when a component is set first before being wrapped in a label", async () => {
+      const componentFirstWrappedPage: E2EPage = await newE2EPage();
+      beforeContent?.(componentFirstWrappedPage);
+      await componentFirstWrappedPage.setContent(componentHtml);
+      await componentFirstWrappedPage.waitForChanges();
+      await componentFirstWrappedPage.evaluate((id: string) => {
+        const componentEl = document.querySelector(`[id='${id}']`);
+        const labelEl = document.createElement("calcite-label");
+        document.body.append(labelEl);
+        labelEl.append(componentEl!);
+      }, id);
+      await componentFirstWrappedPage.waitForChanges();
+
+      await assertLabelable({
+        page: componentFirstWrappedPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector,
+        shadowFocusTargetSelector,
+      });
+    });
+
+    it("only sets focus on the first labelable when label is clicked", async () => {
+      const firstLabelableId = `${id}`;
+      const componentFirstWrappedPage: E2EPage = await newE2EPage();
+      beforeContent?.(componentFirstWrappedPage);
+      const content = html`
+        <calcite-label>
+          <!-- duplicate tags should be fine as assertion uses first match -->
+          ${componentHtml.replace(id, firstLabelableId)} ${componentHtml.replace(id, `${id}-2`)}
+          ${componentHtml.replace(id, `${id}-3`)}
+        </calcite-label>
+      `;
+
+      await componentFirstWrappedPage.setContent(content);
+      await componentFirstWrappedPage.waitForChanges();
+
+      const firstLabelableSelector = `#${firstLabelableId}`;
+
+      await assertLabelable({
+        page: componentFirstWrappedPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector:
+          focusTargetSelector === firstLabelableSelector
+            ? firstLabelableSelector
+            : `${firstLabelableSelector} ${focusTargetSelector}`,
+      });
+    });
+  });
+
+  describe("label is sibling to labelables", () => {
+    it("is labelable with label set as a sibling to the component", async () => {
+      const siblingHtml = html`
+        <calcite-label for="${id}">${labelTitle}</calcite-label>
+        ${componentHtml}
+      `;
+      const siblingPage: E2EPage = await newE2EPage();
+      beforeContent?.(siblingPage);
+
+      await siblingPage.setContent(siblingHtml);
+      await siblingPage.waitForChanges();
+
+      await assertLabelable({
+        page: siblingPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector,
+        shadowFocusTargetSelector,
+      });
+    });
+
+    it("is labelable when sibling label is set prior to component", async () => {
+      const labelFirstSiblingPage: E2EPage = await newE2EPage();
+      beforeContent?.(labelFirstSiblingPage);
+      await labelFirstSiblingPage.setContent(html`
+        <calcite-label for="${id}"></calcite-label>
+        <template>${componentHtml}</template>
+      `);
+      await labelFirstSiblingPage.waitForChanges();
+      await labelFirstSiblingPage.evaluate(() => {
+        const template = document.querySelector("template");
+
+        document.body.append(template!.content.cloneNode(true));
+      }, componentHtml);
+      await labelFirstSiblingPage.waitForChanges();
+
+      await assertLabelable({
+        page: labelFirstSiblingPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector,
+        shadowFocusTargetSelector,
+      });
+    });
+
+    it("is labelable for a component set before sibling label", async () => {
+      const componentFirstSiblingPage: E2EPage = await newE2EPage();
+      beforeContent?.(componentFirstSiblingPage);
+      await componentFirstSiblingPage.setContent(componentHtml);
+      await componentFirstSiblingPage.waitForChanges();
+      await componentFirstSiblingPage.evaluate((id: string) => {
+        const label = document.createElement("calcite-label");
+        label.setAttribute("for", `${id}`);
+        document.body.append(label);
+      }, id);
+      await componentFirstSiblingPage.waitForChanges();
+
+      await assertLabelable({
+        page: componentFirstSiblingPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector,
+        shadowFocusTargetSelector,
+      });
+    });
+
+    it("is labelable when label's for is set after initialization", async () => {
+      const siblingHtml = html`
+        <calcite-label>${labelTitle}</calcite-label>
+        ${componentHtml}
+      `;
+      const siblingPage: E2EPage = await newE2EPage();
+      beforeContent?.(siblingPage);
+
+      await siblingPage.setContent(siblingHtml);
+      await siblingPage.waitForChanges();
+
+      const label = await siblingPage.find("calcite-label");
+      label.setProperty("for", id);
+      await siblingPage.waitForChanges();
+
+      await assertLabelable({
+        page: siblingPage,
+        componentTag,
+        propertyToToggle,
+        focusTargetSelector,
+        shadowFocusTargetSelector,
+      });
+    });
+  });
+}
