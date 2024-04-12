4c0134b70a7812a006d30caadf56b14bf2705f01
Ben Elan <no-reply@benelan.dev>
2024-04-12 12:13:02 -0700

    chore: use dist instead of www build

--- packages/calcite-components/stencil.storybook.config.ts
+++ packages/calcite-components/stencil.storybook.config.ts
@@ -1,19 +1,21 @@
-import { OutputTargetDistCustomElements, OutputTargetWww } from "@stencil/core/internal";
+import { OutputTargetDist, OutputTargetDistCustomElements } from "@stencil/core/internal";
 import { create as baseConfigCreator } from "./stencil.config";
 
 export const create: typeof baseConfigCreator = () => {
   const docsConfig = baseConfigCreator();
-
-  const wwwOutputTarget = docsConfig.outputTargets?.find((element) => element.type === "www") as OutputTargetWww;
-  wwwOutputTarget.copy = wwwOutputTarget.copy?.filter((item) => !item.src.includes("demos"));
-  wwwOutputTarget.dir = "__docs-temp__";
 
   const componentsOutputTarget = docsConfig.outputTargets?.find(
     (element) => element.type === "dist-custom-elements",
   ) as OutputTargetDistCustomElements;
   componentsOutputTarget.dir = "__docs-temp__/components";
 
-  docsConfig.outputTargets = [componentsOutputTarget, wwwOutputTarget];
+  const distOutputTarget = docsConfig.outputTargets?.find((element) => element.type === "dist") as OutputTargetDist;
+  distOutputTarget.buildDir = "../__docs-temp__";
+  // distOutputTarget.collectionDir = "../__docs-temp__/collection";
+  // distOutputTarget.typesDir = "../__docs-temp__/types";
+  // distOutputTarget.esmLoaderPath = "../__docs-temp__/loader";
+
+  docsConfig.outputTargets = [componentsOutputTarget, distOutputTarget];
   return docsConfig;
 };
 
