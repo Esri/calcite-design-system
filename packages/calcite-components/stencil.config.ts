7cf3fa25f2386fa26eef6c7843577ffb41c43760
JC Franco <jfranco@esri.com>
2024-04-16 11:53:23 -0700

    remove CI troubleshooting option

--- packages/calcite-components/stencil.config.ts
+++ packages/calcite-components/stencil.config.ts
@@ -146,7 +146,6 @@ export const create: () => Config = () => ({
       "^lodash-es$": "lodash",
     },
     setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
-    testRegex: "(/__tests__/.*|(\\.|/)combobox.e2e)\\.[jt]sx?$",
     transform: {
       "calcite-design-tokens/dist/es6/.*\\.js$":
         "<rootDir>../../node_modules/@stencil/core/testing/jest-preprocessor.js",
