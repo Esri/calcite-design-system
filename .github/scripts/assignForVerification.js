f4e934dc3b80dad6da82ce6dee32ddde4c307985
JC Franco <jfranco@esri.com>
2024-04-15 17:49:32 -0700

    chore(actions): extract label values and utils to support module (#9024)
    **Related Issue:** N/A
    
    ## Summary
    
    ✨🧹✨

--- .github/scripts/assignForVerification.js
+++ .github/scripts/assignForVerification.js
@@ -1,7 +1,10 @@
+const { handoff, issueWorkflow } = require("./support/resources");
+
 module.exports = async ({ github, context }) => {
   const { ISSUE_VERIFIERS, CALCITE_DESIGNERS } = process.env;
   const { label } = context.payload;
-  if (label && label.name === "3 - installed") {
+
+  if (label && label.name === issueWorkflow.installed) {
     const issueProps = {
       issue_number: context.issue.number,
       owner: context.repo.owner,
@@ -21,7 +24,7 @@ module.exports = async ({ github, context }) => {
     const assignees = ISSUE_VERIFIERS.split(",").map((v) => v.trim());
 
     // assign designers if figma updates are required
-    if (issue.labels.map((label) => label.name).includes("figma changes")) {
+    if (issue.labels.map((label) => label.name).includes(handoff.figmaChanges)) {
       assignees.push(...CALCITE_DESIGNERS.split(",").map((v) => v.trim()));
     }
 
