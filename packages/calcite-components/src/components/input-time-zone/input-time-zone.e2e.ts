52b53a187ecac49e023667d37c0810465e61b505
JC Franco <jfranco@esri.com>
2024-04-21 09:42:35 -0700

    feat(input-time-zone): allow clearing values

--- packages/calcite-components/src/components/input-time-zone/input-time-zone.e2e.ts
+++ packages/calcite-components/src/components/input-time-zone/input-time-zone.e2e.ts
@@ -304,25 +304,65 @@ describe("calcite-input-time-zone", () => {
     });
   });
 
-  it("does not allow users to deselect a time zone offset", async () => {
-    const page = await newE2EPage();
-    await page.emulateTimezone(testTimeZoneItems[0].name);
-    await page.setContent(
-      addTimeZoneNamePolyfill(html`
-        <calcite-input-time-zone value="${testTimeZoneItems[1].offset}" open></calcite-input-time-zone>
-      `),
-    );
-    await page.waitForChanges();
+  describe("selection behavior", () => {
+    it("does not allow users to deselect a time zone value by default", async () => {
+      const page = await newE2EPage();
+      await page.emulateTimezone(testTimeZoneItems[0].name);
+      await page.setContent(
+        addTimeZoneNamePolyfill(html`
+          <calcite-input-time-zone value="${testTimeZoneItems[1].offset}" open></calcite-input-time-zone>
+        `),
+      );
+      await page.waitForChanges();
 
-    let selectedTimeZoneItem = await page.find("calcite-input-time-zone >>> calcite-combobox-item[selected]");
-    await selectedTimeZoneItem.click();
-    await page.waitForChanges();
+      let selectedTimeZoneItem = await page.find("calcite-input-time-zone >>> calcite-combobox-item[selected]");
+      await selectedTimeZoneItem.click();
+      await page.waitForChanges();
 
-    selectedTimeZoneItem = await page.find("calcite-input-time-zone >>> calcite-combobox-item[selected]");
-    const input = await page.find("calcite-input-time-zone");
+      selectedTimeZoneItem = await page.find("calcite-input-time-zone >>> calcite-combobox-item[selected]");
+      const input = await page.find("calcite-input-time-zone");
 
-    expect(await input.getProperty("value")).toBe(`${testTimeZoneItems[1].offset}`);
-    expect(await selectedTimeZoneItem.getProperty("textLabel")).toMatch(testTimeZoneItems[1].label);
+      expect(await input.getProperty("value")).toBe(`${testTimeZoneItems[1].offset}`);
+      expect(await selectedTimeZoneItem.getProperty("textLabel")).toMatch(testTimeZoneItems[1].label);
+
+      input.setProperty("value", null);
+      await page.waitForChanges();
+
+      selectedTimeZoneItem = await page.find("calcite-input-time-zone >>> calcite-combobox-item[selected]");
+      expect(await input.getProperty("value")).toBe(`${testTimeZoneItems[1].offset}`);
+      expect(await selectedTimeZoneItem.getProperty("textLabel")).toMatch(testTimeZoneItems[1].label);
+    });
+
+    it("allows users to deselect a time zone value when clearable is enabled", async () => {
+      const page = await newE2EPage();
+      await page.emulateTimezone(testTimeZoneItems[0].name);
+      await page.setContent(
+        addTimeZoneNamePolyfill(
+          html`<calcite-input-time-zone value="${testTimeZoneItems[1].offset}" clearable></calcite-input-time-zone>`,
+        ),
+      );
+
+      const input = await page.find("calcite-input-time-zone");
+      await input.callMethod("setFocus");
+
+      expect(await input.getProperty("value")).toBe(`${testTimeZoneItems[1].offset}`);
+
+      await input.press("Escape");
+      await page.waitForChanges();
+
+      expect(await input.getProperty("value")).toBe(null);
+    });
+
+    it("can be cleared on initialization when clearable is enabled", async () => {
+      const page = await newE2EPage();
+      await page.emulateTimezone(testTimeZoneItems[0].name);
+      await page.setContent(
+        addTimeZoneNamePolyfill(html`<calcite-input-time-zone value="" clearable></calcite-input-time-zone>`),
+      );
+
+      const input = await page.find("calcite-input-time-zone");
+      expect(await input.getProperty("value")).toBe(null);
+    });
   });
 
   describe("selection of subsequent items with the same offset", () => {
--- packages/calcite-components/src/components/input-time-zone/input-time-zone.tsx
+++ packages/calcite-components/src/components/input-time-zone/input-time-zone.tsx
@@ -78,6 +78,13 @@ export class InputTimeZone
   //--------------------------------------------------------------------------
 
   /**
+   * When `true`, an empty value (`null`) will be allowed as a `value`.
+   *
+   * When `false`, an offset or name value is enforced, and clearing the input or blurring will restore the last valid `value`.
+   */
+  @Prop({ reflect: true }) clearable = false;
+
+  /**
    * When `true`, interaction is prevented and the component is displayed with lower opacity.
    */
   @Prop({ reflect: true }) disabled = false;
@@ -188,6 +195,11 @@ export class InputTimeZone
 
   @Watch("value")
   handleValueChange(value: string, oldValue: string): void {
+    if (!value && this.clearable) {
+      this.selectedTimeZoneItem = null;
+      return;
+    }
+
     const timeZoneItem = this.findTimeZoneItem(value);
 
     if (!timeZoneItem) {
@@ -302,7 +314,17 @@ export class InputTimeZone
   private onComboboxChange = (event: CustomEvent): void => {
     event.stopPropagation();
     const combobox = event.target as HTMLCalciteComboboxElement;
-    const selected = this.findTimeZoneItemByLabel(combobox.selectedItems[0].textLabel);
+    const selectedItem = combobox.selectedItems[0];
+
+    if (!selectedItem) {
+      this.value = null;
+      this.selectedTimeZoneItem = null;
+      this.calciteInputTimeZoneChange.emit();
+      return;
+    }
+
+    const selected = this.findTimeZoneItemByLabel(selectedItem.textLabel);
+
     const selectedValue = `${selected.value}`;
 
     if (this.value === selectedValue && selected.label === this.selectedTimeZoneItem.label) {
@@ -326,25 +348,27 @@ export class InputTimeZone
     this.calciteInputTimeZoneOpen.emit();
   };
 
-  private findTimeZoneItem(value: number | string): TimeZoneItem {
+  private findTimeZoneItem(value: number | string | null): TimeZoneItem | null {
     return findTimeZoneItemByProp(this.timeZoneItems, "value", value);
   }
 
-  private findTimeZoneItemByLabel(label: string): TimeZoneItem {
+  private findTimeZoneItemByLabel(label: string | null): TimeZoneItem | null {
     return findTimeZoneItemByProp(this.timeZoneItems, "label", label);
   }
 
   private async updateTimeZoneItemsAndSelection(): Promise<void> {
     this.timeZoneItems = await this.createTimeZoneItems();
 
+    if (!this.value && this.clearable) {
+      this.selectedTimeZoneItem = null;
+      return;
+    }
+
     const fallbackValue = this.mode === "offset" ? getUserTimeZoneOffset() : getUserTimeZoneName();
     const valueToMatch = this.value ?? fallbackValue;
 
-    this.selectedTimeZoneItem = this.findTimeZoneItem(valueToMatch);
-
-    if (!this.selectedTimeZoneItem) {
-      this.selectedTimeZoneItem = this.findTimeZoneItem(fallbackValue);
-    }
+    this.selectedTimeZoneItem =
+      this.findTimeZoneItem(valueToMatch) || this.findTimeZoneItem(fallbackValue);
   }
 
   private async createTimeZoneItems(): Promise<TimeZoneItem[]> {
@@ -388,7 +412,7 @@ export class InputTimeZone
 
     await this.updateTimeZoneItemsAndSelection();
 
-    const selectedValue = `${this.selectedTimeZoneItem.value}`;
+    const selectedValue = this.selectedTimeZoneItem ? `${this.selectedTimeZoneItem.value}` : null;
     afterConnectDefaultValueSet(this, selectedValue);
     this.value = selectedValue;
   }
@@ -406,7 +430,7 @@ export class InputTimeZone
       <Host>
         <InteractiveContainer disabled={this.disabled}>
           <calcite-combobox
-            clearDisabled={true}
+            clearDisabled={!this.clearable}
             disabled={this.disabled}
             label={this.messages.chooseTimeZone}
             lang={this.effectiveLocale}
@@ -420,7 +444,7 @@ export class InputTimeZone
             overlayPositioning={this.overlayPositioning}
             readOnly={this.readOnly}
             scale={this.scale}
-            selectionMode="single-persist"
+            selectionMode={this.clearable ? "single" : "single-persist"}
             status={this.status}
             validation-icon={this.validationIcon}
             validation-message={this.validationMessage}
