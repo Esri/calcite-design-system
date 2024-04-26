be026345dbeda9f52b99f3eae23b7539afede16d
eliza <eli97736@esri.com>
2024-04-25 20:53:55 -0700

    themed for button and toggle

--- packages/calcite-components/src/components/block-section/block-section.stories.ts
+++ packages/calcite-components/src/components/block-section/block-section.stories.ts
@@ -5,23 +5,31 @@ export default {
 };
 
 export const themed_TestOnly = (): string => html`
-  <calcite-block
-    collapsible
-    open
-    heading="Planes, trains, and automobiles are some examples of modes of transportation"
-    description="Planes, trains, and automobiles are some examples of modes of transportation"
-  >
+  <calcite-block heading="Heading" description="summary" collapsible open>
     <calcite-block-section
       text="Planes, trains, and automobiles are some examples of modes of transportation"
-      toggle-display="button"
-      icon-start="pen"
+      open
       icon-end="pen"
+      icon-start="pen"
+      toggle-display="switch"
+      status="valid"
       style="
         --calcite-block-section-icon-end-color: red;
         --calcite-block-section-icon-end-color-hover: blue;
         --calcite-block-section-icon-start-color: orange;
         --calcite-block-section-icon-start-color-hover: purple;
       "
+    >
+      <p>Block section content</p>
+    </calcite-block-section>
+
+    <calcite-block-section
+      text="Planes, trains, and automobiles are some examples of modes of transportation"
+      open
+      icon-end="pen"
+      icon-start="pen"
+      toggle-display="button"
+      status="valid"
     >
       <p>Block section content</p>
     </calcite-block-section>
