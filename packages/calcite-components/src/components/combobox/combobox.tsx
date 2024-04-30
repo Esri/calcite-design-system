21983f00c23f797d9f2ef2797ed9b6ff9ba637f8
JC Franco <jfranco@esri.com>
2024-04-29 19:11:05 -0700

    fix(combobox, input-time-zone): enforce readOnly behavior and styling

--- packages/calcite-components/src/components/combobox/combobox.tsx
+++ packages/calcite-components/src/components/combobox/combobox.tsx
@@ -635,6 +635,10 @@ export class Combobox
   }
 
   private keyDownHandler = (event: KeyboardEvent): void => {
+    if (this.readOnly) {
+      return;
+    }
+
     const { key } = event;
 
     switch (key) {
@@ -1360,7 +1364,7 @@ export class Combobox
       return (
         <calcite-chip
           class={chipClasses}
-          closable
+          closable={!this.readOnly}
           icon={item.icon}
           iconFlipRtl={item.iconFlipRtl}
           id={item.guid ? `${chipUidPrefix}${item.guid}` : null}
@@ -1579,6 +1583,7 @@ export class Combobox
           onFocus={this.comboboxFocusHandler}
           onInput={this.inputHandler}
           placeholder={placeholder}
+          readOnly={this.readOnly}
           role="combobox"
           type="text"
           // eslint-disable-next-line react/jsx-sort-props -- ref should be last so node attrs/props are in sync (see https://github.com/Esri/calcite-design-system/pull/6530)
@@ -1665,12 +1670,13 @@ export class Combobox
   }
 
   render(): VNode {
-    const { selectionDisplay, guid, label, open } = this;
+    const { selectionDisplay, guid, label, open, readOnly } = this;
     const singleSelectionMode = isSingleLike(this.selectionMode);
     const allSelectionDisplay = selectionDisplay === "all";
     const singleSelectionDisplay = selectionDisplay === "single";
     const fitSelectionDisplay = !singleSelectionMode && selectionDisplay === "fit";
     const isClearable = !this.clearDisabled && this.value?.length > 0;
+
     return (
       <Host onClick={this.comboboxFocusHandler}>
         <InteractiveContainer disabled={this.disabled}>
@@ -1714,7 +1720,7 @@ export class Combobox
               </label>
               {this.renderInput()}
             </div>
-            {isClearable ? (
+            {!readOnly && isClearable ? (
               <XButton
                 disabled={this.disabled}
                 key="close-button"
@@ -1722,7 +1728,7 @@ export class Combobox
                 scale={this.scale}
               />
             ) : null}
-            {this.renderChevronIcon()}
+            {!readOnly && this.renderChevronIcon()}
           </div>
           <ul
             aria-labelledby={`${labelUidPrefix}${guid}`}
