39c2f87c36db36456946a6070d3bd68878aa00fd
Jose Carcamo <jcarcamo@esri.com>
2024-05-03 14:26:55 -0700

    Changed the UI test

--- packages/calcite-components/src/components/accordion/accordion.stories.ts
+++ packages/calcite-components/src/components/accordion/accordion.stories.ts
@@ -297,20 +297,11 @@ export const longHeading_MediumIconForLargeAccordionItem_TestOnly = (): string =
   <calcite-accordion scale="l" style="width: 600px"> ${accordionItemsIconHeaderUseCases} </calcite-accordion>
 `;
 export const slottedItemsStretched = (): string => html`
-  <calcite-accordion>
+  <calcite-accordion scale="s">
     <calcite-accordion-item description="Yachts, boats, and dinghies" heading="Watercraft" icon-start="embark">
+      <calcite-action slot="actions-start" icon="smile"> </calcite-action>
       <calcite-notice open>
         <div slot="message">Recommended for coastal use</div>
-      </calcite-notice>
-    </calcite-accordion-item>
-    <calcite-accordion-item icon-start="car" heading="Automobiles" description="Cars, trucks, and buses">
-      <calcite-notice open>
-        <div slot="message">A good choice for inland adventure</div>
-      </calcite-notice>
-    </calcite-accordion-item>
-    <calcite-accordion-item icon-start="plane" heading="Aircrafts" description="Planes, helicopters, and jets">
-      <calcite-notice open>
-        <div slot="message">Cross continents quickly</div>
       </calcite-notice>
     </calcite-accordion-item>
   </calcite-accordion>
