72d1f030accab125643144af6cc0a6f22c048956
Anveshreddy mekala <amekala@esri.com>
2024-05-23 23:31:37 -0500

    test(navigation): add token themeing tests

--- packages/calcite-components/src/components/navigation/navigation.e2e.ts
+++ packages/calcite-components/src/components/navigation/navigation.e2e.ts
@@ -1,6 +1,8 @@
 import { newE2EPage } from "@stencil/core/testing";
 import { accessible, defaults, focusable, hidden, reflects, renders } from "../../tests/commonTests";
 import { html } from "../../../support/formatting";
+import { ComponentTestTokens, themed } from "../../tests/commonTests/themed";
+import { CSS } from "./resources";
 
 describe("calcite-navigation", () => {
   describe("renders", () => {
@@ -66,5 +68,43 @@ describe("calcite-navigation", () => {
 
     await hamburgerMenu.click();
     expect(eventSpy).toHaveReceivedEventTimes(3);
+  });
+
+  describe("theme", () => {
+    const navigationHtml = html` <calcite-navigation navigation-action>
+      <calcite-navigation-logo heading="Walt's Chips"> </calcite-navigation-logo>
+    </calcite-navigation>`;
+
+    describe("default", () => {
+      const tokens: ComponentTestTokens = {
+        "--calcite-navigation-action-background-color": {
+          shadowSelector: `.${CSS.container} >>> .${CSS.containerContent} >>> calcite-action >>> button`,
+          targetProp: "backgroundColor",
+        },
+        "--calcite-navigation-action-background-color-active": {
+          shadowSelector: `.${CSS.container} >>> .${CSS.containerContent} >>> calcite-action >>> button`,
+          targetProp: "backgroundColor",
+          state: { press: { attribute: "appearance", value: "solid" } },
+        },
+        "--calcite-navigation-action-background-color-hover": {
+          shadowSelector: `.${CSS.container} >>> .${CSS.containerContent} >>> calcite-action >>> button`,
+          targetProp: "backgroundColor",
+          state: { hover: { attribute: "appearance", value: "solid" } },
+        },
+        "--calcite-navigation-background-color": {
+          shadowSelector: `.${CSS.container}`,
+          targetProp: "backgroundColor",
+        },
+        "--calcite-navigation-border-color": {
+          shadowSelector: `.${CSS.container}`,
+          targetProp: "borderBlockEndColor",
+        },
+        "--calcite-navigation-width": {
+          shadowSelector: `.${CSS.containerContent}`,
+          targetProp: "width",
+        },
+      };
+      themed(navigationHtml, tokens);
+    });
   });
 });
