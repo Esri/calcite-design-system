b55832a31c75817f16ee3b5c0a2f0adf0bb70fb9
Erik Harper <eriklharper@pm.me>
2024-05-28 16:10:51 -0700

    padStarting the year as well

--- packages/calcite-components/src/utils/date.ts
+++ packages/calcite-components/src/utils/date.ts
@@ -151,9 +151,10 @@ export function datePartsFromLocalizedString(
  */
 export function dateToISO(date?: Date): string {
   if (date instanceof Date) {
-    const month = String(date.getMonth() + 1);
-    const day = String(date.getDate());
-    return `${date.getFullYear()}-${month.length < 2 ? month.padStart(2, "0") : month}-${day.length < 2 ? day.padStart(2, "0") : day}`;
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const year = String(date.getFullYear()).padStart(4, "0");
+    return `${year}-${month}-${day}`;
   }
   return "";
 }
