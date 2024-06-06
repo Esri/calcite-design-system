b34aff3c3cb3670e35dfa1baa1a766310e096aac
Erik Harper <eharperdicianno@esri.com>
2024-06-06 15:56:51 -0700

    test(time-picker, action, input-time-picker): add token theming tests and fix action, input-time-picker token theming tests (#9475)
    **Related Issue:** #7180 
    
    ## Summary
    
    Add token theming tests for `time-picker`.

--- packages/calcite-components/src/components/input-time-picker/input-time-picker.e2e.ts
+++ packages/calcite-components/src/components/input-time-picker/input-time-picker.e2e.ts
@@ -1040,11 +1040,11 @@ describe("calcite-input-time-picker", () => {
         "--calcite-input-time-picker-shadow-focus": {
           selector: "calcite-input-time-picker",
           shadowSelector: "calcite-time-picker",
-          targetProp: "--calcite-time-picker-input-box-shadow-focus",
+          targetProp: "--calcite-time-picker-input-border-color-focus",
         },
         "--calcite-input-time-picker-shadow": {
           shadowSelector: "calcite-time-picker",
-          targetProp: "--calcite-time-picker-input-box-shadow",
+          targetProp: "--calcite-time-picker-input-border-color-hover",
         },
         "--calcite-input-time-picker-icon-color": {
           shadowSelector: "calcite-time-picker",
