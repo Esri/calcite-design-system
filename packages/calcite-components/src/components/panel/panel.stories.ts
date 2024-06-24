be09d2b4979f096d10a982c78fbd366c6690e869
eliza <eli97736@esri.com>
2024-06-24 10:08:26 -0700

    tidy up

--- packages/calcite-components/src/components/panel/panel.stories.ts
+++ packages/calcite-components/src/components/panel/panel.stories.ts
@@ -413,21 +413,14 @@ export const footerStartAndEndSlots = (): string => html`
       id="card-icon-test-1"
       icon-start="check"
     ></calcite-button>
-    <div slot="footer-end">
-      <calcite-button type="button" scale="s" kind="neutral" id="card-icon-test-2" icon-start="stairs">
-      </calcite-button>
-      <calcite-button type="button" scale="s" kind="neutral" id="card-icon-test-3" icon-start="ellipsis">
-      </calcite-button>
-      <calcite-dropdown type="hover">
-        <calcite-button id="card-icon-test-4" slot="trigger" scale="s" kind="neutral" icon-start="caret-down">
-        </calcite-button>
-        <calcite-dropdown-group selection-mode="none">
-          <calcite-dropdown-item>View details</calcite-dropdown-item>
-          <calcite-dropdown-item>Duplicate</calcite-dropdown-item>
-          <calcite-dropdown-item>Delete</calcite-dropdown-item>
-        </calcite-dropdown-group>
-      </calcite-dropdown>
-    </div>
+    <calcite-button
+      type="button"
+      slot="footer-end"
+      kind="neutral"
+      scale="s"
+      id="card-icon-test-1"
+      icon-start="check"
+    ></calcite-button>
   </calcite-panel>
 `;
 
