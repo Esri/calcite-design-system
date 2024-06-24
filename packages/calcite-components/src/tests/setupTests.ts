635b1f92c1c53a115fda8d52eee4ebc43c3bd883
JC Franco <jfranco@esri.com>
2024-06-24 12:52:49 -0700

    skip custom error handling for debugging

--- packages/calcite-components/src/tests/setupTests.ts
+++ packages/calcite-components/src/tests/setupTests.ts
@@ -1,28 +1,4 @@
-import { E2EPage } from "@stencil/core/testing";
-
 const testErrorMessages: string[] = [];
-
-jest.mock("@stencil/core/testing", () => {
-  const originalModule = jest.requireActual("@stencil/core/testing");
-  return {
-    ...originalModule,
-    newE2EPage: async (options) => {
-      const page: E2EPage = await originalModule.newE2EPage(options);
-
-      page.on("console", async (message) => {
-        if (!message.text().includes("JSHandle@error")) {
-          return;
-        }
-
-        const messages = await Promise.all(message.args().map((arg) => arg.getProperty("message")));
-
-        testErrorMessages.push(`${messages.filter(Boolean)}`);
-      });
-
-      return page;
-    },
-  };
-});
 
 beforeEach(() => (testErrorMessages.length = 0));
 
