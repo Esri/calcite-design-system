d35affa1b052818824fb1475deca7cd61473488d
JC Franco <jfranco@esri.com>
2024-07-02 09:04:53 -0700

    include fill icons in icon name type

--- packages/calcite-components/src/components/icon/interfaces.ts
+++ packages/calcite-components/src/components/icon/interfaces.ts
@@ -1,11 +1,13 @@
 import * as icons from "@esri/calcite-ui-icons";
 import { KebabCase } from "type-fest";
 
-type ExtractBaseIcon<T> = T extends `${infer Base}${"16" | "24" | "32"}`
-  ? Base extends `i${infer Rest}`
-    ? Rest
-    : Base
-  : never;
+type ExtractBaseIcon<T> = T extends `${infer Base}${"16" | "24" | "32"}F`
+  ? `${Base}F`
+  : T extends `${infer Base}F`
+    ? `${Base}F`
+    : T extends `${infer Base}${"16" | "24" | "32"}`
+      ? Base
+      : never;
 
 type CamelCaseIcons = ExtractBaseIcon<keyof typeof icons>;
 type KebabCaseIcons = KebabCase<CamelCaseIcons>;
