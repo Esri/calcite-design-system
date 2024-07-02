927ddf79dba8e8711caf0f1c0d20fe3336671747
JC Franco <jfranco@esri.com>
2024-07-02 15:25:10 -0700

    test(loader): add component token E2E tests

--- packages/calcite-components/src/components/loader/loader.e2e.ts
+++ packages/calcite-components/src/components/loader/loader.e2e.ts
@@ -1,5 +1,6 @@
 import { newE2EPage } from "@stencil/core/testing";
-import { hidden, renders } from "../../tests/commonTests";
+import { hidden, renders, themed } from "../../tests/commonTests";
+import { html } from "../../../support/formatting";
 
 describe("calcite-loader", () => {
   describe("renders", () => {
@@ -54,5 +55,136 @@ describe("calcite-loader", () => {
     const loader = await page.find("calcite-loader");
     expect(loader).toHaveAttribute("id");
     expect(loader.getAttribute("id").length).toEqual(36);
+  });
+
+  describe("theme", () => {
+    describe("loader colors", () => {
+      function buildStylesToFreezeAnimationAt(delayInSeconds: number): string {
+        return html`
+          :root { --calcite-duration-factor: 0; } calcite-loader { animation-play-state: paused; animation-duration:
+          10s; animation-direction: initial; animation-iteration-count: 1; animation-delay: -${delayInSeconds}s;
+          stroke-width: 10px; /* providing initial values for loader color segments to ensure true negatives */
+          --calcite-loader-color-start: black; --calcite-loader-color-middle: black; --calcite-loader-color-end: black;
+          }
+        `;
+      }
+
+      describe("start", () => {
+        themed(
+          async () => {
+            const page = await newE2EPage();
+            await page.setContent(html`<calcite-loader></calcite-loader>`);
+            await page.addStyleTag({
+              content: buildStylesToFreezeAnimationAt(1),
+            });
+
+            return { page, tag: "calcite-loader" };
+          },
+          {
+            "--calcite-loader-color-start": {
+              targetProp: "stroke",
+            },
+          },
+        );
+      });
+
+      describe("middle", () => {
+        themed(
+          async () => {
+            const page = await newE2EPage();
+            await page.setContent(html`<calcite-loader></calcite-loader>`);
+            await page.addStyleTag({
+              content: buildStylesToFreezeAnimationAt(4),
+            });
+
+            return { page, tag: "calcite-loader" };
+          },
+          {
+            "--calcite-loader-color-middle": {
+              targetProp: "stroke",
+            },
+          },
+        );
+      });
+
+      describe("end", () => {
+        themed(
+          async () => {
+            const page = await newE2EPage();
+            await page.setContent(html`<calcite-loader></calcite-loader>`);
+            await page.addStyleTag({
+              content: buildStylesToFreezeAnimationAt(7),
+            });
+
+            return { page, tag: "calcite-loader" };
+          },
+          {
+            "--calcite-loader-color-end": {
+              targetProp: "stroke",
+            },
+          },
+        );
+      });
+    });
+
+    describe("determinate", () => {
+      themed(html`<calcite-loader type="determinate" value="10"></calcite-loader>`, {
+        "--calcite-loader-track-color-determinate": {
+          targetProp: "stroke",
+        },
+      });
+    });
+
+    describe("deprecated", () => {
+      describe("default", () => {
+        themed(`calcite-loader`, {
+          "--calcite-loader-bar-width": {
+            targetProp: "strokeWidth",
+          },
+          "--calcite-loader-font-size": {
+            targetProp: "fontSize",
+          },
+          "--calcite-loader-padding": {
+            targetProp: "paddingBlock",
+          },
+          "--calcite-loader-size": [
+            {
+              targetProp: "blockSize",
+            },
+            {
+              targetProp: "minBlockSize",
+            },
+            {
+              targetProp: "inlineSize",
+            },
+          ],
+        });
+
+        describe("inline", () => {
+          themed(html`<calcite-loader inline></calcite-loader>`, {
+            "--calcite-loader-size-inline": [
+              {
+                targetProp: "blockSize",
+              },
+              {
+                targetProp: "minBlockSize",
+              },
+              {
+                targetProp: "inlineSize",
+              },
+            ],
+          });
+        });
+
+        describe("determinate", () => {
+          themed(html`<calcite-loader type="determinate" value="10"></calcite-loader>`, {
+            "--calcite-loader-font-size": {
+              shadowSelector: ".loader__percentage",
+              targetProp: "fontSize",
+            },
+          });
+        });
+      });
+    });
   });
 });
