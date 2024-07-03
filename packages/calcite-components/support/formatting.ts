03a3fa711a7e4b1aeb6fb2a1c2bdf51d20967599
JC Franco <jfranco@esri.com>
2024-07-02 18:52:45 -0700

    chore(formatting): add css tagged template function to leverage Prettier formatting

--- packages/calcite-components/support/formatting.ts
+++ packages/calcite-components/support/formatting.ts
@@ -3,7 +3,7 @@ import dedent from "dedent";
 /**
  * Use this tagged template to help Prettier format any HTML template literals.
  *
- * @param strings the
+ * @param strings the input HTML text
  * @example
  *
  * ```ts
@@ -33,5 +33,31 @@ import dedent from "dedent";
 export function html(strings: string): string;
 export function html(strings: TemplateStringsArray, ...placeholders: any[]): string;
 export function html(strings: any, ...placeholders: any[]): string {
+  return dedent(strings, ...placeholders);
+}
+
+/**
+ * Use this tagged template to help Prettier format any CSS template literals.
+ *
+ * **Note**: this should only be used when the input string only contains CSS. For style tags and style attributes, use the `html` tagged template literal.
+ *
+ * @param strings the input CSS text
+ * @example
+ *
+ * ```ts
+ * const page = await newE2EPage();
+ * await page.setContent(html`...`);
+ * await page.addStyleTag({
+ *   content: css`
+ *     .my-component {
+ *        color: red;
+ *      }
+ * `
+ * });
+ * ```
+ */
+export function css(strings: string): string;
+export function css(strings: TemplateStringsArray, ...placeholders: any[]): string;
+export function css(strings: any, ...placeholders: any[]): string {
   return dedent(strings, ...placeholders);
 }
