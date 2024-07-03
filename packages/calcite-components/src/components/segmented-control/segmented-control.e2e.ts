79c500d723f7b7a978bc8bcbac60818b1b29e236
Jose Carcamo <jcarcamo@esri.com>
2024-07-03 14:57:55 -0700

    Moved and improved test

--- packages/calcite-components/src/components/segmented-control/segmented-control.e2e.ts
+++ packages/calcite-components/src/components/segmented-control/segmented-control.e2e.ts
@@ -193,6 +193,33 @@ describe("calcite-segmented-control", () => {
     expect(await getSelectedItemValue(page)).toBe("2");
   });
 
+  it("updates selection when cleared with undefined", async () => {
+    async function getSelectedItemValue(page: E2EPage): Promise<string> {
+      return page.$eval(
+        "calcite-segmented-control",
+        (segmentedControl: HTMLCalciteSegmentedControlElement) => segmentedControl.selectedItem.value,
+      );
+    }
+    const page = await newE2EPage();
+    await page.setContent(
+      `<calcite-segmented-control>
+            <calcite-segmented-control-item value="1" checked>one</calcite-segmented-control-item>
+            <calcite-segmented-control-item value="2">two</calcite-segmented-control-item>
+          </calcite-segmented-control>`,
+    );
+    await page.waitForChanges();
+    expect(await getSelectedItemValue(page)).toBe("1");
+    const [first, second] = await page.findAll("calcite-segmented-control-item");
+    first.setProperty("checked", undefined);
+    second.setProperty("checked", true);
+    await page.waitForChanges();
+    expect(await getSelectedItemValue(page)).toBe("2");
+    first.setProperty("checked", true);
+    second.setProperty("checked", undefined);
+    await page.waitForChanges();
+    expect(await getSelectedItemValue(page)).toBe("1");
+  });
+
   it("does not emit extraneous events (edge case from #3210)", async () => {
     const page = await newE2EPage();
     await page.setContent(
@@ -330,31 +357,6 @@ describe("calcite-segmented-control", () => {
       });
 
       await assertArrowSelection(page);
-    });
-
-    it("updates selection with undefined", async () => {
-      async function getSelectedItemValue(page: E2EPage): Promise<string> {
-        return page.$eval(
-          "calcite-segmented-control",
-          (segmentedControl: HTMLCalciteSegmentedControlElement) => segmentedControl.selectedItem.value,
-        );
-      }
-      const page = await newE2EPage();
-      await page.setContent(
-        `<calcite-segmented-control>
-              <calcite-segmented-control-item value="1" checked>one</calcite-segmented-control-item>
-              <calcite-segmented-control-item value="2">two</calcite-segmented-control-item>
-            </calcite-segmented-control>`,
-      );
-      const [first, second] = await page.findAll("calcite-segmented-control-item");
-      first.setProperty("checked", undefined);
-      second.setProperty("checked", true);
-      await page.waitForChanges();
-      expect(await getSelectedItemValue(page)).toBe("2");
-      first.setProperty("checked", true);
-      second.setProperty("checked", undefined);
-      await page.waitForChanges();
-      expect(await getSelectedItemValue(page)).toBe("1");
     });
   });
 
