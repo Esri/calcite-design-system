5794d29ae899ab7d6bf902c200ce126b756af135
aPreciado88 <apreciado@esri.com>
2024-07-12 10:56:42 -0700

    fix(shell): remove _TestOnly suffix

--- packages/calcite-components/src/components/shell/shell.stories.ts
+++ packages/calcite-components/src/components/shell/shell.stories.ts
@@ -848,7 +848,7 @@ export const slottedPanelTop_TestOnly = (): string =>
   </calcite-shell>
 `);
 
-export const contentBehindPanelBottom_TestOnly = (): string =>
+export const contentBehindPanelBottom = (): string =>
   html(`
   <calcite-shell
     content-behind
