97ff344dac73a0b8aa6f86eb33acaf28539ec863
DitwanP <price.ditwan@gmail.com>
2024-07-23 16:19:56 -0700

    Create workflow for commenting on blocked issues when the blocking issue has closed.

---
+++ .github/scripts/addUnblockedComment.js
@@ -0,0 +1,39 @@
+// When a blocking issue is closed:
+// 1. Leaves a comment on all the issues listed as blocked in body,
+
+module.exports = async ({ github, context }) => {
+  const issueBody = context.payload.issue.body;
+  const blockedIssuesRegex = /Blocked issues:\s*(#\d+(?:,\s*#\d+)*)/i;
+
+  if (!issueBody) {
+    console.log("No issue body was found");
+    return;
+  }
+
+  // Get the list of issues blocked by this issue if there are any
+  const blockedIssuesLine = issueBody.match(blockedIssuesRegex);
+
+  // If "Blocked issues" line is matched in the body then create a comment on each issue listed
+  if (blockedIssuesLine) {
+    const issueNumbers = blockedIssuesLine[0]
+      .slice(16)
+      .split(", ")
+      .map((number) => number.slice(1));
+
+    for (const issueNumber of issueNumbers) {
+      const issueProps = {
+        issue_number: issueNumber,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+      };
+
+      await github.rest.issues.createComment({
+        ...issueProps,
+        body: `Issue #${context.issue.number} has been closed, this issue can now move forward.`,
+      });
+    }
+  } else {
+    console.log("No blocked issues listed.");
+    return;
+  }
+};
