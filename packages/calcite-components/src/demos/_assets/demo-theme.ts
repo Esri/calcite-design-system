e10a09745693eb58ef483a704a2b02a3dbededd5
Ali Stump <astump@esri.com>
2024-07-26 10:03:12 -0700

    chore(demo-theme): demo-theme helper reference shared util

---
+++ packages/calcite-components/src/demos/_assets/demo-theme.ts
@@ -0,0 +1,69 @@
+/*
+ * EXAMPLE USAGE:
+ *
+ * <demo-theme tokens="
+ *     --calcite-button-background-color,
+ *     --calcite-button-border-color,
+ *     --calcite-button-corner-radius,
+ *     --calcite-button-corner-radius-start-start,
+ *     --calcite-button-corner-radius-start-end,
+ *     --calcite-button-corner-radius-end-start,
+ *     --calcite-button-corner-radius-end-end,
+ *     --calcite-button-loader-color,
+ *     --calcite-button-shadow,
+ *     --calcite-button-text-color,
+ *     --calcite-button-icon-color"
+ * ><calcite-button kind="inverse" scale="l"> Button </calcite-button></demo-theme>
+ *
+ */
+
+import { setCSSVariables } from "../../utils/variableValue";
+
+export class DemoTheme extends HTMLElement {
+  _slot: HTMLSlotElement;
+
+  _el: HTMLElement;
+
+  static observedAttributes = ["tokens"];
+
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement("slot");
+    shadow.append(slot);
+    this._slot = slot;
+    if (this._slot.assignedNodes().length === 1 && this._slot.assignedNodes()[0].nodeName.includes("calcite")) {
+      this._el = this._slot.assignedNodes()[0] as HTMLElement;
+    }
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
+    if (newValue !== oldValue && name === "tokens") {
+      this.updateTheme(newValue);
+    }
+  }
+
+  updateTheme(newValue: string): void {
+    if (typeof newValue === "string") {
+      let tokensList;
+
+      try {
+        tokensList = JSON.parse(newValue);
+      } catch (error) {
+        tokensList = newValue.split(",").map((t) => t.trim());
+      }
+
+      if (Array.isArray(tokensList)) {
+        const stringifiedTheme = setCSSVariables(tokensList);
+
+        if (this._el) {
+          this._el.style.cssText = stringifiedTheme;
+        } else {
+          this.setAttribute("style", stringifiedTheme);
+        }
+      }
+    }
+  }
+}
+
+customElements.define("demo-theme", DemoTheme);
