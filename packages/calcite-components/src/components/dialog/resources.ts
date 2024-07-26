0111c236a30aebe55654ee442134962df3d62042
Matt Driscoll <mdriscoll@esri.com>
2024-07-26 08:43:42 -0700

    feat(dialog): adds new dialog component and deprecates the modal component (#9751)
    **Related Issue:** #7886
    
    ## Summary
    
    - Adds new `calcite-dialog` component.
      - Includes e2e, stories, demos, resources
      - Updates stencil config to note new component
    - Panel esc key should emit close event  
    - Adds `dialogs` slot to `calcite-shell` for slotting dialogs.
    - Deprecates `calcite-modal` component.
    - cleans up global style imports
    - makes `focusTrapDisabled` optional on FocusTrapComponent
    
    ## Notes
    
    - Animations need review

---
+++ packages/calcite-components/src/components/dialog/resources.ts
@@ -0,0 +1,44 @@
+import { DialogPlacement } from "./interfaces";
+
+export const CSS = {
+  dialog: "dialog",
+  panel: "panel",
+  scrim: "scrim",
+  container: "container",
+  containerOpen: "container--open",
+  containerEmbedded: "container--embedded",
+
+  // these classes help apply the animation in phases to only set transform on open/close
+  // this helps avoid a positioning issue for any floating-ui-owning children
+  openingIdle: "dialog--opening-idle",
+  openingActive: "dialog--opening-active",
+  closingIdle: "dialog--closing-idle",
+  closingActive: "dialog--closing-active",
+};
+
+export const SLOTS = {
+  actionBar: "action-bar",
+  alerts: "alerts",
+  content: "content",
+  contentTop: "content-top",
+  contentBottom: "content-bottom",
+  headerActionsStart: "header-actions-start",
+  headerActionsEnd: "header-actions-end",
+  headerMenuActions: "header-menu-actions",
+  headerContent: "header-content",
+  fab: "fab",
+  footer: "footer",
+  footerStart: "footer-start",
+  footerEnd: "footer-end",
+};
+
+export const dialogPlacements: DialogPlacement[] = [
+  "top",
+  "top-start",
+  "top-end",
+  "bottom",
+  "bottom-start",
+  "bottom-end",
+  "cover",
+  "center",
+];
