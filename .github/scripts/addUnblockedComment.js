cbe28370d2c5dc7e9d6afb8783b1287b5ae5949c
DitwanP <price.ditwan@gmail.com>
2024-08-07 10:18:12 -0700

    Feedback changes and other tweaks

--- .github/scripts/addUnblockedComment.js
+++ .github/scripts/addUnblockedComment.js
@@ -1,10 +1,14 @@
+// @ts-check
 // When a blocking issue is closed:
 // 1. Leaves a comment on all the issues listed as blocked in body,
 
+/** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, context }) => {
+  const { repo, owner } = context.repo;
+  const payload = /** @type {import('@octokit/webhooks-types').IssuesLabeledEvent} */ (context.payload);
   const { ISSUE_VERIFIERS } = process.env;
-  const issueBody = context.payload.issue.body;
-  const blockedIssuesRegex = /Blocked issues:\s*(#\d+(?:,\s*#\d+)*)/i;
+  const issueBody = payload.issue.body;
+  const blockedIssuesRegex = /(?!Blocked issues:\s)(#\d+)/gi;
 
   if (!issueBody) {
     console.log("No issue body was found");
@@ -12,28 +16,25 @@ module.exports = async ({ github, context }) => {
   }
 
   // Get the list of issues blocked by this issue if there are any
-  const blockedIssuesLine = issueBody.match(blockedIssuesRegex);
+  const blockedIssues = issueBody.match(blockedIssuesRegex);
 
   // Add a "@" character to notify the user
-  const verifiers = ISSUE_VERIFIERS.split(",").map((v) => " @" + v.trim());
+  const verifiers = ISSUE_VERIFIERS?.split(",").map((v) => `@${v.trim()}`);
 
   // If "Blocked issues" line is matched in the body then create a comment on each issue listed
-  if (blockedIssuesLine) {
-    const issueNumbers = blockedIssuesLine[0]
-      .slice(16)
-      .split(", ")
-      .map((number) => number.slice(1));
+  if (blockedIssues) {
+    const issueNumbers = blockedIssues.map((number) => number.slice(1));
 
     for (const issueNumber of issueNumbers) {
       const issueProps = {
+        repo,
+        owner,
         issue_number: issueNumber,
-        owner: context.repo.owner,
-        repo: context.repo.repo,
       };
 
       await github.rest.issues.createComment({
         ...issueProps,
-        body: `Issue #${context.issue.number} has been closed, this issue can now move forward. cc ${verifiers}`,
+        body: `Issue #${context.issue.number} has been closed, this issue is ready for re-evaluation. \n\ncc ${verifiers}`,
       });
 
       try {
@@ -50,7 +51,6 @@ module.exports = async ({ github, context }) => {
       }
     }
   } else {
-    console.log(`No blocked issues listed in the body of issue #${context.payload.issue.number}.`);
-    return;
+    console.log(`No blocked issues listed in the body of issue #${payload.issue.number}.`);
   }
 };
