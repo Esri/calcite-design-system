476317a23604a6fbe5c43af531d3f95da78d4e62
Ben Elan <no-reply@benelan.dev>
2024-08-06 17:14:07 -0700

    chore: remove my portion of the examples

--- examples/components/preact-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-import './style/index.css';
-import App from './components/app';
-import '@esri/calcite-components/dist/calcite/calcite.css';
-import '@esri/calcite-components';
-import { defineCustomElements } from '@esri/calcite-components/dist/loader';
-
-defineCustomElements(window);
-
-export default App;
