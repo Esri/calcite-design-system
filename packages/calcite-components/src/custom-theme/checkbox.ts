971e309a89fd70dc7e30bbfdf9cfb2031b2b927c
Ali Stump <alisonailea@users.noreply.github.com>
2024-08-20 13:58:14 -0700

    chore(custom-theme): refactor storybook theme for component tokens (#10111)
    **Related Issue:** #7180 
    
    ## Summary
    
    Breaks the custom-theme storybook page into component files for easier
    maintenance. This will allow us to move all component token chromatic
    tests to a single file.

---
+++ packages/calcite-components/src/custom-theme/checkbox.ts
@@ -0,0 +1,6 @@
+import { html } from "../../support/formatting";
+
+export const checkbox = html`<label>
+  <calcite-checkbox indeterminate></calcite-checkbox>
+  Initially indeterminate and unchecked
+</label>`;
