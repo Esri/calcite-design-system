4a0ceda235521d0a490f850fdcc1f10b670237d2
Ali Stump <astump@esri.com>
2024-08-27 10:49:31 -0700

    test(action-group): remove extranious template

--- packages/calcite-components/src/components/action-group/action-group.stories.ts
+++ packages/calcite-components/src/components/action-group/action-group.stories.ts
@@ -94,26 +94,3 @@ export const TurkishLocale_TestOnly = (): string =>
       <calcite-dropdown slot="menu-actions"> </calcite-dropdown>
     </calcite-action-group>
   </div>`;
-html`<div class="container">
-  <calcite-action-bar>
-    <calcite-action-group
-      ><calcite-action id="plus" slot="menu-actions" text="Add" icon="plus"></calcite-action>
-      <calcite-action id="banana" slot="menu-actions" text="Banana" icon="banana"></calcite-action
-    ></calcite-action-group>
-    <calcite-action-group
-      ><calcite-action id="plus" slot="menu-actions" text="Add" icon="plus"></calcite-action>
-      <calcite-action id="banana" slot="menu-actions" text="Banana" icon="banana"></calcite-action
-    ></calcite-action-group>
-  </calcite-action-bar>
-  <calcite-action-group layout="grid">
-    <calcite-action text="Add" icon="arrow-up-left"></calcite-action>
-    <calcite-action text="Save" icon="chevron-up"></calcite-action>
-    <calcite-action text="Layers" icon="arrow-up-right"></calcite-action>
-    <calcite-action text="Basemaps" icon="chevron-left"></calcite-action>
-    <calcite-action text="Layers" icon="layers"></calcite-action>
-    <calcite-action text="Basemaps" icon="chevron-right"></calcite-action>
-    <calcite-action text="Basemaps" icon="arrow-down-left"></calcite-action>
-    <calcite-action text="Layers" icon="chevron-down"></calcite-action>
-    <calcite-action text="Basemaps" icon="arrow-down-right"></calcite-action>
-  </calcite-action-group>
-</div>`;
