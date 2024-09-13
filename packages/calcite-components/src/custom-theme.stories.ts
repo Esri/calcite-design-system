ca932b5c4a3a5d7bf9bcf032ed2a0325f6920026
Ali Stump <alisonailea@users.noreply.github.com>
2024-09-13 11:33:53 -0700

    feat(accordion-item): update component tokens
    **Related Issue:** #7180 #4060
    
    ## Summary
    
    While this does not create additional tokens for the background color in
    a -hover or -press state explicitly because this is outside of our
    current design patterns, it does allow a user to accomplish this by only
    targeting the top-level component styles.
    
    ```css
    calcite-accordion-item:hover {
    --calcite-accordion-item-background-color: blue;
    }
    calcite-accordion-item[expanded] {
    --calcite-accordion-item-header-background-color: blue;
    }
    ```
    
    ### Tokens
    
    --calcite-accordion-item-background-color: Specifies the component's
    background color.
    --calcite-accordion-item-border-color: Specifies the component's border
    color.
    --calcite-accordion-item-content-space: Specifies the component's
    padding.
    --calcite-accordion-item-header-background-color: Specifies the
    background color of the component's header.
    --calcite-accordion-item-text-color: Specifies the component's text
    color.
    --calcite-accordion-item-heading-text-color: Specifies the component's
    heading text color.
    --calcite-accordion-item-icon-color: Specifies the component's default
    icon color.
    --calcite-accordion-item-start-icon-color: Specifies the component's
    start icon color. Fallback to --calcite-accordion-item-icon-color or
    current color.
    --calcite-accordion-item-end-icon-color: Specifies the component's end
    icon color. Fallback to --calcite-accordion-item-icon-color or current
    color.
    --calcite-accordion-item-expand-icon-color: Specifies the component's
    expand icon color.
     
    ### Deprecated
    
    --calcite-accordion-border-color: Use
    --calcite-accordion-item-border-color. Specifies the component's border
    color.
    --calcite-accordion-text-color-hover: Use
    --calcite-accordion-item-text-color-hover. Specifies the component's
    main text color on hover.
    --calcite-accordion-text-color-pressed: Use
    --calcite-accordion-item-text-color-press. Specifies the component's
    main text color when pressed.
    --calcite-accordion-text-color: Use --calcite-accordion-item-text-color.
    Specifies the component's text color.

--- packages/calcite-components/src/custom-theme.stories.ts
+++ packages/calcite-components/src/custom-theme.stories.ts
@@ -10,7 +10,8 @@ import {
   actionPadTokens,
   actionGroupTokens,
 } from "./custom-theme/action";
-import { accordion } from "./custom-theme/accordion";
+import { accordionItemTokens } from "./custom-theme/accordion-item";
+import { accordion, accordionTokens } from "./custom-theme/accordion";
 import { buttons } from "./custom-theme/button";
 import { card, cardThumbnail, cardTokens } from "./custom-theme/card";
 import { checkbox } from "./custom-theme/checkbox";
@@ -119,6 +120,8 @@ export default {
   title: "Theming/Custom Theme",
   args: {
     ...globalTokens,
+    ...accordionTokens,
+    ...accordionItemTokens,
     ...actionTokens,
     ...actionBarTokens,
     ...actionMenuTokens,
@@ -138,6 +141,8 @@ export const themingInteractive = (args: Record<string, string>): string => {
 export const theming_TestOnly = (): string => {
   return kitchenSink(
     {
+      ...accordionTokens,
+      ...accordionItemTokens,
       ...actionTokens,
       ...actionBarTokens,
       ...actionMenuTokens,
