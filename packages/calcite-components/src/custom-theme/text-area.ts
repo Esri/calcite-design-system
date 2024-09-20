51d59f46f8e5756468d5cffa355f977f3841fadc
Ben Elan <no-reply@benelan.dev>
2024-09-19 17:14:33 -0700

    chore: cleanup

--- packages/calcite-components/src/custom-theme/text-area.ts
+++ packages/calcite-components/src/custom-theme/text-area.ts
@@ -11,7 +11,6 @@ export const textAreaTokens = {
   calciteTextAreaMinHeight: "",
   calciteTextAreaMaxWidth: "",
   calciteTextAreaMinWidth: "",
-  calciteTextAreaPlaceholderTextColor: "",
   calciteTextAreaTextColor: "",
 };
 
