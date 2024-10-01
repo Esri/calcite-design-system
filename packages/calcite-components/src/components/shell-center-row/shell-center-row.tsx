5b15a0252afa5e7d8fefd19f806f42b7641831a2
Matt Driscoll <mdriscoll@esri.com>
2024-10-01 10:24:18 -0700

    fix(shell-center-row): fix rendering tied to named-slot content (#10451)
    **Related Issue:** #6059
    
    - remove use of `getSlotted` utility
    - replace with `slotchange` event and `@State` variables to update the
    display of elements.
    - existing tests should suffice

--- packages/calcite-components/src/components/shell-center-row/shell-center-row.tsx
+++ packages/calcite-components/src/components/shell-center-row/shell-center-row.tsx
@@ -1,80 +1,56 @@
-// @ts-strict-ignore
-import { LitElement, property, Fragment, h, state, JsxNode } from "@arcgis/lumina";
+import { Component, Element, Fragment, h, Prop, State, VNode } from "@stencil/core";
 import { Position, Scale } from "../interfaces";
 import { slotChangeGetAssignedElements } from "../../utils/dom";
-import { logger } from "../../utils/logger";
-import type { ActionBar } from "../action-bar/action-bar";
 import { CSS, SLOTS } from "./resources";
-import { styles } from "./shell-center-row.scss";
-
-declare global {
-  interface DeclareElements {
-    "calcite-shell-center-row": ShellCenterRow;
-  }
-}
 
 /**
- * @deprecated Use the `calcite-shell-panel` component instead.
  * @slot - A slot for adding content to the `calcite-shell-panel`.
  * @slot action-bar - A slot for adding a `calcite-action-bar` to the `calcite-shell-panel`.
  */
-export class ShellCenterRow extends LitElement {
-  // #region Static Members
+@Component({
+  tag: "calcite-shell-center-row",
+  styleUrl: "shell-center-row.scss",
+  shadow: true,
+})
+export class ShellCenterRow {
+  // --------------------------------------------------------------------------
+  //
+  //  Properties
+  //
+  // --------------------------------------------------------------------------
 
-  static override styles = styles;
+  /**
+   * When `true`, the content area displays like a floating panel.
+   */
+  @Prop({ reflect: true }) detached = false;
 
-  // #endregion
+  /**
+   * Specifies the maximum height of the component.
+   */
+  @Prop({ reflect: true }) heightScale: Scale = "s";
 
-  // #region State Properties
+  /**
+   * Specifies the component's position. Will be flipped when the element direction is right-to-left (`"rtl"`).
+   */
+  @Prop({ reflect: true }) position: Extract<"start" | "end", Position> = "end";
 
-  @state() actionBar: ActionBar["el"];
+  // --------------------------------------------------------------------------
+  //
+  //  Private Properties
+  //
+  // --------------------------------------------------------------------------
 
-  // #endregion
+  @Element() el: HTMLCalciteShellCenterRowElement;
 
-  // #region Public Properties
+  @State() actionBar: HTMLCalciteActionBarElement;
 
-  /** When `true`, the content area displays like a floating panel. */
-  @property({ reflect: true }) detached = false;
+  // --------------------------------------------------------------------------
+  //
+  //  Render Methods
+  //
+  // --------------------------------------------------------------------------
 
-  /** Specifies the maximum height of the component. */
-  @property({ reflect: true }) heightScale: Scale = "s";
-
-  /** Specifies the component's position. Will be flipped when the element direction is right-to-left (`"rtl"`). */
-  @property({ reflect: true }) position: Extract<"start" | "end", Position> = "end";
-
-  // #endregion
-
-  // #region Lifecycle
-
-  load(): void {
-    logger.deprecated("component", {
-      name: "shell-center-row",
-      removalVersion: 4,
-      suggested: "shell-panel",
-    });
-  }
-
-  loaded(): void {
-    if (this.actionBar?.position === "end") {
-      this.requestUpdate();
-    }
-  }
-
-  // #endregion
-
-  // #region Private Methods
-
-  private handleActionBarSlotChange(event: Event): void {
-    this.actionBar = slotChangeGetAssignedElements(event).filter((el): el is ActionBar["el"] =>
-      el.matches("calcite-action-bar"),
-    )[0];
-  }
-
-  // #endregion
-
-  // #region Rendering
-
-  override render(): JsxNode {
+  render(): VNode {
     const { actionBar } = this;
 
     const contentNode = (
@@ -85,18 +61,22 @@ export class ShellCenterRow extends LitElement {
 
     const actionBarNode = (
       <div class={CSS.actionBarContainer} hidden={!this.actionBar} key="action-bar">
-        <slot name={SLOTS.actionBar} onSlotChange={this.handleActionBarSlotChange} />
+        <slot name={SLOTS.actionBar} onSlotchange={this.handleActionBarSlotChange} />
       </div>
     );
 
-    const children: JsxNode[] = [actionBarNode, contentNode];
+    const children: VNode[] = [actionBarNode, contentNode];
 
     if (actionBar?.position === "end") {
       children.reverse();
     }
 
-    return <>{children}</>;
+    return <Fragment>{children}</Fragment>;
   }
 
-  // #endregion
+  private handleActionBarSlotChange = (event: Event): void => {
+    this.actionBar = slotChangeGetAssignedElements(event).filter(
+      (el): el is HTMLCalciteActionBarElement => el.matches("calcite-action-bar"),
+    )[0];
+  };
 }
