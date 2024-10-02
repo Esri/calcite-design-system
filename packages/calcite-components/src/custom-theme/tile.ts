c7c4f5e324629807de59df1c09d183478f3513c5
Erik Harper <eriklharper@pm.me>
2024-10-02 15:48:15 -0700

    updating storybook and demo pages for theming examples

---
+++ packages/calcite-components/src/custom-theme/tile.ts
@@ -0,0 +1,21 @@
+import { html } from "../../support/formatting";
+
+export const tileTokens = {
+  calciteTileBackgroundColor: "",
+  calciteTileBorderColor: "",
+  calciteTileDescriptionTextColor: "",
+  calciteTileHeadingTextColor: "",
+  calciteTileIconColor: "",
+  calciteTileSelectionIconColor: "",
+  calciteTileSelectionIconColorHover: "",
+  calciteTileShadow: "",
+};
+
+export const tile = html`
+  <calcite-tile
+    heading="Tile heading lorem ipsum"
+    description="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collab on thinking to further the overall."
+    icon="layers"
+    selected
+  ></calcite-tile>
+`;
