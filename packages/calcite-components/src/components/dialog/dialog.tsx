e93fdbcf361d59efdee625fb0ef50d8044b6e545
Matt Driscoll <mdriscoll@esri.com>
2024-10-08 16:09:30 -0700

    fix(dialog): no longer apply transform styling unless dragEnabled or resizable (#10503)
    **Related Issue:** #10500
    
    ## Summary
    
    - no longer apply transform styling unless dragEnabled or resizable
    - add e2e test

--- packages/calcite-components/src/components/dialog/dialog.tsx
+++ packages/calcite-components/src/components/dialog/dialog.tsx
@@ -605,9 +605,16 @@ export class Dialog
       dragPosition: { x, y },
       resizePosition,
       transitionEl,
+      dragEnabled,
+      resizable,
     } = this;
 
     if (!transitionEl) {
+      return;
+    }
+
+    if (!dragEnabled && !resizable) {
+      transitionEl.style.transform = null;
       return;
     }
 
@@ -616,7 +623,8 @@ export class Dialog
     const translateX = Math.round(x + left + right);
     const translateY = Math.round(y + top + bottom);
 
-    transitionEl.style.transform = `translate(${translateX}px, ${translateY}px)`;
+    transitionEl.style.transform =
+      translateX || translateY ? `translate(${translateX}px, ${translateY}px)` : null;
   }
 
   private updateSize({
