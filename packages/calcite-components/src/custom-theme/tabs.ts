21a0accb691d6f07c8636fa4114784555774fff9
JC Franco <jfranco@esri.com>
2024-10-18 12:54:31 -0700

    improve theming screenshot test

--- packages/calcite-components/src/custom-theme/tabs.ts
+++ packages/calcite-components/src/custom-theme/tabs.ts
@@ -13,7 +13,13 @@ export const tabsTokens = {
 };
 
 export const tabs = html`
-  <calcite-tabs>
+  <style>
+    calcite-tabs {
+      width: 400px;
+    }
+  </style>
+
+  <calcite-tabs position="bottom" style="--calcite-tab-icon-start-color: purple">
     <calcite-tab-nav slot="title-group">
       <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right" selected
         >Tab 1 Title
@@ -21,23 +27,99 @@ export const tabs = html`
       <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 2 Title</calcite-tab-title>
       <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 3 Title</calcite-tab-title>
       <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 4 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 5 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 6 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 7 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 8 Title</calcite-tab-title>
     </calcite-tab-nav>
-    <calcite-tab selected>Tab 1 content</calcite-tab>
+    <calcite-tab selected>Tab 1 Content</calcite-tab>
     <calcite-tab>Tab 2 Content</calcite-tab>
     <calcite-tab>Tab 3 Content</calcite-tab>
     <calcite-tab>Tab 4 Content</calcite-tab>
+    <calcite-tab>Tab 5 Content</calcite-tab>
+    <calcite-tab>Tab 6 Content</calcite-tab>
+    <calcite-tab>Tab 7 Content</calcite-tab>
+    <calcite-tab>Tab 8 Content</calcite-tab>
+  </calcite-tabs>
+
+  <br />
+
+  <calcite-tabs position="top" style="--calcite-tab-icon-end-color: purple">
+    <calcite-tab-nav slot="title-group">
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right" selected
+        >Tab 1 Title
+      </calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 2 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 3 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 4 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 5 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 6 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 7 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 8 Title</calcite-tab-title>
+    </calcite-tab-nav>
+    <calcite-tab selected>Tab 1 Content</calcite-tab>
+    <calcite-tab>Tab 2 Content</calcite-tab>
+    <calcite-tab>Tab 3 Content</calcite-tab>
+    <calcite-tab>Tab 4 Content</calcite-tab>
+    <calcite-tab>Tab 5 Content</calcite-tab>
+    <calcite-tab>Tab 6 Content</calcite-tab>
+    <calcite-tab>Tab 7 Content</calcite-tab>
+    <calcite-tab>Tab 8 Content</calcite-tab>
   </calcite-tabs>
 `;
 
-export const tabsBordered = html` <calcite-tabs bordered>
-  <calcite-tab-nav slot="title-group">
-    <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right" selected>Tab 1 Title </calcite-tab-title>
-    <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 2 Title</calcite-tab-title>
-    <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 3 Title</calcite-tab-title>
-    <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 4 Title</calcite-tab-title>
-  </calcite-tab-nav>
-  <calcite-tab selected>Tab 1 content</calcite-tab>
-  <calcite-tab>Tab 2 Content</calcite-tab>
-  <calcite-tab>Tab 3 Content</calcite-tab>
-  <calcite-tab>Tab 4 Content</calcite-tab>
-</calcite-tabs>`;
+export const tabsBordered = html`
+  <style>
+    calcite-tabs {
+      width: 400px;
+    }
+  </style>
+
+  <calcite-tabs bordered position="bottom" style="--calcite-tab-icon-start-color: purple">
+    <calcite-tab-nav slot="title-group">
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right" selected
+        >Tab 1 Title
+      </calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 2 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 3 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 4 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 5 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 6 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 7 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 8 Title</calcite-tab-title>
+    </calcite-tab-nav>
+    <calcite-tab selected>Tab 1 Content</calcite-tab>
+    <calcite-tab>Tab 2 Content</calcite-tab>
+    <calcite-tab>Tab 3 Content</calcite-tab>
+    <calcite-tab>Tab 4 Content</calcite-tab>
+    <calcite-tab>Tab 5 Content</calcite-tab>
+    <calcite-tab>Tab 6 Content</calcite-tab>
+    <calcite-tab>Tab 7 Content</calcite-tab>
+    <calcite-tab>Tab 8 Content</calcite-tab>
+  </calcite-tabs>
+
+  <br />
+
+  <calcite-tabs position="top" style="--calcite-tab-icon-end-color: purple">
+    <calcite-tab-nav slot="title-group">
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right" selected
+        >Tab 1 Title
+      </calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 2 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 3 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 4 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 5 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 6 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 7 Title</calcite-tab-title>
+      <calcite-tab-title closable icon-start="arrow-left" icon-end="arrow-right">Tab 8 Title</calcite-tab-title>
+    </calcite-tab-nav>
+    <calcite-tab selected>Tab 1 Content</calcite-tab>
+    <calcite-tab>Tab 2 Content</calcite-tab>
+    <calcite-tab>Tab 3 Content</calcite-tab>
+    <calcite-tab>Tab 4 Content</calcite-tab>
+    <calcite-tab>Tab 5 Content</calcite-tab>
+    <calcite-tab>Tab 6 Content</calcite-tab>
+    <calcite-tab>Tab 7 Content</calcite-tab>
+    <calcite-tab>Tab 8 Content</calcite-tab>
+  </calcite-tabs>
+`;
