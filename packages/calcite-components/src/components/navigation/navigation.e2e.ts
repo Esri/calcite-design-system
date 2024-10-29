43acf035a0ad5a9a77bee1845e4585e9e0ad6281
Anveshreddy mekala <amekala@esri.com>
2024-10-29 14:30:02 -0500

    feat(navigation): add component tokens

--- packages/calcite-components/src/components/navigation/navigation.e2e.ts
+++ packages/calcite-components/src/components/navigation/navigation.e2e.ts
@@ -1,6 +1,8 @@
 import { newE2EPage } from "@stencil/core/testing";
 import { accessible, defaults, focusable, hidden, reflects, renders } from "../../tests/commonTests";
 import { html } from "../../../support/formatting";
+import { ComponentTestTokens, themed } from "../../tests/commonTests/themed";
+import { CSS } from "./resources";
 
 describe("calcite-navigation", () => {
   describe("renders", () => {
@@ -66,5 +68,68 @@ describe("calcite-navigation", () => {
 
     await hamburgerMenu.click();
     expect(eventSpy).toHaveReceivedEventTimes(3);
+  });
+
+  describe("theme", () => {
+    const navigationHtml = html`
+      <calcite-navigation>
+        <calcite-navigation-logo
+          heading="Walt's Chips"
+          description="Eastern Potato Chip Company"
+          icon="layers"
+          slot="logo"
+        >
+        </calcite-navigation-logo>
+        <calcite-navigation-user slot="user" full-name="Walt McChipson" username="waltChip"> </calcite-navigation-user>
+        <calcite-navigation slot="navigation-secondary">
+          <calcite-menu slot="content-start">
+            <calcite-menu-item breadcrumb text="All Routes" icon-start="book" text-enabled></calcite-menu-item>
+          </calcite-menu>
+        </calcite-navigation>
+        <calcite-navigation slot="navigation-tertiary">
+          <calcite-menu slot="content-end">
+            <calcite-menu-item breadcrumb text="All Routes" icon-start="book" text-enabled></calcite-menu-item>
+          </calcite-menu>
+        </calcite-navigation>
+      </calcite-navigation>
+    `;
+
+    describe("default", () => {
+      const tokens: ComponentTestTokens = {
+        "--calcite-navigation-background-color": [
+          {
+            shadowSelector: `.${CSS.container}`,
+            targetProp: "backgroundColor",
+          },
+          {
+            selector: "calcite-navigation-logo",
+            targetProp: "backgroundColor",
+          },
+          {
+            selector: "calcite-navigation-user",
+            targetProp: "backgroundColor",
+          },
+        ],
+        "--calcite-navigation-width": {
+          shadowSelector: `.${CSS.containerContent}`,
+          targetProp: "width",
+        },
+        "--calcite-navigation-border-color": [
+          {
+            shadowSelector: `.${CSS.primary}`,
+            targetProp: "borderColor",
+          },
+          {
+            shadowSelector: `.${CSS.secondary}`,
+            targetProp: "borderColor",
+          },
+          {
+            shadowSelector: `.${CSS.tertiary}`,
+            targetProp: "borderColor",
+          },
+        ],
+      };
+      themed(navigationHtml, tokens);
+    });
   });
 });
