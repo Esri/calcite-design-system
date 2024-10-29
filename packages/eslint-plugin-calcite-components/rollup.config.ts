2815c9e4ec141511104ed3092cab494593cbae86
JC Franco <jfranco@esri.com>
2024-10-29 10:55:44 -0700

    fix eslint-plugin-calcite-components build output

--- packages/eslint-plugin-calcite-components/rollup.config.ts
+++ packages/eslint-plugin-calcite-components/rollup.config.ts
@@ -1,12 +1,11 @@
-import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { version } from "./package.json";
 
 export default {
+  external: ["eslint", "@typescript-eslint/utils"],
   input: "src/index.ts",
   plugins: [
-    commonjs(),
     nodeResolve({
       preferBuiltins: true,
     }),
