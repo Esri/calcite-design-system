1e792e356ffd01a1c3095797a9100b07965c9891
Matt Driscoll <mdriscoll@esri.com>
2024-10-31 11:46:42 -0700

    cleanup stories

--- packages/calcite-components/src/components/sort-handle/sort-handle.stories.ts
+++ packages/calcite-components/src/components/sort-handle/sort-handle.stories.ts
@@ -2,6 +2,11 @@ import { html } from "../../../support/formatting";
 
 export default {
   title: "Components/SortHandle",
+  parameters: {
+    chromatic: {
+      delay: 500,
+    },
+  },
 };
 
 export const closed = (): string => html`
@@ -12,28 +17,48 @@ export const open = (): string =>
   html`<calcite-sort-handle label="test" set-position="4" set-size="10" open></calcite-sort-handle>`;
 
 export const positions = (): string => html`
-  <h2>First Position</h2>
-  <calcite-sort-handle label="test" set-position="1" set-size="10" open></calcite-sort-handle>
-
-  <h2>Second Position</h2>
-  <calcite-sort-handle label="test" set-position="2" set-size="10" open></calcite-sort-handle>
-
-  <h2>Second to Last Position</h2>
-  <calcite-sort-handle label="test" set-position="9" set-size="10" open></calcite-sort-handle>
-
-  <h2>Last Position</h2>
-  <calcite-sort-handle label="test" set-position="10" set-size="10" open></calcite-sort-handle>
+  <style>
+    .wrapper {
+      display: grid;
+      grid-template-columns: 300px 300px;
+      grid-gap: 50px;
+    }
+    .box {
+      height: 200px;
+    }
+  </style>
+  <div class="wrapper">
+    <div class="box">
+      <strong>First Position</strong>
+      <calcite-sort-handle label="test" set-position="1" set-size="10" open></calcite-sort-handle>
+    </div>
+    <div class="box">
+      <strong>Second Position</strong>
+      <calcite-sort-handle label="test" set-position="2" set-size="10" open></calcite-sort-handle>
+    </div>
+    <div class="box">
+      <strong>Second to Last Position</strong>
+      <calcite-sort-handle label="test" set-position="9" set-size="10" open></calcite-sort-handle>
+    </div>
+    <div class="box">
+      <strong>Last Position</strong>
+      <calcite-sort-handle label="test" set-position="10" set-size="10" open></calcite-sort-handle>
+    </div>
+  </div>
 `;
 
-export const withItems = (): string =>
-  html`<calcite-sort-handle label="test" set-position="4" set-size="10" open></calcite-sort-handle>
-    <script>
-      const sortHandle = document.querySelector("calcite-sort-handle");
-      sortHandle.moveToItems = [
-        { element: document.createElement("div"), id: "1", label: "Group 1" },
-        { element: document.createElement("div"), id: "2", label: "Group 2" },
-      ];
-    </script>`;
+export const withItems = (): string => html`
+  <div style="height:300px">
+    <calcite-sort-handle label="test" set-position="4" set-size="10" open></calcite-sort-handle>
+  </div>
+  <script>
+    const sortHandle = document.querySelector("calcite-sort-handle");
+    sortHandle.moveToItems = [
+      { element: document.createElement("div"), id: "1", label: "Group 1" },
+      { element: document.createElement("div"), id: "2", label: "Group 2" },
+    ];
+  </script>
+`;
 
 export const disabled = (): string => html`
   <calcite-sort-handle label="test" set-position="4" set-size="10" disabled></calcite-sort-handle>
