eed4a7323bae46cfb39b701a3fdc18f447ad526a
Max Patiiuk <max@patii.uk>
2024-11-01 09:10:24 -0700

    refactor: prepare tests for migration (#10509)
    Make minor adjustments to Calcite code to have post-migration tests
    pass.
    
    Changes that need to be done post-migration are encoded in the following
    codemod files in the arcgis-web-components repository:
    - /packages/support-packages/stencil-to-lit/src/code/fileSpecific.ts
    -
    /packages/support-packages/stencil-to-lit/src/component/fileSpecific.ts
    
    At this point, I am finishing up the failing tests in the S components
    and will be pretty much done with all A-S component (with exception of
    https://github.com/Esri/calcite-design-system/issues/10394 and
    https://github.com/Esri/calcite-design-system/issues/10495)

--- packages/calcite-components/src/components/shell-panel/shell-panel.e2e.ts
+++ packages/calcite-components/src/components/shell-panel/shell-panel.e2e.ts
@@ -118,7 +118,7 @@ describe("calcite-shell-panel", () => {
       "calcite-shell-panel",
       (panel: HTMLCalciteShellPanelElement, containerClass: string, contentClass: string) => {
         const container = panel.shadowRoot.querySelector(containerClass);
-        return container.firstElementChild.className == contentClass;
+        return container.firstElementChild.classList.contains(contentClass);
       },
       `.${CSS.container}`,
       CSS.content,
