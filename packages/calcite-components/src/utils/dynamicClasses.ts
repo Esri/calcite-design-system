fba447f1d7763ea52bf3a00187708c2ba25547d1
eliza <eli97736@esri.com>
2024-11-12 17:34:37 -0800

    return a valid key from util

--- packages/calcite-components/src/utils/dynamicClasses.ts
+++ packages/calcite-components/src/utils/dynamicClasses.ts
@@ -5,5 +5,5 @@ export function getDimensionClass(
   size: Width | Height,
   scale: Scale,
 ): `${typeof type}-${Scale | Width | Height}` {
-  return size ? `${type}-${size}` : scale ? `${type}-${scale}` : undefined;
+  return size ? `${type}-${size}` : scale ? `${type}-${scale}` : "width-m";
 }
