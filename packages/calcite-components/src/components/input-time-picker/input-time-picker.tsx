89fdbf42bae8c49957f7c6ccf12a2f7646dceb15
Erik Harper <eriklharper@pm.me>
2024-11-14 12:21:59 -0800

    fix(input-time-picker): retain focus inside the input when clicked

--- packages/calcite-components/src/components/input-time-picker/input-time-picker.tsx
+++ packages/calcite-components/src/components/input-time-picker/input-time-picker.tsx
@@ -969,6 +969,7 @@ export class InputTimePicker
         <calcite-popover
           autoClose={true}
           focusTrapDisabled={this.focusTrapDisabled}
+          initialFocus={false}
           label={messages.chooseTime}
           lang={this.messages._lang}
           oncalcitePopoverBeforeClose={this.popoverBeforeCloseHandler}
