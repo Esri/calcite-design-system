1f2d6ed7e414846b75fca2f1bcf3aab30f7a783c
Erik Harper <eriklharper@pm.me>
2024-11-14 15:17:10 -0800

    review comments, rolling back package.json changes

--- packages/calcite-components/src/components/input-time-picker/input-time-picker.tsx
+++ packages/calcite-components/src/components/input-time-picker/input-time-picker.tsx
@@ -966,7 +966,7 @@ export class InputTimePicker
         <calcite-popover
           autoClose={true}
           focusTrapDisabled={this.focusTrapDisabled}
-          initialFocus={false}
+          initialFocusTrapFocus={false}
           label={messages.chooseTime}
           lang={this.messages._lang}
           oncalcitePopoverBeforeClose={this.popoverBeforeCloseHandler}
