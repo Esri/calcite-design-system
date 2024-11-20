997b7dabc3ecff03d8fede3de0de58aa57f052a7
Matt Driscoll <mdriscoll@esri.com>
2024-11-20 09:47:14 -0800

    feat(list, list-item, list-item-group): add component tokens  (#10669)
    **Related Issue:** #7180
    
    ## Summary
    
    adds new component tokens, e2e tests, demo page examples, and chromatic
    theme tests.
    
    ### New CSS variables
    
    #### List
    
    --calcite-list-background-color: Specifies the component's background
    color.
    
    #### ListItem
    
    --calcite-list-background-color-hover: Specifies the component's
    background color when hovered.
    --calcite-list-background-color-press: Specifies the component's
    background color when pressed.
    --calcite-list-background-color: Specifies the component's background
    color.
     --calcite-list-border-color: Specifies the component's border color.
     --calcite-list-content-color: Specifies the content color.
     --calcite-list-description-color: Specifies the description color.
     --calcite-list-icon-color: Specifies the component's icon color.
     --calcite-list-label-color: Specifies the label color.
    --calcite-list-selection-border-color: Specifies the component's
    selection border color.
    
    #### ListItemGroup
    
    --calcite-list-background-color: Specifies the component's background
    color.
    --calcite-list-color: Specifies the component's color.

--- packages/calcite-components/src/custom-theme.stories.ts
+++ packages/calcite-components/src/custom-theme.stories.ts
@@ -27,6 +27,7 @@ import { inputNumber } from "./custom-theme/input-number";
 import { inputText } from "./custom-theme/input-text";
 import { label, labelTokens } from "./custom-theme/label";
 import { link, linkTokens } from "./custom-theme/link";
+import { list, listTokens } from "./custom-theme/list";
 import { loader } from "./custom-theme/loader";
 import { notices } from "./custom-theme/notice";
 import { pagination } from "./custom-theme/pagination";
@@ -122,7 +123,7 @@ const kitchenSink = (args: Record<string, string>, useTestValues = false) =>
         ${chips} ${pagination} ${slider}
       </div>
       <div class="demo-column">
-        ${datePicker} ${tabs} ${label} ${link} ${loader} ${calciteSwitch} ${avatarIcon} ${avatarInitials}
+        ${datePicker} ${tabs} ${label} ${link} ${list} ${loader} ${calciteSwitch} ${avatarIcon} ${avatarInitials}
         ${avatarThumbnail} ${progress} ${handle} ${textArea} ${popover} ${tile} ${tooltip}
       </div>
       ${alert} ${navigation}
@@ -146,6 +147,7 @@ const componentTokens = {
   ...handleTokens,
   ...labelTokens,
   ...linkTokens,
+  ...listTokens,
   ...popoverTokens,
   ...progressTokens,
   ...inputTokens,
