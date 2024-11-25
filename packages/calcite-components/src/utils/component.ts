ae11edb95e7424f43e21ea23bda39cfafc380dd3
JC Franco <jfranco@esri.com>
2024-11-25 11:39:00 -0800

    fix(combobox-item): replace type-enforced deprecated + required `textLabel` prop with runtime warning (#10855)
    **Related Issue:** #10321
    
    ## Summary
    
    Addressed usage issues caused by TypeScript-enforced required property
    that had been marked as deprecated.
    
    ### Noteworthy changes:
    
    * adds `warnIfMissingRequiredProp` util for cases where TS-enforced
    required props are not suitable
    * enable
    [`jsdoc/no-restricted-syntax`](https://github.com/gajus/eslint-plugin-jsdoc/blob/main/docs/rules/no-restricted-syntax.md)
    rule to catch props tagged as both required and deprecated

--- packages/calcite-components/src/utils/component.ts
+++ packages/calcite-components/src/utils/component.ts
@@ -1,5 +1,6 @@
-import { PublicLitElement } from "@arcgis/lumina";
+import type { LitElement, PublicLitElement } from "@arcgis/lumina";
 import { Scale } from "../components/interfaces";
+import { logger } from "./logger";
 
 export function getIconScale(componentScale: Scale): Extract<Scale, "s" | "m"> {
   return componentScale === "l" ? "m" : "s";
@@ -20,4 +21,25 @@ export async function componentOnReady(el: HTMLElement): Promise<void> {
 
 function isStencilEl(el: HTMLElement): el is PublicLitElement {
   return typeof (el as PublicLitElement).componentOnReady === "function";
+}
+
+/**
+ * This utility logs a warning message when a required property is missing.
+ *
+ * Note: this should only be used for cases where using the @required JSDoc tag is not possible, such as:
+ * - when a required property is being deprecated
+ * - when a required property can be computed from another property
+ *
+ * @param component the component that requires the property
+ * @param newProp the new property that is required
+ * @param deprecatedProp the deprecated property that is required
+ */
+export function warnIfMissingRequiredProp<C extends LitElement>(
+  component: C,
+  newProp: keyof C,
+  deprecatedProp: keyof C,
+): void {
+  if (!component[newProp] && !component[deprecatedProp]) {
+    logger.warn(`[${component.el.localName}] "${newProp.toString()}" or "${deprecatedProp.toString()}" is required.`);
+  }
 }
