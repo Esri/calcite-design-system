13c2df8a9d392aed03475f1dc315d509c9e1957a
Ali Stump <alisonailea@users.noreply.github.com>
2024-11-27 15:26:15 -0800

    fix(tokens)!: correct space and size tokens (#10727)
    **Related Issue:** #10012 #10014
    
    ## Summary
    
    Corrects space and size tokens to align with the Calcite UI Kit and uses
    REM based tokens for the web platform token output. Updates Calcite
    Components to ensure there are no visual breaking changes to components
    using the updates size and space tokens.
    
    BREAKING CHANGE: Size and Space token values have changed. Line Height
    tokens exported for JS and ES6 are now unitless.
    
    ### Size
    
    calcite-size-px: 1px;
    calcite-size-xxxs: 0.75rem; // 12px was 2px
    calcite-size-xxs: 0.875rem; // 14px was 4px
    calcite-size-xs: 1rem; // 16px was 6px
    calcite-size-sm: 1.5rem; // 24px was 8px
    calcite-size-sm-plus; // removed
    calcite-size-md: 2rem; // 32px was 12px
    calcite-size-md-plus; // removed
    calcite-size-lg: 2.75rem; // 44px was 16px
    calcite-size-xl: 3rem; // 48px was 20px
    calcite-size-xxl: 4rem; //  64px was 24px
    calcite-size-xxxl: 6rem;  // 96px was 32px
    
    ### Space
    
    calcite-spacing-none: 0; // new
    calcite-spacing-px: 1px;
    calcite-spacing-base: 2px;
    calcite-spacing-xxs: 0.25rem; // 4px
    calcite-spacing-xs: 0.375rem; // 6px
    calcite-spacing-sm: 0.5rem; // 8px
    calcite-spacing-sm-plus: 0.625rem; // new
    calcite-spacing-md: 0.75rem; // 12px
    calcite-spacing-md-plus: 0.875rem; // new
    calcite-spacing-lg: 1rem; // 16px was 14px
    calcite-spacing-xl: 1.25rem; // 20px was 16px
    calcite-spacing-xxl: 1.5rem; // 24px was 20px
    calcite-spacing-xxxl: 2rem; // 32px
    
    ### Pixel based tokens are now categorized under "fixed"
    
    calcite-size-fixed-xxxl: 32px; // deprecated
    calcite-size-fixed-xxl: 24px; // deprecated
    calcite-size-fixed-xl: 20px; // deprecated
    calcite-size-fixed-lg: 16px; // deprecated
    calcite-size-fixed-md: 12px; // deprecated
    calcite-size-fixed-sm: 8px; // deprecated
    calcite-size-fixed-xs: 6px; // deprecated
    calcite-size-fixed-xxs: 4px; // deprecated
    calcite-size-fixed-xxxs: 2px; // deprecated
    
    calcite-spacing-fixed-lg: 14px; // deprecated
    calcite-spacing-fixed-md: 12px; // deprecated
    calcite-spacing-fixed-sm: 8px; // deprecated
    calcite-spacing-fixed-xl: 16px; // deprecated
    calcite-spacing-fixed-xs: 6px; // deprecated
    calcite-spacing-fixed-xxl: 20px; // deprecated
    calcite-spacing-fixed-xxs: 4px; // deprecated
    calcite-spacing-fixed-xxxl: 32px; // deprecated

--- packages/calcite-design-tokens/support/token-transformer/styleDictionary/transformer/value/valueToUnitless.ts
+++ packages/calcite-design-tokens/support/token-transformer/styleDictionary/transformer/value/valueToUnitless.ts
@@ -3,13 +3,12 @@ import { CalledTransformerFunction, TransformerConfig } from "../utils.js";
 import { Matcher } from "style-dictionary/types/Matcher.js";
 
 export const matcher: Matcher = (token) => {
-  const matchingRegex = /dimension/g;
+  const matchingRegex = /lineHeight/g;
 
-  return matchingRegex.test(token.type);
+  return matchingRegex.test(token.type) && !token.path.includes("fixed");
 };
 
-export const transformToREM: CalledTransformerFunction<string> = (token) => {
-  const basePxFontSize = 16;
+export const transformToUnitless: CalledTransformerFunction<string> = (token) => {
   const regex = /[px%]/g;
 
   if (typeof token.value === "string") {
@@ -20,17 +19,17 @@ export const transformToREM: CalledTransformerFunction<string> = (token) => {
         returnValue = returnValue * 0.01;
       }
 
-      return `${returnValue / basePxFontSize}rem`;
+      return `${returnValue}`;
     }
   }
 
   return token.value;
 };
 
-export const registerValueToREM = (sd: StyleDictionary): void => {
+export const registerValueToUnitless = (sd: StyleDictionary): void => {
   const transformerConfig: TransformerConfig = {
-    name: CalciteValueToREM,
-    transformer: transformToREM,
+    name: CalciteValueToUnitless,
+    transformer: transformToUnitless,
     type: "value",
     transitive: true,
     matcher,
@@ -39,4 +38,4 @@ export const registerValueToREM = (sd: StyleDictionary): void => {
   sd.registerTransform(transformerConfig);
 };
 
-export const CalciteValueToREM = "value/calcite/rem";
+export const CalciteValueToUnitless = "value/calcite/unitless";
