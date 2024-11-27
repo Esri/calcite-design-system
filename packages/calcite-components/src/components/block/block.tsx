fc0ca13c5d173294167ce2c90927c64667e232a4
Anveshreddy mekala <amekala@esri.com>
2024-11-27 17:05:02 -0600

    add util

--- packages/calcite-components/src/components/block/block.tsx
+++ packages/calcite-components/src/components/block/block.tsx
@@ -23,7 +23,7 @@ import {
 } from "../../utils/floating-ui";
 import { IconNameOrString } from "../icon/interfaces";
 import { useT9n } from "../../controllers/useT9n";
-import { logger } from "../../utils/logger";
+import { warnIfMissingProps } from "../../utils/component";
 import { CSS, ICONS, IDS, SLOTS } from "./resources";
 import T9nStrings from "./assets/t9n/block.t9n.en.json";
 import { styles } from "./block.scss";
@@ -198,11 +198,12 @@ export class Block
       onToggleOpenCloseComponent(this);
     }
 
-    if (!this.heading && !this.label) {
-      logger.warn(
-        `${this.el.tagName} is missing both heading & label. Please provide a heading or label for the component to be accessible.`,
-      );
-    }
+    warnIfMissingProps(
+      this,
+      "heading",
+      "Please provide a heading or label for the component to be accessible.",
+      "label",
+    );
   }
 
   override willUpdate(changes: PropertyValues<this>): void {
