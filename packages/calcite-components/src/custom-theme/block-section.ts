691cbd7d8695fd6c9946f72fe466f7538dd69792
eliza <eli97736@esri.com>
2024-12-12 12:59:13 -0800

    add e2e tests

--- packages/calcite-components/src/custom-theme/block-section.ts
+++ packages/calcite-components/src/custom-theme/block-section.ts
@@ -1,19 +1,14 @@
 import { html } from "../../support/formatting";
 
-export const blockTokens = {
+export const blockSectionTokens = {
   calciteBlockSectionBorderColor: "",
   calciteBlockSectionHeaderTextColor: "",
   calciteBlockSectionHeaderTextColorHover: "",
   calciteBlockSectionTextColor: "",
 };
 
-export const block = html`<calcite-block-section
-  text="Planes, trains, and automobiles"
-  open
-  icon-end="pen"
-  icon-start="pen"
-  toggle-display="switch"
-  text="a block-section"
-  status="valid"
->
-</calcite-block-section>`;
+export const blockSection = html`
+  <calcite-block-section text="Planes" open icon-end="pen" icon-start="pen" text="a block-section">
+    <p>Block section content</p>
+  </calcite-block-section>
+`;
