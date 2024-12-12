927eef53d4d3288606a9ae2bd89c11b878b6a7a0
Matt Driscoll <mdriscoll@esri.com>
2024-12-12 14:33:02 -0800

    feat(sheet): add resizable property (#10521)
    **Related Issue:** #7591
    
    ## Summary
    
    - adds resize functionality
    - adds assistive text live region for keyboard instructions
    - adds tests
    - updates story
    - adds dom utility
    - adds t9n files
    - use new resize icon
    - uses separator role, related to #3535

--- packages/calcite-components/src/components/sheet/resources.ts
+++ packages/calcite-components/src/components/sheet/resources.ts
@@ -1,8 +1,14 @@
 export const CSS = {
+  assistiveText: "assistive-text",
   scrim: "scrim",
   container: "container",
   containerOpen: "container--open",
   content: "content",
   containerEmbedded: "container--embedded",
+  resizeHandle: "resize-handle",
+  resizeHandleBar: "resize-handle-bar",
   height: "height",
 };
+
+export const sheetResizeStep = 10;
+export const sheetResizeShiftStep = 25;
