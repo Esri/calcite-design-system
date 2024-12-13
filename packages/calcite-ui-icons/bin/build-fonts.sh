34b911399a7ca0074d857543b80c1a6f99e6feb5
Ben Elan <no-reply@benelan.dev>
2024-12-12 17:04:41 -0800

    chore: move intermediary font icons directory

--- packages/calcite-ui-icons/bin/build-fonts.sh
+++ packages/calcite-ui-icons/bin/build-fonts.sh
@@ -4,26 +4,26 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$(dirname "$SCRIPT_DIR")" || exit
 
-# remove and remake the intermediary font-icons directories
-rm -rf font-icons/
-mkdir -p font-icons/16/ font-icons/24/ font-icons/32/
+# remove and remake the intermediary font icons directories
+rm -rf fonts/icons/
+mkdir -p fonts/icons/{16,24,32}/
 
 # filter out icons with opacity and create hardlinks in the intermediary directory
-grep -LF opacity icons/*.svg | xargs -I{} ln {} font-icons/
+grep -LF opacity icons/*.svg | xargs -I{} ln {} fonts/icons/
 
-cd font-icons || exit
+cd fonts/icons || exit
 
 # separate the icons into subdirectories by size and remove the size from the filenames
 for file in ./*-16{-f,}.svg; do mv "$file" "16/${file/-16/}"; done
 for file in ./*-24{-f,}.svg; do mv "$file" "24/${file/-24/}"; done
 for file in ./*-32{-f,}.svg; do mv "$file" "32/${file/-32/}"; done
 
-cd .. || exit
+cd ../.. || exit
 
 # create a separate font file for each icon size
-fantasticon font-icons/16/ -n calcite-ui-icons-16 --normalize true -t ttf -g json -o fonts/
-fantasticon font-icons/24/ -n calcite-ui-icons-24 --normalize true -t ttf -g json -o fonts/
-fantasticon font-icons/32/ -n calcite-ui-icons-32 --normalize true -t ttf -g json -o fonts/
+fantasticon fonts/icons/16/ -n calcite-ui-icons-16 --normalize true -t ttf -g json -o fonts/
+fantasticon fonts/icons/24/ -n calcite-ui-icons-24 --normalize true -t ttf -g json -o fonts/
+fantasticon fonts/icons/32/ -n calcite-ui-icons-32 --normalize true -t ttf -g json -o fonts/
 
 # update codepoints in config file
 echo "{\"codepoints\": $(cat fonts/calcite-ui-icons-16.json)}" >./fantasticonrc.json
