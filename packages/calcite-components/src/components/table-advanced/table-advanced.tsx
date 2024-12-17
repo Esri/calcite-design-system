98df26413312b8d46b5e033810ec2cc286677b1f
aPreciado88 <apreciado@esri.com>
2024-12-17 15:52:05 -0800

    feat(table-advanced): add demos page

--- packages/calcite-components/src/components/table-advanced/table-advanced.tsx
+++ packages/calcite-components/src/components/table-advanced/table-advanced.tsx
@@ -1,3 +1,4 @@
+import { PropertyValues } from "lit";
 import { LitElement, property, h, state, JsxNode } from "@arcgis/lumina";
 import { TabulatorFull as Tabulator } from "tabulator-tables";
 import type { ColumnDefinition, OptionsData } from "tabulator-tables";
@@ -62,15 +63,27 @@ export class TableAdvanced extends LitElement implements LoadableComponent {
     setUpLoadableComponent(this);
   }
 
-  override connectedCallback(): void {
-    this.tabulator = new Tabulator(this.tableEl, {
-      data: this.data,
-      columns: this.columns,
-    });
+  override willUpdate(changes: PropertyValues<this>): void {
+    /* TODO: [MIGRATION] First time Lit calls willUpdate(), changes will include not just properties provided by the user, but also any default values your component set.
+    To account for this semantics change, the checks for (this.hasUpdated || value != defaultValue) was added in this method
+    Please refactor your code to reduce the need for this check.
+    Docs: https://qawebgis.esri.com/arcgis-components/?path=/docs/lumina-transition-from-stencil--docs#watching-for-property-changes */
+    if (changes.has("data") && this.hasUpdated) {
+      this.dataWatcher(this.data);
+    }
+
+    if (changes.has("columns") && this.hasUpdated) {
+      this.columnsWatcher(this.columns);
+    }
   }
 
   loaded(): void {
     setComponentLoaded(this);
+
+    this.tabulator = new Tabulator(this.tableEl, {
+      data: this.data,
+      columns: this.columns,
+    });
   }
 
   // #endregion
@@ -79,6 +92,18 @@ export class TableAdvanced extends LitElement implements LoadableComponent {
 
   private setTableElRef(el: HTMLElement) {
     this.tableEl = el;
+  }
+
+  private dataWatcher(data: OptionsData[]): void {
+    if (this.tabulator) {
+      this.tabulator.setData(data);
+    }
+  }
+
+  private columnsWatcher(columns: ColumnDefinition[]): void {
+    if (this.tabulator) {
+      this.tabulator.setColumns(columns);
+    }
   }
   // #endregion
 
