f78d989faac452ad5b4d04dd56a5b7714de96dcd
Ali Stump <astump@esri.com>
2024-12-17 14:48:46 -0800

    chore(scss): update platform config

--- packages/calcite-design-tokens/src/config/scss.ts
+++ packages/calcite-design-tokens/src/config/scss.ts
@@ -1,27 +1,19 @@
-import { TransformedToken } from "style-dictionary";
-import { formats, transformGroups, transforms } from "style-dictionary/enums";
+import { formats, transforms } from "style-dictionary/enums";
 import { nameRemoveTier } from "../../support/transforms/name/removeTier.js";
 import { nameRemoveColorMode } from "../../support/transforms/name/removeColorMode.js";
 import { nameIncludePlus } from "../../support/transforms/name/includePlus.js";
 import { nameIncludeMinus } from "../../support/transforms/name/includeMinus.js";
 import { HeaderCalciteDefault } from "../../support/header/calcite-default.js";
-import { ValueMathSum } from "../../support/transforms/value/mathSum.js";
-import { ActionGenerateSCSSBreakpoints } from "../../support/actions/scss/generateBreakpoints.js";
+import { ActionGenerateBreakpoints } from "../../support/actions/generateBreakpoints.js";
 import { FilterLightColorTokens } from "../../support/filter/light.js";
 import { FilterDarkColorTokens } from "../../support/filter/dark.js";
+import { FilterGlobalTokens } from "../../support/filter/global.js";
 
 export default {
-  actions: [ActionGenerateSCSSBreakpoints],
-  transformGroup: transformGroups.scss,
-  transforms: [
-    ValueMathSum,
-    transforms.nameKebab,
-    nameRemoveTier,
-    nameRemoveColorMode,
-    nameIncludePlus,
-    nameIncludeMinus,
-  ],
-  buildPath: "dist/",
+  actions: [ActionGenerateBreakpoints],
+  transformGroup: "tokens-studio",
+  transforms: [transforms.nameKebab, nameRemoveTier, nameRemoveColorMode, nameIncludePlus, nameIncludeMinus],
+  buildPath: "dist/scss/",
   prefix: "calcite",
   files: [
     {
@@ -37,17 +29,7 @@ export default {
     {
       destination: "global.scss",
       format: formats.scssVariables,
-      filter: (token: TransformedToken): boolean => {
-        return (
-          token.isSource &&
-          token.type !== "dark" &&
-          token.type !== "light" &&
-          token.type !== "breakpoint" &&
-          token.type !== "min" &&
-          token.type !== "max" &&
-          token.type !== "typography"
-        );
-      },
+      filter: FilterGlobalTokens,
     },
   ],
   options: {
