caed514e13b8375973a6489fb905725b9f170bb8
Ali Stump <astump@esri.com>
2024-12-17 14:48:26 -0800

    fix: remove unused StyleDictionary value transformers

--- packages/calcite-design-tokens/support/transforms/registerTransforms.ts
+++ packages/calcite-design-tokens/support/transforms/registerTransforms.ts
@@ -3,14 +3,10 @@ import { registerNameRemoveTier } from "./name/removeTier.js";
 import { registerNameRemoveColorMode } from "./name/removeColorMode.js";
 import { registerNameIncludePlus } from "./name/includePlus.js";
 import { registerNameIncludeMinus } from "./name/includeMinus.js";
-import { registerValueMathSum } from "./value/mathSum.js";
-import { registerValueShadowShorthand } from "./value/shadow.js";
 
 export async function registerCalciteTransformers(sd: typeof StyleDictionary): Promise<void> {
-  registerValueShadowShorthand(sd);
   registerNameRemoveTier(sd);
   registerNameRemoveColorMode(sd);
   registerNameIncludePlus(sd);
   registerNameIncludeMinus(sd);
-  registerValueMathSum(sd);
 }
