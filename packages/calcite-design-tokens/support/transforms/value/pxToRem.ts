a1ee0a0329388f94af322b48ba9ec18029e51d0a
Ali Stump <astump@esri.com>
2024-12-20 18:34:05 -0800

    feat(pxToRem): transform token values from px to rems

---
+++ packages/calcite-design-tokens/support/transforms/value/pxToRem.ts
@@ -0,0 +1,75 @@
+import StyleDictionary, { Config, PlatformConfig } from "style-dictionary";
+import { TransformedToken } from "style-dictionary/types";
+
+function getBasePxFontSize(config) {
+  return (config && config.basePxFontSize) || 16;
+}
+
+function isDimension(token: TransformedToken, options: Config) {
+  return (options.usesDtcg ? token.$type : token.type) === "dimension";
+}
+
+function isFontSize(token: TransformedToken, options: Config) {
+  return (options.usesDtcg ? token.$type : token.type) === "fontSize";
+}
+
+function isSource(token: TransformedToken) {
+  return token.isSource;
+}
+
+function isPxUnit(token: TransformedToken) {
+  const matcher = /(?<number>[\d\.]+)(?<unit>[a-z%]*)/g.exec(
+    token.value || token.value.value || token.original.value || token.original.value.value,
+  );
+
+  if (!matcher || !matcher.groups.unit || ["", "rem", "%"].includes(matcher.groups?.unit)) {
+    return false;
+  }
+
+  return true;
+}
+
+function throwSizeError(name, value, unitType) {
+  throw `Invalid Number: '${name}: ${value}' is not a valid number, cannot transform to '${unitType}' \n`;
+}
+
+function isNotBorderOrFixed(token: TransformedToken) {
+  return !token.path.some((path) => ["border", "fixed"].includes(path));
+}
+
+function filter(token: TransformedToken, options: Config) {
+  return (
+    isSource(token) &&
+    isNotBorderOrFixed(token) &&
+    (isDimension(token, options) || isFontSize(token, options)) &&
+    isPxUnit(token)
+  );
+}
+
+function transformValueSizePxToRem(token: TransformedToken, config: PlatformConfig, options: Config) {
+  const value = options.usesDtcg ? token.$value : token.value;
+  const parsedVal = parseFloat(value);
+  const baseFont = getBasePxFontSize(config);
+
+  if (isNaN(parsedVal)) {
+    if (value === "auto") {
+      return "auto";
+    }
+    debugger;
+    throwSizeError(token.name, value, "rem");
+  }
+
+  return `${parsedVal / baseFont}rem`;
+}
+
+export function registerValueSizePxToRem(sd: typeof StyleDictionary): void {
+  sd.registerTransform({
+    name: ValueSizePxToRem,
+    type: "value",
+    filter,
+    transitive: true,
+    transform: transformValueSizePxToRem,
+  });
+}
+
+export const ValueSizePxToRem = "calcite/value/pxToRem";
