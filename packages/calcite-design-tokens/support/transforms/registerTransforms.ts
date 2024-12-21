a3ae510a5a955f52cbb40c954b8563ca35ac17ba
Ali Stump <astump@esri.com>
2024-12-20 18:34:34 -0800

    chore(registerTransforms): add transform registerations

--- packages/calcite-design-tokens/support/transforms/registerTransforms.ts
+++ packages/calcite-design-tokens/support/transforms/registerTransforms.ts
@@ -4,13 +4,19 @@ import { registerNameRemoveColorMode } from "./name/removeColorMode.js";
 import { registerNameIncludePlus } from "./name/includePlus.js";
 import { registerNameIncludeMinus } from "./name/includeMinus.js";
 import { registerValueMathSum } from "./value/mathSum.js";
-import { registerValueShadowShorthand } from "./value/shadow.js";
+import { registerValueCSSShadow } from "./value/cssShadow.js";
+import { registerValueSizePxToRem } from "./value/pxToRem.js";
+import { registerNameRemoveDefault } from "./name/removeDefault.js";
+import { registerCalciteTransformGroup } from "./group/calcite.js";
 
 export async function registerCalciteTransformers(sd: typeof StyleDictionary): Promise<void> {
-  registerValueShadowShorthand(sd);
+  registerValueCSSShadow(sd);
   registerNameRemoveTier(sd);
+  registerNameRemoveDefault(sd);
   registerNameRemoveColorMode(sd);
   registerNameIncludePlus(sd);
   registerNameIncludeMinus(sd);
   registerValueMathSum(sd);
+  registerValueSizePxToRem(sd);
+  registerCalciteTransformGroup(sd);
 }
