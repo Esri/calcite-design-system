09294cada06d614b4545ba0fe10ca778be49f588
Ali Stump <astump@esri.com>
2024-12-20 18:35:17 -0800

    chore(scss): clean up scss config

--- packages/calcite-design-tokens/src/config/scss.ts
+++ packages/calcite-design-tokens/src/config/scss.ts
@@ -1,18 +1,14 @@
-import { formats, transforms } from "style-dictionary/enums";
-import { nameRemoveTier } from "../../support/transforms/name/removeTier.js";
-import { nameRemoveColorMode } from "../../support/transforms/name/removeColorMode.js";
-import { nameIncludePlus } from "../../support/transforms/name/includePlus.js";
-import { nameIncludeMinus } from "../../support/transforms/name/includeMinus.js";
+import { formats } from "style-dictionary/enums";
 import { HeaderCalciteDefault } from "../../support/header/calcite-default.js";
 import { ActionGenerateBreakpoints } from "../../support/actions/generateBreakpoints.js";
 import { FilterLightColorTokens } from "../../support/filter/light.js";
 import { FilterDarkColorTokens } from "../../support/filter/dark.js";
 import { FilterGlobalTokens } from "../../support/filter/global.js";
+import { CalciteTransformGroup } from "../../support/transforms/group/calcite.js";
 
 export default {
   actions: [ActionGenerateBreakpoints],
-  transformGroup: "tokens-studio",
-  transforms: [transforms.nameKebab, nameRemoveTier, nameRemoveColorMode, nameIncludePlus, nameIncludeMinus],
+  transformGroup: CalciteTransformGroup,
   buildPath: "dist/scss/",
   prefix: "calcite",
   files: [
