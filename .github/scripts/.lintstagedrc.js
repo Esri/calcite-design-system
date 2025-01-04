ad1f1c35b4c430a7c2797f10850033834014d636
JC Franco <jfranco@esri.com>
2025-01-04 01:40:23 -0800

    chore(linting): restore consistent linting on JS/TS files

--- .github/scripts/.lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-const baseConfig = require("../../.lintstagedrc.js");
-
-module.exports = {
-  ...baseConfig,
-  "*.{m,c,}js": ["eslint --fix", "prettier --write"],
-};
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "*.{json,html,yml}": ["prettier --write"],
-  "*.scss": ["stylelint --fix", "prettier --write"],
+  "*.{cjs,mjs,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "*.{html,json,yml}": ["prettier --write"],
   "*.md": ["prettier --write", "markdownlint --fix"],
+  "*.scss": ["stylelint --fix", "prettier --write"],
 };
