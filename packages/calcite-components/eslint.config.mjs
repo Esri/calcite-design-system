c2c1af59c78eaecff7b6d0e41a08d35b1d071786
JC Franco <jfranco@esri.com>
2025-01-10 00:50:48 -0800

    chore(linting): clean up warnings (#11165)
    **Related Issue:** N/A
    
    ## Summary
    
    This PR disables:
    
    * false-positives from
    [`react/forbid-dom-props`](https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/forbid-dom-props.md)
    and
    [`react/forbid-component-props`](https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/forbid-component-props.md)
    * `eslint-plugin-jsdoc` rules that create churn without added value
    (most already covered by TypeScript):
    *
    [`jsdoc/check-param-names`](https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/check-param-names.md)
    *
    [`jsdoc/require-param-description`](https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/require-param-description.md)
    *
    [`jsdoc/require-returns`](https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/require-returns.md)
    *
    [`jsdoc/require-returns-description`](https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/require-returns-description.md)

--- packages/calcite-components/eslint.config.mjs
+++ packages/calcite-components/eslint.config.mjs
@@ -86,9 +86,13 @@ export default tseslint.config(
         },
       ],
 
+      "jsdoc/check-param-names": "off",
       "jsdoc/require-jsdoc": "off",
+      "jsdoc/require-param-description": "off",
       "jsdoc/require-param-type": "off",
       "jsdoc/require-property-type": "off",
+      "jsdoc/require-returns": "off",
+      "jsdoc/require-returns-description": "off",
       "jsdoc/require-returns-type": "off",
       "jsdoc/check-tag-names": "off",
       "jsdoc/tag-lines": [
