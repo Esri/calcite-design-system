31aa3dea8245a196389463160e7d2aabf32db9b4
JC Franco <jfranco@esri.com>
2025-01-09 20:52:12 -0800

    tidy up

--- packages/calcite-components/src/components/action/action.e2e.ts
+++ packages/calcite-components/src/components/action/action.e2e.ts
@@ -472,7 +472,7 @@ describe("calcite-action", () => {
           shadowSelector: `.${CSS.button}`,
           targetProp: "backgroundColor",
           expectedValue: "rgba(0, 0, 0, 0.08)",
-          state: { press: { attribute: "class", value: `${CSS.button}` } },
+          state: { press: { attribute: "class", value: CSS.button } },
         },
       });
     });
