4687d2c02146399f7c1deff9a00428807a34eae4
JC Franco <jfranco@esri.com>
2025-01-10 00:22:00 -0800

    chore(linting): clean up cspell warnings (#11164)
    **Related Issue:** N/A
    
    ## Summary
    
    * adds custom dictionaries for 
      * examples (`dict-calcite-design-system-examples`) and 
    * [`cspell-dicts`](https://github.com/streetsidesoftware/cspell-dicts/)
    contribution candidates (`dict-cspell-pending-upstream`)
    * Spanish and Arabic words were explicitly added instead of using their
    respective dictionaries to keep linting speedy
    * replaces alt lorem-ipsum text with original version
    * adds patterns for common words
    * explicitly ignores rule for test hex codes used in
    color-picker-related tests
    * fixes cspell errors
    * fixes duplicate test name errors
    
    ### Next steps
    
    Create follow-up issues to:
      * contribute candidates
    * update doc with guidance (e.g., use hyphens instead of creating
    compound words, use sequential letters/digits when adding random or long
    text, etc...)
      * discuss and define process for keeping track of words
      * improve config (e.g., scope rules/dictionaries to relevant paths)
      * clean up custom dictionaries

--- packages/calcite-components/src/components/inline-editable/inline-editable.e2e.ts
+++ packages/calcite-components/src/components/inline-editable/inline-editable.e2e.ts
@@ -219,8 +219,8 @@ describe("calcite-inline-editable", () => {
         const input = element.shadowRoot.querySelector("input");
         input.setSelectionRange(input.value.length, input.value.length);
       });
-      await input.type("typo");
-      expect(await input.getProperty("value")).toBe("John Doetypo");
+      await input.type("-typo");
+      expect(await input.getProperty("value")).toBe("John Doe-typo");
       const cancelEvent = page.waitForEvent("calciteInlineEditableEditCancel");
       await cancelEditingButton.click();
       await cancelEvent;
@@ -239,8 +239,8 @@ describe("calcite-inline-editable", () => {
         const input = element.shadowRoot.querySelector("input");
         input.setSelectionRange(input.value.length, input.value.length);
       });
-      await input.type("typo");
-      expect(await input.getProperty("value")).toBe("John Doetypo");
+      await input.type("-typo");
+      expect(await input.getProperty("value")).toBe("John Doe-typo");
       await page.keyboard.press("Escape");
       await calciteInlineEditableEditCancel;
       expect(await input.getProperty("value")).toBe("John Doe");
