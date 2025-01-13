4947368b278b61315637e2c015f9ab5be985c605
JC Franco <jfranco@esri.com>
2025-01-13 10:15:48 -0800

    chore: replace jest with vitest (#11270)
    **Related Issue:** N/A
    
    ## Summary
    
    Unifies testing by leveraging existing `vitest` setup.
    
    ### Notes
    
    - test workflows are mostly unchanged (key change involves imports – see
    https://vitest.dev/api/)
    - `jest-axe` is still used as it works with our current Puppeteer setup
    - [`vitest-axe`](https://github.com/chaance/vitest-axe#readme) is still
    in prerelease and has a known issue when integrated with `happy-dom`
    - updates ESLint, Renovate configs, VS Code launch action and _some_ doc
    - Preact example [still uses
    jest](https://github.com/Esri/calcite-design-system/blob/dev/examples/components/preact/package.json#L16)
    – we can tackle this in a follow-up
    - Moves `vitest` dep to monorepo root (might need to keep under
    `calcite-components` to ensure Lumina doesn't remove matching
    triple-slash directives
    - Removes obsolete `commonTests`-scoped ESLint config
    
    ---------
    
    Co-authored-by: Ben Elan <no-reply@benelan.dev>

--- packages/calcite-design-tokens/support/token-transformer/helpers/createCalciteTokenFiles.spec.ts
+++ packages/calcite-design-tokens/support/token-transformer/helpers/createCalciteTokenFiles.spec.ts
@@ -1,5 +1,6 @@
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import { describe, it, expect } from "vitest";
 import { ConfigOptions } from "../../types/config.js";
 import { createCalciteTokenFiles } from "./createCalciteTokenFiles.js";
 
