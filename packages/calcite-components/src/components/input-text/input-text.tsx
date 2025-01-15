05d82a2575e8e3f7d3a7cf623984e9f72b5fea2d
Kitty Hurley <khurley@esri.com>
2025-01-15 11:58:52 -0600

    remove remaining spaces

--- packages/calcite-components/src/components/input-text/input-text.tsx
+++ packages/calcite-components/src/components/input-text/input-text.tsx
@@ -241,7 +241,7 @@ export class InputText
 
   /**
    * When `true` and the component resides in a form,
-   *  the component must have a value in order for the form to submit.
+   * the component must have a value in order for the form to submit.
    */
   @property({ reflect: true }) required = false;
 
