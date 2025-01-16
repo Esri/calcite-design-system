7aecd89041b65b9ba5bb76541ec12814154a9b96
Matt Driscoll <mdriscoll@esri.com>
2025-01-16 14:35:52 -0800

    cleanup

--- packages/calcite-components/src/components/block-group/block-group.tsx
+++ packages/calcite-components/src/components/block-group/block-group.tsx
@@ -185,17 +185,7 @@ export class BlockGroup
     setUpLoadableComponent(this);
   }
 
-  /**
-   * TODO: [MIGRATION] Consider inlining some of the watch functions called inside of this method to reduce boilerplate code
-   *
-   * @param changes
-   */
   override willUpdate(changes: PropertyValues<this>): void {
-    /* TODO: [MIGRATION] First time Lit calls willUpdate(), changes will include not just properties provided by the user, but also any default values your component set.
-    To account for this semantics change, the checks for (this.hasUpdated || value != defaultValue) was added in this method
-    Please refactor your code to reduce the need for this check.
-    Docs: https://qawebgis.esri.com/arcgis-components/?path=/docs/lumina-transition-from-stencil--docs#watching-for-property-changes */
-
     if (
       changes.has("group") ||
       (changes.has("dragEnabled") && (this.hasUpdated || this.dragEnabled !== false))
