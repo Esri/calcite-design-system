2d2d4e010d67a93fd16f2e4cf381787e066971c6
Matt Driscoll <mdriscoll@esri.com>
2025-01-16 16:30:06 -0800

    additional stories

--- packages/calcite-components/src/components/block-group/block-group.stories.ts
+++ packages/calcite-components/src/components/block-group/block-group.stories.ts
@@ -20,6 +20,32 @@ export default {
   },
 };
 
+const blockHTML = html`<calcite-block
+    collapsible
+    heading="A rubber chicken"
+    description="Why did the chicken cross the road? To avoid being squeezed!"
+    >My block content!</calcite-block
+  >
+  <calcite-block collapsible heading="Invisible ink" description="You can't see me!">My block content!</calcite-block>
+  <calcite-block collapsible heading="Whoopee cushion" description="The sound of laughter!"
+    >My block content!</calcite-block
+  >
+  <calcite-block collapsible heading="Fake mustache" description="Incognito mode activated!"
+    >My block content!</calcite-block
+  >
+  <calcite-block sort-handle-open collapsible heading="Giant foam finger" description="We're number one!"
+    >My block content!</calcite-block
+  >
+  <calcite-block drag-disabled collapsible heading="Clown nose" description="Honk if you love clowns!"
+    >My block content!</calcite-block
+  >
+  <calcite-block
+    collapsible
+    heading="Joke book"
+    description="Why don't scientists trust atoms? Because they make up everything!"
+    >My block content!</calcite-block
+  >`;
+
 export const simple = (args: BlockGroupStoryArgs): string => html`
   <calcite-block-group
     ${boolean("disabled", args.disabled)}
@@ -28,30 +54,18 @@ export const simple = (args: BlockGroupStoryArgs): string => html`
     label="${args.label}"
     group="${args.group}"
   >
-    <calcite-block
-      collapsible
-      heading="A rubber chicken"
-      description="Why did the chicken cross the road? To avoid being squeezed!"
-      >My block content!</calcite-block
-    >
-    <calcite-block collapsible heading="Invisible ink" description="You can't see me!">My block content!</calcite-block>
-    <calcite-block collapsible heading="Whoopee cushion" description="The sound of laughter!"
-      >My block content!</calcite-block
-    >
-    <calcite-block collapsible heading="Fake mustache" description="Incognito mode activated!"
-      >My block content!</calcite-block
-    >
-    <calcite-block collapsible heading="Giant foam finger" description="We're number one!"
-      >My block content!</calcite-block
-    >
-    <calcite-block collapsible heading="Clown nose" description="Honk if you love clowns!"
-      >My block content!</calcite-block
-    >
-    <calcite-block
-      collapsible
-      heading="Joke book"
-      description="Why don't scientists trust atoms? Because they make up everything!"
-      >My block content!</calcite-block
-    >
+    ${blockHTML}
   </calcite-block-group>
+`;
+
+export const dragEnabled = (): string => html`
+  <calcite-block-group dragEnabled label="My Group"> ${blockHTML} </calcite-block-group>
+`;
+
+export const loading = (): string => html`
+  <calcite-block-group loading label="My Group"> ${blockHTML} </calcite-block-group>
+`;
+
+export const disabled = (): string => html`
+  <calcite-block-group disabled label="My Group"> ${blockHTML} </calcite-block-group>
 `;
