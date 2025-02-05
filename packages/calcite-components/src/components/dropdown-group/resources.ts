ba0cdf8f56db23c62f1932c3ff7679aa97aa7a39
Ali Stump <astump@esri.com>
2025-02-05 13:57:51 -0800

    test(dropdown, dropdown-group, dropdown-item): add themed component tokens

---
+++ packages/calcite-components/src/components/dropdown-group/resources.ts
@@ -0,0 +1,4 @@
+export const CSS = {
+  title: "dropdown-title",
+  separator: "dropdown-separator",
+};
--- packages/calcite-components/src/components/dropdown/dropdown.e2e.ts
+++ packages/calcite-components/src/components/dropdown/dropdown.e2e.ts
@@ -23,6 +23,8 @@ import {
 } from "../../tests/utils";
 import type { DropdownItem } from "../dropdown-item/dropdown-item";
 import type { Button } from "../button/button";
+import { ComponentTestTokens, themed } from "../../tests/commonTests/themed";
+import { CSS } from "./resources";
 
 describe("calcite-dropdown", () => {
   const simpleDropdownHTML = html`
@@ -1462,5 +1464,19 @@ describe("calcite-dropdown", () => {
       await page.waitForChanges();
       expect(await isElementFocused(page, "#item-2")).toBe(true);
     });
+  });
+
+  describe("theme", () => {
+    const tokens: ComponentTestTokens = {
+      "--calcite-dropdown-width": {
+        targetProp: "inlineSize",
+        shadowSelector: `.${CSS.content}`,
+      },
+      "--calcite-dropdown-background-color": {
+        targetProp: "backgroundColor",
+        shadowSelector: `.${CSS.content}`,
+      },
+    };
+    themed(`<calcite-dropdown open></calcite-dropdown>`, tokens);
   });
 });
