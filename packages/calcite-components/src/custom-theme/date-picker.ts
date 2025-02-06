e63beb5273acc8dc62233fc0a4f5eebc77863edb
Anveshreddy mekala <anv11827@esri.com>
2025-02-05 18:38:38 -0600

    feat(date-picker): add component tokens (#11380)
    **Related Issue:** #7180 
    
    ## Summary
    
    Adds following tokens for `calcite-date-picker` component:
    
    `--calcite-date-picker-border-color`: Specifies the component's border
    color.
    `--calcite-date-picker-corner-radius`: Specifies the component's border
    radius.
    `--calcite-date-picker-range-calendar-divider-color`: Specifies the
    divider color between calendar's when `range="true"`.
    `--calcite-date-picker-week-header-text-color`: Specifies the week
    header text color.
    `--calcite-date-picker-header-action-background-color`: Specifies the
    background color of header action's of the component.
    `--calcite-date-picker-header-action-background-color-hover`: Specifies
    the background color of header action's of the component when hovered.
    `--calcite-date-picker-header-action-background-color-press`: Specifies
    the background color of header action's of the component when pressed.
    `--calcite-date-picker-header-action-text-color`: Specifies the text
    color of header action's of the component.
    `--calcite-date-picker-header-action-text-color-press`: Specifies the
    text color of header action's of the component when pressed.
    `--calcite-date-picker-year-text-color`: Specifies the text color of
    year & suffix of the component.
    `--calcite-date-picker-month-select-font-size`: Specifies the font size
    of month select of the component.
    `--calcite-date-picker-month-select-text-color`: Specifies the text
    color of month select of the component.
    `--calcite-date-picker-month-select-icon-color`: Specifies the icon
    color of month select of the component.
    `--calcite-date-picker-month-select-icon-color-hover`: Specifies the
    icon color of month select of the component when hovered.
    `--calcite-date-picker-day-background-color`: Specifies the background
    color of day of the component.
    `--calcite-date-picker-day-background-color-hover`: Specifies the
    background color of day of the component when hovered.
    `--calcite-date-picker-day-text-color`: Specifies the text color of day
    of the component.
    `--calcite-date-picker-day-text-color-hover`: Specifies the text color
    of day of the component when hovered.
    `--calcite-date-picker-current-day-text-color`: Specifies the text color
    of current day of the component.
    `--calcite-date-picker-day-background-color-selected`: Specifies the
    background color of selected day of the component.
    `--calcite-date-picker-day-text-color-selected`: Specifies the text
    color of selected day of the component.
    `--calcite-date-picker-day-range-text-color`: Specifies the text color
    of select day range of the component.
    `--calcite-date-picker-day-range-background-color`: Specifies the
    background color of select day range of the component.
    `--calcite-date-picker-day-outside-range-background-color-hover`:
    Specifies the background color of day's outside current range when
    hovered.
    `--calcite-date-picker-day-outside-range-text-color-hover`: Specifies
    the text color of day's outside current range when hovered.

--- packages/calcite-components/src/custom-theme/date-picker.ts
+++ packages/calcite-components/src/custom-theme/date-picker.ts
@@ -1,3 +1,39 @@
 import { html } from "../../support/formatting";
 
-export const datePicker = html`<calcite-date-picker scale="m" value="2020-11-27"></calcite-date-picker>`;
+export const datePickerTokens = {
+  calciteDatePickerBorderColor: "",
+  calciteDatePickerCornerRadius: "",
+  calciteDatePickerRangeCalendarDividerColor: "",
+  calciteDatePickerWeekHeaderTextColor: "",
+  calciteDatePickerHeaderActionBackgroundColor: "",
+  calciteDatePickerHeaderActionBackgroundColorHover: "",
+  calciteDatePickerHeaderActionBackgroundColorPress: "",
+  calciteDatePickerHeaderActionTextColor: "",
+  calciteDatePickerHeaderActionTextColorPress: "",
+  calciteDatePickerYearTextColor: "",
+  calciteDatePickerMonthSelectFontSize: "",
+  calciteDatePickerMonthSelectTextColor: "",
+  calciteDatePickerMonthSelectIconColor: "",
+  calciteDatePickerMonthSelectIconColorHover: "",
+  calciteDatePickerDayBackgroundColor: "",
+  calciteDatePickerDayBackgroundColorHover: "",
+  calciteDatePickerDayTextColor: "",
+  calciteDatePickerDayTextColorHover: "",
+  calciteDatePickerCurrentDayTextColor: "",
+  calciteDatePickerDayBackgroundColorSelected: "",
+  calciteDatePickerDayTextColorSelected: "",
+  calciteDatePickerDayRangeTextColor: "",
+  calciteDatePickerDayRangeBackgroundColor: "",
+  calciteDatePickerDayOutsideRangeBackgroundColorHover: "",
+  calciteDatePickerDayOutsideRangeTextColorHover: "",
+};
+
+export const datePicker = html` <calcite-date-picker value="2020-11-27"></calcite-date-picker> `;
+
+export const datePickerRange = html`
+  <calcite-date-picker range></calcite-date-picker>
+  <script>
+    const datePicker = document.querySelector("calcite-date-picker[range]");
+    datePicker.value = ["2025-01-01", "2025-02-20"];
+  </script>
+`;
