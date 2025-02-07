Esri/calcite-design-system

commit e87d5182486366f2aedb6b103e80de44a9b45809

fix(combobox, dropdown): honor `max-items` when it matches total items (#10971)
**Related Issue:** #6230 

## Summary

Fixes `max-items` logic (thanks @juliannemarik!) to show all items when
it matches the total items.

packages/calcite-components/src/components/dropdown/dropdown.e2e.ts
@@ -605,8 +605,9 @@ describe("calcite-dropdown", () => {
 
     it("control max items displayed", async () => {
       const maxItems = 7;
-      const page = await newE2EPage({
-        html: html`<calcite-dropdown max-items="${maxItems}">
+      const page = await newE2EPage();
+      await page.setContent(
+        html`<calcite-dropdown max-items="${maxItems}">
           <calcite-button slot="trigger">Open Dropdown</calcite-button>
           <calcite-dropdown-group group-title="First group">
             <calcite-dropdown-item id="item-1">1</calcite-dropdown-item>
@@ -623,7 +624,7 @@ describe("calcite-dropdown", () => {
             <calcite-dropdown-item id="item-10">10</calcite-dropdown-item>
           </calcite-dropdown-group>
         </calcite-dropdown>`,
-      });
+      );
 
       const element = await page.find("calcite-dropdown");
       const dropdownOpenEvent = page.waitForEvent("calciteDropdownOpen");
@@ -642,6 +643,14 @@ describe("calcite-dropdown", () => {
 
       for (let i = 0; i < items.length; i++) {
         expect(await items[i].isIntersectingViewport()).toBe(i <= newMaxItems - 1);
+      }
+
+      const totalItems = 10;
+      element.setProperty("maxItems", totalItems);
+      await page.waitForChanges();
+
+      for (let i = 0; i < items.length; i++) {
+        expect(await items[i].isIntersectingViewport()).toBe(true);
       }
     });
   });

packages/calcite-components/src/components/dropdown/dropdown.tsx
@@ -481,10 +481,9 @@ export class Dropdown
 
     this.reposition(true);
     const maxScrollerHeight = this.getMaxScrollerHeight();
-    scrollerEl.style.maxHeight = maxScrollerHeight > 0 ? `${maxScrollerHeight}px` : "";
+    scrollerEl.style.maxBlockSize = maxScrollerHeight > 0 ? `${maxScrollerHeight}px` : "";
     this.reposition(true);
   }
-
   private setScrollerAndTransitionEl(el: HTMLDivElement): void {
     if (el) {
       this.resizeObserver?.observe(el);
@@ -589,7 +588,7 @@ export class Dropdown
       }
     });
 
-    return items.length > maxItems ? maxScrollerHeight : 0;
+    return items.length >= maxItems ? maxScrollerHeight : 0;
   }
 
   private closeCalciteDropdown(focusTrigger = true) {

packages/calcite-components/src/components/combobox/resources.ts
@@ -1,6 +1,6 @@
 export const ComboboxItemSelector = "CALCITE-COMBOBOX-ITEM";
 export const ComboboxItemGroupSelector = "CALCITE-COMBOBOX-ITEM-GROUP";
-export const ComboboxChildSelector = `${ComboboxItemSelector}, ${ComboboxItemGroupSelector}`;
+export const AllComboboxChildrenSelector = `${ComboboxItemSelector}, ${ComboboxItemGroupSelector}`;
 
 export const CSS = {
   input: "input",

packages/calcite-components/src/components/combobox/combobox.e2e.ts
@@ -705,6 +705,14 @@ describe("calcite-combobox", () => {
     for (let i = 0; i < items.length; i++) {
       expect(await items[i].isIntersectingViewport()).toBe(i < maxItems);
     }
+
+    const totalItems = 11;
+    element.setProperty("maxItems", totalItems);
+    await page.waitForChanges();
+
+    for (let i = 0; i < items.length; i++) {
+      expect(await items[i].isIntersectingViewport()).toBe(true);
+    }
   });
 
   it("should control max items displayed with group", async () => {
@@ -739,6 +747,14 @@ describe("calcite-combobox", () => {
 
     for (let i = 0; i < items.length; i++) {
       expect(await items[i].isIntersectingViewport()).toBe(i < maxItems);
+    }
+
+    const totalItems = 11;
+    element.setProperty("maxItems", totalItems);
+    await page.waitForChanges();
+
+    for (let i = 0; i < items.length; i++) {
+      expect(await items[i].isIntersectingViewport()).toBe(true);
     }
   });
 
@@ -780,6 +796,14 @@ describe("calcite-combobox", () => {
 
     for (let i = 0; i < items.length; i++) {
       expect(await items[i].isIntersectingViewport()).toBe(i < maxItems);
+    }
+
+    const totalItems = 16;
+    element.setProperty("maxItems", totalItems);
+    await page.waitForChanges();
+
+    for (let i = 0; i < items.length; i++) {
+      expect(await items[i].isIntersectingViewport()).toBe(true);
     }
   });
 

packages/calcite-components/src/components/combobox/combobox.tsx
@@ -65,13 +65,7 @@ import type { ComboboxItem as HTMLCalciteComboboxItemElement } from "../combobox
 import type { Label } from "../label/label";
 import T9nStrings from "./assets/t9n/messages.en.json";
 import { ComboboxChildElement, GroupData, ItemData, SelectionDisplay } from "./interfaces";
-import {
-  ComboboxChildSelector,
-  ComboboxItemSelector,
-  ComboboxItemGroupSelector,
-  CSS,
-  IDS,
-} from "./resources";
+import { ComboboxItemGroupSelector, ComboboxItemSelector, CSS, IDS } from "./resources";
 import {
   getItemAncestors,
   getItemChildren,
@@ -930,11 +924,10 @@ export class Combobox
 
     await this.reposition(true);
     const maxScrollerHeight = this.getMaxScrollerHeight();
-    listContainerEl.style.maxHeight = maxScrollerHeight > 0 ? `${maxScrollerHeight}px` : "";
+    listContainerEl.style.maxBlockSize = maxScrollerHeight > 0 ? `${maxScrollerHeight}px` : "";
     listContainerEl.style.minWidth = `${referenceEl.clientWidth}px`;
     await this.reposition(true);
   }
-
   private calciteChipCloseHandler(comboboxItem: HTMLCalciteComboboxItemElement["el"]): void {
     this.open = false;
 
@@ -1129,21 +1122,20 @@ export class Combobox
   }
 
   private getMaxScrollerHeight(): number {
-    const items = this.getItemsAndGroups().filter((item) => !item.hidden);
+    const allItemsAndGroups = [...this.groupItems, ...this.getItems(true)];
+    const items = allItemsAndGroups.filter((item) => !item.hidden);
 
     const { maxItems } = this;
 
     let itemsToProcess = 0;
     let maxScrollerHeight = 0;
 
-    if (items.length > maxItems) {
+    if (items.length >= maxItems) {
       items.forEach((item) => {
         if (itemsToProcess < maxItems) {
           const height = this.calculateScrollerHeight(item);
-          if (height > 0) {
-            maxScrollerHeight += height;
-            itemsToProcess++;
-          }
+          maxScrollerHeight += height;
+          itemsToProcess += 1;
         }
       });
     }
@@ -1153,13 +1145,14 @@ export class Combobox
 
   private calculateScrollerHeight(item: ComboboxChildElement): number {
     if (!item) {
-      return;
+      return 0;
     }
 
     // if item has children items, don't count their height twice
     const parentHeight = item.getBoundingClientRect().height;
+    const DirectComboboxChildrenSelector = `:scope > ${ComboboxItemSelector}, :scope > ${ComboboxItemGroupSelector}`;
     const childrenTotalHeight = Array.from(
-      item.querySelectorAll<ComboboxChildElement>(ComboboxChildSelector),
+      item.querySelectorAll<ComboboxChildElement>(DirectComboboxChildrenSelector),
     ).reduce((total, child) => total + child.getBoundingClientRect().height, 0);
 
     return parentHeight - childrenTotalHeight;
@@ -1295,11 +1288,11 @@ export class Combobox
     this.filterText = "";
   }
 
-  private getItems(): HTMLCalciteComboboxItemElement["el"][] {
+  private getItems(withDisabled: boolean = false): HTMLCalciteComboboxItemElement["el"][] {
     const items: HTMLCalciteComboboxItemElement["el"][] = Array.from(
       this.el.querySelectorAll(ComboboxItemSelector),
     );
-    return items.filter((item) => !item.disabled);
+    return items.filter((item) => withDisabled || !item.disabled);
   }
 
   private getGroupItems(): HTMLCalciteComboboxItemGroupElement["el"][] {

packages/calcite-components/src/components/combobox/utils.ts
@@ -2,12 +2,12 @@ import { nodeListToArray } from "../../utils/dom";
 import { isBrowser } from "../../utils/browser";
 import { ComboboxItem } from "../combobox-item/combobox-item";
 import { ComboboxChildElement } from "./interfaces";
-import { ComboboxItemGroupSelector, ComboboxItemSelector, ComboboxChildSelector } from "./resources";
+import { ComboboxItemGroupSelector, ComboboxItemSelector, AllComboboxChildrenSelector } from "./resources";
 import { Combobox } from "./combobox";
 
 export function getAncestors(element: HTMLElement): ComboboxChildElement[] {
-  const parent: ComboboxChildElement = element.parentElement?.closest(ComboboxChildSelector);
-  const grandparent: ComboboxChildElement = parent?.parentElement?.closest(ComboboxChildSelector);
+  const parent: ComboboxChildElement = element.parentElement?.closest(AllComboboxChildrenSelector);
+  const grandparent: ComboboxChildElement = parent?.parentElement?.closest(AllComboboxChildrenSelector);
   return [parent, grandparent].filter((el) => el);
 }
 

packages/calcite-components/src/components/dropdown/dropdown.stories.ts
@@ -243,7 +243,8 @@ export const itemsAsLinksDarkMode = (): string => html`
 itemsAsLinksDarkMode.parameters = { themes: modesDarkDefault };
 
 export const scrollingAfterCertainItems_TestOnly = (): string => html`
-  <calcite-dropdown open placement="${defaultMenuPlacement}" max-items="7" scale="m" width-scale="m" type="click">
+  <!-- we use large scale to test max viewport height-->
+  <calcite-dropdown open placement="${defaultMenuPlacement}" max-items="7" width-scale="m" type="click">
     <calcite-button slot="trigger">Open Dropdown</calcite-button>
     <calcite-dropdown-group group-title="First group">
       <calcite-dropdown-item>1</calcite-dropdown-item>
@@ -259,6 +260,13 @@ export const scrollingAfterCertainItems_TestOnly = (): string => html`
       <calcite-dropdown-item>9</calcite-dropdown-item>
       <calcite-dropdown-item>10</calcite-dropdown-item>
     </calcite-dropdown-group>
+    <calcite-dropdown-group group-title="Third group">
+      <calcite-dropdown-item>11</calcite-dropdown-item>
+      <calcite-dropdown-item>12</calcite-dropdown-item>
+      <calcite-dropdown-item>13</calcite-dropdown-item>
+      <calcite-dropdown-item>14</calcite-dropdown-item>
+      <calcite-dropdown-item>15</calcite-dropdown-item>
+    </calcite-dropdown-group>
   </calcite-dropdown>
 `;
 
@@ -267,6 +275,7 @@ scrollingAfterCertainItems_TestOnly.parameters = {
 };
 
 export const scrollingWithoutMaxItems_TestOnly = (): string => html`
+  <!-- we use large scale to test max viewport height-->
   <calcite-dropdown open>
     <calcite-button slot="trigger">Open Dropdown</calcite-button>
     <calcite-dropdown-group selection-mode="single" group-title="Sort by">
@@ -287,6 +296,13 @@ export const scrollingWithoutMaxItems_TestOnly = (): string => html`
       <calcite-dropdown-item>8</calcite-dropdown-item>
       <calcite-dropdown-item>9</calcite-dropdown-item>
       <calcite-dropdown-item>10</calcite-dropdown-item>
+    </calcite-dropdown-group>
+    <calcite-dropdown-group group-title="Third group">
+      <calcite-dropdown-item>11</calcite-dropdown-item>
+      <calcite-dropdown-item>12</calcite-dropdown-item>
+      <calcite-dropdown-item>13</calcite-dropdown-item>
+      <calcite-dropdown-item>14</calcite-dropdown-item>
+      <calcite-dropdown-item>15</calcite-dropdown-item>
     </calcite-dropdown-group>
   </calcite-dropdown>
 `;

commit 9356de1fcc8ec0654f3b4d6dc59fa8ebbe8288bb

fix(dropdown): avoid showing scroller when max-items equals items (#11480)
**Related Issue:** #10731 

## Summary

Fixes regression caused by
https://github.com/Esri/calcite-design-system/pull/10971.

packages/calcite-components/src/components/dropdown-group/dropdown-group.scss
@@ -1,6 +1,5 @@
 :host {
-  // we make the host relative, so items can consistently compute their offsetTop based on the group
-  @apply block relative;
+  @apply block;
 }
 
 .container {

packages/calcite-components/src/components/dropdown/dropdown.e2e.ts
@@ -1,5 +1,4 @@
 // @ts-strict-ignore
-import dedent from "dedent";
 import { newE2EPage } from "@arcgis/lumina-compiler/puppeteerTesting";
 import { describe, expect, it } from "vitest";
 import { html } from "../../../support/formatting";
@@ -23,6 +22,7 @@ import {
 } from "../../tests/utils";
 import type { DropdownItem } from "../dropdown-item/dropdown-item";
 import type { Button } from "../button/button";
+import { CSS } from "./resources";
 
 describe("calcite-dropdown", () => {
   const simpleDropdownHTML = html`
@@ -654,6 +654,10 @@ describe("calcite-dropdown", () => {
       for (let i = 0; i < items.length; i++) {
         expect(await items[i].isIntersectingViewport()).toBe(true);
       }
+
+      // no scroller should be present when max-items === items
+      const scroller = await page.find(`calcite-dropdown >>> .${CSS.content}`);
+      expect(await scroller.getProperty("scrollHeight")).toBe(await scroller.getProperty("clientHeight"));
     });
   });
 
@@ -1038,12 +1042,12 @@ describe("calcite-dropdown", () => {
   });
 
   describe("accessible", () => {
-    accessible(dedent`${dropdownSelectionModeContent}`);
+    accessible(html`${dropdownSelectionModeContent}`);
   });
 
   it("correct role and aria properties are applied based on selection type", async () => {
     const page = await newE2EPage();
-    await page.setContent(dedent`${dropdownSelectionModeContent}`);
+    await page.setContent(html`${dropdownSelectionModeContent}`);
     await page.waitForChanges();
 
     const element = await page.find("calcite-dropdown");

packages/calcite-components/src/components/dropdown/dropdown.tsx
@@ -1,6 +1,6 @@
 // @ts-strict-ignore
 import { PropertyValues } from "lit";
-import { LitElement, property, createEvent, h, method, JsxNode } from "@arcgis/lumina";
+import { createEvent, h, JsxNode, LitElement, method, property } from "@arcgis/lumina";
 import { focusElement, focusElementInGroup, focusFirstTabbable } from "../../utils/dom";
 import {
   connectFloatingUI,
@@ -261,7 +261,7 @@ export class Dropdown
       this.flipPlacementsHandler();
     }
 
-    if (changes.has("maxItems") && (this.hasUpdated || this.maxItems !== 0)) {
+    if (changes.has("maxItems") && this.hasUpdated) {
       this.setMaxScrollerHeight();
     }
 
@@ -451,6 +451,11 @@ export class Dropdown
   private resizeObserverCallback(entries: ResizeObserverEntry[]): void {
     entries.forEach((entry) => {
       const { target } = entry;
+
+      if (!this.hasUpdated) {
+        return;
+      }
+
       if (target === this.referenceEl) {
         this.setDropdownWidth();
       } else if (target === this.scrollerEl) {
@@ -463,24 +468,15 @@ export class Dropdown
     const { referenceEl, scrollerEl } = this;
     const referenceElWidth = referenceEl?.clientWidth;
 
-    if (!referenceElWidth || !scrollerEl) {
-      return;
-    }
-
     scrollerEl.style.minWidth = `${referenceElWidth}px`;
   }
 
   private setMaxScrollerHeight(): void {
-    const { scrollerEl } = this;
-    if (!scrollerEl) {
-      return;
-    }
-
-    this.reposition(true);
     const maxScrollerHeight = this.getMaxScrollerHeight();
-    scrollerEl.style.maxBlockSize = maxScrollerHeight > 0 ? `${maxScrollerHeight}px` : "";
+    this.scrollerEl.style.maxBlockSize = maxScrollerHeight > 0 ? `${maxScrollerHeight}px` : "";
     this.reposition(true);
   }
+
   private setScrollerAndTransitionEl(el: HTMLDivElement): void {
     if (el) {
       this.resizeObserver?.observe(el);
@@ -562,30 +558,16 @@ export class Dropdown
 
   private getMaxScrollerHeight(): number {
     const { maxItems, items } = this;
-    let itemsToProcess = 0;
-    let maxScrollerHeight = 0;
-    let groupHeaderHeight: number;
 
-    this.groups.forEach((group) => {
-      if (maxItems > 0 && itemsToProcess < maxItems) {
-        Array.from(group.children).forEach((item: DropdownItem["el"], index) => {
-          if (index === 0) {
-            if (isNaN(groupHeaderHeight)) {
-              groupHeaderHeight = item.offsetTop;
-            }
+    return items.length >= maxItems && maxItems > 0
+      ? this.getYDistance(this.scrollerEl, items[maxItems - 1])
+      : 0;
+  }
 
-            maxScrollerHeight += groupHeaderHeight;
-          }
-
-          if (itemsToProcess < maxItems) {
-            maxScrollerHeight += item.offsetHeight;
-            itemsToProcess += 1;
-          }
-        });
-      }
-    });
-
-    return items.length >= maxItems ? maxScrollerHeight : 0;
+  private getYDistance(parent: HTMLElement, child: HTMLElement): number {
+    const parentRect = parent.getBoundingClientRect();
+    const childRect = child.getBoundingClientRect();
+    return childRect.bottom - parentRect.top;
   }
 
   private closeCalciteDropdown(focusTrigger = true) {
@@ -663,7 +645,7 @@ export class Dropdown
           <div
             aria-labelledby={`${guid}-menubutton`}
             class={{
-              ["calcite-dropdown-content"]: true,
+              [CSS.content]: true,
               [FloatingCSS.animation]: true,
               [FloatingCSS.animationActive]: open,
             }}

packages/calcite-components/src/components/dropdown/resources.ts
@@ -3,5 +3,6 @@ export const SLOTS = {
 };
 
 export const CSS = {
+  content: "calcite-dropdown-content",
   wrapper: "calcite-dropdown-wrapper",
 };