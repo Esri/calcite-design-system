f6c7a35d3c92bc0f28018fbb4810d6e4032eea8b
JC Franco <jfranco@esri.com>
2025-03-02 00:27:07 -0800

    tidy up indexFile

--- packages/calcite-design-tokens/support/format/indexFile.ts
+++ packages/calcite-design-tokens/support/format/indexFile.ts
@@ -3,22 +3,21 @@ import { resolve } from "node:path";
 import { FormatFn, FormatFnArguments } from "style-dictionary/types";
 import { RegisterFn } from "../types/interfaces.js";
 
-export async function getFileContents(theme: string, args: FormatFnArguments, count?: number): Promise<string> {
+export async function getFileContents(theme: string, args: FormatFnArguments, count: number = -1): Promise<string> {
   try {
     const content = readFileSync(
-      resolve(process.cwd(), args.platform.buildPath, `${theme}${args.options.fileExtension}`),
+      resolve(process.cwd(), args.platform.buildPath ?? "", `${theme}${args.options.fileExtension}`),
       "utf8",
     );
-    const regex = /((--|\$)[\w-\d]+:\s?[\w\d#-_() ]+)/g;
+    const regex = /((--|\$)[#$%&'()*+,-./0-9:;<=>?@A-Z[\\]^_]+)/g;
     const variables = Array.from(content.matchAll(regex)).map((v) => v[0]);
     return variables.join("\n\t");
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error: any) {
+  } catch {
     if (count > 3) {
       await new Promise((resolve) => setTimeout(resolve, 500));
-      await getFileContents(theme, args, count ? count + 1 : 1);
+      return await getFileContents(theme, args, count ? count + 1 : 1);
     } else {
-      console.error(`Error reading file ${theme}${args.options.fileExtension}`);
+      throw new Error(`Error reading file ${theme}${args.options.fileExtension}`);
     }
   }
 }
@@ -27,14 +26,14 @@ export const formatIndexFile: FormatFn = async (args) => {
   const {
     options: { fileExtension },
   } = args;
-  const themes = ["light", "dark"];
+  const themes = ["light", "dark"] as const;
   const themedVars = await Promise.all(themes.map((theme) => getFileContents(theme, args))).then((res) => ({
     light: res[0],
     dark: res[1],
   }));
   const platformClass = args.options.fileExtension === ".scss" ? "@mixin " : ".";
 
-  const importUrl = (fileName, fileExtension) =>
+  const importUrl = (fileName: string, fileExtension: string) =>
     fileExtension === ".css"
       ? `@import url("./${fileName}${fileExtension}");`
       : `@import "./${fileName}${fileExtension}";`;
@@ -48,7 +47,7 @@ export const formatIndexFile: FormatFn = async (args) => {
           .join("\n\n")
       : "";
   const root = args.options.fileExtension === ".css" ? `:root {\n\t${themedVars.light}\n}` : "";
-  const imports = args.options.imports.map((imp) => importUrl(imp, fileExtension)).join("\n") + "\n";
+  const imports = args.options.imports.map((imp: string) => importUrl(imp, fileExtension)).join("\n") + "\n";
 
   return [imports, root, atMedia, includePlatformClasses].filter((i) => i && i !== "").join("\n\n");
 };
