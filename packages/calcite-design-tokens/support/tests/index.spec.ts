d637f375539f830a20434fccdba406ff2db0db48
JC Franco <jfranco@esri.com>
2025-03-05 16:08:44 -0800

    simplify spec test

--- packages/calcite-design-tokens/support/tests/index.spec.ts
+++ packages/calcite-design-tokens/support/tests/index.spec.ts
@@ -6,73 +6,59 @@ import { describe, it, expect } from "vitest";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+/**
+ * Assert that the output file contents matches the snapshot
+ *
+ * @param outputFilePath - The path to the output file to test, relative to the dist directory
+ */
+function assertOutput(outputFilePath: string) {
+  const filePath = resolve(__dirname, "..", "..", "dist", outputFilePath);
+  let content = readFileSync(filePath, "utf-8");
+  content = content.slice(content.indexOf("*/") + 1);
+  expect(content).toMatchSnapshot();
+}
+
 describe("generated tokens", () => {
   describe("CSS", () => {
     it("global should match", () => {
-      let global = readFileSync(resolve(__dirname, "..", "..", "dist", "css", "global.css"), "utf-8");
-      global = global.slice(global.indexOf("*/") + 1);
-      expect(global).toMatchSnapshot();
+      assertOutput("css/global.css");
     });
     it("core should match", () => {
-      let core = readFileSync(resolve(__dirname, "..", "..", "dist", "css", "core.css"), "utf-8");
-      core = core.slice(core.indexOf("*/") + 1);
-      expect(core).toMatchSnapshot();
+      assertOutput("css/core.css");
     });
     it("light should match", () => {
-      let light = readFileSync(resolve(__dirname, "..", "..", "dist", "css", "light.css"), "utf-8");
-      light = light.slice(light.indexOf("*/") + 1);
-      expect(light).toMatchSnapshot();
+      assertOutput("css/light.css");
     });
     it("dark should match", () => {
-      let dark = readFileSync(resolve(__dirname, "..", "..", "dist", "css", "dark.css"), "utf-8");
-      dark = dark.slice(dark.indexOf("*/") + 1);
-      expect(dark).toMatchSnapshot();
+      assertOutput("css/dark.css");
     });
   });
 
   describe("SCSS", () => {
     it("global should match", () => {
-      let global = readFileSync(resolve(__dirname, "..", "..", "dist", "scss", "global.scss"), "utf-8");
-      global = global.slice(global.indexOf("*/") + 1);
-      expect(global).toMatchSnapshot();
+      assertOutput("scss/global.scss");
     });
     it("core should match", () => {
-      let core = readFileSync(resolve(__dirname, "..", "..", "dist", "scss", "core.scss"), "utf-8");
-      core = core.slice(core.indexOf("*/") + 1);
-      expect(core).toMatchSnapshot();
+      assertOutput("scss/core.scss");
     });
     it("light should match", () => {
-      let light = readFileSync(resolve(__dirname, "..", "..", "dist", "scss", "light.scss"), "utf-8");
-      light = light.slice(light.indexOf("*/") + 1);
-      expect(light).toMatchSnapshot();
+      assertOutput("scss/light.scss");
     });
     it("dark should match", () => {
-      let dark = readFileSync(resolve(__dirname, "..", "..", "dist", "scss", "dark.scss"), "utf-8");
-      dark = dark.slice(dark.indexOf("*/") + 1);
-      expect(dark).toMatchSnapshot();
+      assertOutput("scss/dark.scss");
     });
   });
 
   describe("ES6", () => {
     it("global should match", () => {
-      let global = readFileSync(resolve(__dirname, "..", "..", "dist", "es6", "global.js"), "utf-8");
-      global = global.slice(global.indexOf("*/") + 1);
-      expect(global).toMatchSnapshot();
+      assertOutput("es6/global.js");
     });
     it("core should match", () => {
-      let core = readFileSync(resolve(__dirname, "..", "..", "dist", "es6", "core.js"), "utf-8");
-      core = core.slice(core.indexOf("*/") + 1);
-      expect(core).toMatchSnapshot();
+      assertOutput("es6/core.js");
     });
     it("types should match", () => {
-      let core = readFileSync(resolve(__dirname, "..", "..", "dist", "es6", "core.d.ts"), "utf-8");
-      let global = readFileSync(resolve(__dirname, "..", "..", "dist", "es6", "global.d.ts"), "utf-8");
-
-      global = global.slice(global.indexOf("*/") + 1);
-      core = core.slice(core.indexOf("*/") + 1);
-
-      expect(global).toMatchSnapshot();
-      expect(core).toMatchSnapshot();
+      assertOutput("es6/core.d.ts");
+      assertOutput("es6/global.d.ts");
     });
   });
 });
