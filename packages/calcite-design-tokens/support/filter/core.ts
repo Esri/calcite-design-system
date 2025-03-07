f8711586ca0576f5841b28e8e0433d27b25e05b5
JC Franco <jfranco@esri.com>
2025-03-07 09:57:35 -0800

    remove non-existent types from filters

--- packages/calcite-design-tokens/support/filter/core.ts
+++ packages/calcite-design-tokens/support/filter/core.ts
@@ -4,9 +4,7 @@ import { RegisterFn } from "../types/interfaces.js";
 const mediumLowSaturation = /(medium|low)-{0,1}saturation/i;
 
 export const filterCoreTokens: Filter["filter"] = (token) =>
-  !token.isSource &&
-  !(token.type === "min" || token.type === "max" || token.type === "typography") &&
-  !mediumLowSaturation.test(token.name);
+  !token.isSource && token.type !== "typography" && !mediumLowSaturation.test(token.name);
 
 export const registerFilterCoreTokens: RegisterFn = async (sd) =>
   sd.registerFilter({
--- packages/calcite-design-tokens/support/filter/global.ts
+++ packages/calcite-design-tokens/support/filter/global.ts
@@ -4,16 +4,7 @@ import { RegisterFn } from "../types/interfaces.js";
 const mediumLowSaturation = /(medium|low)-{0,1}saturation/i;
 
 export const filterGlobalTokens: Filter["filter"] = (token) =>
-  token.isSource &&
-  !(
-    token.type === "color" ||
-    token.type === "dark" ||
-    token.type === "light" ||
-    token.type === "min" ||
-    token.type === "max" ||
-    token.type === "typography"
-  ) &&
-  !mediumLowSaturation.test(token.name);
+  token.isSource && !(token.type === "color" || token.type === "typography") && !mediumLowSaturation.test(token.name);
 
 export const registerFilterGlobalTokens: RegisterFn = async (sd) =>
   sd.registerFilter({
