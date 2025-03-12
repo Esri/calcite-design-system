6fca870e077791e53e73a9c83157a76b1c3e9db4
JC Franco <jfranco@esri.com>
2025-03-12 09:54:43 -0700

    fix types

--- packages/calcite-design-tokens/src/build/transforms/value/ensure-type.ts
+++ packages/calcite-design-tokens/src/build/transforms/value/ensure-type.ts
@@ -1,8 +1,10 @@
 import { Filter, ValueTransform } from "style-dictionary/types";
+import { ArrayValues } from "type-fest";
 import { RegisterFn } from "../../types/interfaces.js";
 
 const stringValueTokenTypes = ["fontWeight", "opacity", "typography", "z-index"] as const;
-const filterTypes: Filter["filter"] = (token) => stringValueTokenTypes.includes(token.type);
+const filterTypes: Filter["filter"] = (token) =>
+  stringValueTokenTypes.includes(token.type as ArrayValues<typeof stringValueTokenTypes>);
 
 const transformValueEnsureType: ValueTransform["transform"] = async (token) => {
   return `${token.value}`;
