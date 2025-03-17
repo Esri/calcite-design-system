7ac01d685a4067b469a6be5f10b0cfca885f224b
eliza <eli97736@esri.com>
2025-03-17 13:43:08 -0700

    feat(input-message): add component tokens

---
+++ packages/calcite-components/src/custom-theme/input-message.ts
@@ -0,0 +1,8 @@
+import { html } from "../../support/formatting";
+
+export const inputMessageTokens = {
+  calciteInputMessageIconColor: "",
+  calciteInputMessageSpacing: "",
+};
+
+export const inputMessage = html`<calcite-input-message status="invalid" icon="frown">Message</calcite-input-message>`;
