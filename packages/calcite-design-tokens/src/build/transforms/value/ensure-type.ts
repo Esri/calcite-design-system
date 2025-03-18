f328b2a4f551e41f1c59444b5f0c6ac798b30ca7
JC Franco <jfranco@esri.com>
2025-03-18 14:14:13 -0700

    ensure dimension output types

--- packages/calcite-design-tokens/src/build/transforms/value/ensure-type.ts
+++ packages/calcite-design-tokens/src/build/transforms/value/ensure-type.ts
@@ -2,7 +2,7 @@ import { Filter, ValueTransform } from "style-dictionary/types";
 import { ArrayValues } from "type-fest";
 import { RegisterFn } from "../../types/interfaces.js";
 
-const stringValueTokenTypes = ["fontWeight", "letterSpacing", "lineHeight", "opacity", "z-index"] as const;
+const stringValueTokenTypes = ["fontWeight", "dimension", "lineHeight", "opacity", "z-index"] as const;
 const filterTypes: Filter["filter"] = (token) =>
   stringValueTokenTypes.includes(token.type as ArrayValues<typeof stringValueTokenTypes>);
 
