c3f10fc9acbd00fbe50d70c52228cdb8e5ddde41
eliza <eli97736@esri.com>
2025-03-25 15:05:40 -0700

    input date picker tokens

--- packages/calcite-components/src/custom-theme/input-date-picker.ts
+++ packages/calcite-components/src/custom-theme/input-date-picker.ts
@@ -5,7 +5,14 @@ export const inputDatePickerTokens = {
   calciteInputDatePickerBorderColor: "",
   calciteInputDatePickerIconColor: "",
   calciteInputDatePickerIconColorHover: "",
-  CalciteInputDatePickerShadow: "",
+  calciteInputDatePickerShadow: "",
+  calciteInputDatePickerInputBackgroundColor: "",
+  calciteInputDatePickerInputBorderColor: "",
+  calciteInputDatePickerInputCornerRadius: "",
+  calciteInputDatePickerInputShadow: "",
+  calciteInputDatePickerInputIconColor: "",
+  calciteInputDatePickerInputTextColor: "",
+  calciteInputDatePickerInputPlaceholderTextColor: "",
 };
 
 export const inputDatePicker = html`<calcite-input-date-picker range></calcite-input-date-picker>`;
