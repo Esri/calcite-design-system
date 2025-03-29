ff20550b0ecb861c8e6507422b3817441bb7b670
JC Franco <jfranco@esri.com>
2025-03-29 10:41:35 -0700

    refactor: bump `style-dictionary` and `@tokens-studio/sd-transforms` to latest (#11655)

---
+++ packages/calcite-design-tokens/src/build/filter/breakpoints.ts
@@ -0,0 +1,14 @@
+import { Filter } from "style-dictionary/types";
+import StyleDictionary from "style-dictionary";
+import { RegisterFn } from "../types/interfaces.js";
+import { isBreakpoint } from "../utils/token-types.js";
+
+export const filterBreakpointTokens: Filter["filter"] = (token) => token.isSource && isBreakpoint(token);
+
+export const registerFilterBreakpointTokens: RegisterFn = () =>
+  StyleDictionary.registerFilter({
+    name: FilterBreakpointTokens,
+    filter: filterBreakpointTokens,
+  });
+
+export const FilterBreakpointTokens = "calcite/filter/tokens/breakpoint";
