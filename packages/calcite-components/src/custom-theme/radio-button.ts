926e3072ef518f331b69f32b737b6fae5b8e9922
Matt Driscoll <mdriscoll@esri.com>
2025-04-07 16:41:09 -0700

    eat(radio-button): add component tokens #7180

---
+++ packages/calcite-components/src/custom-theme/radio-button.ts
@@ -0,0 +1,9 @@
+import { html } from "../../support/formatting";
+
+export const radioButtonTokens = {
+  calciteRadioButtonBackgroundColor: "",
+  calciteRadioButtonBorderColor: "",
+  calciteRadioButtonCornerRadius: "",
+};
+
+export const radioButton = html`<calcite-radio-button></calcite-radio-button>`;
