a21d5085b7bed554614c0cfad48836af3888db4d
JC Franco <jfranco@esri.com>
2025-04-07 14:04:26 -0700

    chore(linting): fix lint-staged not finding markdownlint-cli2 config

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
   "*.{json,html,yml}": ["prettier --write"],
   "*.{s,}css": ["prettier --write"],
-  "*.md": ["prettier --write", "markdownlint-cli2 --fix"],
+  "*.md": ["prettier --write", "markdownlint-cli2 --fix --config ../../.markdownlint-cli2.jsonc"],
 };
