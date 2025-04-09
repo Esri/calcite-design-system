db4935259da2c729d5ac0d9d40e8d29890fa4941
Matt Driscoll <mdriscoll@esri.com>
2025-04-09 14:25:57 -0700

    Update radio-button.ts

--- packages/calcite-components/src/custom-theme/radio-button.ts
+++ packages/calcite-components/src/custom-theme/radio-button.ts
@@ -4,6 +4,7 @@ export const radioButtonTokens = {
   calciteRadioButtonBackgroundColor: "",
   calciteRadioButtonBorderColor: "",
   calciteRadioButtonCornerRadius: "",
+  calciteRadioButtonSize: "",
 };
 
 export const radioButton = html`<calcite-radio-button></calcite-radio-button>`;
