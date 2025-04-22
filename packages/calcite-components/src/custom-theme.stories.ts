c6141a71bc3e684fd7ed7ad4d20201f488089d81
Matt Driscoll <mdriscoll@esri.com>
2025-04-22 09:50:04 -0700

    feat(radio-button): add component tokens (#11891)
    **Related Issue:** #7180
    
    ## Summary
    
    Adds the following tokens for `radio-button` component.
    
    - --calcite-radio-button-background-color: Specifies the component's
    background color.
    - --calcite-radio-button-border-color: Specifies the component's border
    color.
    - --calcite-radio-button-corner-radius: Specifies the component's corner
    radius.
    - --calcite-radio-button-size: Specifies the component's size.
    - --calcite-radio-size: [Deprecated] Use `--calcite-radio-button-size`.
    Specifies the component's size.

--- packages/calcite-components/src/custom-theme.stories.ts
+++ packages/calcite-components/src/custom-theme.stories.ts
@@ -44,6 +44,7 @@ import { popover, popoverTokens } from "./custom-theme/popover";
 import { progress, progressTokens } from "./custom-theme/progress";
 import { segmentedControl, segmentedControlTokens } from "./custom-theme/segmented-control";
 import { select, selectTokens } from "./custom-theme/select";
+import { radioButton, radioButtonTokens } from "./custom-theme/radio-button";
 import { radioButtonGroup, radioButtonGroupTokens } from "./custom-theme/radio-button-group";
 import { rating, ratingTokens } from "./custom-theme/rating";
 import { slider, sliderTokens } from "./custom-theme/slider";
@@ -179,6 +180,7 @@ const kitchenSink = (args: Record<string, string>, useTestValues = false) =>
       <div class="demo-column">${inputMessage}</div>
     </div>
     <div class="demo-row">
+      <div class="demo-column">${radioButton}</div>
       <div class="demo-column">${radioButtonGroup}</div>
     </div>
     <div class="demo-row">
@@ -229,6 +231,7 @@ const componentTokens = {
   ...popoverTokens,
   ...progressTokens,
   ...segmentedControlTokens,
+  ...radioButtonTokens,
   ...radioButtonGroupTokens,
   ...ratingTokens,
   ...selectTokens,
