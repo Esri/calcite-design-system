bbcd4763cd89fd5e0316b36d6120d22a9e438b89
Erik Harper <eriklharper@pm.me>
2025-04-22 13:51:03 -0700

    updating accessible test to use new label property

--- packages/calcite-components/src/components/input-date-picker/input-date-picker.e2e.ts
+++ packages/calcite-components/src/components/input-date-picker/input-date-picker.e2e.ts
@@ -25,12 +25,7 @@ const animationDurationInMs = 200;
 
 describe("calcite-input-date-picker", () => {
   describe("accessibility", () => {
-    accessible(html`
-      <calcite-label>
-        Input Date Picker
-        <calcite-input-date-picker></calcite-input-date-picker>
-      </calcite-label>
-    `);
+    accessible(html` <calcite-input-date-picker label="Input Date Picker"></calcite-input-date-picker> `);
   });
 
   describe("renders", () => {
