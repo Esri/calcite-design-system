24590d1682865e1674fdd44c3e1dc843978d125b
JC Franco <jfranco@esri.com>
2025-04-22 11:52:49 -0700

    fix typo

--- packages/calcite-components/vite.config.ts
+++ packages/calcite-components/vite.config.ts
@@ -15,7 +15,7 @@ const runBrowserTests = process.env.EXPERIMENTAL_TESTS === "true";
 const allDirsAndFiles = "**/*";
 const specAndE2EFileExtensions = `{e2e,spec}.?(c|m)[jt]s?(x)`;
 const browserTestMatch = `${allDirsAndFiles}.browser.${specAndE2EFileExtensions}`;
-const allSpecAndE2ETestMatch = `${allDirsAndFiles}.${specAndE2EFileExtensions}}`;
+const allSpecAndE2ETestMatch = `${allDirsAndFiles}.${specAndE2EFileExtensions}`;
 
 export default defineConfig({
   build: { minify: false },
