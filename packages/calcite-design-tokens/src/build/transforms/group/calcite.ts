c9f07e951f409dbcda909c89b8f2aef2d7d8730c
JC Franco <jfranco@esri.com>
2025-04-25 13:44:23 -0700

    add tokens-studio transform to handle drop/inner shadow types

--- packages/calcite-design-tokens/src/build/transforms/group/calcite.ts
+++ packages/calcite-design-tokens/src/build/transforms/group/calcite.ts
@@ -22,6 +22,7 @@ export const platformTransforms = {
     TransformAttributePlatformNames,
     TransformAttributeSchema,
     "ts/color/css/hexrgba",
+    "ts/shadow/innerShadow",
     "shadow/css/shorthand",
   ],
   [Platform.es6]: [
