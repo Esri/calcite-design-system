8bc246fa5085fec8d0a009631c8ed653f53a7136
Matt Gallagher <48596928+matgalla@users.noreply.github.com>
2025-06-17 14:46:11 -0400

    fix(tokens): restore `--calcite-color-focus` (#11960)
    **Related Issue:** #11713 #11711 
    
    ## Summary
    Restores the `--calcite-color-focus` token in both light and dark
    semantic color JSON files.
    
    ### Noteworthy changes
    
    * includes the new focus token in all outputs except `index.(s)css`
    * adds new `components.css` design tokens output for inclusion in
    `calcite-components` shadow DOM (host)
    * defines internal custom CSS property for use by focus utility styles
    (`--calcite-internal-color-focus`)
    * exports `invert` util from `calcite-tailwind-preset` (internal) – can
    be moved to internal shared utils
    * overrides Tailwind focus utilities in `calcite-components` – avoids
    depending on internal CSS props when using the preset
    * restores `ts/color/css/hexrgba` transform override to support focus
    color token value structure
    * extracts shared utilities across `calcite-design-tokens` format
    outputs
    * enhances dictionary utility to support filtered tokens
    * all components define the internal focus CSS prop, which might not be
    necessary in all cases – we can address this in
    https://github.com/Esri/calcite-design-system/issues/12106 and only
    include it in components that are focusable.
    
    BEGIN_COMMIT_OVERRIDE
    omitted from changelog
    END_COMMIT_OVERRIDE
    
    ---------
    
    Co-authored-by: JC Franco <jfranco@esri.com>

--- packages/calcite-components/tailwind.config.ts
+++ packages/calcite-components/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 import calcitePreset from "@esri/calcite-tailwind-preset";
+import { invert } from "@esri/calcite-tailwind-preset/dist/utils";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   presets: [calcitePreset],
@@ -17,6 +19,27 @@ const config: Config = {
       },
     },
   },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        // we override preset focus utils to allow using the internal focus color
+        ".focus-base": {
+          "outline-color": "transparent",
+        },
+        ".focus-normal": {
+          outline: "2px solid var(--calcite-internal-color-focus)",
+        },
+        ".focus-outset": {
+          outline: "2px solid var(--calcite-internal-color-focus)",
+          "outline-offset": invert("2px", "--calcite-offset-invert-focus"),
+        },
+        ".focus-inset": {
+          outline: "2px solid var(--calcite-internal-color-focus)",
+          "outline-offset": invert("-2px", "--calcite-offset-invert-focus"),
+        },
+      });
+    }),
+  ],
 };
 
 export default config;
