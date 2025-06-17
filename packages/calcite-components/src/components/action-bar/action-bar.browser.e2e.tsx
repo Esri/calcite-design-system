c60800c3912587206a12e8f30936024e3c32d069
eliza <eli97736@esri.com>
2025-06-17 15:44:05 -0700

    inverse the spy order - spying on spy causes it to error

--- packages/calcite-components/src/components/action-bar/action-bar.browser.e2e.tsx
+++ packages/calcite-components/src/components/action-bar/action-bar.browser.e2e.tsx
@@ -1,32 +1,23 @@
 // @ts-strict-ignore
 import { mount } from "@arcgis/lumina-compiler/testing";
 import { h } from "@arcgis/lumina";
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { DEBOUNCE } from "../../utils/resources";
+import { mockConsole } from "../../tests/utils/logging";
 
 describe("action-bar", () => {
-  const originalEsriInternalEnv = process.env.ESRI_INTERNAL;
-
-  beforeEach(() => {
-    // override
-    process.env.ESRI_INTERNAL = undefined;
-  });
-
-  afterEach(() => {
-    // Restore the original NODE_ENV
-    process.env.ESRI_INTERNAL = originalEsriInternalEnv;
-  });
+  mockConsole("warn");
 
   it("pending resize is canceled on disconnect", async () => {
     const { component, el } = await mount<"calcite-action-bar">(<calcite-action-bar />);
 
-    const resizeSpy = vi.spyOn(component, "resize");
     const cancelSpy = vi.spyOn(component.resize, "cancel");
+    const resizeSpy = vi.spyOn(component, "resize");
 
     el.remove();
     await new Promise((resolve) => setTimeout(resolve, DEBOUNCE.resize));
 
-    expect(resizeSpy.mock.calls.length).toBe(0);
     expect(cancelSpy.mock.calls.length).toBe(1);
+    expect(resizeSpy.mock.calls.length).toBe(0);
   });
 });
