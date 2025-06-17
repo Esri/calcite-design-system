92bee0ce13fe9f7cbec707f1e4f05f0aa710d5bc
eliza <eli97736@esri.com>
2025-06-17 14:53:17 -0700

    env variables

--- packages/calcite-components/src/components/action-bar/action-bar.browser.e2e.tsx
+++ packages/calcite-components/src/components/action-bar/action-bar.browser.e2e.tsx
@@ -1,12 +1,25 @@
 // @ts-strict-ignore
 import { mount } from "@arcgis/lumina-compiler/testing";
 import { h } from "@arcgis/lumina";
-import { describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { DEBOUNCE } from "../../utils/resources";
 
 describe("action-bar", () => {
+  const originalEsriInternalEnv = process.env.ESRI_INTERNAL;
+
+  beforeEach(() => {
+    // override
+    process.env.ESRI_INTERNAL = undefined;
+  });
+
+  afterEach(() => {
+    // Restore the original NODE_ENV
+    process.env.ESRI_INTERNAL = originalEsriInternalEnv;
+  });
+
   it("pending resize is canceled on disconnect", async () => {
     const { component, el } = await mount<"calcite-action-bar">(<calcite-action-bar />);
+
     const resizeSpy = vi.spyOn(component, "resize");
     const cancelSpy = vi.spyOn(component.resize, "cancel");
 
