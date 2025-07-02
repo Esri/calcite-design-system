8eb6a5efe7dfa052cb4a53f9eaa30a39d817fdf9
JC Franco <jfranco@esri.com>
2025-07-01 18:04:03 -0700

    build(icons): upgrade build setup (#12415)
    **Related Issue:** N/A
    
    ## Summary
    
    * updates dependencies to the latest versions
    * removes dependencies that are already provided by other packages
    * adds the UI icons package to Renovate for automated updates
    * converts scripts to ESM
    * fixes the demo page
      * URL to the GitHub-hosted SVG resource
    * encoded characters used in icon labels (from
    https://github.com/Esri/calcite-design-system/commit/9ca1916692e309ea0efbdfa97527dacc8f9a6ea3)
    * updates icon SVGs
      * output appears smaller in size
      * uses latest [SVGO](https://github.com/svg/svgo)
    
    ---------
    
    Co-authored-by: Allie Raney <araney@esri.com>

--- packages/calcite-ui-icons/lib/spriter/cli.js
+++ packages/calcite-ui-icons/lib/spriter/cli.js
@@ -1,66 +1,52 @@
+import spriter from "./index.js";
+import { readFileSync } from "node:fs";
 "use strict";
-
-const spriter = require("./index");
-const { readFileSync } = require("fs");
 const args = process.argv.slice(2); // skip runtime & script args
-
 function hasArg(name, shorthand) {
-  return getArgIndex(name, shorthand) > -1;
+    return getArgIndex(name, shorthand) > -1;
 }
-
 function getArgIndex(name, shorthand) {
-  return args.findIndex((arg, index) => arg === `--${name}` || arg === `-${shorthand}`);
+    return args.findIndex((arg, index) => arg === `--${name}` || arg === `-${shorthand}`);
 }
-
 function getArg(name, shorthand) {
-  return args[getArgIndex(name, shorthand) + 1];
+    return args[getArgIndex(name, shorthand) + 1];
 }
-
 if (hasArg("help", "h")) {
-  process.stdout.write(`
+    process.stdout.write(`
     --input, -i       path to configuration file
     --output, -o      output location for generated SVG sprite
     --help, -h        display this help message
   `);
-
-  process.exit(0);
+    process.exit(0);
 }
-
 function getConfig() {
-  let input = getArg("input", "i");
-
-  if (input) {
-    try {
-      const configFileContents = readFileSync(input);
-      input = JSON.parse(configFileContents).input;
-    } catch (error) {
-      process.stderr.write(`config - could not read input: ${error}`);
-      process.exit(1);
+    let input = getArg("input", "i");
+    if (input) {
+        try {
+            const configFileContents = readFileSync(input);
+            input = JSON.parse(configFileContents).input;
+        }
+        catch (error) {
+            process.stderr.write(`config - could not read input: ${error}`);
+            process.exit(1);
+        }
     }
-  }
-
-  const output = getArg("output", "o");
-
-  return { input, output };
+    const output = getArg("output", "o");
+    return { input, output };
 }
-
 spriter(getConfig())
-  .then((summary) => {
+    .then((summary) => {
     const { ellapsed, spritesheets } = summary;
-
-    process.stdout.write(
-      `Success!
+    process.stdout.write(`Success!
 
 Generated ${spritesheets.length} spritesheets in ${ellapsed} ms:
 
 ${spritesheets.map((spritesheet) => `* ${spritesheet.output} (${spritesheet.icons.length} icons)`).join("\n")}
 
-`,
-    );
-
+`);
     process.exit(0);
-  })
-  .catch((err) => {
+})
+    .catch((err) => {
     process.stderr.write(`Fail! ${err}`);
     process.exit(1);
-  });
+});
