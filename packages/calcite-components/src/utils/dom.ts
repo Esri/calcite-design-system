7ab86e3511e02d9e8de93007911396385634b1e1
JC Franco <jfranco@esri.com>
2025-07-09 00:29:33 -0700

    refactor(dom): restore currentTarget in slot utils

--- packages/calcite-components/src/utils/dom.ts
+++ packages/calcite-components/src/utils/dom.ts
@@ -483,7 +483,7 @@ export function slotChangeHasAssignedElement(event: Event): boolean {
  * @returns {Element[]} An array of elements.
  */
 export function slotChangeGetAssignedElements<T extends Element>(event: Event, selector?: string): T[] | null {
-  return getSlotAssignedElements(event.target as HTMLSlotElement, selector);
+  return getSlotAssignedElements(event.currentTarget as HTMLSlotElement, selector);
 }
 
 /**
