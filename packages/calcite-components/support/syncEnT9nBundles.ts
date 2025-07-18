93e35d48a5632ddf2ff6618982013c37bdc3255f
JC Franco <jfranco@esri.com>
2025-07-18 09:29:16 -0700

    chore: restore util:sync-t9n-en-bundles script

--- packages/calcite-components/support/syncEnT9nBundles.ts
+++ packages/calcite-components/support/syncEnT9nBundles.ts
@@ -1,9 +1,7 @@
-(async function () {
-  const {
-    promises: { readFile, copyFile },
-  } = await import("fs");
-  const { resolve, sep, win32 } = await import("path");
+import { copyFile, readFile } from "node:fs/promises";
+import { join, resolve, sep, win32 } from "node:path";
 
+(async function () {
   const t9nManifestPath = "../../t9nmanifest.txt";
   const contents = await readFile(t9nManifestPath, { encoding: "utf-8" });
   const entries = contents.split("\n");
@@ -13,18 +11,19 @@
 
   for (const entry of entries) {
     const path = entry.split(win32.sep).join(sep);
-    const component = path.split(sep)[2];
+    const componentName = path.split(sep).at(-3);
+    const monorepoRoot = join(import.meta.dirname, "../../..");
 
-    const source = resolve(`${path}/messages.json`);
-    const destination = resolve(`${path}/messages.en.json`);
+    const source = resolve(monorepoRoot, `${path}/messages.json`);
+    const destination = resolve(monorepoRoot, `${path}/messages.en.json`);
 
     await copyFile(source, destination);
-    synchronized.push(component);
+    synchronized.push(componentName);
   }
 
   console.log(
-    `created messages_en.json file for the following components: \n${synchronized
+    `created messages.en.json file for the following components: \n${synchronized
       .map((synchronized) => `* ${synchronized}`)
-      .join("\n")} `,
+      .join("\n")}`,
   );
 })();
