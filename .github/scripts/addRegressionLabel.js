55ff8932170c144704f991874a0acdc6c6dcdfec
DitwanP <price.ditwan@gmail.com>
2025-07-23 09:31:45 -0700

    ci(actions): create action to add regression label

---
+++ .github/scripts/addRegressionLabel.js
@@ -0,0 +1,38 @@
+// @ts-check
+const {
+  labels: { bug },
+} = require("./support/resources");
+
+/** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+module.exports = async ({ github, context }) => {
+  const { repo, owner } = context.repo;
+
+  const payload = /** @type {import('@octokit/webhooks-types').IssuesEvent} */ (context.payload);
+  const {
+    issue: { body, number: issue_number },
+  } = payload;
+
+  if (!body) {
+    console.log("could not determine the issue body");
+    return;
+  }
+
+  const regressionRegex = /(?<=### Regression\?[\r\n|\r|\n]{2}).+$/m;
+  const regressionRegexMatch = body.match(regressionRegex);
+
+  // If issue has "_No response_" under the regression section then log and exit, otherwise add regression label.
+  if (regressionRegexMatch) {
+    const regressionVersion = (regressionRegexMatch && regressionRegexMatch[0] ? regressionRegexMatch[0] : "").trim();
+
+    if (regressionVersion === "_No response_") {
+      console.log("No regression version provided, not adding regression label.");
+    } else {
+      await github.rest.issues.addLabels({
+        issue_number,
+        owner,
+        repo,
+        labels: [bug.regression],
+      });
+    }
+  }
+};
