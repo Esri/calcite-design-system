1df31971671d416d5e4ef644580f663b074b0703
DitwanP <price.ditwan@gmail.com>
2025-07-23 10:48:59 -0700

    feedback changes

--- .github/scripts/addRegressionLabel.js
+++ .github/scripts/addRegressionLabel.js
@@ -21,18 +21,16 @@ module.exports = async ({ github, context }) => {
   const regressionRegexMatch = body.match(regressionRegex);
 
   // If issue has "_No response_" under the regression section then log and exit, otherwise add regression label.
-  if (regressionRegexMatch) {
-    const regressionVersion = (regressionRegexMatch && regressionRegexMatch[0] ? regressionRegexMatch[0] : "").trim();
+  const regressionVersion = (regressionRegexMatch?.[0] || "").trim();
 
-    if (regressionVersion === "_No response_") {
-      console.log("No regression version provided, not adding regression label.");
-    } else {
-      await github.rest.issues.addLabels({
-        issue_number,
-        owner,
-        repo,
-        labels: [bug.regression],
-      });
-    }
+  if (regressionVersion === "_No response_") {
+    console.log("No regression version provided, not adding regression label.");
+  } else {
+    await github.rest.issues.addLabels({
+      issue_number,
+      owner,
+      repo,
+      labels: [bug.regression],
+    });
   }
 };
