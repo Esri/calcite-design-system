e48277efed46153398cefdfa662f511eb88da5ea
Ditwan Price <price.ditwan@gmail.com>
2025-07-24 12:03:51 -0700

    ci(actions): fix add regression label action (#12573)
    **Related Issue:** #12381
    
    ## Summary
    Updated logic to bail if the `bug` label is not present:
    <img width="3908" height="1800" alt="image"
    src="https://github.com/user-attachments/assets/a0de222b-10f1-4afe-b201-d1784a9077f4"
    />
    
    And if the `bug` label is present but the regression section isn't for
    some reason, an additional check for an empty string on the
    `regressionVersion` should stop the addition of the `regression` label.

--- .github/scripts/addRegressionLabel.js
+++ .github/scripts/addRegressionLabel.js
@@ -9,7 +9,7 @@ module.exports = async ({ github, context }) => {
 
   const payload = /** @type {import('@octokit/webhooks-types').IssuesEvent} */ (context.payload);
   const {
-    issue: { body, number: issue_number },
+    issue: { body, labels, number: issue_number },
   } = payload;
 
   if (!body) {
@@ -17,13 +17,18 @@ module.exports = async ({ github, context }) => {
     return;
   }
 
+  // If bug label is not present, skip adding regression label.
+  if (!labels?.some((label) => label.name === "bug")) {
+    console.log("Issue does not have the 'bug' label, skipping regression label addition.");
+    return;
+  }
+
   const regressionRegex = /(?<=### Regression\?[\r\n|\r|\n]{2}).+$/m;
   const regressionRegexMatch = body.match(regressionRegex);
-
-  // If issue has "_No response_" under the regression section then log and exit, otherwise add regression label.
   const regressionVersion = (regressionRegexMatch?.[0] || "").trim();
 
-  if (regressionVersion === "_No response_") {
+  // If issue has "_No response_" under the regression section or regressionVersion is an empty string then log and exit, otherwise add regression label.
+  if (regressionVersion === "_No response_" || regressionVersion === "") {
     console.log("No regression version provided, not adding regression label.");
   } else {
     await github.rest.issues.addLabels({
