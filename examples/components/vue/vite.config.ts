eec83d58b03076a0a6ff7cdc7103c4fc3a1b6262
Isaac Braun <isaacbraun2000@gmail.com>
2025-07-28 14:45:25 -0700

    docs(examples): treat tags including `calcite-` as custom elements

--- examples/components/vue/vite.config.ts
+++ examples/components/vue/vite.config.ts
@@ -5,7 +5,16 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // Treat all tags including `calcite-` as custom elements
+          isCustomElement: (tag) => tag.includes("calcite-"),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
