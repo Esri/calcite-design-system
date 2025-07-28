78b60e7a5aa928b8fdeaea0d4339252d8bafa669
Adam Tirella <adam@tirel.la>
2025-07-28 15:27:52 -0700

    Clean up

--- packages/calcite-components/src/components/swatch/swatch.stories.ts
+++ packages/calcite-components/src/components/swatch/swatch.stories.ts
@@ -9,20 +9,8 @@ type SwatchStoryArgs = Pick<Swatch, "scale" | "selected" | "label">;
 
 export default {
   title: "Components/Swatch",
-  args: {
-    scale: scale.defaultValue,
-    selected: false,
-    label: "My great swatch",
-  },
-  argTypes: {
-    scale: {
-      options: scale.values,
-      control: { type: "select" },
-    },
-    label: {
-      control: { type: "text" },
-    },
-  },
+  args: { scale: scale.defaultValue, selected: false, label: "My great swatch" },
+  argTypes: { scale: { options: scale.values, control: { type: "select" } }, label: { control: { type: "text" } } },
 };
 
 export const simple = (args: SwatchStoryArgs): string => html`
@@ -31,28 +19,13 @@ export const simple = (args: SwatchStoryArgs): string => html`
   </div>
 `;
 
-export const withIcon = (args: SwatchStoryArgs): string => html`
-  <div style="background-color:white;padding:100px">
-    <calcite-swatch scale="m" label="${args.label}"> </calcite-swatch>
-  </div>
-`;
-
 export const withImage = (args: SwatchStoryArgs): string => html`
   <div style="background-color:white;padding:100px">
-    <calcite-swatch scale="m" label="${args.label}"> </calcite-swatch>
+    <calcite-swatch scale="m" label="${args.label}">
+      <img src="_assets/images/pattern-2.png" slot="image" />
+    </calcite-swatch>
   </div>
 `;
-
-export const withSlottedImage = (): string => {
-  return html`
-    <div style="background-color:white;padding:100px">
-      <calcite-swatch scale="m" label="color"> </calcite-swatch>
-    </div>
-  `;
-};
-
-export const overriddenIconColor = (): string =>
-  html`<calcite-swatch icon="banana" style="--calcite-icon-color: #ac9f42" label="Banana"></calcite-swatch>`;
 
 export const darkModeRTL_TestOnly = (args: SwatchStoryArgs): string => html`
   <div style="background-color:#2b2b2b;padding:100px" dir="rtl">
