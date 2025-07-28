cc2ee8236e89a477ec456895645c18c67660eff5
Adam Tirella <adam@tirel.la>
2025-07-28 15:26:25 -0700

    Clean up

--- packages/calcite-components/src/components/swatch-group/swatch-group.stories.ts
+++ packages/calcite-components/src/components/swatch-group/swatch-group.stories.ts
@@ -9,10 +9,7 @@ type SwatchGroupStoryArgs = Pick<SwatchGroup, "selectionMode" | "scale">;
 
 export default {
   title: "Components/Swatch Group",
-  args: {
-    selectionMode: selectionMode.defaultValue,
-    scale: scale.defaultValue,
-  },
+  args: { selectionMode: selectionMode.defaultValue, scale: scale.defaultValue },
   argTypes: {
     selectionMode: {
       options: selectionMode.values.filter(
@@ -20,29 +17,50 @@ export default {
       ),
       control: { type: "select" },
     },
-    scale: {
-      options: scale.values,
-      control: { type: "select" },
-    },
+    scale: { options: scale.values, control: { type: "select" } },
   },
 };
 
 export const simple = (args: SwatchGroupStoryArgs): string => html`
   <calcite-swatch-group selection-mode="${args.selectionMode}" scale="${args.scale}">
-    <calcite-swatch></calcite-swatch>
-    <calcite-swatch></calcite-swatch>
-    <calcite-swatch></calcite-swatch>
-    <calcite-swatch></calcite-swatch>
+    <calcite-swatch value="calcite swatch"></calcite-swatch>
+    <calcite-swatch color="#ff0000" value="calcite swatch"></calcite-swatch>
+    <calcite-swatch value="calcite swatch">
+      <img src="_assets/images/pattern-2.png" slot="image" />
+    </calcite-swatch>
+    <calcite-swatch color="rgba(250,210,150,0.2)" value="calcite swatch">
+      <img src="_assets/images/pattern-2.png" slot="image" />
+    </calcite-swatch>
+    <calcite-swatch value="calcite swatch">
+      <img src="_assets/images/hatch-3.png" slot="image" />
+    </calcite-swatch>
+    <calcite-swatch selected color="rgba(25,25,25,0.1)" value="calcite swatch"></calcite-swatch>
+    <calcite-swatch disabled color="#ddd" value="calcite swatch">
+      <img src="_assets/images/hatch-3.png" slot="image" />
+    </calcite-swatch>
+    <calcite-swatch disabled color="#ff00ee" value="calcite swatch"> </calcite-swatch>
   </calcite-swatch-group>
 `;
 
 export const darkThemeRTL_TestOnly = (): string => html`
   <div dir="rtl">
     <calcite-swatch-group>
-      <calcite-swatch></calcite-swatch>
-      <calcite-swatch></calcite-swatch>
-      <calcite-swatch></calcite-swatch>
-      <calcite-swatch></calcite-swatch>
+      <calcite-swatch value="calcite swatch"></calcite-swatch>
+      <calcite-swatch color="#ff0000" value="calcite swatch"></calcite-swatch>
+      <calcite-swatch value="calcite swatch">
+        <img src="_assets/images/pattern-2.png" slot="image" />
+      </calcite-swatch>
+      <calcite-swatch color="rgba(250,210,150,0.2)" value="calcite swatch">
+        <img src="_assets/images/pattern-2.png" slot="image" />
+      </calcite-swatch>
+      <calcite-swatch value="calcite swatch">
+        <img src="_assets/images/hatch-3.png" slot="image" />
+      </calcite-swatch>
+      <calcite-swatch selected color="rgba(25,25,25,0.1)" value="calcite swatch"></calcite-swatch>
+      <calcite-swatch disabled color="#ddd" value="calcite swatch">
+        <img src="_assets/images/hatch-3.png" slot="image" />
+      </calcite-swatch>
+      <calcite-swatch disabled color="#ff00ee" value="calcite swatch"> </calcite-swatch>
     </calcite-swatch-group>
   </div>
 `;
