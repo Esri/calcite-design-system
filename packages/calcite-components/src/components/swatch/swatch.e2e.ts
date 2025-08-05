6d6d3d103574f230c16469ac36d6465c14a1ac6a
Adam Tirella <adam@tirel.la>
2025-08-04 17:33:56 -0700

    feat(swatch, swatch-group): Add new components (#12584)
    **Related Issue:** #10288 
    
    ## Summary
    Adds new public Swatch and Swatch Group components: 
    <img width="276" height="102" alt="Screenshot 2025-07-30 at 12 17 59 PM"
    src="https://github.com/user-attachments/assets/2a223907-a489-4db7-8410-17fde84cfcf6"
    />
    
    
    Allows for a variety of visual combinations and interaction modes.
    
    ### Usage
    ```
    <calcite-swatch-group label="demo-group-label" selection-mode="single">
      <calcite-swatch value="calcite swatch"></calcite-swatch>
      <calcite-swatch color="#ff0000" value="calcite swatch"></calcite-swatch>
      <calcite-swatch value="calcite swatch">
        <img src="_assets/images/pattern-2.png" slot="image" />
      </calcite-swatch>
      <calcite-swatch color="rgba(250,210,150,0.2)" value="calcite swatch">
        <img src="_assets/images/pattern-2.png" slot="image" />
      </calcite-swatch>
      <calcite-swatch value="calcite swatch">
        <img src="_assets/images/hatch-3.png" slot="image" />
      </calcite-swatch>
      <calcite-swatch selected color="rgba(25,25,25,0.1)" value="calcite swatch"></calcite-swatch>
      <calcite-swatch disabled color="#ddd" value="calcite swatch">
        <img src="_assets/images/hatch-3.png" slot="image" />
      </calcite-swatch>
      <calcite-swatch disabled color="#ff00ee" value="calcite swatch"> </calcite-swatch>
    </calcite-swatch-group>
    ```
    
    
    Notes:
    - Still WIP on a couple of tests
    cc @jcfranco - this borrows the color logic and tests from existing
    Color Picker Swatch, please verify.
    cc @ashetland - we'll need a couple design review rounds - the spacing /
    size / selection behavior are a bit off.
    cc @Esri/calcite-pes - we'll need to open issues for the following
    tasks:
    - Creation of new doc pages for both components, with samples,
    accessibility behavior, and usage guidelines
    - Update Color Picker to use this new public component
    - Deprecate and eventually remove the private / undocumented Calcite
    Color Swatch component
    - Unblock any relevant issues

---
+++ packages/calcite-components/src/components/swatch/swatch.e2e.ts
@@ -0,0 +1,161 @@
+import { E2EPage, newE2EPage } from "@arcgis/lumina-compiler/puppeteerTesting";
+import { beforeEach, describe, expect, it } from "vitest";
+import { accessible, disabled, focusable, hidden, renders, slots, themed } from "../../tests/commonTests";
+import { html } from "../../../support/formatting";
+import { CSS, SLOTS } from "./resources";
+
+describe("calcite-swatch", () => {
+  describe("renders", () => {
+    renders("calcite-swatch", { display: "block" });
+  });
+
+  describe("honors hidden attribute", () => {
+    hidden("calcite-swatch");
+  });
+
+  describe("accessible", () => {
+    accessible("calcite-swatch");
+    accessible(`<calcite-swatch label="swatch" value="123" selected></calcite-swatch>`);
+    accessible(`<calcite-swatch label="swatch" value="123" color='#c0ffee'></calcite-swatch>`);
+    accessible(`<calcite-swatch label="swatch" value="123" color='#c0ffee'></calcite-swatch>`);
+    accessible(`<calcite-swatch label="swatch" value="123" selected color='#c0ffee'></calcite-swatch>`);
+  });
+
+  describe("slots", () => {
+    slots("calcite-swatch", SLOTS);
+  });
+
+  describe.skip("is focusable", () => {
+    focusable("<calcite-swatch></calcite-swatch>");
+  });
+
+  describe.skip("can be disabled", () => {
+    disabled("<calcite-swatch disabled></calcite-swatch>");
+  });
+
+  it("should not emit event after the swatch is clicked if interactive if not set", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`<calcite-swatch id="swatch-1"></calcite-swatch>`);
+
+    const eventSpy = await page.spyOnEvent("calciteSwatchSelect", "window");
+
+    const swatch1 = await page.find("#swatch-1");
+    await swatch1.click();
+    await page.waitForChanges();
+
+    expect(eventSpy).not.toHaveReceivedEvent();
+  });
+
+  it.skip("should emit event after the swatch button is clicked when interactive", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`<calcite-swatch id="swatch-1" interactive></calcite-swatch>`);
+
+    const eventSpy = await page.spyOnEvent("calciteSwatchSelect", "window");
+
+    const swatch1 = await page.find("#swatch-1");
+    await swatch1.click();
+    await page.waitForChanges();
+
+    expect(eventSpy).toHaveReceivedEvent();
+  });
+
+  it.skip("should receive focus when clicked", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`<calcite-swatch interactive id="swatch-1"></calcite-swatch>`);
+
+    const swatch1 = await page.find("#swatch-1");
+    await swatch1.click();
+    await page.waitForChanges();
+    expect(await page.evaluate(() => document.activeElement.id)).toEqual(swatch1.id);
+  });
+
+  it("renders default props when none are provided", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`<calcite-swatch>Swatch content</calcite-swatch>`);
+
+    const element = await page.find("calcite-swatch");
+    expect(element).toEqualAttribute("scale", "m");
+  });
+
+  it("renders requested props when valid props are provided", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`<calcite-swatch scale="l">Swatch content</calcite-swatch>`);
+
+    const element = await page.find("calcite-swatch");
+    expect(element).toEqualAttribute("scale", "l");
+  });
+
+  describe("accepts CSS color strings", () => {
+    let page: E2EPage;
+    const fillSwatchPartSelector = `.${CSS.swatch} rect:nth-child(4)`;
+
+    beforeEach(async () => (page = await newE2EPage()));
+
+    it("supports rgb", async () => {
+      await page.setContent("<calcite-swatch color='rgb(255, 255, 255)'></calcite-swatch>");
+      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const style = await swatch.getComputedStyle();
+
+      expect(style["fill"]).toBe("rgb(255, 255, 255)");
+    });
+
+    it("supports keywords", async () => {
+      await page.setContent("<calcite-swatch color='chartreuse'></calcite-swatch>");
+      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const style = await swatch.getComputedStyle();
+
+      expect(style["fill"]).toBe("rgb(127, 255, 0)");
+    });
+
+    it("supports hsl", async () => {
+      await page.setContent("<calcite-swatch color='hsl(120, 100%, 97%)'></calcite-swatch>");
+      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const style = await swatch.getComputedStyle();
+
+      expect(style["fill"]).toBe("rgb(240, 255, 240)");
+    });
+
+    it("supports hex", async () => {
+      await page.setContent("<calcite-swatch color='#ff8200'></calcite-swatch>");
+      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const style = await swatch.getComputedStyle();
+
+      expect(style["fill"]).toBe("rgb(255, 130, 0)");
+    });
+
+    describe("with alpha values", () => {
+      const fillSwatchPartSelector = `.${CSS.swatch} rect:nth-child(5)`;
+
+      it("supports rgba", async () => {
+        await page.setContent("<calcite-swatch color='rgba(255, 255, 255, 0.5)'></calcite-swatch>");
+        const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+        const style = await swatch.getComputedStyle();
+
+        expect(style["fill"]).toBe("rgba(255, 255, 255, 0.5)");
+      });
+
+      it("supports hsla", async () => {
+        await page.setContent("<calcite-swatch color='hsla(120, 100%, 97%, 0.5)'></calcite-swatch>");
+        const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+        const style = await swatch.getComputedStyle();
+
+        expect(style["fill"]).toBe("rgba(240, 255, 240, 0.5)");
+      });
+
+      it("supports hexa", async () => {
+        await page.setContent("<calcite-swatch color='#ff820080'></calcite-swatch>");
+        const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+        const style = await swatch.getComputedStyle();
+
+        expect(style["fill"]).toBe("rgba(255, 130, 0, 0.5)");
+      });
+    });
+  });
+  describe("themed", () => {
+    describe("default", () => {
+      themed(html`calcite-swatch`, {
+        "--calcite-swatch-corner-radius": { shadowSelector: `.${CSS.container}`, targetProp: "borderRadius" },
+      });
+    });
+  });
+});
