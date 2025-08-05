3d6c99f04212c50cbe3ff73288f0719cc75790ab
JC Franco <jfranco@esri.com>
2025-08-05 10:26:22 -0600

    chore: restore sass deprecation warning silencing (#12640)

--- packages/calcite-components/vite.config.ts
+++ packages/calcite-components/vite.config.ts
@@ -69,6 +69,7 @@ export default defineConfig({
           }
           return `@import "${globalCss}";\n${code}`;
         },
+        silenceDeprecations: ["import", "global-builtin"],
       },
     },
     postcss: {
