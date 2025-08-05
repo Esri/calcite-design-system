05de45b38a284c7e167d19e45f1829de0aab59a2
JC Franco <jfranco@esri.com>
2025-08-05 00:52:10 -0600

    use compat version of escapeRegExp

--- packages/calcite-components/src/utils/filter.ts
+++ packages/calcite-components/src/utils/filter.ts
@@ -1,5 +1,4 @@
-import { escapeRegExp } from "es-toolkit";
-import { forIn } from "es-toolkit/compat";
+import { escapeRegExp, forIn } from "es-toolkit/compat";
 
 export const filter = (data: Array<object>, value: string, filterProps?: string[]): Array<any> => {
   const escapedValue = escapeRegExp(value);
