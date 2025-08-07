b60b8c4c989d9ed1304f08a11fb26724a909586d
JC Franco <jfranco@esri.com>
2025-08-07 01:01:13 -0600

    sync en bundles if needed via precommit hook

--- packages/calcite-components/support/syncEnT9nBundles.ts
+++ packages/calcite-components/support/syncEnT9nBundles.ts
@@ -2,11 +2,23 @@ import { copyFile, readFile } from "node:fs/promises";
 import { join, resolve, sep, win32 } from "node:path";
 
 (async function () {
-  const t9nManifestPath = "../../t9nmanifest.txt";
-  const contents = await readFile(t9nManifestPath, { encoding: "utf-8" });
-  const entries = contents.split("\n");
-  const synchronized: string[] = [];
+  const args = process.argv.slice(2);
+  let entries: string[];
 
+  if (args.length > 0) {
+    entries = args
+      .filter((file) => file.endsWith("messages.json"))
+      .map((file) => {
+        const repoRelativePath = file.split(win32.sep).join(sep);
+        return repoRelativePath.replace(/\/?messages\.json$/, "");
+      });
+  } else {
+    const t9nManifestPath = "../../t9nmanifest.txt";
+    const contents = await readFile(t9nManifestPath, { encoding: "utf-8" });
+    entries = contents.split("\n").filter(Boolean);
+  }
+
+  const synchronized: string[] = [];
   console.log(`synchronizing t9n messages.json files`);
 
   for (const entry of entries) {
