22ce18c76f5c9807ba396cd73b0571e551f2f081
JC Franco <jfranco@esri.com>
2025-08-20 00:47:08 -0700

    chore: check for volta install at root level

---
+++ support/check-volta.ts
@@ -0,0 +1,21 @@
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+
+const execAsync = promisify(exec);
+
+(async function (): Promise<void> {
+  const localSetup =
+    // see https://docs.npmjs.com/cli/v8/using-npm/scripts#:~:text=Scripts%20are%20run%20from,you%20ran%20npm%20run
+    process.env.INIT_CWD !== process.cwd();
+
+  if (localSetup) {
+    return;
+  }
+
+  try {
+    await execAsync("volta --version", { encoding: "utf8" });
+  } catch {
+    console.error("Volta is not installed. Please install from https://volta.sh");
+    process.exit(1);
+  }
+})();
