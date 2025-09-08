7dbbc9112752313c3c0b48e48084c242361331a8
Adam Tirella <adam@tirel.la>
2025-09-08 15:23:59 -0700

    feat(swatch): Use label for visual title (#12841)
    **Related Issue:** #12760 
    
    ## Summary
    Adjusts the native title tooltip to reference the required `label`
    property. Also updates the component to assign unique id for internal
    svg elements for testing, rather than relying on `nth-child` selectors.

--- packages/calcite-components/src/components/swatch/swatch.e2e.ts
+++ packages/calcite-components/src/components/swatch/swatch.e2e.ts
@@ -2,7 +2,7 @@ import { E2EPage, newE2EPage } from "@arcgis/lumina-compiler/puppeteerTesting";
 import { beforeEach, describe, expect, it } from "vitest";
 import { accessible, disabled, focusable, hidden, renders, slots, themed } from "../../tests/commonTests";
 import { html } from "../../../support/formatting";
-import { CSS, SLOTS } from "./resources";
+import { CSS, IDS, SLOTS } from "./resources";
 
 describe("calcite-swatch", () => {
   describe("renders", () => {
@@ -87,13 +87,12 @@ describe("calcite-swatch", () => {
 
   describe("accepts CSS color strings", () => {
     let page: E2EPage;
-    const fillSwatchPartSelector = `.${CSS.swatch} rect:nth-child(4)`;
 
     beforeEach(async () => (page = await newE2EPage()));
 
     it("supports rgb", async () => {
       await page.setContent("<calcite-swatch color='rgb(255, 255, 255)'></calcite-swatch>");
-      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const swatch = await page.find(`calcite-swatch >>> .${CSS.swatch} #${IDS.swatchSolid}`);
       const style = await swatch.getComputedStyle();
 
       expect(style["fill"]).toBe("rgb(255, 255, 255)");
@@ -101,7 +100,7 @@ describe("calcite-swatch", () => {
 
     it("supports keywords", async () => {
       await page.setContent("<calcite-swatch color='chartreuse'></calcite-swatch>");
-      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const swatch = await page.find(`calcite-swatch >>> .${CSS.swatch} #${IDS.swatchSolid}`);
       const style = await swatch.getComputedStyle();
 
       expect(style["fill"]).toBe("rgb(127, 255, 0)");
@@ -109,7 +108,7 @@ describe("calcite-swatch", () => {
 
     it("supports hsl", async () => {
       await page.setContent("<calcite-swatch color='hsl(120, 100%, 97%)'></calcite-swatch>");
-      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const swatch = await page.find(`calcite-swatch >>> .${CSS.swatch} #${IDS.swatchSolid}`);
       const style = await swatch.getComputedStyle();
 
       expect(style["fill"]).toBe("rgb(240, 255, 240)");
@@ -117,18 +116,16 @@ describe("calcite-swatch", () => {
 
     it("supports hex", async () => {
       await page.setContent("<calcite-swatch color='#ff8200'></calcite-swatch>");
-      const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+      const swatch = await page.find(`calcite-swatch >>> .${CSS.swatch} #${IDS.swatchSolid}`);
       const style = await swatch.getComputedStyle();
 
       expect(style["fill"]).toBe("rgb(255, 130, 0)");
     });
 
     describe("with alpha values", () => {
-      const fillSwatchPartSelector = `.${CSS.swatch} rect:nth-child(5)`;
-
       it("supports rgba", async () => {
         await page.setContent("<calcite-swatch color='rgba(255, 255, 255, 0.5)'></calcite-swatch>");
-        const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+        const swatch = await page.find(`calcite-swatch >>> .${CSS.swatch} #${IDS.swatchTransparent}`);
         const style = await swatch.getComputedStyle();
 
         expect(style["fill"]).toBe("rgba(255, 255, 255, 0.5)");
@@ -136,7 +133,7 @@ describe("calcite-swatch", () => {
 
       it("supports hsla", async () => {
         await page.setContent("<calcite-swatch color='hsla(120, 100%, 97%, 0.5)'></calcite-swatch>");
-        const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+        const swatch = await page.find(`calcite-swatch >>> .${CSS.swatch} #${IDS.swatchTransparent}`);
         const style = await swatch.getComputedStyle();
 
         expect(style["fill"]).toBe("rgba(240, 255, 240, 0.5)");
@@ -144,7 +141,7 @@ describe("calcite-swatch", () => {
 
       it("supports hexa", async () => {
         await page.setContent("<calcite-swatch color='#ff820080'></calcite-swatch>");
-        const swatch = await page.find(`calcite-swatch >>> ${fillSwatchPartSelector}`);
+        const swatch = await page.find(`calcite-swatch >>> .${CSS.swatch} #${IDS.swatchTransparent}`);
         const style = await swatch.getComputedStyle();
 
         expect(style["fill"]).toBe("rgba(255, 130, 0, 0.5)");
