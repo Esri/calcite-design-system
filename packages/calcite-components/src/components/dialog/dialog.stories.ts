28c04fb08aad469303762379eb93873542e2b8be
Matt Driscoll <mdriscoll@esri.com>
2025-09-19 15:18:11 -0700

    Revert "update story"
    This reverts commit fdac95fca9454da1de733ec7ae04b903bfe2c571.

--- packages/calcite-components/src/components/dialog/dialog.stories.ts
+++ packages/calcite-components/src/components/dialog/dialog.stories.ts
@@ -74,15 +74,6 @@ export default {
   parameters: {
     chromatic: {
       delay: 1000,
-      modes: {
-        specific: {
-          viewport: {
-            width: 1200,
-            height: 400,
-          },
-        },
-      },
-      cropToViewport: true,
     },
   },
 };
