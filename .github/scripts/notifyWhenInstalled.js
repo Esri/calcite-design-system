525d4124f8cd401978955bf6f94c5cf105476aed
Isaac Braun <isaacbraun2000@gmail.com>
2025-09-23 14:28:50 -0700

    ci(actions): when installed, tag verifiers in comment instead of assigning

--- .github/scripts/notifyWhenInstalled.js
+++ .github/scripts/notifyWhenInstalled.js
@@ -1,6 +1,6 @@
 // @ts-check
 const {
-  labels: { handoff, issueWorkflow },
+  labels: { issueWorkflow },
 } = require("./support/resources");
 const { removeLabel } = require("./support/utils");
 
@@ -9,32 +9,28 @@ module.exports = async ({ github, context }) => {
   const { repo, owner } = context.repo;
 
   const payload = /** @type {import('@octokit/webhooks-types').IssuesLabeledEvent} */ (context.payload);
-  const {
-    label,
-    issue: { number },
-  } = payload;
+  const number = payload.issue.number;
 
   const { ISSUE_VERIFIERS } = process.env;
 
-  if (label?.name === issueWorkflow.installed) {
-    const issueProps = {
-      owner,
-      repo,
-      issue_number: number,
-    };
+  const issueProps = {
+    owner,
+    repo,
+    issue_number: number,
+  };
 
-    await removeLabel({ github, context, label: issueWorkflow.inDevelopment });
+  await removeLabel({ github, context, label: issueWorkflow.inDevelopment });
 
-    const assignees = ISSUE_VERIFIERS?.split(",").map((v) => v.trim());
+  // Add a "@" character to notify the user
+  const verifiers = ISSUE_VERIFIERS?.split(",").map((v) => " @" + v.trim());
 
-    await github.rest.issues.update({
-      ...issueProps,
-      assignees,
-    });
+  await github.rest.issues.update({
+    ...issueProps,
+    assignees: [],
+  });
 
-    await github.rest.issues.createComment({
-      ...issueProps,
-      body: "Installed and assigned for verification.",
-    });
-  }
+  await github.rest.issues.createComment({
+    ...issueProps,
+    body: `Installed for verification:${verifiers}`,
+  });
 };
