3efcff8c897c9c3db696914f432bd13c175dc41c
Isaac Braun <isaacbraun2000@gmail.com>
2025-09-24 14:19:18 -0700

    ci(actions): sync design tokens labels to Monday (#12954)
    **Related Issue:** #12814
    
    ## Summary
    - Add `design-tokens` and `calcite-design-tokens` labels to
    `resources.js`
    - Sync these tokens to the "Design Tokens" Monday column, allowing
    removal but only if neither of them are still present on the issue.

--- .github/scripts/support/monday.js
+++ .github/scripts/support/monday.js
@@ -2,6 +2,7 @@
 const {
   labels: { issueWorkflow, issueType, priority, devEstimate, designEstimate, planning, handoff },
   milestone,
+  packages,
 } = require("./resources");
 const { notReadyForDev, notInLifecycle } = require("./utils");
 
@@ -52,6 +53,7 @@ module.exports = function Monday(issue) {
     blocked: "color_mkv7x1gw",
     a11y: "color_mksw1sfa",
     spike: "color_mkrt20dy",
+    designTokens: "color_mkvyhk10",
     figmaChanges: "color_mkrvmhg7",
     open: "color_mknkrb2n",
   };
@@ -146,6 +148,13 @@ module.exports = function Monday(issue) {
       {
         column: columnIds.designIssue,
         value: "Design",
+      },
+    ],
+    [
+      issueType.designTokens,
+      {
+        column: columnIds.designTokens,
+        value: "Design Tokens",
       },
     ],
     [
@@ -328,6 +337,13 @@ module.exports = function Monday(issue) {
       {
         column: columnIds.stalled,
         value: "Stalled",
+      },
+    ],
+    [
+      packages.tokens,
+      {
+        column: columnIds.designTokens,
+        value: "Design Tokens",
       },
     ],
   ]);
