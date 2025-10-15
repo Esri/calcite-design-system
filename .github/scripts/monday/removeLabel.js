3ba355d306211c18d91c5f3ab299ddd1aa43b1b5
Isaac Braun <isaacbraun2000@gmail.com>
2025-10-15 12:01:43 -0700

    ci(actions): fix skip label removal logic

--- .github/scripts/monday/removeLabel.js
+++ .github/scripts/monday/removeLabel.js
@@ -1,6 +1,6 @@
 // @ts-check
 const Monday = require("../support/monday");
-const { assertRequired, notInLifecycle } = require("../support/utils");
+const { assertRequired, notInLifecycle, includesLabel } = require("../support/utils");
 const {
   labels: {
     planning: { spike, spikeComplete },
@@ -16,14 +16,18 @@ module.exports = async ({ context }) => {
   const { labels: issueLabels, assignee } = issue;
   const [labelName] = assertRequired([label?.name]);
 
-  if (labelName === spike && issueLabels) {
-    const isSpikeComplete = issueLabels.some((label) => label.name === spikeComplete);
-    assertRequired([isSpikeComplete], "Issue is marked as a spike complete. Skipping label removal.");
+  if (labelName === spike && issueLabels && includesLabel(issueLabels, spikeComplete)) {
+    console.log("Issue is marked as a spike complete. Skipping label removal.");
+    process.exit(0);
   }
 
-  if ([designTokens, tokensPackage].includes(labelName) && issueLabels) {
-    const areTokensStillPresent = issueLabels.some((label) => label.name === designTokens);
-    assertRequired([areTokensStillPresent], "Issue is still marked as a design token issue. Skipping label removal.");
+  const tokensLabels = [designTokens, tokensPackage];
+  if (tokensLabels.includes(labelName) && issueLabels) {
+    const remainingLabel = tokensLabels.find((label) => label !== labelName);
+    if (issueLabels.some((label) => label.name === remainingLabel)) {
+      console.error("Issue is still marked as a design token issue. Skipping label removal.");
+      process.exit(0);
+    }
   }
 
   const monday = Monday(issue);
