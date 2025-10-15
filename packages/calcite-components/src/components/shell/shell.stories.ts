9a779368620158b277e943b26344e3e161724194
JC Franco <jfranco@esri.com>
2025-10-15 10:59:24 -0700

    adjust size of svg replacement

--- packages/calcite-components/src/components/shell/shell.stories.ts
+++ packages/calcite-components/src/components/shell/shell.stories.ts
@@ -2541,7 +2541,12 @@ export const customPanelWithOverflowingContent = (): string =>
     overflow-y: hidden;"
             >
               <div style="width: 100%; height: 100%; position: static; left: 0.195313px; top: -0.5px;">
-                <img alt="placeholder image" src="${placeholderImage({ width: 700, height: 400 })}" width="100%" />
+                <img
+                  alt="placeholder image"
+                  src="${placeholderImage({ width: 631, height: 297 })}"
+                  width="100%"
+                  height="100%"
+                />
               </div>
             </div>
           </div>
