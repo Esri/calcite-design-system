dae908ef69a150b29485baa7dd471364d4203b16
JC Franco <jfranco@esri.com>
2025-10-17 13:53:28 -0700

    drop tip-manager from shell stories

--- packages/calcite-components/src/components/shell/shell.stories.ts
+++ packages/calcite-components/src/components/shell/shell.stories.ts
@@ -167,52 +167,6 @@ const contentHTML = html(`
   ></div>
 `);
 
-const centerPanelAdvancedHTML = html(`
-  <calcite-tip-manager slot="panel-bottom">
-    <calcite-tip-group group-title="Astronomy">
-      <calcite-tip heading="The Red Rocks and Blue Water">
-        <img slot="thumbnail" src="${placeholderImage({ width: 1000, height: 600 })}" alt="This is an image." />
-        <p>
-          This tip is how a tip should really look. It has a landscape or square image and a small amount of text
-          content. This paragraph is in an "info" slot.
-        </p>
-        <p>
-          This is another paragraph in a subsequent "info" slot. In publishing and graphic design, Lorem ipsum is a
-          placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful
-          content (also called greeking). Replacing the actual content with placeholder text allows designers to design
-          the form of the content before the content itself has been produced.
-        </p>
-        <a href="http://www.esri.com">This is the "link" slot.</a>
-      </calcite-tip>
-      <calcite-tip heading="The Long Trees">
-        <img slot="thumbnail" src="${placeholderImage({ width: 1000, height: 600 })}" alt="This is an image." />
-        <p>This tip has an image that is a pretty tall. And the text will run out before the end of the image.</p>
-        <p>In astronomy, the terms object and body are often used interchangeably.</p>
-        <a href="http://www.esri.com">View Esri</a>
-      </calcite-tip>
-    </calcite-tip-group>
-    <calcite-tip heading="Square Nature">
-      <img slot="thumbnail" src="${placeholderImage({ width: 1000, height: 1000 })}" alt="This is an image." />
-      <p>This tip has an image that is square. And the text will run out before the end of the image.</p>
-      <p>In astronomy, the terms object and body are often used interchangeably.</p>
-      <p>
-        In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form
-        of a document without relying on meaningful content (also called greeking). Replacing the actual content with
-        placeholder text allows designers to design the form of the content before the content itself has been produced.
-      </p>
-      <a href="http://www.esri.com">View Esri</a>
-    </calcite-tip>
-    <calcite-tip heading="The lack of imagery">
-      <p>This tip has no image. As such, the content area will take up the entire width of the tip.</p>
-      <p>
-        This is the next paragraph and should show how wide the content area is now. Of course, the width of the overall
-        tip will affect things. In astronomy, the terms object and body are often used interchangeably.
-      </p>
-      <a href="http://www.esri.com">View Esri</a>
-    </calcite-tip>
-  </calcite-tip-manager>
-`);
-
 const advancedLeadingPanelHTML = html(`
   ${actionBarStartHTML}
   <calcite-panel heading="Advanced panel example">
@@ -331,7 +285,6 @@ export const simple = (args: ShellStoryArgs): string => html`
     >
       ${centerPanelHTML}
     </calcite-shell-panel>
-    ${centerPanelAdvancedHTML}
     <calcite-shell-panel
       slot="panel-end"
       ${boolean("collapsed", args.collapsed)}
@@ -350,7 +303,7 @@ export const darkModeRTL_TestOnly = (): string => html`
     <calcite-shell-panel slot="panel-start" display-mode="dock"> ${advancedLeadingPanelHTML} </calcite-shell-panel>
     ${contentHTML}
     <calcite-shell-panel height-scale="s" slot="panel-bottom"> ${centerPanelHTML} </calcite-shell-panel>
-    ${contentHTML} ${centerPanelAdvancedHTML}
+    ${contentHTML}
     <calcite-shell-panel slot="panel-end" display-mode="dock"> ${advancedTrailingPanelHTMl} </calcite-shell-panel>
     ${footerHTML}
   </calcite-shell>
