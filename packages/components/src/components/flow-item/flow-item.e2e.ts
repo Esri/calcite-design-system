6c1f3605c37a26104d0ea8000dac38d7df392819
JC Franco <jfranco@esri.com>
2025-11-04 01:32:13 -0800

    fix test

--- packages/components/src/components/flow-item/flow-item.e2e.ts
+++ packages/components/src/components/flow-item/flow-item.e2e.ts
@@ -197,8 +197,8 @@ describe("calcite-flow-item", () => {
           <div slot="${SLOTS.headerContent}">test content</div>
           <div slot="${SLOTS.headerActionsEnd}">test end</div>
           <p>Content</p>
-          <calcite-button slot="${SLOTS.footerActions}">test button 1</calcite-button>
-          <calcite-button slot="${SLOTS.footerActions}">test button 2</calcite-button>
+          <calcite-button slot="${SLOTS.footerStart}">test button 1</calcite-button>
+          <calcite-button slot="${SLOTS.footerEnd}">test button 2</calcite-button>
         </calcite-flow-item>
       `);
     });
