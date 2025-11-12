59e1fee2ff2dcb93d191aaef1f927a5d40d6a69e
Erik Harper <eharperdicianno@esri.com>
2025-11-12 11:15:44 -0800

    chore: use system color scheme for demo HTML page, clean up unused code (#13302)
    **Related Issue:** #13269 
    
    ## Summary
    
    This follow-up PR adds `prefers-color-scheme` support to the demo
    index.html file and cleans up some unused code that was loading the demo
    header, which we're no longer using.

--- packages/components/src/demos/_assets/head.ts
+++ packages/components/src/demos/_assets/head.ts
@@ -1,8 +1,6 @@
 ((): void => {
   const ASSETS_PATH = "/src/demos/_assets";
   const CSS = [`${ASSETS_PATH}/demos.css`];
-  const urlParams = new URLSearchParams(window.location.search);
-  const DISABLE_HEADER_URL_PARAM = "header-disabled";
 
   interface Script {
     src: string;
@@ -18,32 +16,6 @@
       type: "module",
     },
   ];
-
-  const parseTemplate = (text: string): HTMLTemplateElement | null => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(text, "text/html");
-    return doc.head.querySelector("template");
-  };
-
-  const loadHeader = async (): Promise<void> => {
-    const response = await window.fetch(`${ROOT}${ASSETS_PATH}/demo-template.html`);
-    const text = await response.text();
-    const template = parseTemplate(text);
-    if (template) {
-      const firstChild = document.body.firstChild;
-      if (firstChild) {
-        document.body.insertBefore(template.content, firstChild);
-      }
-    }
-  };
-
-  if (window.location.pathname.includes("/src/demos/") && !urlParams.has(DISABLE_HEADER_URL_PARAM)) {
-    if (document.readyState === "loading") {
-      document.addEventListener("DOMContentLoaded", loadHeader);
-    } else {
-      loadHeader();
-    }
-  }
 
   const ROOT = "";
 
