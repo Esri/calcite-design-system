d27d256922e5e369707fba6d9f26978e10521da3
Isaac Braun <isaacbraun2000@gmail.com>
2025-11-13 13:54:18 -0800

    ci(actions): use `filter` instead of `if`

--- .github/scripts/support/monday.js
+++ .github/scripts/support/monday.js
@@ -464,7 +464,10 @@ module.exports = function Monday(issue, core) {
         label.name !== issueWorkflow.installed &&
         label.name !== issueWorkflow.verified,
     );
-    if (role.id === mondayColumns.productEngineers.id && notInstalledOrVerified) {
+    if (
+      role.id === mondayColumns.productEngineers.id &&
+      notInstalledOrVerified
+    ) {
       role = mondayColumns.developers;
     }
 
@@ -929,13 +932,11 @@ module.exports = function Monday(issue, core) {
       addAssignee(assignee);
     });
 
-    [mondayColumns.developers, mondayColumns.productEngineers].forEach(
-      (role) => {
-        if (!(role.id in columnUpdates)) {
-          setColumnValue(role, "");
-        }
-      },
-    );
+    [mondayColumns.developers, mondayColumns.productEngineers]
+      .filter((role) => !(role.id in columnUpdates))
+      .forEach((role) => {
+        setColumnValue(role, "");
+      });
   }
 
   /**
