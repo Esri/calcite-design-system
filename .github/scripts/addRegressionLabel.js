b421c832900588ec7da0c2940d7e4f9e9917936c
Isaac Braun <isaacbraun2000@gmail.com>
2025-11-17 13:11:45 -0800

    fix: align regexes/patterns and remove the old `regression` label adding logic

--- .github/scripts/addRegressionLabel.js
+++ .github/scripts/addRegressionLabel.js
@@ -4,8 +4,9 @@ const {
 } = require("./support/resources");
 
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
-module.exports = async ({ github, context }) => {
+module.exports = async ({ github, context, core }) => {
   const { repo, owner } = context.repo;
+  const logParams = { title: "Add Regression Label" };
 
   const payload = /** @type {import('@octokit/webhooks-types').IssuesEvent} */ (context.payload);
   const {
@@ -13,43 +14,24 @@ module.exports = async ({ github, context }) => {
   } = payload;
 
   if (!body) {
-    console.log("could not determine the issue body");
+    core.notice("Could not determine the issue body", logParams);
     return;
   }
 
   // If bug label is not present, skip adding regression label.
   if (!labels?.some((label) => label.name === "bug")) {
-    console.log("Issue does not have the 'bug' label, skipping regression label addition.");
+    core.notice("Issue does not have the 'bug' label, skipping regression label addition.", logParams);
     return;
   }
 
-  const regressionRegex = /(?<=### Regression\?[\r\n|\r|\n]{2}).+$/m;
+  const regressionRegex = /### Regression\?(?:\r|\n)+(.+)$/m;
   const regressionRegexMatch = body.match(regressionRegex);
-  const regressionVersionResponse = (regressionRegexMatch?.[0] || "").trim();
+  const regressionVersionResponse = (regressionRegexMatch?.[1] || "").trim();
 
   // If issue has "_No response_" under the regression section or regressionVersion is an empty string then log and exit, otherwise add regression label.
   if (regressionVersionResponse === "_No response_" || regressionVersionResponse === "") {
-    console.log("No regression version provided, not adding regression label.");
-  } else {
-    await github.rest.issues.addLabels({
-      issue_number,
-      owner,
-      repo,
-      labels: [bug.regression],
-    });
-
-    await github.rest.actions.createWorkflowDispatch({
-      owner,
-      repo,
-      workflow_id: "issue-monday-sync.yml",
-      ref: "dev",
-      inputs: {
-        issue_number: issue_number.toString(),
-        event_type: "SyncActionChanges",
-        label_name: bug.regression,
-        label_action: "added",
-      },
-    });
+    core.notice("No regression version provided, not adding regression label.", logParams);
+    return;
   }
 
   // Match x.y.z with an optional prerelease extension of "-next.N".
@@ -58,7 +40,7 @@ module.exports = async ({ github, context }) => {
 
   const containsValidVersion = semVerRegex.test(regressionVersionResponse);
   if (!containsValidVersion) {
-    console.log("No valid version (e.g., 1.0.0 or 1.0.0-next.13) found, not adding regression label.");
+    core.notice("No valid version (e.g., 1.0.0 or 1.0.0-next.13) found, not adding regression label.", logParams);
     return;
   }
 
@@ -68,5 +50,18 @@ module.exports = async ({ github, context }) => {
     owner,
     repo,
     labels: [bug.regression],
+  });
+
+  await github.rest.actions.createWorkflowDispatch({
+    owner,
+    repo,
+    workflow_id: "issue-monday-sync.yml",
+    ref: "dev",
+    inputs: {
+      issue_number: issue_number.toString(),
+      event_type: "SyncActionChanges",
+      label_name: bug.regression,
+      label_action: "added",
+    },
   });
 };
