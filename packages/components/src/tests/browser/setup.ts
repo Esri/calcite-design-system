9e330f3f683c0fb4637a74f7a06733882675b40c
JC Franco <jfranco@esri.com>
2025-11-19 09:56:17 -0800

    tidy up

--- packages/components/src/tests/browser/setup.ts
+++ packages/components/src/tests/browser/setup.ts
@@ -1,4 +1,4 @@
-import { beforeAll } from "vitest";
+import { beforeAll, beforeEach, afterEach, vi } from "vitest";
 import { css } from "../../../support/formatting";
 
 beforeAll(() => {
@@ -9,4 +9,31 @@ beforeAll(() => {
     }
   `;
   document.head.append(style);
+});
+
+beforeEach(() => {
+  // patching Intl to work around bug where `en-US@posix` is set as the locale in the CI environment
+  // this might be resolved by https://github.com/Esri/calcite-design-system/issues/12739
+  const originalDateTimeFormat = Intl.DateTimeFormat;
+  const originalNumberFormat = Intl.NumberFormat;
+
+  vi.spyOn(Intl, "DateTimeFormat").mockImplementation((locale, options?) => {
+    if (locale === "en-US@posix") {
+      locale = "en-US";
+    }
+
+    return originalDateTimeFormat(locale, options);
+  });
+
+  vi.spyOn(Intl, "NumberFormat").mockImplementation((locale, options) => {
+    if (locale === "en-US@posix") {
+      locale = "en-US";
+    }
+
+    return originalNumberFormat(locale, options);
+  });
+});
+
+afterEach(() => {
+  vi.restoreAllMocks();
 });
