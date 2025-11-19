e255ead1c1334ee7eb3a4cc64d71127717d8b774
JC Franco <jfranco@esri.com>
2025-11-18 16:33:07 -0800

    patch Intl in browser mode test setup to workaround issue where `en` resolves as `en-US@POSIX` in the CI environment

--- packages/components/src/components/time-picker/time-picker.browser.e2e.tsx
+++ packages/components/src/components/time-picker/time-picker.browser.e2e.tsx
@@ -1,10 +1,37 @@
-import { describe } from "vitest";
+import { describe, beforeEach, afterEach, vi } from "vitest";
 import { mount } from "@arcgis/lumina-compiler/testing";
 import { defaults } from "../../tests/commonTests/browser";
 import { mockConsole } from "../../tests/utils/logging";
 
 describe("calcite-time-picker", () => {
   mockConsole();
+
+  beforeEach(() => {
+    // patching Intl to work around bug where `en-US@posix` is set as the locale in the CI environment
+    // this might be resolved by https://github.com/Esri/calcite-design-system/issues/12739
+    const originalDateTimeFormat = Intl.DateTimeFormat;
+    const originalNumberFormat = Intl.NumberFormat;
+
+    vi.spyOn(Intl, "DateTimeFormat").mockImplementation((locale, options?) => {
+      if (locale === "en-US@posix") {
+        locale = "en-US";
+      }
+
+      return new originalDateTimeFormat(locale, options);
+    });
+
+    vi.spyOn(Intl, "NumberFormat").mockImplementation((locale, options) => {
+      if (locale === "en-US@posix") {
+        locale = "en-US";
+      }
+
+      return new originalNumberFormat(locale, options);
+    });
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
 
   describe("defaults", () => {
     defaults(
