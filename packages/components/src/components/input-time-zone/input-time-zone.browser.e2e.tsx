e641cd203a300d9ec2ff20d2845bc410e6b34705
JC Franco <jfranco@esri.com>
2025-11-19 16:36:00 -0800

    test(input-time-zone): restore migrated tests

--- packages/components/src/components/input-time-zone/input-time-zone.browser.e2e.tsx
+++ packages/components/src/components/input-time-zone/input-time-zone.browser.e2e.tsx
@@ -1,30 +1,42 @@
 import { describe } from "vitest";
+import { mount } from "@arcgis/lumina-compiler/testing";
+import { defaults, reflects } from "../../tests/commonTests/browser";
+import { mockConsole } from "../../tests/utils/logging";
 
 describe("calcite-input-time-zone", () => {
-  // describe("defaults", () => {
-  //   defaults(() => mount(simpleTestProvider), [
-  //     { propertyName: "disabled", defaultValue: false },
-  //     { propertyName: "maxItems", defaultValue: 0 },
-  //     { propertyName: "messageOverrides", defaultValue: undefined },
-  //     { propertyName: "mode", defaultValue: "offset" },
-  //     { propertyName: "open", defaultValue: false },
-  //     { propertyName: "overlayPositioning", defaultValue: "absolute" },
-  //     { propertyName: "scale", defaultValue: "m" },
-  //     { propertyName: "status", defaultValue: "idle" },
-  //     { propertyName: "validationIcon", defaultValue: undefined },
-  //     { propertyName: "validationMessage", defaultValue: undefined },
-  //   ]);
-  // });
-  // describe("reflects", () => {
-  //   reflects(() => mount(simpleTestProvider), [
-  //     { propertyName: "disabled", value: true },
-  //     { propertyName: "maxItems", value: 0 },
-  //     { propertyName: "mode", value: "offset" },
-  //     { propertyName: "open", value: true },
-  //     { propertyName: "scale", value: "m" },
-  //     { propertyName: "overlayPositioning", value: "absolute" },
-  //     { propertyName: "status", value: "invalid" },
-  //     { propertyName: "validationIcon", value: true },
-  //   ]);
-  // });
+  mockConsole();
+
+  describe("defaults", () => {
+    defaults(
+      () => mount("calcite-input-time-zone"),
+      [
+        { propertyName: "disabled", defaultValue: false },
+        { propertyName: "maxItems", defaultValue: 0 },
+        { propertyName: "messageOverrides", defaultValue: undefined },
+        { propertyName: "mode", defaultValue: "offset" },
+        { propertyName: "open", defaultValue: false },
+        { propertyName: "overlayPositioning", defaultValue: "absolute" },
+        { propertyName: "scale", defaultValue: "m" },
+        { propertyName: "status", defaultValue: "idle" },
+        { propertyName: "validationIcon", defaultValue: undefined },
+        { propertyName: "validationMessage", defaultValue: undefined },
+      ],
+    );
+  });
+
+  describe("reflects", () => {
+    reflects(
+      () => mount("calcite-input-time-zone"),
+      [
+        { propertyName: "disabled", value: true },
+        { propertyName: "maxItems", value: 0 },
+        { propertyName: "mode", value: "offset" },
+        { propertyName: "open", value: true },
+        { propertyName: "scale", value: "m" },
+        { propertyName: "overlayPositioning", value: "absolute" },
+        { propertyName: "status", value: "invalid" },
+        { propertyName: "validationIcon", value: true },
+      ],
+    );
+  });
 });
