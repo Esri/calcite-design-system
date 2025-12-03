07cccecd2206cb91e1a740f312df6fbbb5c096a3
Eliza Khachatryan <eli97736@esri.com>
2025-12-03 09:28:27 -0800

    swap physical with logical props

--- packages/components/src/controllers/useSizeOverride.browser.spec.tsx
+++ packages/components/src/controllers/useSizeOverride.browser.spec.tsx
@@ -28,44 +28,44 @@ describe("useSizeOverride", async () => {
 
   it("applies clamped size within min/max", () => {
     el.controller.resize({ inline: 200 });
-    expect(el.ref.value!.style.width).toBe("200px");
+    expect(el.ref.value!.style.inlineSize).toBe("200px");
     expect(el.resizeValues.inlineSize).toBe(200);
   });
 
   it("clamps size below min", () => {
     el.controller.resize({ inline: 50 });
-    expect(el.ref.value!.style.width).toBe("100px");
+    expect(el.ref.value!.style.inlineSize).toBe("100px");
     expect(el.resizeValues.inlineSize).toBe(100);
   });
 
   it("clamps size above max", () => {
     el.controller.resize({ inline: 600 });
-    expect(el.ref.value!.style.width).toBe("500px");
+    expect(el.ref.value!.style.inlineSize).toBe("500px");
     expect(el.resizeValues.inlineSize).toBe(500);
   });
 
   it("clears override when size is null", () => {
     el.controller.resize({ inline: null });
-    expect(el.ref.value!.style.width).toBe("");
+    expect(el.ref.value!.style.inlineSize).toBe("");
     expect(el.resizeValues.inlineSize).toBeNull();
   });
 
   it("applies block axis", () => {
     el.controller.resize({ block: 300 });
-    expect(el.ref.value!.style.height).toBe("300px");
+    expect(el.ref.value!.style.blockSize).toBe("300px");
     expect(el.resizeValues.blockSize).toBe(300);
   });
 
   it("applies inline axis", () => {
     el.controller.resize({ inline: 250 });
-    expect(el.ref.value!.style.width).toBe("250px");
+    expect(el.ref.value!.style.inlineSize).toBe("250px");
     expect(el.resizeValues.inlineSize).toBe(250);
   });
 
   it("returns applied value and updates style", () => {
     const applied = el.controller.resize({ inline: 400 });
     expect(applied).toEqual({ inline: 400 });
-    expect(el.ref.value!.style.width).toBe("400px");
+    expect(el.ref.value!.style.inlineSize).toBe("400px");
     expect(el.resizeValues.inlineSize).toBe(400);
   });
 });
