18ae1b4ad6cdb5527764d2f0bcddcf955dce16b2
Erik Harper <eriklharper@pm.me>
2025-12-11 16:39:25 -0800

    fixing action background color hover themed test to allow the color to pass in

--- packages/components/src/components/action/action.e2e.ts
+++ packages/components/src/components/action/action.e2e.ts
@@ -16,7 +16,6 @@ describe("calcite-action", () => {
       "--calcite-action-background-color-hover": {
         shadowSelector: `.${CSS.button}`,
         targetProp: "backgroundColor",
-        expectedValue: "rgba(0, 0, 0, 0.04)",
         state: "hover",
       },
       "--calcite-action-background-color-pressed": {
