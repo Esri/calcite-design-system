fefaf405f33b70f5fec0447516e27adf39f10bea
JC Franco <jfranco@esri.com>
2025-12-31 01:08:19 -0800

    improve test coverage

--- packages/components/src/controllers/useTopLayer.browser.spec.tsx
+++ packages/components/src/controllers/useTopLayer.browser.spec.tsx
@@ -153,6 +153,7 @@ describe("useTopLayer", () => {
     component.topLayerDisabled = true;
 
     await component.topLayer.show();
+
     expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
 
     await component.topLayer.hide();
@@ -166,6 +167,87 @@ describe("useTopLayer", () => {
     expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
 
     await component.topLayer.hide();
+
+    expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
+  });
+
+  it("removes target from top layer when disabled while shown", async () => {
+    class Test extends LitElement {
+      @property() topLayerDisabled = false;
+
+      overlayRef = createRef<HTMLDivElement>();
+
+      someInternalProp = false;
+
+      topLayer = useTopLayer({
+        disabledOverride: () => {
+          return this.someInternalProp;
+        },
+        target: this.overlayRef,
+      })(this);
+
+      render(): JsxNode {
+        return (
+          <div>
+            <div popover="manual" ref={this.overlayRef}>
+              overlay
+            </div>
+          </div>
+        );
+      }
+    }
+
+    const { el, component } = await mount(Test);
+
+    await component.topLayer.show();
+
+    expect(isInTopLayer(component.overlayRef.value!)).toBe(true);
+
+    el.topLayerDisabled = true;
+
+    await component.topLayer.show();
+
+    expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
+
+    el.topLayerDisabled = false;
+
+    await component.topLayer.show();
+
+    expect(isInTopLayer(component.overlayRef.value!)).toBe(true);
+
+    component.someInternalProp = true;
+
+    await component.topLayer.show();
+
+    expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
+  });
+
+  it("does not toggle popover API if target element is missing popover attribute", async () => {
+    class Test extends LitElement {
+      overlayRef = createRef<HTMLDivElement>();
+
+      topLayer = useTopLayer({
+        target: this.overlayRef,
+      })(this);
+
+      render(): JsxNode {
+        return (
+          <div>
+            <div ref={this.overlayRef}>overlay</div>
+          </div>
+        );
+      }
+    }
+
+    const { component } = await mount(Test);
+
+    expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
+
+    await expect(component.topLayer.show()).resolves.toBeUndefined();
+
+    expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
+
+    await expect(component.topLayer.hide()).resolves.toBeUndefined();
 
     expect(isInTopLayer(component.overlayRef.value!)).toBe(false);
   });
