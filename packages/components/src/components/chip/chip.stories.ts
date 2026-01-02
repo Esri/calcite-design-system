b3dc465fca59b9730c023f4835f532d9d765f5b0
Brendan Rice <hey@brendanrice.com>
2026-01-02 15:37:36 -0500

    no need for Storybook story for deprecated variable

--- packages/components/src/components/chip/chip.stories.ts
+++ packages/components/src/components/chip/chip.stories.ts
@@ -179,9 +179,6 @@ export const withAvatarAndIconAndClosable = (): string => {
 export const overriddenIconColor = (): string =>
   html`<calcite-chip icon="banana" style="--calcite-icon-color: #ac9f42" label="Banana" closable>Banana</calcite-chip>`;
 
-export const overriddenDeprecatedIconColor = (): string =>
-  html`<calcite-chip icon="banana" style="--calcite-ui-icon-color: blue" label="Banana" closable>Banana</calcite-chip>`;
-
 export const darkModeRTL_TestOnly = (args: ChipStoryArgs): string => html`
   <div style="background-color:#2b2b2b;padding:100px" dir="rtl">
     <calcite-chip class="calcite-mode-dark" label="${args.label}">My great chip</calcite-chip>
