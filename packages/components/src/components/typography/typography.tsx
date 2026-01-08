e18686e218a9f357c2abdfbe75c6a12055dd1378
Anveshreddy mekala <amekala@esri.com>
2026-01-08 14:57:36 -0600

    add events

--- packages/components/src/components/typography/typography.tsx
+++ packages/components/src/components/typography/typography.tsx
@@ -1,5 +1,6 @@
-import { LitElement, h, property, type JsxNode } from "@arcgis/lumina";
+import { LitElement, createEvent, h, property, type JsxNode } from "@arcgis/lumina";
 import { slotChangeGetTextContent } from "../../utils/dom";
+import { createObserver } from "../../utils/observers";
 import { styles } from "./typography.scss";
 
 declare global {
@@ -18,10 +19,34 @@ export class Typography extends LitElement {
 
   //#region Private Properties
 
-  private handleDefaultSlot = (event: Event): void => {
-    const text = slotChangeGetTextContent(event);
-    console.log(text);
-  };
+  private value: string;
+
+  private isTruncated = false;
+
+  //# region Private Methods
+  private resizeObserver = createObserver("resize", (): void => {
+    // if (this.tooltipEnabled) {
+    // const { fontSize, fontFamily } = getComputedStyle(this.el);
+    // const inputTextWidth = getTextWidth(this.defaultText, `${fontSize} ${fontFamily}`);
+
+    const isOverflowing =
+      this.el.scrollWidth > this.el.clientWidth || this.el.scrollHeight > this.el.clientHeight;
+    // const isOverflowing = inputTextWidth > this.el.getBoundingClientRect().width;
+    if (isOverflowing) {
+      console.log(
+        "resize observed & overflowing",
+        // inputTextWidth,
+        this.el.getBoundingClientRect().width,
+      );
+      this.calciteInternalTypographyTruncated.emit();
+    } else {
+      if (this.isTruncated) {
+        this.isTruncated = false;
+        this.calciteTypographyUnTruncated.emit();
+      }
+    }
+    // }
+  });
 
   //#endregion
 
@@ -35,23 +60,69 @@ export class Typography extends LitElement {
 
   @property({ reflect: true }) truncatePosition: "start" | "middle" | "end" = "end";
 
+  @property() tooltipEnabled = false;
+
+  /**
+   * @private
+   */
+  @property({ reflect: true }) title: string;
+
+  //#endregion
+
+  //#region Events
+
+  calciteInternalTypographyTruncated = createEvent({ cancelable: false });
+
+  calciteTypographyTruncated = createEvent({ cancelable: false });
+
+  calciteTypographyUnTruncated = createEvent({ cancelable: false });
+
+  //# endregion
+
   //#endregion
 
   //#region Lifecycle
 
+  constructor() {
+    super();
+    this.listen("calciteInternalTypographyTruncated", this.handleTruncated);
+  }
+
+  override connectedCallback(): void {
+    if (this.tooltipEnabled) {
+      this.resizeObserver?.observe(this.el);
+    }
+  }
+
   async loaded(): Promise<void> {
     this.el.style.setProperty("--calcite-internal-text-max-lines", this.maxLines?.toString());
+  }
+
+  override disconnectedCallback(): void {
+    this.resizeObserver?.disconnect();
+  }
+
+  //#endregion
+
+  //#region Private Methods
+
+  private handleTruncated(): void {
+    if (!this.isTruncated) {
+      this.isTruncated = true;
+      this.calciteTypographyTruncated.emit();
+    }
+  }
+
+  private handleDefaultSlotChange(event: Event): void {
+    this.value = slotChangeGetTextContent(event);
   }
 
   //#endregion
 
   //#region Rendering
 
-  //   override createRenderRoot(): DocumentFragment | HTMLElement {
-  //     return this.el;
-  //   }
   override render(): JsxNode {
-    return <slot onSlotChange={this.handleDefaultSlot} />;
+    return <slot onSlotChange={this.handleDefaultSlotChange} />;
   }
 
   //#endregion
