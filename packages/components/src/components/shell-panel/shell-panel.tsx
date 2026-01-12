83e9da8fcb956b93eb57aca9e30e9c4aefd7dde9
Mel Dintaman <melissagrillot@gmail.com>
2026-01-12 16:38:02 -0600

    docs: add missing updateSize method description

--- packages/components/src/components/shell-panel/shell-panel.tsx
+++ packages/components/src/components/shell-panel/shell-panel.tsx
@@ -146,7 +146,11 @@ export class ShellPanel extends LitElement {
   //#endregion
 
   //#region Public Methods
-
+  /**
+   * Updates the component's size by setting its inline and/or block dimensions.
+   *
+   * @param size - An object specifying the new inline and/or block size values.
+   */
   @method()
   async updateSize(size: { inline?: number | null; block?: number | null }): Promise<void> {
     this.updateSizeInternal(size);
