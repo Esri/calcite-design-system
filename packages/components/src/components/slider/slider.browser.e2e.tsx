9cd09c95b3c5d2812fefc9f0fb548326ec954bd6
Anveshreddy mekala <amekala@esri.com>
2026-01-26 15:48:13 -0600

    add browser tests

--- packages/components/src/components/slider/slider.browser.e2e.tsx
+++ packages/components/src/components/slider/slider.browser.e2e.tsx
@@ -1,5 +1,7 @@
-import { describe } from "vitest";
+import { describe, it, expect } from "vitest";
+import { h, ToElement } from "@arcgis/lumina";
 import { mount } from "@arcgis/lumina-compiler/testing";
+import { commands } from "vitest/browser";
 import {
   defaults,
   reflects,
@@ -9,6 +11,7 @@ import {
   t9n,
   disabled,
 } from "../../tests/commonTests/browser";
+import { Slider } from "./slider";
 
 describe("calcite-slider", () => {
   describe("defaults", () => {
@@ -108,5 +111,59 @@ describe("calcite-slider", () => {
 
   describe("disabled", () => {
     disabled(() => mount("calcite-slider"));
+  });
+
+  it("range: clicking the track to the left of the min handle changes minValue and value on mousedown", async () => {
+    const { el } = await mount<Slider>(
+      <calcite-slider max-value="75" min-value="50" style="width:126px" />,
+    );
+
+    const track = el.shadowRoot.querySelector(".track");
+    const { x: trackX, y: trackY } = track.getBoundingClientRect();
+
+    function assertSliderValues(el: ToElement<Slider>) {
+      expect(el.minValue).toBe(25);
+      expect(el.maxValue).toBe(75);
+      expect(el.value).toEqual([25, 75]);
+    }
+
+    el.addEventListener("calciteSliderInput", () => {
+      assertSliderValues(el);
+    });
+
+    await commands.mouseMove(trackX + 25, trackY);
+    await commands.mouseDown();
+    await commands.mouseUp();
+
+    el.addEventListener("calciteSliderChange", () => {
+      assertSliderValues(el);
+    });
+  });
+
+  it("range: clicking the track to the right of the max handle changes maxValue and value on mousedown", async () => {
+    const { el } = await mount<Slider>(
+      <calcite-slider max-value="75" min-value="50" style="width:126px" />,
+    );
+
+    const track = el.shadowRoot.querySelector(".track");
+    const { x: trackX, y: trackY } = track.getBoundingClientRect();
+
+    function assertSliderValues(el: ToElement<Slider>) {
+      expect(el.minValue).toBe(50);
+      expect(el.maxValue).toBe(85);
+      expect(el.value).toEqual([50, 85]);
+    }
+
+    el.addEventListener("calciteSliderInput", () => {
+      assertSliderValues(el);
+    });
+
+    await commands.mouseMove(trackX + 85, trackY);
+    await commands.mouseDown();
+    await commands.mouseUp();
+
+    el.addEventListener("calciteSliderChange", () => {
+      assertSliderValues(el);
+    });
   });
 });
