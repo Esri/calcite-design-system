9773917d86de1b437958387b0697fcc34e7ab038
Isaac Braun <isaacbraun2000@gmail.com>
2026-01-27 10:46:30 -0800

    ci(actions): handle corrupted or missing monday sync ID in issue body (#13780)
    **Related Issue:** #12932
    
    ## Summary
    Adds support for handling missing or corrupted Monday Sync IDs in issue
    bodies for issues that have a Monday.com counterpart task. Instead of
    erroring due to an improper ID found from the body, it will query
    Monday.com for the ID and retry the sync API call.
    
    In any scenario where an ID is found by querying Monday.com, the issue
    body will be updated with the correct sync ID.
    
    This requires the `github` object to be passed to all Monday scripts in
    order to create a callback that can update the issue body.

--- .github/scripts/monday/syncActionChanges.js
+++ .github/scripts/monday/syncActionChanges.js
@@ -1,6 +1,6 @@
 // @ts-check
 const Monday = require("../support/monday");
-const { assertRequired } = require("../support/utils");
+const { assertRequired, createBodyUpdater } = require("../support/utils");
 
 /**
  * @typedef {object} SyncActionChangesInputs
@@ -40,7 +40,7 @@ module.exports = async ({ github, context, core }) => {
     issue_number,
   });
 
-  const monday = Monday(issue, core);
+  const monday = Monday(issue, core, createBodyUpdater({ github, context, core }));
 
   if (milestone_updated === "true") {
     monday.handleMilestone();
