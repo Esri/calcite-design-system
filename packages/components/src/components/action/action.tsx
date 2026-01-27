113c538cd07db70b28d9a5292e638738931aa906
Kitty Hurley <khurley@esri.com>
2026-01-26 20:25:24 -0600

    refer folks to accessibility section

--- packages/components/src/components/action/action.tsx
+++ packages/components/src/components/action/action.tsx
@@ -76,7 +76,7 @@ export class Action extends LitElement implements FormOwner {
   //#region Public Properties
 
   /**
-   * When specified, overrides or extends ARIA properties and attributes on the component's button.
+   * When specified, overrides or extends ARIA properties and attributes on the component's button. Refer to the component's accessibility section for configuration considerations.
    */
   @property() aria?: Partial<
     Pick<
