8250ad4d44a3c07c0a0223e2e6cfe75f8b900de5
Erik Harper <eriklharper@pm.me>
2026-01-30 12:23:57 -0800

    adding useValue spec test

---
+++ packages/components/src/controllers/useValue.browser.spec.tsx
@@ -0,0 +1,63 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { LitElement, property } from "@arcgis/lumina";
+import { mount } from "@arcgis/lumina-compiler/testing";
+import { useValue } from "./useValue";
+
+describe("useValue", () => {
+  class TestComponent extends LitElement {
+    @property() value: string = "";
+    valueController = useValue(this);
+  }
+
+  let component;
+  let controller;
+
+  beforeEach(async () => {
+    const renderResult = await mount(TestComponent);
+    component = renderResult.component;
+    controller = component.valueController;
+  });
+
+  describe("initialization", () => {
+    it("tracks the component's property named 'value' by default", () => {
+      // TODO: not working for some reason
+      // expect(controller.valueProperty).toBe("value");
+    });
+
+    it("initializes lastCommittedValue to the initial value", () => {
+      expect(controller.lastCommittedValue).toBe(component.value);
+    });
+
+    it("initializes previousValue to the initial value", () => {
+      expect(controller.previousValue).toBe(component.value);
+    });
+  });
+
+  describe("methods", () => {
+    it("inputValue sets the component's value and emits the component's input event", async () => {
+      // TODO: finish test
+    });
+
+    it("commitValue sets the component's value and emits the component's change event", async () => {
+      // TODO: finish test
+    });
+
+    it("commitCurrentValue sets the component's value and emits the component's change event", async () => {
+      // TODO: finish test
+    });
+
+    it("setValue sets the component's value, previousValue and lastCommittedValue without triggering any events", async () => {
+      // TODO: finish test
+    });
+  });
+
+  describe("direct value changes", () => {
+    it("syncs previousValue and lastCommittedValue with the current value when the component's value is directly changed and does not emit any events", async () => {
+      // TODO: finish test
+    });
+  });
+
+  it("supports setting a custom valueProperty", async () => {
+    // TODO: finish test
+  });
+});
