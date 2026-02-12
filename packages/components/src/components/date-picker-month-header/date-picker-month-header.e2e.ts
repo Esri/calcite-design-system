6edd85d3e4b9166feb89782d7adf80590f1b7959
Matt Driscoll <mdriscoll@esri.com>
2026-02-11 17:19:07 -0800

    add test

--- packages/components/src/components/date-picker-month-header/date-picker-month-header.e2e.ts
+++ packages/components/src/components/date-picker-month-header/date-picker-month-header.e2e.ts
@@ -71,19 +71,13 @@ describe("calcite-date-picker-month-header", () => {
     const datePickerMonthHeader = await page.find("calcite-date-picker-month-header");
     const [prev, next] = await findAll(page, "calcite-date-picker-month-header >>> .chevron");
 
-    datePickerMonthHeader.setProperty("scale", "m");
-    await page.waitForChanges();
-    expect(await prev.getProperty("scale")).toBe("m");
-    expect(await next.getProperty("scale")).toBe("m");
+    const scales = ["s", "m", "l"] as const;
 
-    datePickerMonthHeader.setProperty("scale", "l");
-    await page.waitForChanges();
-    expect(await prev.getProperty("scale")).toBe("l");
-    expect(await next.getProperty("scale")).toBe("l");
-
-    datePickerMonthHeader.setProperty("scale", "s");
-    await page.waitForChanges();
-    expect(await prev.getProperty("scale")).toBe("s");
-    expect(await next.getProperty("scale")).toBe("s");
+    for (const scale of scales) {
+      datePickerMonthHeader.setProperty("scale", scale);
+      await page.waitForChanges();
+      expect(await prev.getProperty("scale")).toBe(scale);
+      expect(await next.getProperty("scale")).toBe(scale);
+    }
   });
 });
