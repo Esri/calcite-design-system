2413f6ddcfe36a3b5167a92fbb076e1692d921a2
Eliza <eli97736@esri.com>
2026-02-12 20:57:59 -0800

    remove redundant root describe group across all tests continued

--- packages/components/src/components/loader/loader.e2e.ts
+++ packages/components/src/components/loader/loader.e2e.ts
@@ -5,51 +5,49 @@ import { themed } from "../../tests/commonTests";
 import { html } from "../../../support/formatting";
 import { CSS } from "./resources";
 
-describe("calcite-loader", () => {
-  it("displays label from text prop", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`<calcite-loader text="testing"></calcite-loader>`);
-    const elm = await page.find(`calcite-loader >>> .${CSS.text}`);
-    expect(elm).toEqualText("testing");
-  });
+it("displays label from text prop", async () => {
+  const page = await newE2EPage();
+  await page.setContent(`<calcite-loader text="testing"></calcite-loader>`);
+  const elm = await page.find(`calcite-loader >>> .${CSS.text}`);
+  expect(elm).toEqualText("testing");
+});
 
-  it("sets aria attributes properly for indeterminate loader", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`<calcite-loader></calcite-loader>`);
-    const loader = await page.find("calcite-loader");
-    expect(loader).toEqualAttribute("role", "progressbar");
-    expect(loader).not.toHaveAttribute("aria-valuemin");
-    expect(loader).not.toHaveAttribute("aria-valuemax");
-    expect(loader).not.toHaveAttribute("aria-valuenow");
-  });
+it("sets aria attributes properly for indeterminate loader", async () => {
+  const page = await newE2EPage();
+  await page.setContent(`<calcite-loader></calcite-loader>`);
+  const loader = await page.find("calcite-loader");
+  expect(loader).toEqualAttribute("role", "progressbar");
+  expect(loader).not.toHaveAttribute("aria-valuemin");
+  expect(loader).not.toHaveAttribute("aria-valuemax");
+  expect(loader).not.toHaveAttribute("aria-valuenow");
+});
 
-  it("sets aria attributes properly for determinate loader", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`<calcite-loader type="determinate"></calcite-loader>`);
-    const loader = await page.find("calcite-loader");
-    expect(loader).toHaveAttribute("aria-valuenow");
-    expect(loader).toEqualAttribute("aria-valuenow", 0);
-    expect(loader).toEqualAttribute("aria-valuemin", 0);
-    expect(loader).toEqualAttribute("aria-valuemax", 100);
-    loader.setProperty("value", 24);
-    await page.waitForChanges();
-    expect(loader).toEqualAttribute("aria-valuenow", 24);
-  });
+it("sets aria attributes properly for determinate loader", async () => {
+  const page = await newE2EPage();
+  await page.setContent(`<calcite-loader type="determinate"></calcite-loader>`);
+  const loader = await page.find("calcite-loader");
+  expect(loader).toHaveAttribute("aria-valuenow");
+  expect(loader).toEqualAttribute("aria-valuenow", 0);
+  expect(loader).toEqualAttribute("aria-valuemin", 0);
+  expect(loader).toEqualAttribute("aria-valuemax", 100);
+  loader.setProperty("value", 24);
+  await page.waitForChanges();
+  expect(loader).toEqualAttribute("aria-valuenow", 24);
+});
 
-  it("displays inline with text from inline prop", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`<calcite-loader inline></calcite-loader>`);
-    const rect = await page.find("calcite-loader >>> circle");
-    expect(rect).toEqualAttribute("r", "7.2");
-  });
+it("displays inline with text from inline prop", async () => {
+  const page = await newE2EPage();
+  await page.setContent(`<calcite-loader inline></calcite-loader>`);
+  const rect = await page.find("calcite-loader >>> circle");
+  expect(rect).toEqualAttribute("r", "7.2");
+});
 
-  it("sets a default id when none is provided", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`<calcite-loader></calcite-loader>`);
-    const loader = await page.find("calcite-loader");
-    expect(loader).toHaveAttribute("id");
-    expect(loader.getAttribute("id").length).toEqual(36);
-  });
+it("sets a default id when none is provided", async () => {
+  const page = await newE2EPage();
+  await page.setContent(`<calcite-loader></calcite-loader>`);
+  const loader = await page.find("calcite-loader");
+  expect(loader).toHaveAttribute("id");
+  expect(loader.getAttribute("id").length).toEqual(36);
 });
 
 describe("themed", () => {
--- packages/components/src/components/segmented-control-item/segmented-control-item.e2e.ts
+++ packages/components/src/components/segmented-control-item/segmented-control-item.e2e.ts
@@ -4,128 +4,126 @@ import { themed } from "../../tests/commonTests";
 import { html } from "../../../support/formatting";
 import { CSS } from "./resources";
 
-describe("calcite-segmented-control-item", () => {
-  it("is un-checked by default", async () => {
-    const page = await newE2EPage();
-    await page.setContent("<calcite-segmented-control-item value='test-value'></calcite-segmented-control-item>");
-    const element = await page.find("calcite-segmented-control-item");
+it("is un-checked by default", async () => {
+  const page = await newE2EPage();
+  await page.setContent("<calcite-segmented-control-item value='test-value'></calcite-segmented-control-item>");
+  const element = await page.find("calcite-segmented-control-item");
 
-    const checked = await element.getProperty("checked");
-    expect(checked).toBe(false);
-  });
+  const checked = await element.getProperty("checked");
+  expect(checked).toBe(false);
+});
 
-  it("supports value, label and checked", async () => {
-    const page = await newE2EPage();
-    await page.setContent(
-      "<calcite-segmented-control-item value='test-value' checked>test-label</calcite-segmented-control-item>",
-    );
-    const element = await page.find("calcite-segmented-control-item");
+it("supports value, label and checked", async () => {
+  const page = await newE2EPage();
+  await page.setContent(
+    "<calcite-segmented-control-item value='test-value' checked>test-label</calcite-segmented-control-item>",
+  );
+  const element = await page.find("calcite-segmented-control-item");
 
-    expect(element).toEqualText("test-label");
+  expect(element).toEqualText("test-label");
 
-    const checked = await element.getProperty("checked");
-    expect(checked).toBe(true);
+  const checked = await element.getProperty("checked");
+  expect(checked).toBe(true);
 
-    const value = await element.getProperty("value");
-    expect(value).toBe("test-value");
-  });
+  const value = await element.getProperty("value");
+  expect(value).toBe("test-value");
+});
 
-  it("renders icon at start if requested", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
+it("renders icon at start if requested", async () => {
+  const page = await newE2EPage();
+  await page.setContent(`
     <calcite-segmented-control-item icon-start="car">Content</calcite-segmented-control-item>`);
-    const icon = await page.find(`calcite-segmented-control-item >>> .${CSS.icon}`);
-    expect(icon).not.toBe(null);
-  });
+  const icon = await page.find(`calcite-segmented-control-item >>> .${CSS.icon}`);
+  expect(icon).not.toBe(null);
+});
 
-  it("does not render icon if not requested", async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
+it("does not render icon if not requested", async () => {
+  const page = await newE2EPage();
+  await page.setContent(`
     <calcite-segmented-control-item>Content</calcite-segmented-control-item>`);
-    const icon = await page.find(`calcite-segmented-control-item >>> .${CSS.icon}`);
-    expect(icon).toBe(null);
+  const icon = await page.find(`calcite-segmented-control-item >>> .${CSS.icon}`);
+  expect(icon).toBe(null);
+});
+
+describe("WAI-ARIA Roles, States, and Properties", () => {
+  it(`has a role of 'radio'`, async () => {
+    const page = await newE2EPage();
+    await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
+    const element = await page.find("calcite-segmented-control-item");
+
+    const role = element.getAttribute("role");
+
+    expect(role).toEqualText("radio");
   });
 
-  describe("WAI-ARIA Roles, States, and Properties", () => {
-    it(`has a role of 'radio'`, async () => {
-      const page = await newE2EPage();
-      await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
-      const element = await page.find("calcite-segmented-control-item");
+  it(`updates 'aria-checked' based on 'checked' property`, async () => {
+    const page = await newE2EPage();
+    await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
+    const element = await page.find("calcite-segmented-control-item");
 
-      const role = element.getAttribute("role");
+    let ariaChecked = element.getAttribute("aria-checked");
 
-      expect(role).toEqualText("radio");
-    });
+    expect(ariaChecked).toEqualText("false");
 
-    it(`updates 'aria-checked' based on 'checked' property`, async () => {
-      const page = await newE2EPage();
-      await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
-      const element = await page.find("calcite-segmented-control-item");
+    element.setProperty("checked", true);
+    await page.waitForChanges();
 
-      let ariaChecked = element.getAttribute("aria-checked");
+    ariaChecked = element.getAttribute("aria-checked");
 
-      expect(ariaChecked).toEqualText("false");
+    expect(ariaChecked).toEqualText("true");
 
-      element.setProperty("checked", true);
-      await page.waitForChanges();
+    element.setProperty("checked", false);
+    await page.waitForChanges();
 
-      ariaChecked = element.getAttribute("aria-checked");
+    ariaChecked = element.getAttribute("aria-checked");
 
-      expect(ariaChecked).toEqualText("true");
-
-      element.setProperty("checked", false);
-      await page.waitForChanges();
-
-      ariaChecked = element.getAttribute("aria-checked");
-
-      expect(ariaChecked).toEqualText("false");
-    });
-
-    it("content/value is wrapped by label", async () => {
-      const page = await newE2EPage();
-      await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
-      const defaultSlot = await page.find("calcite-segmented-control-item >>> label slot");
-
-      expect(defaultSlot).toBeDefined();
-    });
-
-    it("renders default prop values", async () => {
-      const page = await newE2EPage();
-      await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
-
-      const element = await page.find("calcite-segmented-control-item");
-      expect(element).not.toHaveAttribute("checked");
-      expect(element).not.toHaveAttribute("icon-flip-rtl");
-      expect(element).not.toHaveAttribute("value");
-      expect(element).not.toHaveAttribute("icon-start");
-      expect(element).not.toHaveAttribute("icon-end");
-    });
+    expect(ariaChecked).toEqualText("false");
   });
 
-  describe("theme", () => {
-    themed("calcite-segmented-control-item", {
-      "--calcite-segmented-control-color": {
-        shadowSelector: `.${CSS.label}`,
-        targetProp: "color",
-      },
-      "--calcite-segmented-control-background-color": {
-        shadowSelector: `.${CSS.label}`,
-        targetProp: "backgroundColor",
-      },
-      "--calcite-segmented-control-border-color": {
-        shadowSelector: `.${CSS.label}`,
-        targetProp: "borderColor",
-      },
-      "--calcite-segmented-control-shadow": {
-        shadowSelector: `.${CSS.label}`,
-        targetProp: "boxShadow",
-      },
-    });
-    themed(html`<calcite-segmented-control-item icon-start="car">Content</calcite-segmented-control-item>`, {
-      "--calcite-segmented-control-icon-color": {
-        shadowSelector: `.${CSS.icon}`,
-        targetProp: "--calcite-icon-color",
-      },
-    });
+  it("content/value is wrapped by label", async () => {
+    const page = await newE2EPage();
+    await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
+    const defaultSlot = await page.find("calcite-segmented-control-item >>> label slot");
+
+    expect(defaultSlot).toBeDefined();
+  });
+
+  it("renders default prop values", async () => {
+    const page = await newE2EPage();
+    await page.setContent("<calcite-segmented-control-item></calcite-segmented-control-item>");
+
+    const element = await page.find("calcite-segmented-control-item");
+    expect(element).not.toHaveAttribute("checked");
+    expect(element).not.toHaveAttribute("icon-flip-rtl");
+    expect(element).not.toHaveAttribute("value");
+    expect(element).not.toHaveAttribute("icon-start");
+    expect(element).not.toHaveAttribute("icon-end");
+  });
+});
+
+describe("theme", () => {
+  themed("calcite-segmented-control-item", {
+    "--calcite-segmented-control-color": {
+      shadowSelector: `.${CSS.label}`,
+      targetProp: "color",
+    },
+    "--calcite-segmented-control-background-color": {
+      shadowSelector: `.${CSS.label}`,
+      targetProp: "backgroundColor",
+    },
+    "--calcite-segmented-control-border-color": {
+      shadowSelector: `.${CSS.label}`,
+      targetProp: "borderColor",
+    },
+    "--calcite-segmented-control-shadow": {
+      shadowSelector: `.${CSS.label}`,
+      targetProp: "boxShadow",
+    },
+  });
+  themed(html`<calcite-segmented-control-item icon-start="car">Content</calcite-segmented-control-item>`, {
+    "--calcite-segmented-control-icon-color": {
+      shadowSelector: `.${CSS.icon}`,
+      targetProp: "--calcite-icon-color",
+    },
   });
 });
