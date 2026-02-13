72334feac5ffa4709f76830636d1c9f724333d55
Eliza <eli97736@esri.com>
2026-02-13 10:01:47 -0800

    remove redundant root describe group across all tests continued

--- packages/components/src/components/text-area/text-area.browser.e2e.tsx
+++ packages/components/src/components/text-area/text-area.browser.e2e.tsx
@@ -12,96 +12,94 @@ import {
   t9n,
 } from "../../tests/commonTests/browser";
 
-describe("calcite-text-area", () => {
-  describe("cancelable", () => {
-    cancelable("calcite-text-area");
-  });
+describe("cancelable", () => {
+  cancelable("calcite-text-area");
+});
 
-  describe("defaults", () => {
-    defaults(
-      () => mount("calcite-text-area"),
-      [
-        {
-          propertyName: "limitText",
-          defaultValue: false,
-        },
-        {
-          propertyName: "scale",
-          defaultValue: "m",
-        },
-        {
-          propertyName: "status",
-          defaultValue: "idle",
-        },
-        {
-          propertyName: "validationIcon",
-          defaultValue: undefined,
-        },
-        {
-          propertyName: "validationMessage",
-          defaultValue: undefined,
-        },
-        {
-          propertyName: "wrap",
-          defaultValue: "soft",
-        },
-      ],
-    );
-  });
+describe("defaults", () => {
+  defaults(
+    () => mount("calcite-text-area"),
+    [
+      {
+        propertyName: "limitText",
+        defaultValue: false,
+      },
+      {
+        propertyName: "scale",
+        defaultValue: "m",
+      },
+      {
+        propertyName: "status",
+        defaultValue: "idle",
+      },
+      {
+        propertyName: "validationIcon",
+        defaultValue: undefined,
+      },
+      {
+        propertyName: "validationMessage",
+        defaultValue: undefined,
+      },
+      {
+        propertyName: "wrap",
+        defaultValue: "soft",
+      },
+    ],
+  );
+});
 
-  describe("reflects", () => {
-    reflects(
-      () => mount("calcite-text-area"),
-      [
-        {
-          propertyName: "columns",
-          value: "10",
-        },
-        {
-          propertyName: "limitText",
-          value: true,
-        },
-        {
-          propertyName: "rows",
-          value: "50",
-        },
-        {
-          propertyName: "scale",
-          value: "s",
-        },
-        {
-          propertyName: "status",
-          value: "invalid",
-        },
-        {
-          propertyName: "validationIcon",
-          value: true,
-        },
-      ],
-    );
-  });
+describe("reflects", () => {
+  reflects(
+    () => mount("calcite-text-area"),
+    [
+      {
+        propertyName: "columns",
+        value: "10",
+      },
+      {
+        propertyName: "limitText",
+        value: true,
+      },
+      {
+        propertyName: "rows",
+        value: "50",
+      },
+      {
+        propertyName: "scale",
+        value: "s",
+      },
+      {
+        propertyName: "status",
+        value: "invalid",
+      },
+      {
+        propertyName: "validationIcon",
+        value: true,
+      },
+    ],
+  );
+});
 
-  describe("honors hidden attribute", () => {
-    hidden(() => mount("calcite-text-area"));
-  });
+describe("honors hidden attribute", () => {
+  hidden(() => mount("calcite-text-area"));
+});
 
-  describe("internal label", () => {
-    internalLabel(() => mount(`calcite-text-area`));
-  });
+describe("internal label", () => {
+  internalLabel(() => mount(`calcite-text-area`));
+});
 
-  describe("renders", () => {
-    renders(() => mount("calcite-text-area"), { display: "inline-block" });
-  });
+describe("renders", () => {
+  renders(() => mount("calcite-text-area"), { display: "inline-block" });
+});
 
-  describe("is focusable", () => {
-    focusable(() => mount("calcite-text-area"));
-  });
+describe("is focusable", () => {
+  focusable(() => mount("calcite-text-area"));
+});
 
-  describe("translation support", () => {
-    t9n(() => mount("calcite-text-area"));
-  });
+describe("translation support", () => {
+  t9n(() => mount("calcite-text-area"));
+});
 
-  describe("disabled", () => {
-    disabled(() => mount("calcite-text-area"));
-  });
+describe("disabled", () => {
+  disabled(() => mount("calcite-text-area"));
 });
