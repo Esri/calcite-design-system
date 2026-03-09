5ce6758f92787fef8123056cb0aeb0440f3c84ec
JC Franco <jfranco@esri.com>
2026-03-09 09:47:26 -0700

    drop transitional form associated test helper

--- packages/components/src/tests/commonTests/index.ts
+++ packages/components/src/tests/commonTests/index.ts
@@ -1,7 +1,6 @@
 export { accessible } from "./accessible";
 export { openClose } from "./openClose";
 export { formAssociated } from "./formAssociated";
-export { formAssociated as formAssociatedEI } from "./formAssociatedEI";
 export { labelable } from "./labelable";
 export { themed } from "./themed";
 export { HYDRATED_ATTR } from "./puppeteer/utils";
