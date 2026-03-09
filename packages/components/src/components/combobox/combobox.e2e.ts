139667f2dfc95fd8d1e060f0ed45b71cd24669a4
Aaron Shetland <ashetland@esri.com>
2026-03-09 13:45:11 -0700

    fix tests

--- packages/components/src/components/combobox/combobox.e2e.ts
+++ packages/components/src/components/combobox/combobox.e2e.ts
@@ -56,8 +56,8 @@ describe("accessible with open selected items", () => {
 describe("accessible with highlight selection appearance", () => {
   accessible(html`
     <calcite-combobox label="Trees" selection-appearance="highlight">
-      <calcite-combobox-item selected value="Pine" heading="Pine"></calcite-combobox-item>
-      <calcite-combobox-item selected value="Spruce" heading="Spruce"></calcite-combobox-item>
+      <calcite-combobox-item value="Pine" heading="Pine"></calcite-combobox-item>
+      <calcite-combobox-item value="Spruce" heading="Spruce"></calcite-combobox-item>
       <calcite-combobox-item value="Fir" heading="Fir"></calcite-combobox-item>
     </calcite-combobox>
   `);
