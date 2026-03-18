44267423ca6e394e57710a5c0441132297b96c4d
Zhaoting Zhou <zhaotingzhou@esri.com>
2026-03-18 09:39:59 -0700

    fix(date-picker): show adjacent-month day cells in single-calendar range mode

--- packages/components/src/components/date-picker/date-picker.browser.e2e.tsx
+++ packages/components/src/components/date-picker/date-picker.browser.e2e.tsx
@@ -1,4 +1,4 @@
-import { describe } from "vitest";
+import { describe, expect, it } from "vitest";
 import { mount } from "@arcgis/lumina-compiler/testing";
 import { defaults, focusable, hidden, renders, t9n } from "../../tests/commonTests/browser";
 
@@ -38,4 +38,23 @@ describe("focusable", () => {
 
 describe("translation support", () => {
   t9n(() => mount("calcite-date-picker"));
+});
+
+describe("range calendars", () => {
+  it("does not hide adjacent-month days when set to one calendar", async () => {
+    const { el } = await mount<"calcite-date-picker">("calcite-date-picker", {
+      afterConnect: (element) => {
+        element.range = true;
+        element.calendars = 1;
+        element.value = ["2024-01-15", ""];
+      },
+    });
+
+    const hiddenAdjacentMonthDays =
+      el.shadowRoot
+        ?.querySelector("calcite-date-picker-month")
+        ?.shadowRoot?.querySelectorAll("calcite-date-picker-day.noncurrent") ?? [];
+
+    expect(hiddenAdjacentMonthDays.length).toBe(0);
+  });
 });
