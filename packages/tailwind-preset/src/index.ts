ee4e06cc754880244bdb685c09e00a11e6d30f34
Matt Gallagher <48596928+matgalla@users.noreply.github.com>
2026-03-23 02:03:25 -0400

    update deprecation notes to use tailwind package versions

--- packages/tailwind-preset/src/index.ts
+++ packages/tailwind-preset/src/index.ts
@@ -227,10 +227,10 @@ const config: Omit<Config, "content"> = {
         disabled: "var(--calcite-opacity-disabled)",
       },
       spacing: {
-        2.5: "var(--calcite-space-sm-plus)", // Deprecated in v5.0.0, removal target v6.0.0 - use `--calcite-space-sm-plus` instead.
-        4.5: "1.125rem", // Deprecated in v5.0.0, removal target v6.0.0 - No longer needed.
-        11: "2.75rem", // Deprecated in v5.0.0, removal target v6.0.0 - Not intended for sizing, use `--calcite-size-lg` instead.
-        13: "3.25rem", // Deprecated in v5.0.0, removal target v6.0.0 - No longer needed.
+        2.5: "var(--calcite-space-sm-plus)", // Deprecated in v1.1.1, removal target v2.0.0 - use `--calcite-space-sm-plus` instead.
+        4.5: "1.125rem", // Deprecated in v1.1.1, removal target v2.0.0 - No longer needed.
+        11: "2.75rem", // Deprecated in v1.1.1, removal target v2.0.0 - Not intended for sizing, use `--calcite-size-lg` instead.
+        13: "3.25rem", // Deprecated in v1.1.1, removal target v2.0.0 - No longer needed.
       },
       transitionProperty: {
         margin: "margin",
