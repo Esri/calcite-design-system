42f0d69fc735c972ae898ac03589bee0a25daeb6
Eliza Khachatryan <eli97736@esri.com>
2026-03-25 15:29:04 -0700

    test: remove redundant root describe block (#14138)
    **Related Issue:** #13881
    
    ## Summary
    Remove redundant root describe block and cleanup.

--- packages/eslint-plugin-components/tests/lib/rules/require-deprecation-and-removal-versions/require-deprecation-and-removal-version.spec.ts
+++ packages/eslint-plugin-components/tests/lib/rules/require-deprecation-and-removal-versions/require-deprecation-and-removal-version.spec.ts
@@ -6,42 +6,40 @@ import wrongMissingRemoval from "./require-removal-version.wrong.tsx?raw";
 import wrongMissingBoth from "./require-deprecation-and-removal-version.wrong.tsx?raw";
 import { ruleTester } from "../../../../src/utils/rule-tester";
 
-describe("require-deprecation-and-removal-version", () => {
-  ruleTester().run("require-deprecation-and-removal-version", rule, {
-    valid: [
-      {
-        code: good,
-        filename: "require-deprecation-and-removal-version.good.tsx",
-      },
-    ],
-    invalid: [
-      {
-        code: wrongMissingDeprecation,
-        filename: "require-deprecation-version.wrong.tsx",
-        errors: [
-          {
-            messageId: "missingDeprecation",
-          },
-        ],
-      },
-      {
-        code: wrongMissingRemoval,
-        filename: "require-removal-version.wrong.tsx",
-        errors: [
-          {
-            messageId: "missingRemoval",
-          },
-        ],
-      },
-      {
-        code: wrongMissingBoth,
-        filename: "require-deprecation-and-removal-version.wrong.tsx",
-        errors: [
-          {
-            messageId: "missingBoth",
-          },
-        ],
-      },
-    ],
-  });
+ruleTester().run("require-deprecation-and-removal-version", rule, {
+  valid: [
+    {
+      code: good,
+      filename: "require-deprecation-and-removal-version.good.tsx",
+    },
+  ],
+  invalid: [
+    {
+      code: wrongMissingDeprecation,
+      filename: "require-deprecation-version.wrong.tsx",
+      errors: [
+        {
+          messageId: "missingDeprecation",
+        },
+      ],
+    },
+    {
+      code: wrongMissingRemoval,
+      filename: "require-removal-version.wrong.tsx",
+      errors: [
+        {
+          messageId: "missingRemoval",
+        },
+      ],
+    },
+    {
+      code: wrongMissingBoth,
+      filename: "require-deprecation-and-removal-version.wrong.tsx",
+      errors: [
+        {
+          messageId: "missingBoth",
+        },
+      ],
+    },
+  ],
 });
