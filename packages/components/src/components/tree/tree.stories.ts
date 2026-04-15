Esri/calcite-design-system

commit f00f58c4487a711962fcd7a7a1b2541722ecd0ff

fix(tree-item): smooth out expanding items transition (#13712)
**Related Issue:** #12669

## Summary
Smooth expanding items transition.

packages/components/src/components/tree-item/tree-item.scss
@@ -162,17 +162,24 @@
 }
 
 .children-container {
-  @apply relative h-0 overflow-hidden opacity-0 origin-top;
-  transform: scaleY(0);
+  opacity: 0;
+  max-block-size: 0;
+  overflow: hidden;
   transition:
-    var(--calcite-animation-timing) animation.$easing-function,
-    opacity var(--calcite-animation-timing) animation.$easing-function,
-    all var(--calcite-animation-timing) ease-in-out; // note that we're transitioning transform, not height!
+    opacity var(--calcite-internal-animation-timing-slow) animation.$easing-function,
+    max-block-size var(--calcite-internal-animation-timing-medium) animation.$easing-function;
+}
 
-  .item--expanded > & {
-    @apply overflow-visible opacity-100;
-    transform: none;
-    block-size: auto;
+.item--expanded > .children-container {
+  opacity: 1;
+  max-block-size: 100dvb;
+  transition:
+    opacity var(--calcite-internal-animation-timing-slow) animation.$easing-function,
+    max-block-size calc(2 * var(--calcite-internal-animation-timing-slow)) animation.$easing-function;
+
+  @starting-style {
+    opacity: 0;
+    max-block-size: 0;
   }
 }
 

packages/components/src/components/tree-item/tree-item.tsx
@@ -469,7 +469,6 @@ export class TreeItem extends LitElement {
       this.selectionMode === "single-persist"
         ? toAriaBoolean(this.selected)
         : undefined;
-    this.el.toggleAttribute("calcite-hydrated-hidden", hidden);
     /* TODO: [MIGRATION] This used <Host> before. In Stencil, <Host> props overwrite user-provided props. If you don't wish to overwrite user-values, replace "=" here with "??=" */
     this.el.role = "treeitem";
     /* TODO: [MIGRATION] This used <Host> before. In Stencil, <Host> props overwrite user-provided props. If you don't wish to overwrite user-values, add a check for this.el.hasAttribute() before calling setAttribute() here */

commit b95ec84022a42cb401d34c2d3dff2749ab220e06

fix(tree): fix content clipping (#14262)
**Related Issue:** #14187 

## Summary

This fixes a regression introduced in
https://github.com/Esri/calcite-design-system/pull/13712.

packages/components/src/components/tree-item/tree-item.scss
@@ -173,6 +173,7 @@
 
 .item--expanded > .children-container {
   opacity: 1;
+  overflow: visible;
   max-block-size: 100dvb;
   transition:
     opacity var(--calcite-internal-animation-timing-slow) animation.$easing-function,

packages/components/src/components/tree/tree.stories.ts
@@ -211,19 +211,13 @@ darkModeRTL.decorators = [allScaleTreeBuilder];
 export const OverflowingSubtree = (): string =>
   html`<div style="width:400px">
       <calcite-tree>
-        <calcite-tree-item label="test item" expanded id="two">
+        <calcite-tree-item label="nested items" expanded>
           Layer 2
           <calcite-tree slot="children">
-            <calcite-tree-item label="test item">
+            <calcite-tree-item label="dropdown item">
               <span class="title">Layer 2.1</span>
-              <calcite-dropdown placement="bottom-trailing">
-                <calcite-button
-                  appearance="transparent"
-                  color="neutral"
-                  icon-start="ellipsis"
-                  slot="trigger"
-                  id="trigger"
-                ></calcite-button>
+              <calcite-dropdown>
+                <calcite-button icon-start="ellipsis" id="trigger" slot="trigger"></calcite-button>
                 <calcite-dropdown-group>
                   <calcite-dropdown-item icon-start="trash">Remove</calcite-dropdown-item>
                 </calcite-dropdown-group>
@@ -231,16 +225,51 @@ export const OverflowingSubtree = (): string =>
             </calcite-tree-item>
           </calcite-tree>
         </calcite-tree-item>
-        <calcite-tree-item label="test item">
+        <calcite-tree-item label="single item">
           <span class="title">Layer 3</span>
+        </calcite-tree-item>
+        <calcite-tree-item expanded label="multiple flat items">
+          Layer 4
+          <calcite-tree slot="children">
+            <calcite-tree-item>Layer 4.1</calcite-tree-item>
+            <calcite-tree-item>Layer 4.2</calcite-tree-item>
+            <calcite-tree-item>Layer 4.3</calcite-tree-item>
+            <calcite-tree-item>Layer 4.4</calcite-tree-item>
+            <calcite-tree-item>Layer 4.5</calcite-tree-item>
+            <calcite-tree-item>Layer 4.6</calcite-tree-item>
+            <calcite-tree-item>Layer 4.7</calcite-tree-item>
+            <calcite-tree-item>Layer 4.8</calcite-tree-item>
+            <calcite-tree-item>Layer 4.9</calcite-tree-item>
+            <calcite-tree-item>Layer 4.10</calcite-tree-item>
+            <calcite-tree-item>Layer 4.11</calcite-tree-item>
+            <calcite-tree-item>Layer 4.12</calcite-tree-item>
+            <calcite-tree-item>Layer 4.13</calcite-tree-item>
+            <calcite-tree-item>Layer 4.14</calcite-tree-item>
+            <calcite-tree-item>Layer 4.15</calcite-tree-item>
+            <calcite-tree-item>Layer 4.16</calcite-tree-item>
+            <calcite-tree-item>Layer 4.17</calcite-tree-item>
+            <calcite-tree-item>Layer 4.18</calcite-tree-item>
+            <calcite-tree-item>Layer 4.19</calcite-tree-item>
+            <calcite-tree-item>Layer 4.20</calcite-tree-item>
+            <calcite-tree-item>Layer 4.21</calcite-tree-item>
+            <calcite-tree-item>Layer 4.22</calcite-tree-item>
+            <calcite-tree-item>Layer 4.23</calcite-tree-item>
+            <calcite-tree-item>Layer 4.24</calcite-tree-item>
+            <calcite-tree-item>Layer 4.25</calcite-tree-item>
+            <calcite-tree-item>Layer 4.26</calcite-tree-item>
+            <calcite-tree-item>Layer 4.27</calcite-tree-item>
+            <calcite-tree-item>Layer 4.28</calcite-tree-item>
+            <calcite-tree-item>Layer 4.29</calcite-tree-item>
+            <calcite-tree-item>Layer 4.30</calcite-tree-item>
+          </calcite-tree>
         </calcite-tree-item>
       </calcite-tree>
     </div>
     <script>
-      window.addEventListener("load", () => {
-        setTimeout(() => {
-          const dropdownTriggerEl = document.querySelector("calcite-button#trigger");
-          dropdownTriggerEl.click();
-        }, 1000);
-      });
+      (async () => {
+        const dropdownTriggerEl = document.querySelector("calcite-button#trigger");
+        await customElements.whenDefined("calcite-button");
+        await dropdownTriggerEl.componentOnReady();
+        dropdownTriggerEl.click();
+      })();
     </script>`;