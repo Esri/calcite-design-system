e78c54fab36b1d72641eea7fecd47436f75191de
JC Franco <jfranco@esri.com>
2026-04-14 21:54:20 -0700

    revert E2E test

--- packages/components/src/components/tree-item/tree-item.browser.e2e.tsx
+++ packages/components/src/components/tree-item/tree-item.browser.e2e.tsx
@@ -1,5 +1,5 @@
 import { h } from "@arcgis/lumina";
-import { describe, expect, it } from "vitest";
+import { describe } from "vitest";
 import { mount } from "@arcgis/lumina-compiler/testing";
 import { page } from "vitest/browser";
 import { defaults, disabled, hidden, renders, slots } from "../../tests/commonTests/browser";
@@ -79,51 +79,4 @@ describe("renders", () => {
 
 describe("slots", () => {
   slots(() => mount("calcite-tree-item"), SLOTS);
-});
-
-it("shows overflowing content", async () => {
-  await mount(
-    <calcite-tree>
-      <calcite-tree-item expanded>
-        <calcite-tree slot="children">
-          <calcite-tree-item>Child B-1</calcite-tree-item>
-          <calcite-tree-item>Child B-2</calcite-tree-item>
-          <calcite-tree-item>Child B-3</calcite-tree-item>
-          <calcite-tree-item>Child B-4</calcite-tree-item>
-          <calcite-tree-item>Child B-5</calcite-tree-item>
-          <calcite-tree-item>Child B-6</calcite-tree-item>
-          <calcite-tree-item>Child B-7</calcite-tree-item>
-          <calcite-tree-item>Child B-8</calcite-tree-item>
-          <calcite-tree-item>Child B-9</calcite-tree-item>
-          <calcite-tree-item>Child B-10</calcite-tree-item>
-          <calcite-tree-item>Child B-11</calcite-tree-item>
-          <calcite-tree-item>Child B-12</calcite-tree-item>
-          <calcite-tree-item>Child B-13</calcite-tree-item>
-          <calcite-tree-item>Child B-14</calcite-tree-item>
-          <calcite-tree-item>Child B-15</calcite-tree-item>
-          <calcite-tree-item>Child B-16</calcite-tree-item>
-          <calcite-tree-item>Child B-17</calcite-tree-item>
-          <calcite-tree-item>Child B-18</calcite-tree-item>
-          <calcite-tree-item>Child B-19</calcite-tree-item>
-          <calcite-tree-item>Child B-20</calcite-tree-item>
-          <calcite-tree-item>Child B-21</calcite-tree-item>
-          <calcite-tree-item>Child B-22</calcite-tree-item>
-          <calcite-tree-item>Child B-23</calcite-tree-item>
-          <calcite-tree-item>Child B-24</calcite-tree-item>
-          <calcite-tree-item>Child B-25</calcite-tree-item>
-          <calcite-tree-item>Child B-26</calcite-tree-item>
-          <calcite-tree-item>Child B-27</calcite-tree-item>
-          <calcite-tree-item>Child B-28</calcite-tree-item>
-          <calcite-tree-item>Child B-29</calcite-tree-item>
-          <calcite-tree-item>Child B-30</calcite-tree-item>
-          <calcite-tree-item>Child B-31</calcite-tree-item>
-        </calcite-tree>
-      </calcite-tree-item>
-    </calcite-tree>,
-  );
-  const items = page.getBySelector("calcite-tree-item");
-
-  for (let i = 0; i < items.elements().length; i++) {
-    await expect.element(items.nth(i)).toBeVisible();
-  }
 });
