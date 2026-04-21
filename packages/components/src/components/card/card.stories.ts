e0469a5a68efe50a865e5b5ab57e467b48b82c57
aPreciado88 <apreciado@esri.com>
2026-04-21 09:05:54 -0700

    feat(card, card-group): add scale prop

--- packages/components/src/components/card/card.stories.ts
+++ packages/components/src/components/card/card.stories.ts
@@ -4,18 +4,23 @@ import { boolean, modesDarkDefault } from "../../../.storybook/utils";
 import { ATTRIBUTES } from "../../../.storybook/resources";
 import { Card } from "./card";
 
-const { logicalFlowPosition } = ATTRIBUTES;
+const { scale, logicalFlowPosition } = ATTRIBUTES;
 
-type CardStoryArgs = Pick<Card, "loading" | "selected" | "thumbnailPosition">;
+type CardStoryArgs = Pick<Card, "loading" | "scale" | "selected" | "thumbnailPosition">;
 
 export default {
   title: "Components/Card",
   args: {
     loading: false,
+    scale: scale.defaultValue,
     selected: false,
     thumbnailPosition: logicalFlowPosition.defaultValue,
   },
   argTypes: {
+    scale: {
+      options: scale.values,
+      control: { type: "select" },
+    },
     thumbnailPosition: {
       options: logicalFlowPosition.values,
       control: { type: "select" },
@@ -62,6 +67,7 @@ export const simple = (args: CardStoryArgs): string => html`
   <div style="width: 260px">
     <calcite-card
       ${boolean("loading", args.loading)}
+      scale="${args.scale}"
       ${boolean("selected", args.selected)}
       thumbnail-position="${args.thumbnailPosition}"
     >
