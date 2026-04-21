c8d507bdf1e1b1c7a9720cde686cf962261a63b9
Abraham Preciado Morales <apreciado@esri.com>
2026-04-21 14:45:43 -0700

    feat(card, card-group): add scale prop (#14299)
    **Related Issue:**
    [#12345](https://github.com/Esri/calcite-design-system/issues/12345)
    
    ## Summary
    
    - Add `scale` prop.
    - Update browser.e2e tests.
    - Update storybook tests.

--- packages/components/src/components/card/card.tsx
+++ packages/components/src/components/card/card.tsx
@@ -10,8 +10,9 @@ import {
   state,
   ToEvents,
 } from "@arcgis/lumina";
+import { getIconScale } from "../../utils/component";
 import { slotChangeHasAssignedElement } from "../../utils/dom";
-import { LogicalFlowPosition, SelectionMode } from "../interfaces";
+import { LogicalFlowPosition, Scale, SelectionMode } from "../interfaces";
 import { isActivationKey } from "../../utils/key";
 import { IconName } from "../icon/interfaces";
 import { useT9n } from "../../controllers/useT9n";
@@ -89,6 +90,13 @@ export class Card extends LitElement {
 
   /** Overrides individual strings used by the component. */
   @property() messageOverrides?: typeof this.messages._overrides;
+
+  /**
+   * Specifies the size of the component inherited from `calcite-card-group`, defaults to `m`.
+   *
+   * @private
+   */
+  @property({ reflect: true }) scale: Scale = "m";
 
   /**
    * When `true`, the component is selectable.
@@ -246,7 +254,7 @@ export class Card extends LitElement {
 
     return (
       <div class={CSS.checkboxWrapper} onPointerDown={this.cardSelectClick} tabIndex={-1}>
-        <calcite-icon icon={icon} scale="s" />
+        <calcite-icon icon={icon} scale={getIconScale(this.scale)} />
       </div>
     );
   }
