21abd832d935db67b6fb0ee3841d70899857da0e
Isaac Braun <isaacbraun2000@gmail.com>
2026-05-05 16:51:56 -0700

    deprecate(dom): deprecate `hasVisibleContent` utility

--- packages/components/src/utils/dom.ts
+++ packages/components/src/utils/dom.ts
@@ -399,6 +399,9 @@ export function slotChangeGetTextContent(event: Event): string {
 /**
  * This helper checks if an element has visible content.
  *
+ * @deprecated Has problems with nested slots and dynamic content,
+ * refer to https://github.com/Esri/calcite-design-system/issues/14270.
+ *
  * @param element The element to check.
  * @returns True if the element has visible content, otherwise false.
  */
