Esri/calcite-design-system

commit 8cad6f6f9cb5b29ad89de62a0b787fafe133ad0a

build(deps): bump `@arcgis` deps (#14403)
**Related Issue:** N/A

## Summary

This also bumps related dependencies and updates linting packages to be
compatible.

### Noteworthy changes

* Bumps the following deps as well:
  * `eslint`
  * `typescript`
* Fixed bug in `component#isHidden` util to correctly detect hidden
elements (see
https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hidden)
* Updated `tsconfig.json` in `components-react` to specify `rootDir`
(see
https://github.com/microsoft/TypeScript/issues/62508#issuecomment-3348659946)
and ignore TSX files as it does not include any
* Replaced
[`eslint-plugin-react`](https://github.com/jsx-eslint/eslint-plugin-react/)
with
*
[`@eslint-react/eslint-plugin`](https://github.com/Rel1cx/eslint-react)
+ `@eslint-react/kit` (for
[recipes](https://www.eslint-react.xyz/docs/recipes) to replace relevant
rules)
*
[`eslint-stylistic`](https://github.com/eslint-stylistic/eslint-stylistic)
*
[`eslint-plugin-perfectionist`](https://github.com/azat-io/eslint-plugin-perfectionist)
* Simplified `eslint-config` package
* Relaxed `ESLint` radix rule to address later in
https://github.com/Esri/calcite-design-system/issues/14401
* Removed `eslint-plugin-import`, which was missed in #14385

packages/components/eslint.config.mjs
@@ -23,7 +23,11 @@ export default tseslint.config(
     },
 
     languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parser: tseslint.parser,
       parserOptions: {
+        jsxPragma: "h",
         tsconfigRootDir: import.meta.dirname,
         project: ["tsconfig.eslint.json"],
       },

packages/components/src/components/input-number/input-number.tsx
@@ -1000,7 +1000,7 @@ export class InputNumber
         onFocus={this.inputNumberFocusHandler}
         onInput={this.inputNumberInputHandler}
         onKeyDown={this.inputNumberKeyDownHandler}
-        // eslint-disable-next-line react/forbid-dom-props -- intentional onKeyUp usage
+        // eslint-disable-next-line @eslint-react/kit/forbid-dom-props -- intentional onKeyUp usage
         onKeyUp={this.inputNumberKeyUpHandler}
         placeholder={this.placeholder || ""}
         readOnly={this.readOnly}

packages/components/src/components/input/input.tsx
@@ -1047,7 +1047,7 @@ export class Input
           onFocus={this.inputFocusHandler}
           onInput={this.inputNumberInputHandler}
           onKeyDown={this.inputNumberKeyDownHandler}
-          // eslint-disable-next-line react/forbid-dom-props -- intentional onKeyUp usage
+          // eslint-disable-next-line @eslint-react/kit/forbid-dom-props -- intentional onKeyUp usage
           onKeyUp={this.inputKeyUpHandler}
           pattern={this.pattern}
           placeholder={this.placeholder || ""}
@@ -1086,6 +1086,7 @@ export class Input
           onFocus={this.inputFocusHandler}
           onInput={this.inputInputHandler}
           onKeyDown={this.inputKeyDownHandler}
+          // eslint-disable-next-line @eslint-react/kit/forbid-dom-props -- intentional onKeyUp usage
           onKeyUp={this.inputKeyUpHandler}
           pattern={this.pattern}
           placeholder={this.placeholder || ""}

packages/components/src/utils/component.ts
@@ -31,7 +31,7 @@ export function warnIfMissingRequiredProp<C extends LitElement>(
 }
 
 export function isHidden<C extends ComboboxChildElement | StepperItem["el"] | TableRow["el"]>(el: C): boolean {
-  return el.hidden || el.itemHidden;
+  return el.hidden !== false || el.itemHidden;
 }
 
 /**

packages/eslint-config/core.js
@@ -15,20 +15,12 @@ export default tseslint.config(
       "@cspell": cspellPlugin,
       unicorn: unicornPlugin,
     },
-
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      parser: tseslint.parser,
-    },
-
     settings: {
       jsdoc: {
         ignoreInternal: true,
         ignorePrivate: true,
       },
     },
-
     rules: {
       "@cspell/spellchecker": ["warn", {}],
 
@@ -81,7 +73,7 @@ export default tseslint.config(
           max: 1,
         },
       ],
-      radix: ["error", "as-needed"],
+      radix: ["warn"], // "error" severity will be restored by https://github.com/Esri/calcite-design-system/issues/14401
 
       "unicorn/filename-case": [
         "error",

packages/eslint-config/extensions/eslint-react/index.js
@@ -1,0 +1,38 @@
+/** Forbid certain props on DOM elements */
+export function forbidDomProps(options) {
+  const { forbidden } = options;
+  return (context) => ({
+    JSXAttribute(node) {
+      // › Extract prop name
+      const propName = node.name.type === "JSXIdentifier" ? node.name.name : null;
+      if (propName == null) return;
+      const forbiddenEntry = forbidden.find((entry) => entry.propName === propName);
+      if (!forbiddenEntry) return;
+
+      const parent = node.parent;
+      if (parent?.type !== "JSXOpeningElement") return;
+
+      const elName = parent.name.type === "JSXIdentifier" ? parent.name.name : null;
+
+      if (elName == null || elName[0] !== elName[0]?.toLowerCase()) return;
+
+      context.report({
+        node,
+        message: forbiddenEntry.message || `Prop "${propName}" is forbidden on elements.`,
+      });
+    },
+  });
+}
+
+/** Disallow JSX props spreading. */
+export function jsxPropsNoSpreading() {
+  return (context) => ({
+    JSXSpreadAttribute(node) {
+      context.report({
+        node,
+        message: "Props spreading is not allowed.",
+      });
+    },
+  });
+}
+

packages/eslint-config/jsx.js
@@ -1,49 +1,50 @@
-import reactPlugin from "eslint-plugin-react";
+import eslintReact from "@eslint-react/eslint-plugin";
+import eslintReactKit from "@eslint-react/kit";
+import perfectionist from "eslint-plugin-perfectionist";
+import stylistic from "@stylistic/eslint-plugin";
 import tseslint from "typescript-eslint";
+import { forbidDomProps, jsxPropsNoSpreading } from "./extensions/eslint-react/index.js";
+
+const kit = eslintReactKit()
+  .use(forbidDomProps, {
+    forbidden: [
+      {
+        propName: "onKeyPress",
+        message: "Use onKeyDown instead for consistent interaction behavior (e.g., closing, moving focus, etc.).",
+      },
+      {
+        propName: "onKeyUp",
+        message: "Use onKeyDown instead for consistent interaction behavior (e.g., closing, moving focus, etc.).",
+      },
+    ],
+  })
+  .use(jsxPropsNoSpreading);
 
 export default tseslint.config({
   files: ["**/*.{t,j}sx"],
-  ...reactPlugin.configs.flat?.recommended,
-  settings: {
-    react: {
-      pragma: "h",
-    },
+  extends: [eslintReact.configs["jsx"]],
+  plugins: {
+    "@stylistic": stylistic,
+    perfectionist: perfectionist,
+    "@eslint-react/kit": kit.getPlugin(),
   },
   rules: {
-    "react/jsx-props-no-spreading": "error",
-    "react/jsx-sort-props": "error",
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "react/self-closing-comp": "error",
-    "react/forbid-component-props": [
-      "warn",
-      {
-        forbid: [
-          {
-            propName: "onKeyPress",
-            message: "Use onKeyDown instead for consistent interaction behavior (e.g., closing, moving focus, etc.).",
-          },
-          {
-            propName: "onKeyUp",
-            message: "Use onKeyDown instead for consistent interaction behavior (e.g., closing, moving focus, etc.).",
-          },
-        ],
-      },
-    ],
-    "react/forbid-dom-props": [
-      "warn",
-      {
-        forbid: [
-          {
-            propName: "onKeyPress",
-            message: "Use onKeyDown instead for consistent interaction behavior (e.g., closing, moving focus, etc.).",
-          },
-          {
-            propName: "onKeyUp",
-            message: "Use onKeyDown instead for consistent interaction behavior (e.g., closing, moving focus, etc).",
-          },
-        ],
-      },
-    ],
+    "@stylistic/jsx-self-closing-comp": "error",
+
+    // https://www.eslint-react.xyz/docs/rules#x-rules
+    "@eslint-react/no-array-index-key": "error",
+
+    // https://www.eslint-react.xyz/docs/rules#dom-rules
+    "@eslint-react/dom-no-missing-button-type": "warn",
+    "@eslint-react/dom-no-missing-iframe-sandbox": "error",
+    "@eslint-react/dom-no-script-url": "error",
+    "@eslint-react/dom-no-unsafe-iframe-sandbox": "error",
+    "@eslint-react/dom-no-unsafe-target-blank": "error",
+    "@eslint-react/dom-no-void-elements-with-children": "error",
+
+    "@eslint-react/kit/forbid-dom-props": "warn",
+    "@eslint-react/kit/jsx-props-no-spreading": "error",
+
+    "perfectionist/sort-jsx-props": "error",
   },
 });

commit bcdc97098fb710382bcb242f3a8089752921a360

chore(linting): specify Fragment name (#14447)
**Related Issue:** N/A

## Summary

This might have been caused by
https://github.com/Esri/calcite-design-system/pull/14403.

packages/components/eslint.config.mjs
@@ -27,6 +27,7 @@ export default tseslint.config(
       sourceType: "module",
       parser: tseslint.parser,
       parserOptions: {
+        jsxFragmentName: "Fragment",
         jsxPragma: "h",
         tsconfigRootDir: import.meta.dirname,
         project: ["tsconfig.eslint.json"],