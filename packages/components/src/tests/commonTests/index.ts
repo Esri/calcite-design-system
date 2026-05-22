9ec63a91861a23aeba03d8919c5774edbda09fc3
JC Franco <jfranco@esri.com>
2026-05-22 16:44:12 -0700

    refactor(form): drop legacy utils (#14481)

--- packages/components/src/tests/commonTests/index.ts
+++ packages/components/src/tests/commonTests/index.ts
@@ -1,5 +1,4 @@
 export { accessible } from "./accessible";
-export { formAssociated } from "./formAssociated";
 export { labelable } from "./labelable";
 export { themed } from "./themed";
 export { HYDRATED_ATTR } from "./puppeteer/utils";
