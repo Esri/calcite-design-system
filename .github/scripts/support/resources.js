56eacc9b9de1efd0107e2a78db851f3aecf1ca24
Isaac Braun <isaacbraun2000@gmail.com>
2026-06-01 11:52:06 -0700

    ci(actions): add `pr` explanation comment

--- .github/scripts/support/resources.js
+++ .github/scripts/support/resources.js
@@ -17,6 +17,7 @@ const resources = {
       i18nL10n: "i18n-l10n",
       newComponent: "new component",
       perf: "perf",
+      // "pr" is not a GitHub label, rather a keyword to sync the `PR` issue type to Monday.com
       pull_request: "pr",
       refactor: "refactor",
       research: "research",
