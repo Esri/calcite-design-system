5daeb673ee3e60de5d5480a0963a73105de3f046
JC Franco <jfranco@esri.com>
2026-06-03 17:47:35 -0700

    fix(input): fix file selection validation for input[type=file]

--- packages/components/src/components/input/input.tsx
+++ packages/components/src/components/input/input.tsx
@@ -2,14 +2,13 @@
 import { PropertyValues } from "lit";
 import { createRef } from "lit/directives/ref.js";
 import {
-  LitElement,
-  property,
   createEvent,
-  h,
-  method,
-  state,
   JsxNode,
+  LitElement,
   LuminaJsx,
+  method,
+  property,
+  state,
   stringOrBoolean,
 } from "@arcgis/lumina";
 import { useDirection, useWatchAttributes } from "@arcgis/lumina/controllers";
@@ -41,12 +40,12 @@ import T9nStrings from "./assets/t9n/messages.en.json";
 import { InputPlacement, NumberNudgeDirection, SetValueOrigin } from "./interfaces";
 import {
   CSS,
+  DIRECTION,
+  ICONS,
   IDS,
   INPUT_TYPE_ICONS,
-  SLOTS,
-  ICONS,
-  DIRECTION,
   NUDGE_DELAY_IN_MS,
+  SLOTS,
 } from "./resources";
 import { NumericInputComponent, TextualInputComponent } from "./common/input";
 import { styles } from "./input.scss";
@@ -94,6 +93,7 @@ export class Input
 
   formSupport = useForm<this>({
     inputType: "text",
+    getValue: () => (this.type === "file" ? this.files || this.value : this.value),
   })(this);
 
   private inlineEditableEl: InlineEditable["el"];
--- packages/components/src/controllers/useForm.browser.spec.tsx
+++ packages/components/src/controllers/useForm.browser.spec.tsx
@@ -1,9 +1,11 @@
 import { afterEach, beforeEach, describe, it, expect } from "vitest";
 import { mount } from "@arcgis/lumina-compiler/testing";
-import { LitElement, method, property } from "@arcgis/lumina";
+import { h, JsxNode, LitElement, method, property } from "@arcgis/lumina";
 import { useForm } from "./useForm";
 import { defaultValidity } from "../tests/commonTests/browser/defaults";
-import { html } from "lit";
+import { html, PropertyValues } from "lit";
+import { createRef } from "lit/directives/ref.js";
+import { page, userEvent } from "vitest/browser";
 
 describe("useForm", () => {
   class TestComponent extends LitElement {
@@ -70,6 +72,146 @@ describe("useForm", () => {
       });
 
       expect(el.validity).toMatchObject(defaultValidity);
+    });
+  });
+
+  it("allows mapping value when the component's value is different than the form submit value", async () => {
+    class MappedValueFormComponent extends LitElement {
+      static formAssociated = true;
+
+      @property({ type: Boolean })
+      disabled = false;
+
+      @property()
+      value = "";
+
+      @property()
+      name?: string;
+
+      @property()
+      form?: string;
+
+      @property({ type: Boolean })
+      required: boolean = false;
+
+      @property()
+      validity!: ValidityState;
+
+      defaultValue?: TestComponent["value"];
+
+      @method()
+      async setFocus(): Promise<void> {}
+
+      formSupport = useForm<this>({
+        inputType: "text",
+        getValue: () => `mapped-${this.value}`,
+      })(this);
+    }
+
+    await mount(
+      html`<mapped-value-form-component
+        name="mapped-value"
+        value="test"
+      ></mapped-value-form-component>`,
+      {
+        dynamicComponents: [MappedValueFormComponent],
+        parent: form,
+      },
+    );
+
+    const formData = new FormData(form);
+    expect(formData.get("mapped-value")).toBe("mapped-test");
+  });
+
+  describe("input types", () => {
+    class FileTypeFormComponent extends LitElement {
+      static formAssociated = true;
+
+      inputRef = createRef<HTMLInputElement>();
+
+      @property({ type: Boolean })
+      disabled = false;
+
+      @property()
+      value = "";
+
+      @property()
+      name?: string;
+
+      @property()
+      form?: string;
+
+      @property({ type: Boolean })
+      required: boolean = false;
+
+      @property()
+      type!: HTMLInputElement["type"];
+
+      @property()
+      validity!: ValidityState;
+
+      defaultValue?: TestComponent["value"];
+
+      formSupport = useForm<this>({
+        inputType: "text",
+        getValue: () =>
+          this.type === "file" ? this.inputRef.value?.files || this.value : this.value,
+      })(this);
+
+      @method()
+      async setFocus(): Promise<void> {}
+
+      private handleChange(event: Event): void {
+        this.value = (event.target as HTMLInputElement).value;
+      }
+
+      protected willUpdate(changes: PropertyValues) {
+        if (changes.has("type")) {
+          this.formSupport.overrideInputType(this.type);
+        }
+      }
+
+      protected render(): JsxNode {
+        return (
+          <input
+            data-testid="internal-input"
+            onChange={this.handleChange}
+            ref={this.inputRef}
+            type={this.type}
+          />
+        );
+      }
+    }
+
+    it("supports file type", async () => {
+      const { el, reRender } = await mount(
+        html`<file-type-form-component name="file-type" type="file"></file-type-form-component>`,
+        {
+          dynamicComponents: [FileTypeFormComponent],
+          parent: form,
+        },
+      );
+
+      expect(el.validity.valid).toBe(true);
+
+      el.required = true;
+      await reRender();
+
+      expect(el.validity.valid).toBe(false);
+
+      const input = page.getByTestId("internal-input");
+      const file = new File(["file"], "fake-file.png", { type: "image/png" });
+      await userEvent.upload(input, file);
+      await reRender();
+
+      expect(el.validity.valid).toBe(true);
+
+      const formData = new FormData(form);
+
+      const submittedFile = formData.get("file-type");
+
+      expect(submittedFile).toBeInstanceOf(File);
+      expect((submittedFile as File).name).toBe(file.name);
     });
   });
 });
