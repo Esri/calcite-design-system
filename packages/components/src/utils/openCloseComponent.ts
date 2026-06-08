5a949bcdf42decc1d9f1120eb36fb49f9349296a
JC Franco <jfranco@esri.com>
2026-06-08 14:52:13 -0700

    use dev for util changes

--- packages/components/src/utils/openCloseComponent.ts
+++ packages/components/src/utils/openCloseComponent.ts
@@ -33,7 +33,7 @@ interface OpenCloseComponentBase extends LitElement {
 
 export interface OpenCloseComponentWithEl extends OpenCloseComponentBase {
   /** Specifies element that the transition is allowed to emit on. */
-  transitionEl?: HTMLElement;
+  transitionEl: HTMLElement;
 }
 
 export interface OpenCloseComponentWithRef extends OpenCloseComponentBase {
@@ -41,9 +41,7 @@ export interface OpenCloseComponentWithRef extends OpenCloseComponentBase {
   transitionRef: Ref<HTMLElement>;
 }
 
-export type OpenCloseComponent =
-  | (OpenCloseComponentWithRef & { transitionEl?: never })
-  | (OpenCloseComponentWithEl & { transitionRef?: never });
+export type OpenCloseComponent = OpenCloseComponentWithEl | OpenCloseComponentWithRef;
 
 function isOpen(component: OpenCloseComponent): boolean {
   return component[component.openProp || "open"];
@@ -89,5 +87,5 @@ export async function toggleOpenClose(component: OpenCloseComponent): Promise<vo
 }
 
 function hasRef(component: OpenCloseComponent): component is OpenCloseComponentWithRef {
-  return "transitionRef" in component;
+  return !!(component as OpenCloseComponentWithRef).transitionRef;
 }
