f2171171da61e1c6470deafd96f80e828757ab83
Eliza <eli97736@esri.com>
2026-07-09 16:03:58 -0700

    modify doc

--- packages/components/src/components/date-picker/date-picker.tsx
+++ packages/components/src/components/date-picker/date-picker.tsx
@@ -138,7 +138,7 @@ export class DatePicker extends LitElement {
   /** Specifies the size of the component. */
   @property({ reflect: true }) scale: "s" | "m" | "l" = "m";
 
-  /** Specifies the selected date as a string (`"yyyy-mm-dd"`), or an array of strings for `range` values (`["yyyy-mm-dd", "yyyy-mm-dd"]`). */
+  /** Specifies the selected date as a string (`"yyyy-mm-dd"`), or an array of strings for `range` values (`["yyyy-mm-dd", "yyyy-mm-dd"]`). Set to `null`, `undefined`, or an empty string (`""`) to clear the selection. */
   @property() value?: string | string[] | null;
 
   /** Specifies the selected date as a full date object (`new Date("yyyy-mm-dd")`), or an array containing full date objects (`[new Date("yyyy-mm-dd"), new Date("yyyy-mm-dd")]`). */
