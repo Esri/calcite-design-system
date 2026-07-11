f73e301b067936274efc7edfc48b4ce7cf32383d
Matt Gallagher <48596928+matgalla@users.noreply.github.com>
2026-07-10 21:19:14 -0400

    revert component updates

--- packages/tailwind-preset/src/index.ts
+++ packages/tailwind-preset/src/index.ts
@@ -141,7 +141,7 @@ const config: Omit<Config, "content"> = {
     fontWeight: {
       // assets/styles/_type
       light: "var(--calcite-font-weight-light)",
-      normal: "var(--calcite-font-weight-normal)",
+      normal: "var(--calcite-font-weight-regular)",
       medium: "var(--calcite-font-weight-medium)",
       bold: "var(--calcite-font-weight-bold)",
     },
