17437482bc1c20753c663a5ed57e520837edba68
Brendan Rice <hey@brendanrice.com>
2026-07-29 16:02:38 -0400

    add Form "Mix" story

--- packages/components/src/components/form/form.stories.ts
+++ packages/components/src/components/form/form.stories.ts
@@ -21,3 +21,52 @@ export const sample = (): string => html`
     </calcite-field-set>
   </calcite-form>
 `;
+
+export const mix = (): string => html`
+  <div style="display: flex; flex-direction: column; gap: 50px;">
+    <calcite-form>
+      <calcite-field-set columns="2" layout="columns">
+        <div slot="legend">Field Set legend</div>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+      </calcite-field-set>
+      <calcite-field-set>
+        <div slot="legend">Field Set legend</div>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+      </calcite-field-set>
+      <calcite-field-set layout="horizontal">
+        <div slot="legend">Field Set legend</div>
+        <calcite-checkbox label-text="Allows large dogs"></calcite-checkbox>
+        <calcite-checkbox label-text="Must have parking"></calcite-checkbox>
+        <calcite-checkbox label-text="Allows cats"></calcite-checkbox>
+        <calcite-checkbox label-text="Must be on ground floor"></calcite-checkbox>
+        <calcite-checkbox label-text="On-site laundry"></calcite-checkbox>
+        <calcite-checkbox label-text="Waterfront"></calcite-checkbox>
+        <calcite-checkbox label-text="On-site parking"></calcite-checkbox>
+      </calcite-field-set>
+    </calcite-form>
+    <calcite-form style="inline-size: 382px;">
+      <calcite-field-set columns="2" layout="columns">
+        <div slot="legend">Field Set legend</div>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+      </calcite-field-set>
+      <calcite-field-set>
+        <div slot="legend">Field Set legend</div>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+        <calcite-input label-text="Label" placeholder="Placeholder"></calcite-input>
+      </calcite-field-set>
+      <calcite-field-set layout="horizontal">
+        <div slot="legend">Field Set legend</div>
+        <calcite-checkbox label-text="Allows large dogs"></calcite-checkbox>
+        <calcite-checkbox label-text="Must have parking"></calcite-checkbox>
+        <calcite-checkbox label-text="Allows cats"></calcite-checkbox>
+        <calcite-checkbox label-text="Must be on ground floor"></calcite-checkbox>
+        <calcite-checkbox label-text="On-site laundry"></calcite-checkbox>
+        <calcite-checkbox label-text="Waterfront"></calcite-checkbox>
+        <calcite-checkbox label-text="On-site parking"></calcite-checkbox>
+      </calcite-field-set>
+    </calcite-form>
+  </div>
+`;
