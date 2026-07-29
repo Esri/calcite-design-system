1dd4e026eb2fb84fa6f3a27b2f83f0ed9d0969ef
aPreciado88 <apreciado@esri.com>
2026-07-29 16:48:46 -0700

    feat(button, split-button, card, input, input-number, input-text, input-time-picker, color-picker, date-picker, select, table): update corner-radius-fallback stories

--- packages/components/src/corner-radius-fallbacks.stories.ts
+++ packages/components/src/corner-radius-fallbacks.stories.ts
@@ -2,15 +2,20 @@ import { html } from "../support/formatting";
 import { buttons } from "./custom-theme/button";
 import { card } from "./custom-theme/card";
 import { colorPicker } from "./custom-theme/color-picker";
+import { defaultCombobox } from "./custom-theme/combobox";
 import { datePicker } from "./custom-theme/date-picker";
 import { inputNumber } from "./custom-theme/input-number";
 import { inputText } from "./custom-theme/input-text";
 import { inputTimePicker } from "./custom-theme/input-time-picker";
+import { inputTimeZone } from "./custom-theme/input-time-zone";
 import { input } from "./custom-theme/input";
+import { menuItem } from "./custom-theme/menu-item";
 import { select } from "./custom-theme/select";
+import { segmentedControl } from "./custom-theme/segmented-control";
 import { slider } from "./custom-theme/slider";
 import { splitButton } from "./custom-theme/split-button";
 import { table } from "./custom-theme/table";
+import { tile } from "./custom-theme/tile";
 
 const kitchenSink = () =>
   html`<div style="--calcite-corner-radius: 24px; padding: 1rem;">
@@ -43,11 +48,26 @@ const kitchenSink = () =>
 
       <div class="fallback-row">${colorPicker} ${datePicker} ${inputNumber}</div>
 
-      <div class="fallback-row">${inputText} ${inputTimePicker} ${input} ${select}</div>
+      <div class="fallback-row">${inputText} ${inputTimePicker} ${inputTimeZone} ${input} ${select}</div>
+
+      <div class="fallback-row">${defaultCombobox}</div>
+
+      <div class="fallback-row">${segmentedControl}</div>
+
+      <div class="fallback-row">
+        <calcite-segmented-control>
+          <calcite-segmented-control-item value="alpha" checked>Alpha</calcite-segmented-control-item>
+          <calcite-segmented-control-item value="beta">Beta</calcite-segmented-control-item>
+        </calcite-segmented-control>
+      </div>
+
+      <div class="fallback-row">${tile}</div>
 
       <div class="fallback-row">${slider}</div>
 
       <div class="fallback-row">${splitButton}</div>
+
+      <div class="fallback-row">${menuItem}</div>
 
       <div class="fallback-row">${table}</div>
     </div>
