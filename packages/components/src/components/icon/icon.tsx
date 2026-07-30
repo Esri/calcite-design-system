12ce1c50e90d5768cd1107dcd35bc71e0fa07f4b
JC Franco <jfranco@esri.com>
2026-07-29 18:16:25 -0700

    update default willUpdate check

--- packages/components/src/components/icon/icon.tsx
+++ packages/components/src/components/icon/icon.tsx
@@ -99,7 +99,7 @@ export class Icon extends LitElement {
     Please refactor your code to reduce the need for this check.
     Docs: https://webgis.esri.com/arcgis-components/?path=/docs/lumina-transition-from-stencil--docs#watching-for-property-changes */
     if (
-      (changes.has("icon") && (this.hasUpdated || this.icon !== null)) ||
+      (changes.has("icon") && (this.hasUpdated || this.icon !== undefined)) ||
       (changes.has("scale") && (this.hasUpdated || this.scale !== "m"))
     ) {
       this.loadIconPathData();
