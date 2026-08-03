70708bce8ab4fcea99cc8cb5e93aeb95b2c3f24d
Matt Driscoll <mdriscoll@esri.com>
2026-08-03 11:28:48 -0700

    chore: fix types

--- packages/components/src/utils/clone-safe-types.ts
+++ packages/components/src/utils/clone-safe-types.ts
@@ -1,5 +1,7 @@
+/** @public */
 export type CloneSafeValue = string | number | boolean | null | CloneSafeValue[] | CloneSafeRecord;
 
+/** @public */
 export type CloneSafeRecord = {
   [key: string]: CloneSafeValue;
 };
