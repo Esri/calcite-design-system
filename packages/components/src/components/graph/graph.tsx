c0d96aa9c673dfa23d5008cf3962adeecd3af518
JC Franco <jfranco@esri.com>
2026-08-17 13:36:27 -0700

    test(graph): add utils test (#14535)

--- packages/components/src/components/graph/graph.tsx
+++ packages/components/src/components/graph/graph.tsx
@@ -2,7 +2,7 @@ import { h, LitElement, property, JsxNode } from "@arcgis/lumina";
 import { guid } from "../../utils/guid";
 import { createObserver } from "../../utils/observers";
 import { ColorStop, DataSeries, Point } from "./types";
-import { area, range, translate } from "./util";
+import { area, range, translate } from "./utils";
 import { styles } from "./graph.scss";
 import { CSS, IDS } from "./resources";
 
