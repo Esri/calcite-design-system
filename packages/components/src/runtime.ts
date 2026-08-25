dfbba06627182d8d08f14cc1a78b3552f2e4f019
Max Patiiuk <max@patii.uk>
2026-08-25 11:23:25 -0700

    fix: assetPathChanged must be true after defineCustomElement's resourcesUrl

--- packages/components/src/runtime.ts
+++ packages/components/src/runtime.ts
@@ -3,6 +3,15 @@ import "./utils/globalScript";
 
 export const runtime = makeRuntime();
 
+const originalSetAssetPath = runtime.setAssetPath;
+runtime.setAssetPath = (path) => {
+  assetPathChanged = true;
+  originalSetAssetPath(path);
+};
+
+/** @internal */
+export let assetPathChanged = false;
+
 /**
  * "customElement" needs to be exported - it will be used by the build system.
  * You should not call it directly.
