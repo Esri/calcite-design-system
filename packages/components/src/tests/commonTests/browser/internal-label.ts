65128a49cfaffc80c6a355201014dde0a0b1a57f
Anveshreddy mekala <amekala@esri.com>
2026-09-03 15:59:12 -0500

    remove unused properties

--- packages/components/src/tests/commonTests/browser/internal-label.ts
+++ packages/components/src/tests/commonTests/browser/internal-label.ts
@@ -90,13 +90,13 @@ export function internalLabel(setup: () => ReturnType<typeof mount>): void {
         messagesCommon?: T9nMessages;
       };
       const t9nComponent = component as T9nComponent;
-      const { messages } = t9nComponent;
+      const { messages, messagesCommon } = t9nComponent;
 
       // required indicator has associated tooltip, so we need to ensure messages are loaded
-      if (t9nComponent.messages?._loading || t9nComponent.messagesCommon?._loading) {
+      if (messages?._loading || messagesCommon?._loading) {
         await new Promise<void>((resolve) => {
           const intervalId = setInterval(async () => {
-            if (!t9nComponent.messages?._loading && !t9nComponent.messagesCommon?._loading) {
+            if (!messages?._loading && !messagesCommon?._loading) {
               clearInterval(intervalId);
               resolve();
             }
