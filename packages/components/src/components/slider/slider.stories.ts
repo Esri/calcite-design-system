5972b1a52e07fc26e25d3ccb21ee20c6063501ce
JC Franco <jfranco@esri.com>
2026-09-14 14:10:54 -0700

    add screenshot test

--- packages/components/src/components/slider/slider.stories.ts
+++ packages/components/src/components/slider/slider.stories.ts
@@ -1014,3 +1014,35 @@ export const validationMessageAllScales = (): string => html`
     ></calcite-slider>
   </div>
 `;
+
+export const negativeSignHandlingForDifferentNumberingSystems = (): string => html`
+  <calcite-slider
+    label-text="numbering-system=latn"
+    numbering-system="latn"
+    label-ticks
+    min="-10"
+    max="10"
+    ticks="5"
+    value="0"
+  ></calcite-slider>
+  <br />
+  <calcite-slider
+    label-text="numbering-system=arab"
+    numbering-system="arab"
+    label-ticks
+    min="-10"
+    max="10"
+    ticks="5"
+    value="0"
+  ></calcite-slider>
+  <br />
+  <calcite-slider
+    label-text="numbering-system=arabext"
+    numbering-system="arabext"
+    label-ticks
+    min="-10"
+    max="10"
+    ticks="5"
+    value="0"
+  ></calcite-slider>
+`;
